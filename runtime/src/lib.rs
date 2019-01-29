282c9ddaad7aa73115f433c93202f40a9a99669e
Gav Wood <gavin@parity.io>
2019-01-29 13:58:25 +0100

    Bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 1,
-	spec_version: 105,
+	spec_version: 106,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
