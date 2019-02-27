f0eede70f96a5089b71b276e6395a7be364fb8b8
joe petrowski <25483142+joepetrowski@users.noreply.github.com>
2019-02-27 20:14:45 +0100

    remove TODO
    TODO should have been removed after the last update

--- service/src/chain_spec.rs
+++ service/src/chain_spec.rs
@@ -54,7 +54,7 @@ fn staging_testnet_config_genesis() -> GenesisConfig {
 	const DAYS: u64 = HOURS * 24;
 	GenesisConfig {
 		consensus: Some(ConsensusConfig {
-			code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/polkadot_runtime.compact.wasm").to_vec(),	// TODO change
+			code: include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/polkadot_runtime.compact.wasm").to_vec(),
 			authorities: initial_authorities.clone(),
 		}),
 		system: None,
