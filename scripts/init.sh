1dd4b9661feadd01195913e1826fe68a6159c078
Xiliang Chen <xlchen1291@gmail.com>
2019-04-05 19:08:39 +1300

    update init script (#211)

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-echo "*** Initialising WASM build environment"
+echo "*** Initializing WASM build environment"
 
 rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-# Temporarily install llvm-tools.
-# See https://github.com/rust-lang/rust/issues/53813
-# TODO: Remove this when fixed.
-rustup component add llvm-tools-preview --toolchain nightly
 rustup update stable
+
+rustup target add wasm32-unknown-unknown --toolchain nightly
 
 # Install wasm-gc. It's useful for stripping slimming down wasm binaries.
 command -v wasm-gc || \
-	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc
+	cargo +nightly install --git https://github.com/alexcrichton/wasm-gc --force
