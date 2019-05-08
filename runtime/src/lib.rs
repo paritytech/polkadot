aa497540ddfb86e4d876e5e3952c35dcb3c5a707
André Silva <andre.beat@gmail.com>
2019-05-08 15:35:41 +0100

    polkadot v0.4.4 (#252)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -109,7 +109,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 1,
-	spec_version: 112,
+	spec_version: 113,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
