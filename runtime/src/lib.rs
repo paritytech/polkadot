d29d0bfdaa0f33bb3bc952dbe3999ea308da2bc3
André Silva <andre.beat@gmail.com>
2019-05-17 15:36:12 +0100

    Bump version to v0.5 (#262)
    * bump version to 0.5
    
    * bump spec_version to 1000

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -119,7 +119,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 1,
-	spec_version: 108,
+	spec_version: 1000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
