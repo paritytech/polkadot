a90d575fc9a7fb86784a1cd3fdc7413ace7d8d14
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-06-11 22:01:26 +0200

    Require --locked on CI for WASM runtime locks (#285)
    * Require --locked on CI for WASM runtime locks
    
    * Update runtime Cargo.lock
    
    * Add quotes

--- test-parachains/build.sh
+++ test-parachains/build.sh
@@ -7,7 +7,7 @@ export RUSTFLAGS="-C link-arg=--import-memory -C link-arg=--export-table -C pani
 for i in adder
 do
 	cd $i/wasm
-	cargo +nightly build --target=wasm32-unknown-unknown --release --no-default-features --target-dir target
+	cargo +nightly build --target=wasm32-unknown-unknown --release --no-default-features --target-dir target "$@"
 	wasm-gc target/wasm32-unknown-unknown/release/$i'_'wasm.wasm target/wasm32-unknown-unknown/release/$i.wasm
 	cp target/wasm32-unknown-unknown/release/$i.wasm ../../../parachain/tests/res/
 	rm -rf target
