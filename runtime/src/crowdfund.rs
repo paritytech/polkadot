1433a13167ffbc8362ffa95eecec43c75eec1f5d
Gav Wood <gavin@parity.io>
2019-07-05 15:01:22 +0200

    Rest of logic.

--- runtime/src/crowdfund.rs
+++ runtime/src/crowdfund.rs
@@ -15,6 +15,50 @@
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
 //! # Parachain Crowdfunding module
+//!
+//! The point of this is to allow parachain projects to offer the ability to help fund a deposit for
+//! the parachain. When the parachain is retired, the funds may be returned.
+//!
+//! Contributing funds is permissionless. Each fund has a child-trie which stores all
+//! contributors account IDs together with the amount they contributed; the root of this can then be
+//! used by the parachain to allow contributors to prove that they made some particular contribution
+//! to the project (e.g. to be rewarded through some token or badge).
+//!
+//! Contributions must be of at least `MIN_CONTRIBUTION` (to account for the resouces taken in
+//! tracking contributions), and may never tally greater than the fund's `cap`, set and fixed at the
+//! time of creation. In order to create a new fund, then a deposit must be paid of the amount
+//! `SUBMISSION_DEPOSIT`. Substantial resources are taken on the main trie in tracking a fund and
+//! this accounts for that.
+//!
+//! Funds may be set up at any time; their closing time is fixed at creation (as a block number) and
+//! if the fund is not successful by the closing time, then it will become *retired*. Contributors
+//! may get a refund of their contributions from retired funds. After a period (`RETIREMENT_PERIOD`)
+//! the fund may be dissolved entirely. At this point any unrefunded contributions are considered
+//! `orphaned` and are disposed of through the `OrphanedFunds` handler (which may e.g. place them
+//! into the treasury).
+//!
+//! Funds may accept contributions at any point before their success or retirement. When a parachain
+//! slot auction enters its ending period, then parachains will each place a bid; the bid will be
+//! raised once per block if the parachain had additional funds contributed since the last bid.
+//!
+//! Funds may set their deploy data (the code hash and head data of their parachain) at any point.
+//! It may only be done once and once set cannot be changed. Good procedure would be to set them
+//! ahead of receiving any contributions in order that contributors may verify that their parachain
+//! contains all expected functionality. However, this is not enforced and deploy data may happen
+//! at any point, even after a slot has been successfully won or, indeed, never.
+//!
+//! Funds that are successful winners of a slot may have their slot claimed through the `onboard`
+//! call. This may only be done once and must be after the deploy data has been fixed. Successful
+//! funds remain tracked (in the `Funds` storage item and the associated child trie) as long as
+//! the parachain remains active. Once it does not, it is up to the parachain to ensure that the
+//! funds are returned to this module's fund sub-account in order that they be redistributed back to
+//! contributors. *Retirement* may be initiated by any account (using the `begin_retirement` call)
+//! once the parachain is removed from the its slot.
+//!
+//! @WARNING: For funds to be returned, it is imperative that this module's account is provided as
+//! the offboarding account for the slot. In the case that a parachain supplemented these funds in
+//! order to win a later auction, then it is the parachain's duty to ensure that the right amount of
+//! funds ultimately end up in module's fund sub-account. If the funds do not arrive, then
 
 use srml_support::{
 	StorageValue, dispatch::Result, decl_module, decl_storage, decl_event, storage::child,
@@ -45,7 +89,7 @@ pub trait Trait: slots::Trait {
 
 	/// The period of time (in blocks) between after an unsuccessful crowdfund ending where
 	/// contributors are able to withdraw their funds. After this period, their funds are lost.
-	const WITHDRAW_PERIOD: Self::BlockNumber;
+	const RETIREMENT_PERIOD: Self::BlockNumber;
 
 	/// What to do with funds that were not withdrawn.
 	type OrphanedFunds: OnUnbalanced<NegativeImbalanceOf<T>>;
@@ -54,8 +98,6 @@ pub trait Trait: slots::Trait {
 pub type FundIndex = u32;
 
 pub struct FundInfo<AccountId, Balance, Hash, BlockNumber> {
-	/// The id of the child-trie.
-	id: Hash,
 	/// The parachain that this fund has funded, if there is one. As long as this is `Some`, then
 	/// the funds may not be withdrawn and the fund cannot be disolved.
 	parachain: Option<ParaId>,
@@ -71,7 +113,8 @@ pub struct FundInfo<AccountId, Balance, Hash, BlockNumber> {
 	/// A hard-cap on the amount that may be contributed.
 	cap: Balance,
 	/// The most recent block that this had a contribution. Determines if we make a bid or not.
-	last_contribution: BlockNumber,
+	/// If this is `None`, then the last contribution was made outside of the ending period.
+	last_contribution: Option<BlockNumber>,
 	/// First slot in range to bid on; it's actually a LeasePeriod, but that's the same type as
 	/// BlockNumber.
 	first_slot: BlockNumber,
@@ -95,6 +138,9 @@ decl_storage! {
 		/// The funds that have had additional contributions during the last block. This is used
 		/// in order to determine which funds should submit updated bids.
 		NewRaise: Vec<FundId>;
+
+		/// True if the fund was ending at the last block.
+		WasEnding: bool;
 	}
 }
 
@@ -125,15 +171,18 @@ decl_module! {
 				WithdrawReason::Transfer,
 				ExistenceRequirement::AllowDeath
 			)?;
-			// No fees are paid here if we need to create this account; that's why we don't just
-			// use the stock `transfer`.
-			T::Currency::resolve_creating(Self::account_id(), imb);
 
 			let index = <FundCount<T>>::mutate(|c| { let r = *c; *c += 1; r });
-			let id = Self::id_from_index(index);
+
+			// No fees are paid here if we need to create this account; that's why we don't just
+			// use the stock `transfer`.
+			T::Currency::resolve_creating(Self::fund_account_id(index), imb);
+
 			<Funds<T>>::insert(index, FundInfo {
 				raised: Zero::zero(),
-				last_contribution: Zero::zero(),
+				// Ensure it's Some, so that the first contribution causes it to be inserted into
+				// `NewRaise`.
+				last_contribution: Some(Zero::zero()),
 				deploy_data: None,
 				..
 			});
@@ -153,15 +202,44 @@ decl_module! {
 			let now = <system::Module<T>>::block_number();
 			ensure!(fund.end > now, "contribution period ended");
 
-			T::Currency::transfer(&who, &Self::account_id(), value)?;
+			T::Currency::transfer(&who, &Self::fund_account_id(index), value)?;
 
-			let balance = child::get_or_default::<Balance>(&origin_contract.fund.id, &who);
+			let id = Self::id_from_index(index);
+			let balance = child::get_or_default::<Balance>(&id, &who);
 			let balance = balance.saturating_add(&value);
-			child::put(&origin_contract.fund.id, &owner, balance);
+			child::put(&id, &owner, balance);
 
-			if fund.last_contribution != now {
-				fund.last_contribution = now;
+			let is_ending = <slots::Module<T>>::is_ending();
+			let (maybe_last, push) = match (is_ending, fund.last_contribution) {
+				// Now in end period; last was at earlier time in end period: reset last and insert
+				(true, Some(c)) if c != now => (Some(Some(now)), true),
+				// Now in end period; last was either same block or before period: reset last (don't
+				// insert because it's already in).
+				(true, _) => (Some(Some(now)), false),
+				// Now outside end period; last was inside period end period: reset last to None and
+				// insert (because it will have been removed at end of last block).
+				(false, Some(_)) => (Some(None), true),
+				// Now outside end period; last was also outside end period. Don't do anything.
+				(false, _) => (None, false),
+			};
+			if push {
 				NewRaise::mutate(|v| v.push(index));
+			}
+			if let Some(last) = maybe_last {
+				fund.last_contribution = last;
+			}
+
+			if <slots::Module<T>>::is_ending() {
+				if let Some(c) = fund.last_contribution {
+					if c != now {
+					}
+				}
+				fund.last_contribution = Some(now);
+			} else {
+				if fund.last_contribution.is_some() {
+					fund.last_contribution = None;
+					NewRaise::mutate(|v| v.push(index));
+				}
 			}
 
 			<Funds<T>>::insert(index, &fund);
@@ -175,18 +253,20 @@ decl_module! {
 			let now = <system::Module<T>>::block_number();
 			ensure!(now >= fund.end, "contribution period not over");
 
-			let balance = child::get::<Balance>(&origin_contract.fund.id, &who)
+			let id = Self::id_from_index(index);
+			let balance = child::get::<Balance>(&id, &who)
 				.ok_or("not a contributor")?;
 
 			// Avoid using transfer to ensure we don't pay any fees.
 			T::Currency::resolve_into_existing(&who, T::Currency::withdraw(
-				&Self::account_id(),
+				&Self::fund_account_id(index),
 				balance,
 				WithdrawReason::Transfer,
 				ExistenceRequirement::AllowDeath
 			)?);
 
-			child::kill(&origin_contract.fund.id, &who);
+			let id = Self::id_from_index(index);
+			child::kill(&id, &who);
 			fund.raised = fund.raised.saturating_sub(&balance);
 
 			<Funds<T>>::insert(index, &fund);
@@ -195,12 +275,10 @@ decl_module! {
 		/// Note that a successful fund has lost its parachain slot, and place it into retirement.
 		fn begin_retirement(_, #[compact] index: FundIndex) {
 			// origin unimportant.
-
 			let mut fund = Self::funds(index).ok_or("invalid fund index")?;
-
 			let parachain_id = fund.parachain.take().ok_or("fund has no parachain")?;
-
-			// TODO: check that parachain_id has been retired.
+			let account = Self::fund_account_id(fund.index);
+			ensure!(T::Currency::free_balance(&account) >= fund.raised, "funds not yet returned");
 
 			// This fund just ended. Withdrawal period begins.
 			let now = <system::Module<T>>::block_number();
@@ -218,24 +296,27 @@ decl_module! {
 			let fund = Self::funds(index).ok_or("invalid fund index")?;
 			ensure!(fund.parachain.is_none(), "cannot disolve fund with active parachain");
 			let now = <system::Module<T>>::block_number();
-			ensure!(now >= fund.end + T::WITHDRAWAL_PERIOD, "withdrawal period not over");
+			ensure!(now >= fund.end + T::RETIREMENT_PERIOD, "retirement period not over");
+
+			let account = Self::fund_account_id(index);
 
 			// Avoid using transfer to ensure we don't pay any fees.
 			T::Currency::resolve_into_existing(&fund.owner, T::Currency::withdraw(
-				&Self::account_id(),
+				&account,
 				fund.deposit,
 				WithdrawReason::Transfer,
 				ExistenceRequirement::AllowDeath
 			)?);
 
 			T::OrphanedFunds::on_unbalanced(T::Currency::withdraw(
-				&Self::account_id(),
+				&account,
 				fund.raised,
 				WithdrawReason::Transfer,
 				ExistenceRequirement::AllowDeath
 			)?);
 
-			child::kill_storage(&origin_contract.fund.id);
+			let id = Self::id_from_index(index);
+			child::kill_storage(&id);
 			<Funds<T>>::kill(index);
 		}
 
@@ -302,7 +383,7 @@ decl_module! {
 			ensure!(fund.parachain.is_none(), "fund already onboarded");
 			fund.parachain = Some(para_id);
 
-			let fund_origin = system::RawOrigin::Signed(Self::account_id()).into();
+			let fund_origin = system::RawOrigin::Signed(Self::fund_account_id(index)).into();
 			slots::fix_deploy_data(fund_origin, index, para_id, code_hash, initial_head_data)?;
 
 			<Funds<T>>::insert(index, &fund);
@@ -313,10 +394,10 @@ decl_module! {
 				for fund in NewRaise::take().into_iter().filter_map(Self::funds) {
 					if fund.last_contribution == n {
 						let bidder = slots::Bidder::New(slots::NewBidder {
-							who: Self::account_id(),
+							who: Self::fund_account_id(fund.index),
 							/// FundIndex and slots::SubId happen to be the same type (u32). If this
 							/// ever changes, then some sort of coversion will be needed here.
-							sub: fund.index,
+							sub: 0,
 						});
 						<slots::Module<T>>::handle_bid(
 							bidder,
@@ -333,12 +414,12 @@ decl_module! {
 }
 
 impl<T: Trait> Module<T> {
-	/// The account ID of the treasury pot.
+	/// The account ID of the fund pot.
 	///
 	/// This actually does computation. If you need to keep using it, then make sure you cache the
 	/// value and only call this once.
-	pub fn account_id() -> T::AccountId {
-		MODULE_ID.into_account()
+	pub fn fund_account_id(index: FundIndex) -> T::AccountId {
+		MODULE_ID.into_sub_account(index)
 	}
 
 	pub fn id_from_index(index: FundIndex) -> T::Hash {
