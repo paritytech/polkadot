163845e9d1eb8af00e9be91795a5ed8c400f4b81
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-08-15 17:54:28 +0200

    Fix warning

--- runtime/src/claims.rs
+++ runtime/src/claims.rs
@@ -206,7 +206,7 @@ mod tests {
 
 	use sr_io::with_externalities;
 	use substrate_primitives::{H256, Blake2Hasher};
-	use codec::{Decode, Encode};
+	use codec::Encode;
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
 	use sr_primitives::{Perbill, traits::{BlakeTwo256, IdentityLookup, ConvertInto}, testing::Header};
