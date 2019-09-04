d2dac0863ae88c8b771166d7284fbfbbcc183e06
thiolliere <gui.thiolliere@gmail.com>
2019-09-04 12:47:25 +0200

    forward ported version bump (#418)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -98,7 +98,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 1,
-	spec_version: 1001,
+	spec_version: 1002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
