5b94ee6c7c2543f1569516adee8a2012de6143a2
Ashley <ashley.ruglys@gmail.com>
2019-10-11 00:50:41 +1300

    Changes for substrate#3699 (#466)
    * change upstream and branch
    
    * Fix build
    
    * remove doc renamings
    
    * Fix tests
    
    * .
    
    * Revert changing fork and branch
    
    * Update Cargo.toml
    
    * Update parachains.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -517,6 +517,7 @@ construct_runtime!(
 	{
 		// Basic stuff; balances is uncallable initially.
 		System: system::{Module, Call, Storage, Config, Event},
+		RandomnessCollectiveFlip: randomness_collective_flip::{Module, Storage},
 
 		// Must be before session.
 		Babe: babe::{Module, Call, Storage, Config, Inherent(Timestamp)},
@@ -624,7 +625,7 @@ impl_runtime_apis! {
 		}
 
 		fn random_seed() -> <Block as BlockT>::Hash {
-			System::random_seed()
+			RandomnessCollectiveFlip::random_seed()
 		}
 	}
 
