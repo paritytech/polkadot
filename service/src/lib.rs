946940e6ff435338901568a34a2bf94c9e3d71ec
Bastian Köcher <bkchr@users.noreply.github.com>
2019-10-20 21:23:16 +0200

    Update to latest Substrate master (#486)

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -307,6 +307,7 @@ pub fn new_full(config: Configuration<CustomConfiguration, GenesisConfig>)
 				inherent_data_providers: inherent_data_providers.clone(),
 				on_exit: service.on_exit(),
 				telemetry_on_connect: Some(service.telemetry_on_connect_stream()),
+				voting_rule: grandpa::VotingRulesBuilder::default().build(),
 			};
 			service.spawn_essential_task(Box::new(grandpa::run_grandpa_voter(grandpa_config)?));
 		},
