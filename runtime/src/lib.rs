00b4f7a65eeac4a4e134e111bd37d8425b716f55
Gavin Wood <gavin@parity.io>
2019-11-03 18:49:49 +0100

    Bump Substrate and versions (#531)
    * Bump versions
    
    * Build fix.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -99,7 +99,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 1,
-	spec_version: 1012,
+	spec_version: 1013,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
