1edf2874c935b68455e98eb207f0cbb6cdd691a2
Bastian Köcher <bkchr@users.noreply.github.com>
2019-11-11 12:16:10 +0100

    Prepare Polkadot update for Substrate runtime interface 2.0 (#563)
    * Prepare Polkadot update for Substrate runtime interface 2.0
    
    * bump substrate to release specific v0.6.15
    
    * Switch to `polkadot-master`
    
    * Version bump

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -728,7 +728,6 @@ sr_api::impl_runtime_apis! {
 
 	impl substrate_session::SessionKeys<Block> for Runtime {
 		fn generate_session_keys(seed: Option<Vec<u8>>) -> Vec<u8> {
-			let seed = seed.as_ref().map(|s| rstd::str::from_utf8(&s).expect("Seed is an utf8 string"));
 			SessionKeys::generate(seed)
 		}
 	}
