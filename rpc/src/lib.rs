578fcb82088b3f1b5ac5ff2314a9f641128dc316
Gavin Wood <gavin@parity.io>
2019-11-23 00:33:32 +0100

    Backports and a version bump (#607)
    * Fix `run_to_block` for Crowdfund module (#603)
    
    Probably a copy paste error.
    
    * Update for Substrate master (#600)
    
    * update substrate for change to palette
    
    * change paint to palette
    
    * update lock
    
    * Fix missing import
    
    * change to polkadot-master
    
    * Use same commit hash of parity-common
    
    * Resolve linking errors
    
    * Rename to frame
    
    * bump spec
    
    * Subsume #602 and #596
    
    * Fix DispatchInfo
    
    * Merge `futures03` and `joe-update-to-palette` (#606)
    
    * Change repo and branch
    
    * Made changes
    
    * Bumped async-std version
    
    * Fix line width
    
    * Bump spec_version
    
    * Fix `run_to_block` for Crowdfund module (#603)
    
    Probably a copy paste error.
    
    * Bump dependencies
    
    * Update trie-db to be inline with substrate
    
    * Fix documentation warning
    
    * Fix test compilation
    
    * Bump versions

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -33,12 +33,12 @@ pub fn create<C, P>(client: Arc<C>, pool: Arc<Pool<P>>) -> RpcExtension where
 	C: ProvideRuntimeApi,
 	C: client::blockchain::HeaderBackend<Block>,
 	C: Send + Sync + 'static,
-	C::Api: paint_system_rpc::AccountNonceApi<Block, AccountId, Nonce>,
-	C::Api: paint_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance, UncheckedExtrinsic>,
+	C::Api: frame_system_rpc::AccountNonceApi<Block, AccountId, Nonce>,
+	C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance, UncheckedExtrinsic>,
 	P: ChainApi + Sync + Send + 'static,
 {
-	use paint_system_rpc::{System, SystemApi};
-	use paint_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
+	use frame_system_rpc::{System, SystemApi};
+	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
 
 	let mut io = jsonrpc_core::IoHandler::default();
 	io.extend_with(
--- runtime/src/claims.rs
+++ runtime/src/claims.rs
@@ -18,8 +18,9 @@
 
 use rstd::prelude::*;
 use sr_io::{hashing::keccak_256, crypto::secp256k1_ecdsa_recover};
-use paint_support::{decl_event, decl_storage, decl_module};
-use paint_support::traits::{Currency, Get};
+use frame_support::{decl_event, decl_storage, decl_module};
+use frame_support::weights::SimpleDispatchInfo;
+use frame_support::traits::{Currency, Get};
 use system::{ensure_root, ensure_none};
 use codec::{Encode, Decode};
 #[cfg(feature = "std")]
@@ -27,7 +28,7 @@ use serde::{self, Serialize, Deserialize, Serializer, Deserializer};
 #[cfg(feature = "std")]
 use sr_primitives::traits::Zero;
 use sr_primitives::{
-	RuntimeDebug, weights::SimpleDispatchInfo, transaction_validity::{
+	RuntimeDebug, transaction_validity::{
 		TransactionLongevity, TransactionValidity, ValidTransaction, InvalidTransaction
 	},
 };
@@ -249,7 +250,7 @@ mod tests {
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
 	use sr_primitives::{Perbill, traits::{BlakeTwo256, IdentityLookup}, testing::Header};
 	use balances;
-	use paint_support::{impl_outer_origin, assert_ok, assert_err, assert_noop, parameter_types};
+	use frame_support::{impl_outer_origin, assert_ok, assert_err, assert_noop, parameter_types};
 
 	impl_outer_origin! {
 		pub enum Origin for Test {}
