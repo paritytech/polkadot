cd470a1b1468f230789037f7f670fd82d6e2207c
gabriel <gabriel@parity.io>
2019-11-27 13:19:20 -1000

    merge check_substrate.sh into check_runtime.sh

--- scripts/gitlab/check_runtime.sh
+++ scripts/gitlab/check_runtime.sh
@@ -16,6 +16,9 @@ set -e # fail on any error
 BASE_BRANCH="v0.6"
 VERSIONS_FILE="runtime/src/lib.rs"
 
+SUBSTRATE_REPO="https://github.com/paritytech/substrate"
+SUBSTRATE_REPO_CARGO="git\+${SUBSTRATE_REPO}\?branch=polkadot-master"
+SUBSTRATE_VERSIONS_FILE="node/runtime/src/lib.rs"
 
 
 # figure out the latest release tag
@@ -52,13 +55,61 @@ then
 
 	EOT
 
-	# check if substrate references have changed
-	. ./scripts/gitlab/check_substrate.sh
+	# continue checking if Cargo.lock was updated with a new substrate reference 
+	# and if that change includes a {spec|impl}_version update.
 
-	# exit statements in there will end the script from there
-	check_substrate
+	SUBSTRATE_REFS_CHANGED="$(git diff refs/tags/${LATEST_TAG}...${CI_COMMIT_SHA} Cargo.lock \
+		| sed -n -r "s~^[\+\-]source = \"${SUBSTRATE_REPO_CARGO}#([a-f0-9]+)\".*$~\1~p" | sort -u | wc -l)"
 	
-	# exit 0
+	# check Cargo.lock for substrate ref change
+	case "${SUBSTRATE_REFS_CHANGED}" in
+	  (0)
+	    echo "| substrate refs not changed in Cargo.lock"
+	    exit 0
+	    ;;
+	  (2)
+	    echo "|\n| substrate refs updated since ${LATEST_TAG}"
+	    ;;
+	  (*)
+	    echo "| check unsupported: more than one commit targeted in repo ${SUBSTRATE_REPO_CARGO}"
+	    exit 1
+	esac
+	
+	
+	SUBSTRATE_PREV_REF="$(git diff refs/tags/${LATEST_TAG}...${CI_COMMIT_SHA} Cargo.lock \
+		| sed -n -r "s~^\-source = \"${SUBSTRATE_REPO_CARGO}#([a-f0-9]+)\".*$~\1~p" | sort -u | head -n 1)"
+	
+	SUBSTRATE_NEW_REF="$(git diff refs/tags/${LATEST_TAG}...${CI_COMMIT_SHA} Cargo.lock \
+		| sed -n -r "s~^\+source = \"${SUBSTRATE_REPO_CARGO}#([a-f0-9]+)\".*$~\1~p" | sort -u | head -n 1)"
+	
+	
+	cat <<-EOT
+	|
+	| previous substrate commit id ${SUBSTRATE_PREV_REF}
+	| new substrate commit id      ${SUBSTRATE_NEW_REF}
+	|
+	EOT
+
+	# okay so now need to fetch the substrate repository and check whether spec_version or impl_version has changed there
+	SUBSTRATE_CLONE_DIR="$(mktemp -t -d substrate-XXX)"
+	trap "rm -rf ${SUBSTRATE_CLONE_DIR}" INT QUIT TERM ABRT EXIT
+
+
+	git clone --branch polkadot-master --depth 100 --no-tags \
+	  ${SUBSTRATE_REPO} ${SUBSTRATE_CLONE_DIR}
+
+
+	# check if there are changes to the spec|impl versions
+	git -C ${SUBSTRATE_CLONE_DIR} diff \
+		${SUBSTRATE_PREV_REF}..${SUBSTRATE_NEW_REF} ${SUBSTRATE_VERSIONS_FILE} \
+		| grep -E '^[\+\-][[:space:]]+(spec|impl)_version: +([0-9]+),$' || exit 0
+
+	cat <<-EOT
+	|
+	| spec_version or or impl_version have changed in substrate after updating Cargo.lock
+	| please make sure versions are bumped in polkadot accordingly
+	|
+	EOT
 fi
 
 
