8c942d2ec1b27811feba12e6ce0af241e4dbe7f8
gabriel <gabriel@parity.io>
2020-01-03 19:18:22 +0800

    ci: check_runtime for both runtimes

--- scripts/gitlab/check_runtime.sh
+++ scripts/gitlab/check_runtime.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
 #
-# check for any changes in the node/src/runtime, srml/ and core/sr_* trees. if
-# there are any changes found, it should mark the PR breaksconsensus and
-# "auto-fail" the PR if there isn't a change in the runtime/src/lib.rs file 
-# that alters the version.
+# check for any changes in the ^runtime/ tree. if there are any changes found, 
+# it should mark the PR B2-breaksapi and "auto-fail" the PR if there 
+# isn't a change in the runtime/{kusama,polkadot}/src/lib.rs file that alters 
+# the version.
 
 set -e # fail on any error
 
@@ -27,42 +27,39 @@ github_label () {
 
 
 
-# make sure the master branch is available in shallow clones
-git fetch --depth=${GIT_DEPTH:-100} origin master
-
-
-# check if the wasm sources changed
-if ! git diff --name-only origin/master...${CI_COMMIT_SHA} \
-	| grep -q -e '^runtime/'
-then
-	cat <<-EOT
+for VERSIONS_FILE in runtime/kusama/src/lib.rs runtime/polkadot/src/lib.rs
+do
+	# check if the wasm sources changed
+	if ! git diff --name-only origin/master...${CI_COMMIT_SHA} \
+		| grep -q -e '^runtime/'
+	then
+		cat <<-EOT
+		
+		no changes to the runtime source code detected
 	
-	no changes to the runtime source code detected
-
-	EOT
-
-	exit 0
-fi
-
-
-check_runtime () {
-	echo "# checking ${1}"
+		EOT
+	
+		continue
+	fi
+	
+	
+	
 	# check for spec_version updates: if the spec versions changed, then there is
 	# consensus-critical logic that has changed. the runtime wasm blobs must be
 	# rebuilt.
-
-	add_spec_version="$(git diff origin/master...${CI_COMMIT_SHA} ${1} \
+	
+	add_spec_version="$(git diff origin/master...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 		| sed -n -r "s/^\+[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
-	sub_spec_version="$(git diff origin/master...${CI_COMMIT_SHA} ${1} \
+	sub_spec_version="$(git diff origin/master...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 		| sed -n -r "s/^\-[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
-
-
+	
+	
 	# see if the version and the binary blob changed
 	if [ "${add_spec_version}" != "${sub_spec_version}" ]
 	then
-
+	
 		github_label "B2-breaksapi"
-
+	
 		cat <<-EOT
 			
 			changes to the runtime sources and changes in the spec version.
@@ -70,54 +67,50 @@ check_runtime () {
 			spec_version: ${sub_spec_version} -> ${add_spec_version}
 		
 		EOT
-		return
-
+		continue
+	
 	else
 		# check for impl_version updates: if only the impl versions changed, we assume
 		# there is no consensus-critical logic that has changed.
-
-		add_impl_version="$(git diff origin/master...${CI_COMMIT_SHA} ${1} \
+	
+		add_impl_version="$(git diff origin/master...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 			| sed -n -r 's/^\+[[:space:]]+impl_version: +([0-9]+),$/\1/p')"
-		sub_impl_version="$(git diff origin/master...${CI_COMMIT_SHA} ${1} \
+		sub_impl_version="$(git diff origin/master...${CI_COMMIT_SHA} ${VERSIONS_FILE} \
 			| sed -n -r 's/^\-[[:space:]]+impl_version: +([0-9]+),$/\1/p')"
-
-
+	
+	
 		# see if the impl version changed
 		if [ "${add_impl_version}" != "${sub_impl_version}" ]
 		then
 			cat <<-EOT
 			
 			changes to the runtime sources and changes in the impl version.
-
+	
 			impl_version: ${sub_impl_version} -> ${add_impl_version}
-
+	
 			EOT
-			exit 0
+			continue
 		fi
-
-
+	
+	
 		cat <<-EOT
-
+	
 		wasm source files changed but not the spec/impl version and the runtime
 		binary blob. If changes made do not alter logic, just bump 'impl_version'.
 		If they do change logic, bump 'spec_version' and rebuild wasm.
-
+	
 		source file directories:
 		- runtime
-
-		versions file: ${1}
-
+	
+		versions file: ${VERSIONS_FILE}
+	
 		EOT
 
-		# drop through into pushing `gotissues` and exit 1...
+		exit 1
 	fi
+done
 
-	# dropped through. there's something wrong;  exit 1.
 
-	exit 1
-}
-
-check_runtime "runtime/kusama/src/lib.rs"
-check_runtime "runtime/polkadot/src/lib.rs"
+exit 0
 
 # vim: noexpandtab
