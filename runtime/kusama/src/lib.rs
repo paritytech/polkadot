b2509f9d7c1bc224d472aeca8a6779bef1f622c0
Gavin Wood <gavin@parity.io>
2020-01-08 14:44:49 +0100

    Bumped version to v0.7.15, use minimallly-changed Substrate. (#741)
    * Bumped versions
    
    * Bump to latest substrate that exposes load_spec

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -78,7 +78,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1035,
+	spec_version: 1036,
 	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 };
