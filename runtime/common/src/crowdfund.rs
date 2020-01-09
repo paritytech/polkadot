62c38afd8cd11cfbaf6f1b1813224a9c68919546
Gav Wood <gavin@parity.io>
2020-01-10 00:00:27 +0100

    Fixes

--- runtime/common/src/crowdfund.rs
+++ runtime/common/src/crowdfund.rs
@@ -506,7 +506,7 @@ mod tests {
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are requried.
 	use sp_runtime::{
-		Perbill, Permill, testing::Header, DispatchResult,
+		Perbill, Permill, Percent, testing::Header, DispatchResult,
 		traits::{BlakeTwo256, OnInitialize, OnFinalize, IdentityLookup},
 	};
 	use crate::registrar::Registrar;
