c2591390ee2869d05254e237e586882b0fc0563a
Gav Wood <gavin@parity.io>
2020-01-13 12:51:48 +0100

    Bump RT version

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -77,7 +77,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1039,
+	spec_version: 1040,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
