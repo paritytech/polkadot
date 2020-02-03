3312083fa0f34f9b8080437b9ced176bae1e72df
gabriel <gabriel@parity.io>
2020-02-03 15:26:51 +0100

    ci: fix check_runtime on busybox

--- scripts/gitlab/check_runtime.sh
+++ scripts/gitlab/check_runtime.sh
@@ -89,7 +89,7 @@ then
 	EOT
 
 	# okay so now need to fetch the substrate repository and check whether spec_version or impl_version has changed there
-	SUBSTRATE_CLONE_DIR="$(mktemp -t -d substrate-XXX)"
+	SUBSTRATE_CLONE_DIR="$(mktemp -t -d substrate-XXXXXX)"
 	trap "rm -rf ${SUBSTRATE_CLONE_DIR}" INT QUIT TERM ABRT EXIT
 
 
