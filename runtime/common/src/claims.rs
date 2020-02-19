6c4c166ac4e8ffd7030d4c6e24670b16e125ea97
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-02-19 15:31:26 +0100

    Make claim tests a bit better

--- runtime/common/src/claims.rs
+++ runtime/common/src/claims.rs
@@ -422,6 +422,7 @@ mod tests {
 			assert_ok!(Claims::claim(Origin::NONE, 42, sig(&alice(), &42u64.encode())));
 			assert_eq!(Balances::free_balance(&42), 100);
 			assert_eq!(Vesting::vesting_balance(&42), 50);
+			assert_eq!(Claims::total(), 0);
 		});
 	}
 
@@ -438,9 +439,11 @@ mod tests {
 				Error::<Test>::SignerHasNoClaim
 			);
 			assert_ok!(Claims::mint_claim(Origin::ROOT, eth(&bob()), 200, None));
+			assert_eq!(Claims::total(), 300);
 			assert_ok!(Claims::claim(Origin::NONE, 69, sig(&bob(), &69u64.encode())));
 			assert_eq!(Balances::free_balance(&69), 200);
 			assert_eq!(Vesting::vesting_balance(&69), 0);
+			assert_eq!(Claims::total(), 100);
 		});
 	}
 
