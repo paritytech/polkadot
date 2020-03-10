951e38bc2348fbf05fc633e34b2ddbdaa3f103d9
Ashley Ruglys <ashley.ruglys@gmail.com>
2020-03-10 14:58:47 +0100

    Bump kusama impl version

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -77,7 +77,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
 	spec_version: 1050,
-	impl_version: 0,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 };
 
