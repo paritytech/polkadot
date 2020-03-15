753ea08f8231f53d3e16f729c2807aab711bd6de
Robert Habermeier <rphmeier@gmail.com>
2020-03-15 16:42:03 -0400

    set up storage schema for old parachains code

--- runtime/common/src/parachains.rs
+++ runtime/common/src/parachains.rs
@@ -132,6 +132,19 @@ pub trait Trait: attestations::Trait {
 
 	/// Max head data size.
 	type MaxHeadDataSize: Get<u32>;
+	/// The frequency at which paras can upgrade their validation function.
+	type ValidationUgradeFrequency: Get<BlockNumber>;
+
+	/// The delay before a validation function upgrade is applied.
+	type ValidationUpgradeDelay: Get<BlockNumber>;
+
+	/// The period (in blocks) that slash reports are permitted against an
+	/// included candidate.
+	///
+	/// After validation function upgrades, the old code is persisted on-chain
+	/// for this period, to ensure that candidates validated under old functions
+	/// can be re-checked.
+	type SlashPeriod: Get<BlockNumber>;
 }
 
 /// Origin for the parachains module.
@@ -149,13 +162,113 @@ const MAX_QUEUE_COUNT: usize = 100;
 /// single message.
 const WATERMARK_QUEUE_SIZE: usize = 20000;
 
+#[derive(Default)]
+struct PastCodeMeta {
+	// Block numbers where the code was replaced. These can be used as indices
+	// into the `PastCode` map along with the `ParaId` to fetch the code itself.
+	upgrade_times: Vec<BlockNumber>,
+	// This tracks the highest pruned code-replacement, if any.
+	last_pruned: Option<BlockNumber>,
+}
+
+enum UseCodeAt {
+	// Use the current code.
+	Current,
+	// Use the code that was replaced at the given block number.
+	ReplacedAt(BlockNumber),
+}
+
+impl PastCodeMeta {
+	// Yields the block number of the code that should be used for validating at
+	// the given block number.
+	//
+	// a return value of `None` means that there is no code we are aware of that
+	// should be used to validate at the given height.
+	fn code_at(&self, at: BlockNumber) -> Option<UseCodeAt> {
+		// The `PastCode` map stores the code which was replaced at `t`.
+		let end_position = self.upgrade_times.iter().position(|t| t < at);
+		if let Some(end_position) = end_position {
+			Some(if end_position != 0 {
+				// `end_position` gives us the replacement time where the code used at `at`
+				// was set. But that code has been replaced: `end_position - 1` yields
+				// that index.
+				UseCodeAt::ReplacedAt(self.upgrade_times[end_position - 1])
+			} else {
+				// the most recent tracked replacement is before `at`.
+				// this means that the code put in place then (i.e. the current code)
+				// is correct for validating at `at`.
+				UseCodeAt::Current
+			})
+		} else {
+			if self.last_pruned.as_ref().map_or(true, |n| n < at) {
+				// Our `last_pruned` is before `at`, so we still have the code!
+				// but no code upgrade entries found before the `at` parameter.
+				//
+				// this means one of two things is true:
+				// 1. there are no non-pruned upgrade logs. in this case use `Current`
+				// 2. there are non-pruned upgrade logs all after `at`.
+				//    in this case use the oldest upgrade log.
+				Some(self.upgrade_times.last()
+					.map(|n| UseCodeAt::ReplacedAt(*n))
+					.unwrap_or(UseCodeAt::Current)
+				)
+			} else {
+				// We don't have the code anymore.
+				None
+			}
+			None
+		}
+	}
+
+	// The block at which the most recently tracked code change occurred.
+	fn most_recent_change(&self) -> Option<BlockNumber> {
+		self.upgrade_times.first().map(|x| x.clone())
+	}
+
+	// The block at which the earliest tracked code change occurred.
+	fn earliest_tracked_change(&self) -> Option<BlockNumber> {
+		self.upgrade_times.last().map(|x| x.clone())
+	}
+
+	// prunes all code upgrade logs occurring at or before `max`.
+	// note that code replaced at `x` is the code used to validate all blocks before
+	// `x`. Thus, `max` should be outside of the slashing window when this is invoked.
+	// TODO [now]: pruning.
+	fn prune_up_to(&mut self, max: BlockNumber) -> impl Iterator<Item=BlockNumber> {
+		match self.upgrade_times.iter().position(|t| t <= max) {
+			None => {
+				// this is effectively a no-op `drain` - desired because all
+				// logged code upgrades occurred after `max`.
+				self.upgrade_times.drain(self.upgrade_times.len()..)
+			}
+			Some(pos) => self.upgrade_times.drain(pos..),
+		}
+	}
+}
+
+struct PlannedCodeChange {
+	// The block number at which the planned code change is expected,
+	// It will be applied after the first parablock included at or after
+	// this relay chain block.
+	expected_at: BlockNumber,
+	new_code: Vec<u8>,
+}
+
 decl_storage! {
 	trait Store for Module<T: Trait> as Parachains
 	{
 		/// All authorities' keys at the moment.
 		pub Authorities get(authorities): Vec<ValidatorId>;
-		/// The parachains registered at present.
+		/// The code of parachains registered at present.
 		pub Code get(parachain_code): map hasher(blake2_256) ParaId => Option<Vec<u8>>;
+		/// Past code of parachains. The parachains themselves may not be registered anymore,
+		/// but we also keep their code on-chain for the same amount of time as outdated code
+		/// to assist with availability.
+		PastCodeMeta: map hasher(blake2_256) ParaId => PastCodeMeta,
+		/// Actual past code, indicated by the parachain and the block number at which it
+		/// was outdated.
+		PastCode: map hasher(blake2_256) (ParaId, BlockNumber) => Option<Vec<u8>>,
+
 		/// The heads of the parachains registered at present.
 		pub Heads get(parachain_head): map hasher(blake2_256) ParaId => Option<Vec<u8>>;
 		/// Messages ready to be dispatched onto the relay chain. It is subject to
@@ -335,11 +448,19 @@ impl<T: Trait> Module<T> {
 		<Heads>::insert(id, initial_head_data);
 	}
 
+	/// Cleanup all storage related to a para. Some pieces of data may remain
+	/// available in the on-chain state.
 	pub fn cleanup_para(
 		id: ParaId,
 	) {
-		<Code>::remove(id);
+		let code = <Code>::take(id);
 		<Heads>::remove(id);
+
+		Self::note_past_code(id, code);
+	}
+
+	fn note_past_code(id: ParaId, code: Vec<u8>) {
+		// TODO [now] implement this.
 	}
 
 	/// Dispatch some messages from a parachain.
@@ -698,6 +819,7 @@ impl<T: Trait> Module<T> {
 			// orphaned before a block containing C is finalized. Care must be taken
 			// not to prune the data for C simply because an orphaned block contained
 			// it.
+
 			ensure!(
 				candidate.candidate().relay_parent.as_ref() == parent_hash.as_ref(),
 				Error::<T>::UnexpectedRelayParent,
