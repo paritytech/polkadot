da85041197f0f96a6dcc5ddc3940b659479e4ed6
NikVolf <nikvolf@gmail.com>
2020-03-20 15:41:56 +0300

    update spec_version

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -78,8 +78,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1054,
-	impl_version: 1,
+	spec_version: 1055,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
 
