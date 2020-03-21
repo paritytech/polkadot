3c06299d24eb9ca485c2c18106a676b25829664b
Gav Wood <gavin@parity.io>
2020-03-21 16:12:33 +0100

    Introduce progressive democracy
    Also move to "master" branch of Substrate ready for continuous
    sync with Substrate master.

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -307,6 +307,7 @@ parameter_types! {
 	pub const CooloffPeriod: BlockNumber = 7 * DAYS;
 	// One cent: $10,000 / MB
 	pub const PreimageByteDeposit: Balance = 10 * MILLICENTS;
+	pub const InstantAllowed: bool = true;
 }
 
 impl democracy::Trait for Runtime {
@@ -327,6 +328,8 @@ impl democracy::Trait for Runtime {
 	/// Two thirds of the technical committee can have an ExternalMajority/ExternalDefault vote
 	/// be tabled immediately and with a shorter voting/enactment period.
 	type FastTrackOrigin = collective::EnsureProportionAtLeast<_2, _3, AccountId, TechnicalCollective>;
+	type InstantOrigin = collective::EnsureProportionAtLeast<_1, _1, AccountId, TechnicalCollective>;
+	type InstantAllowed = InstantAllowed;
 	type FastTrackVotingPeriod = FastTrackVotingPeriod;
 	// To cancel a proposal which has been passed, 2/3 of the council must agree to it.
 	type CancellationOrigin = collective::EnsureProportionAtLeast<_2, _3, AccountId, CouncilCollective>;
