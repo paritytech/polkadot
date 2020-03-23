b6df9dc000e6b54804f298f0eb9417525eb4cc32
Fedor Sakharov <fedor.sakharov@gmail.com>
2020-03-23 19:30:22 +0300

    Bump spec versions

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -79,8 +79,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1055,
-	impl_version: 1,
+	spec_version: 1056,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
 
--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -75,7 +75,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot-test-runtime"),
 	impl_name: create_runtime_str!("parity-polkadot-test-runtime"),
 	authoring_version: 2,
-	spec_version: 1048,
+	spec_version: 1049,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 };
