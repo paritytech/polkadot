fb358e8df4b5a7980f59e07867357ebd0284272c
Gavin Wood <gavin@parity.io>
2020-04-01 19:25:19 +0200

    A couple spaces where emojis were improperly laid out (#959)
    * Fixes for democracy using Scheduler
    
    * Revert branch change
    
    * Emojis

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -54,11 +54,11 @@ pub fn run(version: VersionInfo) -> sc_cli::Result<()> {
 			info!("  version {}", config.full_version());
 			info!("  by {}, 2017-2020", version.author);
 			info!("📋 Chain specification: {}", config.expect_chain_spec().name());
-			info!("🏷 Node name: {}", config.name);
+			info!("🏷  Node name: {}", config.name);
 			info!("👤 Roles: {}", config.display_role());
 
 			if is_kusama {
-				info!("⛓ Native runtime: {}", service::KusamaExecutor::native_version().runtime_version);
+				info!("⛓  Native runtime: {}", service::KusamaExecutor::native_version().runtime_version);
 				info!("----------------------------");
 				info!("This chain is not in any way");
 				info!("      endorsed by the       ");
@@ -71,7 +71,7 @@ pub fn run(version: VersionInfo) -> sc_cli::Result<()> {
 					service::kusama_runtime::UncheckedExtrinsic,
 				>(config, opt.authority_discovery_enabled, grandpa_pause)
 			} else {
-				info!("⛓ Native runtime: {}", service::PolkadotExecutor::native_version().runtime_version);
+				info!("⛓  Native runtime: {}", service::PolkadotExecutor::native_version().runtime_version);
 
 				run_service_until_exit::<
 					service::polkadot_runtime::RuntimeApi,
