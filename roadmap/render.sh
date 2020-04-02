1dd9fcb132abf4a030467f72b9e1111df0be985b
Robert Habermeier <rphmeier@gmail.com>
2020-04-01 23:51:33 -0400

    render to svg instead

--- roadmap/render.sh
+++ roadmap/render.sh
@@ -3,8 +3,8 @@
 render () {
 	echo "Rendering $1"
 	skill-tree $1.toml output
-	python3 -c "from graphviz import render; render('dot', 'png', 'output/skill-tree.dot')"
-	mv output/skill-tree.dot.png "$1.png"
+	python3 -c "from graphviz import render; render('dot', 'svg', 'output/skill-tree.dot')"
+	mv output/skill-tree.dot.svg "$1.svg"
 	rm -rf output
 }
 
