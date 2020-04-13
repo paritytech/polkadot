4b39837b565d0b73a7f2daed7b1b3ad02e0cab2f
Scott Piriou <scott@parity.io>
2020-04-13 15:07:10 +0200

    Update expect error message in run_command_and_kill

--- tests/running_the_node_and_interrupt.rs
+++ tests/running_the_node_and_interrupt.rs
@@ -28,7 +28,7 @@ fn running_the_node_works_and_can_be_interrupted() {
 
 	fn run_command_and_kill(signal: Signal) {
 		let tmpdir = tempdir().expect("coult not create temp dir");
-		let base_path = tmpdir.path().to_str().expect("path should be valid utf8");
+		let base_path = tmpdir.path().to_str().expect("path should consist of valid utf8 characters");
 
 		let mut cmd = Command::new(cargo_bin("polkadot"))
 			.args(&["--dev", "-d", base_path])
