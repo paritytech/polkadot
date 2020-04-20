faf4e3d76179650127ec058b3044cbb0eca41465
cheme <emericchevalier.pro@gmail.com>
2020-04-20 19:02:42 +0200

    bump spec versions

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -86,7 +86,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 2,
-	spec_version: 1007,
+	spec_version: 1008,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
