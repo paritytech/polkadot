50810682551e4a1c7ba800278738745830db0557
Hernando Castano <castano.ha@gmail.com>
2020-05-13 10:28:59 -0400

    Move weight constant checks into one test

--- runtime/polkadot/tests/weights.rs
+++ runtime/polkadot/tests/weights.rs
@@ -42,31 +42,13 @@ use system::Call as SystemCall;
 use treasury::Call as TreasuryCall;
 
 #[test]
-fn sanity_check_weight_per_second_is_as_expected() {
-	// This value comes from Substrate, we want to make sure that if it
+fn sanity_check_weight_per_time_constants_are_as_expected() {
+	// These values comes from Substrate, we want to make sure that if it
 	// ever changes we don't accidently break Polkadot
-	assert_eq!(WEIGHT_PER_SECOND, 1_000_000_000_000)
-}
-
-#[test]
-fn sanity_check_weight_per_milli_is_as_expected() {
-	// This value comes from Substrate, we want to make sure that if it
-	// ever changes we don't accidently break Polkadot
-	assert_eq!(WEIGHT_PER_MILLIS, 1_000_000_000)
-}
-
-#[test]
-fn sanity_check_weight_per_micros_is_as_expected() {
-	// This value comes from Substrate, we want to make sure that if it
-	// ever changes we don't accidently break Polkadot
-	assert_eq!(WEIGHT_PER_MICROS, 1_000_000)
-}
-
-#[test]
-fn sanity_check_weight_per_nanos_is_as_expected() {
-	// This value comes from Substrate, we want to make sure that if it
-	// ever changes we don't accidently break Polkadot
-	assert_eq!(WEIGHT_PER_NANOS, 1_000)
+	assert_eq!(WEIGHT_PER_SECOND, 1_000_000_000_000);
+	assert_eq!(WEIGHT_PER_MILLIS, WEIGHT_PER_SECOND / 1000);
+	assert_eq!(WEIGHT_PER_MICROS, WEIGHT_PER_MILLIS / 1000);
+	assert_eq!(WEIGHT_PER_NANOS, WEIGHT_PER_MICROS / 1000);
 }
 
 #[test]
