e550ef0ffb60c30d442dc7e8b6fa0be4cccef866
Gav Wood <gavin@parity.io>
2020-05-14 17:22:19 +0200

    Enable PrevalidateAttests on Polkadot

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -60,6 +60,7 @@ use im_online::sr25519::AuthorityId as ImOnlineId;
 use authority_discovery_primitives::AuthorityId as AuthorityDiscoveryId;
 use transaction_payment_rpc_runtime_api::RuntimeDispatchInfo;
 use session::{historical as session_historical};
+use claims::PrevalidateAttests;
 
 #[cfg(feature = "std")]
 pub use staking::StakerStatus;
@@ -846,6 +847,7 @@ pub type SignedExtra = (
 	registrar::LimitParathreadCommits<Runtime>,
 	parachains::ValidateDoubleVoteReports<Runtime>,
 	grandpa::ValidateEquivocationReport<Runtime>,
+	claims::PrevalidateAttests<Runtime>,
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
