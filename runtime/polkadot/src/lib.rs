d8328043160511b2685112d0cbaff8fdbfea5948
Gavin Wood <gavin@parity.io>
2020-05-16 12:56:49 +0200

    Extra requirements for claimants (#1091)
    * Introduce mandatory statement signing into claims
    
    * Introduce SignedExtension
    
    * Tests passing
    
    * Bump runtime version
    
    * Bump version, fix test
    
    * Test for validate
    
    * Another couple of tests
    
    * Enable PrevalidateAttests on Polkadot
    
    * Enable PrevalidateAttests on Polkadot
    
    * Fix build
    
    * Fixes
    
    * More fixes
    
    * Fix bench tests
    
    * Fix & test Preclaim clobbering.
    
    * Fix for errant claim logic
    
    * Add test
    
    * Update tests, always use Vec as input
    
    * mint_claim can add signature, some_benchmarks
    
    * Add claim with statement test
    
    * finish benchmarks
    
    * put the correct number of claims with benchmarks
    
    * fix compiler warning
    
    * Update weights
    
    * Weight comments for validation
    
    Co-authored-by: Shawn Tabrizi <shawntabrizi@gmail.com>

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -614,6 +614,7 @@ impl<LocalCall> system::offchain::CreateSignedTransaction<LocalCall> for Runtime
 			registrar::LimitParathreadCommits::<Runtime>::new(),
 			parachains::ValidateDoubleVoteReports::<Runtime>::new(),
 			grandpa::ValidateEquivocationReport::<Runtime>::new(),
+			claims::PrevalidateAttests::<Runtime>::new(),
 		);
 		let raw_payload = SignedPayload::new(call, extra).map_err(|e| {
 			debug::warn!("Unable to create signed payload: {:?}", e);
@@ -773,6 +774,7 @@ pub type SignedExtra = (
 	registrar::LimitParathreadCommits<Runtime>,
 	parachains::ValidateDoubleVoteReports<Runtime>,
 	grandpa::ValidateEquivocationReport<Runtime>,
+	claims::PrevalidateAttests<Runtime>,
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
