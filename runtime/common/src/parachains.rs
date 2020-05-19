58957662ead2a9b6a72f709ef052730a8cb92c40
Tomasz Drwięga <tomasz@parity.io>
2020-05-19 14:06:41 +0200

    Fix tests?

--- runtime/common/src/parachains.rs
+++ runtime/common/src/parachains.rs
@@ -1624,6 +1624,7 @@ mod tests {
 		type DbWeight = ();
 		type BlockExecutionWeight = ();
 		type ExtrinsicBaseWeight = ();
+		type MaximumExtrinsicWeight = MaximumBlockWeight;
 		type MaximumBlockLength = MaximumBlockLength;
 		type AvailableBlockRatio = AvailableBlockRatio;
 		type Version = ();
