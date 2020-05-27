a3a200f7b747081e45c9fd6614cfee9c332b4767
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2020-05-27 17:05:12 +0200

    Apply suggestions from code review

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -72,7 +72,7 @@ pub use sp_runtime::BuildStorage;
 pub use timestamp::Call as TimestampCall;
 pub use balances::Call as BalancesCall;
 pub use attestations::{Call as AttestationsCall, MORE_ATTESTATIONS_IDENTIFIER};
-use parachains::Call as ParachainsCall;
+pub use parachains::Call as ParachainsCall;
 
 /// Constant values used within the runtime.
 pub mod constants;
