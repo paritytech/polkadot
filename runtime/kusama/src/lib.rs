3c92e26cc248dbaa4d4eb652bf947deb630d0348
Kirill Pimenov <kirill@parity.io>
2020-05-29 10:59:22 +0200

    Bump Kusama spec version to 2000 (#1165)

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -84,7 +84,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 1065,
+	spec_version: 2000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
