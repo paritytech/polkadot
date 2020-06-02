d2eb2479d50624bf2c4eece24dc79e2a2bc8149e
Robert Habermeier <rphmeier@gmail.com>
2020-06-02 18:57:38 -0400

    update scheduler skeleton to match latest version of guide

--- runtime/parachains/src/scheduler.rs
+++ runtime/parachains/src/scheduler.rs
@@ -29,10 +29,10 @@
 //!   manipulate the scheduler to assign themselves as desired.
 //! - High or close to optimal throughput of parachains and parathreads. Work among validator groups should be balanced.
 //!
-//! The Scheduler manages resource allocation using the concept of "Execution Cores".
-//! There will be one execution core for each parachain, and a fixed number of cores
+//! The Scheduler manages resource allocation using the concept of "Availability Cores".
+//! There will be one availability core for each parachain, and a fixed number of cores
 //! used for multiplexing parathreads. Validators will be partitioned into groups, with the same
-//! number of groups as execution cores. Validator groups will be assigned to different execution cores
+//! number of groups as availability cores. Validator groups will be assigned to different availability cores
 //! over time.
 
 use sp_std::prelude::*;
@@ -49,50 +49,87 @@ use system::ensure_root;
 
 use crate::{configuration, paras};
 
-// A claim on authorship for a specific parathread.
-#[derive(Encode, Decode)]
-struct ParathreadClaim(ParaId, CollatorId);
+/// The unique (during session) index of a core.
+#[derive(Encode, Decode, Default)]
+pub(crate) struct CoreIndex(u32);
 
-// A parathread that is scheduled onto a specific core.
-#[derive(Encode, Decode)]
-struct ParathreadEntry {
-	claim: ParathreadClaim,
+/// The unique (during session) index of a validator group.
+#[derive(Encode, Decode, Default)]
+pub(crate) struct GroupIndex(u32);
+
+/// A claim on authoring the next block for a given parathread.
+#[derive(Clone, Encode, Decode, Default)]
+pub(crate) struct ParathreadClaim(ParaId, CollatorId);
+
+/// An entry tracking a claim to ensure it does not pass the maximum number of retries.
+#[derive(Clone, Encode, Decode, Default)]
+pub(crate) struct ParathreadEntry {
+  claim: ParathreadClaim,
+  retries: u32,
+}
+
+/// A queued parathread entry, pre-assigned to a core.
+#[derive(Encode, Decode, Default)]
+pub(crate) struct QueuedParathread {
+	claim: ParathreadEntry,
 	core: CoreIndex,
 }
 
-// what a core is occupied by
-#[derive(Encode, Decode)]
-enum CoreOccupied {
-	Parathread(ParathreadClaim, u32), // claim & retries
-	Parachain,
+/// The queue of all parathread claims.
+#[derive(Encode, Decode, Default)]
+pub(crate) struct ParathreadClaimQueue {
+	queue: Vec<QueuedParathread>,
+	// this value is between 0 and config.parathread_cores
+	next_core: CoreIndex,
 }
 
-/// The unique (during session) index of a core.
-#[derive(Encode, Decode)]
-pub(crate) struct CoreIndex(u32);
+/// What is occupying a specific availability core.
+#[derive(Clone, Encode, Decode)]
+pub(crate) enum CoreOccupied {
+  Parathread(ParathreadEntry),
+  Parachain,
+}
+
+/// How a free core is scheduled to be assigned.
+#[derive(Encode, Decode, Default)]
+pub(crate) struct CoreAssignment {
+  core: CoreIndex,
+  para_id: ParaId,
+  collator: Option<CollatorId>,
+  group_idx: GroupIndex,
+}
 
 pub trait Trait: system::Trait + configuration::Trait + paras::Trait { }
 
 decl_storage! {
 	trait Store for Module<T: Trait> as Scheduler {
-		/// All of the validator groups, one for each core.
+		/// All the validator groups. One for each core.
 		ValidatorGroups: Vec<Vec<ValidatorIndex>>;
 		/// A queue of upcoming claims and which core they should be mapped onto.
-		ParathreadQueue: Vec<ParathreadEntry>;
-		/// One entry for each execution core. Entries are `None` if the core is not currently occupied.
-		/// Can be temporarily `Some` if scheduled but not occupied.
+		ParathreadQueue: ParathreadClaimQueue;
+		/// One entry for each availability core. Entries are `None` if the core is not currently occupied. Can be
+		/// temporarily `Some` if scheduled but not occupied.
 		/// The i'th parachain belongs to the i'th core, with the remaining cores all being
 		/// parathread-multiplexers.
-		ExecutionCores: Vec<Option<CoreOccupied>>;
-		/// An index used to ensure that only one claim on a parathread exists in the queue or retry queue
-		/// or is currently being handled by an occupied core.
-		ParathreadClaimIndex: Vec<(ParaId, CollatorId)>;
-		/// The block number where the session start occurred. Used to track how many group rotation have
-		/// occurred.
-		SessionStartBlock: BlockNumber,
-		/// Currently scheduled cores - free but up to be occupied. Ephemeral storage item that's wiped on
-		/// finalization.
-		Scheduled get(fn scheduled): Vec<CoreAssignment>, // sorted by ParaId
+		AvailabilityCores: Vec<Option<CoreOccupied>>;
+		/// An index used to ensure that only one claim on a parathread exists in the queue or is
+		/// currently being handled by an occupied core.
+		ParathreadClaimIndex: Vec<ParaId>;
+		/// The block number where the session start occurred. Used to track how many group rotations have occurred.
+		SessionStartBlock: T::BlockNumber;
+		/// Currently scheduled cores - free but up to be occupied. Ephemeral storage item that's wiped on finalization.
+		Scheduled: Vec<CoreAssignment>; // sorted ascending by CoreIndex.
+	}
+}
+
+decl_error! {
+	pub enum Error for Module<T: Trait> { }
+}
+
+decl_module! {
+	/// The scheduler module.
+	pub struct Module<T: Trait> for enum Call where origin: <T as system::Trait>::Origin {
+		type Error = Error<T>;
 	}
 }
 
@@ -110,26 +147,39 @@ impl<T: Trait> Module<T> {
 	 }
 
 	/// Called by the initializer to note that a new session has started.
-	pub(crate) fn initializer_on_new_session(_validators: &[ValidatorId], _queued: &[ValidatorId]) {
+	pub(crate) fn initializer_on_new_session(validators: &[ValidatorId], _queued: &[ValidatorId]) {
 		let config = <configuration::Module<T>>::config();
 
-		SessionStartBlock::set(<system::Module<T>>::block_number());
-		ExecutionCores::mutate(|cores| {
+		let mut thread_queue = ParathreadQueue::get();
+		let n_parachains = <paras::Module<T>>::parachains().len() as u32;
+		let n_cores = n_parachains + config.parathread_cores;
+
+		<SessionStartBlock<T>>::set(<system::Module<T>>::block_number());
+		AvailabilityCores::mutate(|cores| {
 			// clear all occupied cores.
 			for maybe_occupied in cores.iter_mut() {
-				if let Some(CoreOccupied::Parathread(claim, retries)) = maybe_occupied.take() {
-					// TODO [now]: return to parathread queue, do not increment retries
+				if let Some(CoreOccupied::Parathread(claim)) = maybe_occupied.take() {
+					let queued = QueuedParathread {
+						claim,
+						core: CoreIndex(0), // this gets set later in the re-balancing.
+					};
+
+					thread_queue.queue.push(queued);
 				}
 			}
 
-			let n_parachains = <paras::Module<T>>::parachains().len();
-
-			cores.resize(n_parachains + config.parathread_cores, None);
+			cores.resize(n_cores as _, None);
 		});
 
 		// TODO [now]: shuffle validators into groups
+		if n_cores == 0 || validators.is_empty() {
+			ValidatorGroups::set(Vec::new());
+		} else {
+			let group_base_size = validators.len() as u32 / n_cores;
+		}
 
 		// TODO [now]: prune out all parathread claims with too many retries.
+		ParathreadQueue::set(thread_queue);
 	}
 
 	pub(crate) fn schedule(just_freed_cores: Vec<CoreIndex>) {
