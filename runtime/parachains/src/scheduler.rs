cad53e66d3e25ce0be9d57f243d56fde0eb03cbf
Robert Habermeier <rphmeier@gmail.com>
2020-06-02 20:19:08 -0400

    implement initialzation and finalization routines

--- runtime/parachains/src/scheduler.rs
+++ runtime/parachains/src/scheduler.rs
@@ -67,15 +67,15 @@ pub(crate) struct ParathreadClaim(ParaId, CollatorId);
 /// An entry tracking a claim to ensure it does not pass the maximum number of retries.
 #[derive(Clone, Encode, Decode, Default)]
 pub(crate) struct ParathreadEntry {
-  claim: ParathreadClaim,
-  retries: u32,
+	claim: ParathreadClaim,
+	retries: u32,
 }
 
 /// A queued parathread entry, pre-assigned to a core.
 #[derive(Encode, Decode, Default)]
 pub(crate) struct QueuedParathread {
 	claim: ParathreadEntry,
-	core: CoreIndex,
+	core_offset: u32,
 }
 
 /// The queue of all parathread claims.
@@ -83,23 +83,59 @@ pub(crate) struct QueuedParathread {
 pub(crate) struct ParathreadClaimQueue {
 	queue: Vec<QueuedParathread>,
 	// this value is between 0 and config.parathread_cores
-	next_core_offset: CoreIndex,
+	next_core_offset: u32,
+}
+
+impl ParathreadClaimQueue {
+	// Queue a parathread entry to be processed.
+	//
+	// Provide the entry and the number of parathread cores, which must be greater than 0.
+	fn queue_entry(&mut self, entry: ParathreadEntry, n_parathread_cores: u32) {
+		let core_offset = self.next_core_offset;
+		self.next_core_offset = (self.next_core_offset + 1) % n_parathread_cores;
+
+		self.queue.push(QueuedParathread {
+			claim: entry,
+			core_offset,
+		})
+	}
 }
 
 /// What is occupying a specific availability core.
 #[derive(Clone, Encode, Decode)]
 pub(crate) enum CoreOccupied {
-  Parathread(ParathreadEntry),
-  Parachain,
+	Parathread(ParathreadEntry),
+	Parachain,
+}
+
+/// The assignment type.
+#[derive(Clone, Encode, Decode)]
+pub(crate) enum AssignmentKind {
+	Parachain,
+	Parathread(CollatorId, u32),
 }
 
 /// How a free core is scheduled to be assigned.
-#[derive(Encode, Decode, Default)]
+#[derive(Encode, Decode)]
 pub(crate) struct CoreAssignment {
-  core: CoreIndex,
-  para_id: ParaId,
-  collator: Option<CollatorId>,
-  group_idx: GroupIndex,
+	/// The core that is assigned.
+	pub core: CoreIndex,
+	/// The unique ID of the para that is assigned to the core.
+	pub para_id: ParaId,
+	/// The kind of the assigment.
+	pub kind: AssignmentKind,
+	/// The index of the validator group assigned to the core.
+	pub group_idx: GroupIndex,
+}
+
+impl CoreAssignment {
+	/// Get the ID of a collator who is required to collate this block.
+	pub(crate) fn required_collator(&self) -> Option<&CollatorId> {
+		match self.kind {
+			AssignmentKind::Parachain => None,
+			AssignmentKind::Parathread(ref id, _) => Some(id),
+		}
+	}
 }
 
 pub trait Trait: system::Trait + configuration::Trait + paras::Trait { }
@@ -146,7 +182,22 @@ impl<T: Trait> Module<T> {
 
 	/// Called by the initializer to finalize the scheduler module.
 	pub(crate) fn initializer_finalize() {
-		// TODO [now]: free all scheduled cores and return parathread claims to queue, with retries incremented.
+		// Free all scheduled cores and return parathread claims to queue, with retries incremented.
+		let config = <configuration::Module<T>>::config();
+		ParathreadQueue::mutate(|queue| {
+			for core_assignment in Scheduled::take() {
+				if let AssignmentKind::Parathread(collator, retries) = core_assignment.kind {
+					let entry = ParathreadEntry {
+						claim: ParathreadClaim(core_assignment.para_id, collator),
+						retries: retries + 1,
+					};
+
+					if entry.retries > config.parathread_retries { continue }
+					queue.queue_entry(entry, config.parathread_cores);
+				}
+			}
+		})
+
 	 }
 
 	/// Called by the initializer to note that a new session has started.
@@ -170,7 +221,7 @@ impl<T: Trait> Module<T> {
 				if let Some(CoreOccupied::Parathread(claim)) = maybe_occupied.take() {
 					let queued = QueuedParathread {
 						claim,
-						core: CoreIndex(0), // this gets set later in the re-balancing.
+						core_offset: 0, // this gets set later in the re-balancing.
 					};
 
 					thread_queue.queue.push(queued);
@@ -215,7 +266,7 @@ impl<T: Trait> Module<T> {
 			if config.parathread_cores == 0 {
 				thread_queue = ParathreadClaimQueue {
 					queue: Vec::new(),
-					next_core_offset: CoreIndex(0),
+					next_core_offset: 0,
 				};
 				claim_index.clear();
 				return;
@@ -240,12 +291,11 @@ impl<T: Trait> Module<T> {
 			// do re-balancing of claims.
 			{
 				for (i, queued) in thread_queue.queue.iter_mut().enumerate() {
-					// offset by the number of parachains.
-					queued.core = CoreIndex((i as u32) % config.parathread_cores + n_parachains);
+					queued.core_offset = (i as u32) % config.parathread_cores;
 				}
 
 				thread_queue.next_core_offset =
-					CoreIndex(((thread_queue.queue.len() + 1) as u32) % config.parathread_cores);
+					((thread_queue.queue.len() + 1) as u32) % config.parathread_cores;
 			}
 		});
 		ParathreadQueue::set(thread_queue);
