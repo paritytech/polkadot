1fc6e5f81900deaf935eb6eb1c160c114308a3cb
Gavin Wood <gavin@parity.io>
2020-06-09 12:26:50 +0200

    Bump versions. (#1219)
    * Bump versions.
    
    * Update lock

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 5,
+	spec_version: 6,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
