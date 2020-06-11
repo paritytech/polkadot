ab7bdf81fe2139e6c7c95074c1a2aec878815c52
Fedor Sakharov <fedor.sakharov@gmail.com>
2020-06-11 20:32:31 +0300

    Remove duplicate json files

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -72,15 +72,15 @@ pub type WestendChainSpec = service::GenericChainSpec<
 >;
 
 pub fn polkadot_config() -> Result<PolkadotChainSpec, String> {
-	PolkadotChainSpec::from_json_bytes(&include_bytes!("../res/polkadot.json")[..])
+	PolkadotChainSpec::from_json_bytes(&include_bytes!("../../../service/res/polkadot.json")[..])
 }
 
 pub fn kusama_config() -> Result<KusamaChainSpec, String> {
-	KusamaChainSpec::from_json_bytes(&include_bytes!("../res/kusama.json")[..])
+	KusamaChainSpec::from_json_bytes(&include_bytes!("../../../service/res/kusama.json")[..])
 }
 
 pub fn westend_config() -> Result<PolkadotChainSpec, String> {
-	PolkadotChainSpec::from_json_bytes(&include_bytes!("../res/westend.json")[..])
+	PolkadotChainSpec::from_json_bytes(&include_bytes!("../../../service/res/westend.json")[..])
 }
 
 fn polkadot_session_keys(
