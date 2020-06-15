7f4909bb959ad14f6a716fb38f03b02027fbe6d9
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-06-15 11:34:29 +0200

    CLEANUP
    Forked at: 4234dee5b7f67bc912de39a02c09511d12b02ff4
    Parent branch: origin/master

--- polkadot-test/src/lib.rs
+++ polkadot-test/src/lib.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Polkadot service. Specialized wrapper over substrate service.
+//! Polkadot test service only.
 
 use std::sync::Arc;
 use std::time::Duration;
