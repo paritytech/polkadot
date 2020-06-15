555715a8711f844e216771185138a8423f6fc4af
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-06-15 14:32:20 +0200

    Move code to macro to avoid adding pub fn to API

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -137,15 +137,6 @@ impl IdentifyVariant for Box<dyn ChainSpec> {
 	}
 }
 
-// If we're using prometheus, use a registry with a prefix of `polkadot`.
-pub fn set_prometheus_registry(config: &mut Configuration) -> Result<(), ServiceError> {
-	if let Some(PrometheusConfig { registry, .. }) = config.prometheus_config.as_mut() {
-		*registry = Registry::new_custom(Some("polkadot".into()), None)?;
-	}
-
-	Ok(())
-}
-
 /// Starts a `ServiceBuilder` for a full service.
 ///
 /// Use this macro if you don't actually need the full service, but just the builder in order to
@@ -153,7 +144,10 @@ pub fn set_prometheus_registry(config: &mut Configuration) -> Result<(), Service
 #[macro_export]
 macro_rules! new_full_start {
 	($config:expr, $runtime:ty, $executor:ty, $informant_prefix:expr $(,)?) => {{
-		$crate::set_prometheus_registry(&mut $config)?;
+		// If we're using prometheus, use a registry with a prefix of `polkadot`.
+		if let Some(PrometheusConfig { registry, .. }) = $config.prometheus_config.as_mut() {
+			*registry = Registry::new_custom(Some("polkadot".into()), None)?;
+		}
 
 		let mut import_setup = None;
 		let mut rpc_setup = None;
@@ -186,7 +180,141 @@ macro_rules! new_full_start {
 					.ok_or_else(|| service::Error::SelectChainRequired)?;
 
 				let grandpa_hard_forks = if config.chain_spec.is_kusama() {
-					grandpa_support::kusama_hard_forks()
+					/// GRANDPA hard forks due to borked migration of session keys after a runtime
+					/// upgrade (at #1491596), the signalled authority set changes were invalid
+					/// (blank keys) and were impossible to finalize. The authorities for these
+					/// intermediary pending changes are replaced with a static list comprised of
+					/// w3f validators and randomly selected validators from the latest session (at
+					/// #1500988).
+
+					use sp_core::crypto::Ss58Codec;
+					use std::str::FromStr;
+
+					let forks = vec![
+						(
+							623,
+							"01e94e1e7e9cf07b3b0bf4e1717fce7448e5563901c2ef2e3b8e9ecaeba088b1",
+							1492283,
+						),
+						(
+							624,
+							"ddc4323c5e8966844dfaa87e0c2f74ef6b43115f17bf8e4ff38845a62d02b9a9",
+							1492436,
+						),
+						(
+							625,
+							"38ba115b296663e424e32d7b1655cd795719cef4fd7d579271a6d01086cf1628",
+							1492586,
+						),
+						(
+							626,
+							"f3172b6b8497c10fc772f5dada4eeb1f4c4919c97de9de2e1a439444d5a057ff",
+							1492955,
+						),
+						(
+							627,
+							"b26526aea299e9d24af29fdacd5cf4751a663d24894e3d0a37833aa14c58424a",
+							1493338,
+						),
+						(
+							628,
+							"3980d024327d53b8d01ef0d198a052cd058dd579508d8ed6283fe3614e0a3694",
+							1493913,
+						),
+						(
+							629,
+							"31f22997a786c25ee677786373368cae6fd501fd1bc4b212b8e267235c88179d",
+							1495083,
+						),
+						(
+							630,
+							"1c65eb250cf54b466c64f1a4003d1415a7ee275e49615450c0e0525179857eef",
+							1497404,
+						),
+						(
+							631,
+							"9e44116467cc9d7e224e36487bf2cf571698cae16b25f54a7430f1278331fdd8",
+							1498598,
+						),
+					];
+
+					let authorities = vec![
+						"CwjLJ1zPWK5Ao9WChAFp7rWGEgN3AyXXjTRPrqgm5WwBpoS",
+						"Dp8FHpZTzvoKXztkfrUAkF6xNf6sjVU5ZLZ29NEGUazouou",
+						"DtK7YfkhNWU6wEPF1dShsFdhtosVAuJPLkoGhKhG1r5LjKq",
+						"FLnHYBuoyThzqJ45tdb8P6yMLdocM7ir27Pg1AnpYoygm1K",
+						"FWEfJ5UMghr52UopgYjawAg6hQg3ztbQek75pfeRtLVi8pB",
+						"ECoLHAu7HKWGTB9od82HAtequYj6hvNHigkGSB9g3ApxAwB",
+						"GL1Tg3Uppo8GYL9NjKj4dWKcS6tW98REop9G5hpu7HgFwTa",
+						"ExnjU5LZMktrgtQBE3An6FsQfvaKG1ukxPqwhJydgdgarmY",
+						"CagLpgCBu5qJqYF2tpFX6BnU4yHvMGSjc7r3Ed1jY3tMbQt",
+						"DsrtmMsD4ijh3n4uodxPoiW9NZ7v7no5wVvPVj8fL1dfrWB",
+						"HQB4EctrVR68ozZDyBiRJzLRAEGh1YKgCkAsFjJcegL9RQA",
+						"H2YTYbXTFkDY1cGnv164ecnDT3hsD2bQXtyiDbcQuXcQZUV",
+						"H5WL8jXmbkCoEcLfvqJkbLUeGrDFsJiMXkhhRWn3joct1tE",
+						"DpB37GDrJDYcmg2df2eqsrPKMay1u8hyZ6sQi2FuUiUeNLu",
+						"FR8yjKRA9MTjvFGK8kfzrdC23Fr6xd7rfBvZXSjAsmuxURE",
+						"DxHPty3B9fpj3duu6Gc6gCSCAvsydJHJEY5G3oVYT8S5BYJ",
+						"DbVKC8ZJjevrhqSnZyJMMvmPL7oPPL4ed1roxawYnHVgyin",
+						"DVJV81kab2J6oTyRJ9T3NCwW2DSrysbWCssvMcE6cwZHnAd",
+						"Fg4rDAyzoVzf39Zo8JFPo4W314ntNWNwm3shr4xKe8M1fJg",
+						"GUaNcnAruMVxHGTs7gGpSUpigRJboQYQBBQyPohkFcP6NMH",
+						"J4BMGF4W9yWiJz4pkhQW73X6QMGpKUzmPppVnqzBCqw5dQq",
+						"E1cR61L1tdDEop4WdWVqcq1H1x6VqsDpSHvFyUeC41uruVJ",
+						"GoWLzBsj1f23YtdDpyntnvN1LwXKhF5TEeZvBeTVxofgWGR",
+						"CwHwmbogSwtRbrkajVBNubPvWmHBGU4bhMido54M9CjuKZD",
+						"FLT63y9oVXJnyiWMAL4RvWxsQx21Vymw9961Z7NRFmSG7rw",
+						"FoQ2y6JuHuHTG4rHFL3f2hCxfJMvtrq8wwPWdv8tsdkcyA8",
+						"D7QQKqqs8ocGorRA12h4QoBSHDia1DkHeXT4eMfjWQ483QH",
+						"J6z7FP35F9DiiU985bhkDTS3WxyeTBeoo9MtLdLoD3GiWPj",
+						"EjapydCK25AagodRbDECavHAy8yQY1tmeRhwUXhVWx4cFPv",
+						"H8admATcRkGCrF1dTDDBCjQDsYjMkuPaN9YwR2mSCj4DWMQ",
+						"FtHMRU1fxsoswJjBvyCGvECepC7gP2X77QbNpyikYSqqR6k",
+						"DzY5gwr45GVRUFzRMmeg8iffpqYF47nm3XbJhmjG97FijaE",
+						"D3HKWAihSUmg8HrfeFrftSwNK7no261yA9RNr3LUUdsuzuJ",
+						"D82DwwGJGTcSvtB3SmNrZejnSertbPzpkYvDUp3ibScL3ne",
+						"FTPxLXLQvMDQYFA6VqNLGwWPKhemMYP791XVj8TmDpFuV3b",
+						"FzGfKmS7N8Z1tvCBU5JH1eBXZQ9pCtRNoMUnNVv38wZNq72",
+						"GDfm1MyLAQ7Rh8YPtF6FtMweV4hz91zzeDy2sSABNNqAbmg",
+						"DiVQbq7sozeKp7PXPM1HLFc2m7ih8oepKLRK99oBY3QZak1",
+						"HErWh7D2RzrjWWB2fTJfcAejD9MJpadeWWZM2Wnk7LiNWfG",
+						"Es4DbDauYZYyRJbr6VxrhdcM1iufP9GtdBYf3YtSEvdwNyb",
+						"EBgXT6FaVo4WsN2LmfnB2jnpDFf4zay3E492RGSn6v1tY99",
+						"Dr9Zg4fxZurexParztL9SezFeHsPwdP8uGgULeRMbk8DDHJ",
+						"JEnSTZJpLh91cSryptj57RtFxq9xXqf4U5wBH3qoP91ZZhN",
+						"DqtRkrmtPANa8wrYR7Ce2LxJxk2iNFtiCxv1cXbx54uqdTN",
+						"GaxmF53xbuTFKopVEseWiaCTa8fC6f99n4YfW8MGPSPYX3s",
+						"EiCesgkAaighBKMpwFSAUdvwE4mRjBjNmmd5fP6d4FG8DAx",
+						"HVbwWGUx7kCgUGap1Mfcs37g6JAZ5qsfsM7TsDRcSqvfxmd",
+						"G45bc8Ajrd6YSXav77gQwjjGoAsR2qiGd1aLzkMy7o1RLwd",
+						"Cqix2rD93Mdf7ytg8tBavAig2TvhXPgPZ2mejQvkq7qgRPq",
+						"GpodE2S5dPeVjzHB4Drm8R9rEwcQPtwAspXqCVz1ooFWf5K",
+						"CwfmfRmzPKLj3ntSCejuVwYmQ1F9iZWY4meQrAVoJ2G8Kce",
+						"Fhp5NPvutRCJ4Gx3G8vCYGaveGcU3KgTwfrn5Zr8sLSgwVx",
+						"GeYRRPkyi23wSF3cJGjq82117fKJZUbWsAGimUnzb5RPbB1",
+						"DzCJ4y5oT611dfKQwbBDVbtCfENTdMCjb4KGMU3Mq6nyUMu",
+					];
+
+					let authorities = authorities
+						.into_iter()
+						.map(|address| {
+							(
+								grandpa_primitives::AuthorityId::from_ss58check(address)
+									.expect("hard fork authority addresses are static and they should be carefully defined; qed."),
+								1,
+							)
+						})
+						.collect::<Vec<_>>();
+
+					forks
+						.into_iter()
+						.map(|(set_id, hash, number)| {
+							let hash = Hash::from_str(hash)
+								.expect("hard fork hashes are static and they should be carefully defined; qed.");
+
+							(set_id, (hash, number), authorities.clone())
+						})
+						.collect()
 				} else {
 					Vec::new()
 				};
@@ -555,7 +683,10 @@ macro_rules! new_full {
 #[macro_export]
 macro_rules! new_light {
 	($config:expr, $runtime:ty, $dispatch:ty) => {{
-		$crate::set_prometheus_registry(&mut $config)?;
+		// If we're using prometheus, use a registry with a prefix of `polkadot`.
+		if let Some(PrometheusConfig { registry, .. }) = $config.prometheus_config.as_mut() {
+			*registry = Registry::new_custom(Some("polkadot".into()), None)?;
+		}
 		let inherent_data_providers = inherents::InherentDataProviders::new();
 
 		ServiceBuilder::new_light::<Block, $runtime, $dispatch>($config)?
