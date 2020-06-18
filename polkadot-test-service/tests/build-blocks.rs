fcfc4b7bb35992d0d0c6c8b408ef67f216a29dd2
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-06-18 13:13:10 +0200

    WIP
    Forked at: 4234dee5b7f67bc912de39a02c09511d12b02ff4
    Parent branch: origin/master

--- polkadot-test-service/tests/build-blocks.rs
+++ polkadot-test-service/tests/build-blocks.rs
@@ -1,9 +1,11 @@
-use futures::{pin_mut, select, FutureExt as _};
+use futures::{future, pin_mut, select, FutureExt as _, StreamExt};
 use polkadot_test_service::*;
 use service::TaskType;
 use std::pin::Pin;
 use std::sync::Arc;
-use std::time::Duration;
+use sp_keyring::Sr25519Keyring;
+use sc_client_api::client::BlockchainEvents;
+use std::collections::HashSet;
 
 fn task_executor(
 ) -> Arc<dyn Fn(Pin<Box<dyn futures::Future<Output = ()> + Send>>, TaskType) + Send + Sync> {
@@ -17,39 +19,50 @@ fn task_executor(
 #[async_std::test]
 async fn ensure_test_service_build_blocks() {
 	sc_cli::init_logger("");
-	let (service_alice, _client_alice, _handles_alice, _base_path_alice) = run_test_node(task_executor(), 27015, "alice", || {
+	let (service_alice, client_alice, _handles_alice, _base_path_alice) = run_test_node(task_executor(), 27015, Sr25519Keyring::Alice, || {
 		use polkadot_test_runtime::*;
-		//EpochDuration::set(&42);
-		//ExpectedBlockTime::set(&6000);
-		//panic!("{:?}", ExpectedBlockTime::get());
-		//panic!("{:?}", MinimumPeriod::get());
 		SlotDuration::set(&2000);
-		//MinimumPeriod::set(&1000);
 	})
 	.unwrap();
 
-	let (service_bob, _client_bob, _handles_bob, _base_path_bob) = run_test_node(task_executor(), 27016, "bob", || {
+	let (service_bob, client_bob, _handles_bob, _base_path_bob) = run_test_node(task_executor(), 27016, Sr25519Keyring::Bob, || {
 		use polkadot_test_runtime::*;
-		//EpochDuration::set(&42);
-		//ExpectedBlockTime::set(&6000);
-		//panic!("{:?}", ExpectedBlockTime::get());
-		//panic!("{:?}", MinimumPeriod::get());
 		SlotDuration::set(&2000);
-		//MinimumPeriod::set(&1000);
 	})
 	.unwrap();
 
 	let t1 = service_alice.fuse();
 	let t2 = service_bob.fuse();
-	let t3 = async_std::task::sleep(Duration::from_secs(20)).fuse();
+	let t3 = future::join(
+		async move {
+			let mut import_notification_stream = client_alice.import_notification_stream();
+			let mut blocks = HashSet::new();
+
+			while let Some(notification) = import_notification_stream.next().await {
+				blocks.insert(notification.hash);
+				if blocks.len() == 3 {
+					break;
+				}
+			}
+		},
+		async move {
+			let mut import_notification_stream = client_bob.import_notification_stream();
+			let mut blocks = HashSet::new();
+
+			while let Some(notification) = import_notification_stream.next().await {
+				blocks.insert(notification.hash);
+				if blocks.len() == 3 {
+					break;
+				}
+			}
+		},
+	).fuse();
 
 	pin_mut!(t1, t2, t3);
 
 	select! {
-		_ = t1 => {},
-		_ = t2 => {},
+		_ = t1 => panic!("service Alice failed"),
+		_ = t2 => panic!("service Bob failed"),
 		_ = t3 => {},
 	}
-
-	assert!(false);
 }
