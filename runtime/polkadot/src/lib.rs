60dc97c67fc5c6b591242562cdf9533084722d94
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-06-22 22:35:50 +0200

    bump spec

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 11,
+	spec_version: 12,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
