cce262e023c565239d2d2545ba713f098d947375
Bastian Köcher <git@kchr.de>
2020-07-02 11:13:36 +0200

    Bump runtimes

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 13,
+	spec_version: 14,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
