36c8c654dca5c0ace5f05874841017096c1370a8
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-07-02 17:15:26 +0200

    Simplify test

--- polkadot-test-service/tests/send-transaction.rs
+++ polkadot-test-service/tests/send-transaction.rs
@@ -1,9 +1,7 @@
 use async_std::task::{block_on, sleep};
 use futures::{pin_mut, select, FutureExt as _};
-use polkadot_primitives::parachain::{Info, Scheduling};
-use polkadot_runtime_common::{registrar};
 use polkadot_test_service::*;
-use sp_keyring::Sr25519Keyring::Alice;
+use sp_keyring::Sr25519Keyring::{Alice, Bob};
 use std::time::Duration;
 
 static INTEGRATION_TEST_ALLOWED_TIME: Option<&str> = option_env!("INTEGRATION_TEST_ALLOWED_TIME");
@@ -27,21 +25,8 @@ fn send_transaction_actually_work() {
 	))
 	.fuse();
 	let t2 = async {
-		let wasm = vec![0_u8; 32];
-		let genesis_state = b"0x0x000000000000000000000000000000000000000000000000000000000000000000eb21415d4113e9bb8c\
-							0c3fa5533d873c439e94960c56f4c1dd1105ddc6b7b2e903170a2e7597b7b7e3d84c05391d139a62b157e78786\
-							d8c082f29dcf4c11131400".to_vec();
-		let function = polkadot_test_runtime::Call::Sudo(pallet_sudo::Call::sudo(Box::new(
-			polkadot_test_runtime::Call::Registrar(registrar::Call::register_para(
-				100.into(),
-				Info {
-					scheduling: Scheduling::Always,
-				},
-				wasm.into(),
-				genesis_state.into(),
-			)),
-		)));
-		let (res, _mem, _rx) = alice.call_function(function, Alice).await;
+		let function = polkadot_test_runtime::Call::Balances(pallet_balances::Call::transfer(Default::default(), 1));
+		let (res, _mem, _rx) = alice.call_function(function, Bob).await;
 
 		let res = res.expect("return value expected");
 		let json = serde_json::from_str::<serde_json::Value>(res.as_str()).expect("valid JSON");
