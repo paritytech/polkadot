7afb386d33be0d6dc359f5cac1a2fb3c8e06c099
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-07-06 12:06:02 +0200

    Companion for #6567 (Benchmark Writer CLI) (#1349)
    * Update usage of macro
    
    * 'Update substrate'
    
    * cargo update -p sp-io
    
    Co-authored-by: parity-processbot <>

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1287,23 +1287,23 @@ sp_api::impl_runtime_apis! {
 			let mut batches = Vec::<BenchmarkBatch>::new();
 			let params = (&pallet, &benchmark, &lowest_range_values, &highest_range_values, &steps, repeat, &whitelist);
 			// Polkadot
-			add_benchmark!(params, batches, b"claims", Claims);
+			add_benchmark!(params, batches, claims, Claims);
 			// Substrate
-			add_benchmark!(params, batches, b"balances", Balances);
-			add_benchmark!(params, batches, b"collective", Council);
-			add_benchmark!(params, batches, b"democracy", Democracy);
-			add_benchmark!(params, batches, b"elections-phragmen", ElectionsPhragmen);
-			add_benchmark!(params, batches, b"identity", Identity);
-			add_benchmark!(params, batches, b"im-online", ImOnline);
-			add_benchmark!(params, batches, b"offences", OffencesBench::<Runtime>);
-			add_benchmark!(params, batches, b"scheduler", Scheduler);
-			add_benchmark!(params, batches, b"session", SessionBench::<Runtime>);
-			add_benchmark!(params, batches, b"staking", Staking);
-			add_benchmark!(params, batches, b"system", SystemBench::<Runtime>);
-			add_benchmark!(params, batches, b"timestamp", Timestamp);
-			add_benchmark!(params, batches, b"treasury", Treasury);
-			add_benchmark!(params, batches, b"utility", Utility);
-			add_benchmark!(params, batches, b"vesting", Vesting);
+			add_benchmark!(params, batches, balances, Balances);
+			add_benchmark!(params, batches, collective, Council);
+			add_benchmark!(params, batches, democracy, Democracy);
+			add_benchmark!(params, batches, elections_phragmen, ElectionsPhragmen);
+			add_benchmark!(params, batches, identity, Identity);
+			add_benchmark!(params, batches, im_online, ImOnline);
+			add_benchmark!(params, batches, offences, OffencesBench::<Runtime>);
+			add_benchmark!(params, batches, scheduler, Scheduler);
+			add_benchmark!(params, batches, session, SessionBench::<Runtime>);
+			add_benchmark!(params, batches, staking, Staking);
+			add_benchmark!(params, batches, system, SystemBench::<Runtime>);
+			add_benchmark!(params, batches, timestamp, Timestamp);
+			add_benchmark!(params, batches, treasury, Treasury);
+			add_benchmark!(params, batches, utility, Utility);
+			add_benchmark!(params, batches, vesting, Vesting);
 
 			if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
 			Ok(batches)
