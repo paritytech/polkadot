fa46a8a338448a6c554655721b40ea4b1ecd3016
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-07-09 17:41:37 +0200

    start extracting jobs management into helper traits + structs

--- node/subsystem/src/util.rs
+++ node/subsystem/src/util.rs
@@ -23,10 +23,11 @@
 use crate::messages::{AllMessages, RuntimeApiMessage, RuntimeApiRequest, SchedulerRoster};
 use futures::{
 	channel::{mpsc, oneshot},
-	future::AbortHandle,
+	future::{AbortHandle, Either},
 	prelude::*,
-	task::SpawnError,
+	task::{Spawn, SpawnError, SpawnExt},
 };
+use futures_timer::Delay;
 use keystore::KeyStorePtr;
 use parity_scale_codec::Encode;
 use polkadot_primitives::{
@@ -41,37 +42,15 @@ use std::{
 	collections::HashMap,
 	convert::{TryFrom, TryInto},
 	ops::{Deref, DerefMut},
+	pin::Pin,
+	time::Duration,
 };
+use streamunordered::{StreamUnordered, StreamYield};
 
-/// JobCanceler aborts all contained abort handles on drop
-#[derive(Debug, Default)]
-pub struct JobCanceler(HashMap<Hash, AbortHandle>);
-
-impl Drop for JobCanceler {
-	fn drop(&mut self) {
-		for abort_handle in self.0.values() {
-			abort_handle.abort();
-		}
-	}
-}
-
-// JobCanceler is a smart pointer wrapping the contained hashmap;
-// it only cares about the wrapped data insofar as it's necessary
-// to implement proper Drop behavior. Therefore, it's appropriate
-// to impl Deref and DerefMut.
-impl Deref for JobCanceler {
-	type Target = HashMap<Hash, AbortHandle>;
-
-	fn deref(&self) -> &Self::Target {
-		&self.0
-	}
-}
-
-impl DerefMut for JobCanceler {
-	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.0
-	}
-}
+/// Duration a job will wait after sending a stop signal before hard-aborting.
+pub const JOB_GRACEFUL_STOP_DURATION: Duration = Duration::from_secs(1);
+/// Capacity of channels to and from individual jobs
+pub const JOB_CHANNEL_CAPACITY: usize = 64;
 
 #[derive(Debug, derive_more::From)]
 pub enum Error {
@@ -84,6 +63,8 @@ pub enum Error {
 	SenderConversion(String),
 	/// The local node is not a validator.
 	NotAValidator,
+	/// The desired job is not present in the jobs list.
+	JobNotFound(Hash),
 }
 
 /// Request some data from the `RuntimeApi`.
@@ -282,5 +263,176 @@ impl Validator {
 		signed: Signed<Payload, RealPayload>,
 	) -> Result<(), ()> {
 		signed.check_signature(&self.signing_context, &self.id())
+	}
+}
+
+/// ToJob is expected to be an enum declaring messages which can be sent to a particular job.
+///
+/// Normally, this will be some subset of `Allmessages`, and a `Stop` variant.
+pub trait ToJobTrait: TryFrom<AllMessages> {
+	/// Produce the `Stop` variant of the ToJob enum.
+	fn stop() -> Self;
+}
+
+/// A JobHandle manages a particular job for a subsystem.
+pub struct JobHandle<ToJob> {
+	abort_handle: future::AbortHandle,
+	to_job: mpsc::Sender<ToJob>,
+	finished: oneshot::Receiver<()>,
+	outgoing_msgs_handle: usize,
+}
+
+impl<ToJob> JobHandle<ToJob> {
+	/// Send a message to the job.
+	pub async fn send_msg(&mut self, msg: ToJob) -> Result<(), Error> {
+		self.to_job.send(msg).await.map_err(Into::into)
+	}
+
+	/// Abort the job without waiting for a graceful shutdown
+	pub fn abort(self) {
+		self.abort_handle.abort();
+	}
+}
+
+impl<ToJob: ToJobTrait> JobHandle<ToJob> {
+	/// Stop this job gracefully.
+	///
+	/// If it hasn't shut itself down after `JOB_GRACEFUL_STOP_DURATION`, abort it.
+	pub async fn stop(mut self) {
+		// we don't actually care if the message couldn't be sent
+		let _ = self.to_job.send(ToJob::stop()).await;
+		let stop_timer = Delay::new(JOB_GRACEFUL_STOP_DURATION);
+
+		match future::select(stop_timer, self.finished).await {
+			Either::Left((_, _)) => {}
+			Either::Right((_, _)) => {
+				self.abort_handle.abort();
+			}
+		}
+	}
+}
+
+/// This trait governs jobs.
+///
+/// Jobs are instantiated and killed automatically on appropriate overseer messages.
+/// Other messages are passed along to and from the job via the overseer to other
+/// subsystems.
+pub trait JobTrait {
+	/// Message type to the job. Typically a subset of AllMessages.
+	type ToJob: 'static + ToJobTrait + Send;
+	/// Message type from the job. Typically a subset of AllMessages.
+	type FromJob: 'static + Into<AllMessages> + Send;
+	/// Job runtime error.
+	type Error: std::error::Error;
+	/// Extra arguments this job needs to run properly.
+	///
+	/// If no extra information is needed, it is perfectly acceptable to set it to `()`.
+	type RunArgs: 'static + Send;
+
+	/// Run a job for the parent block indicated
+	fn run(
+		parent: Hash,
+		run_args: Self::RunArgs,
+		rx_to: mpsc::Receiver<Self::ToJob>,
+		tx_from: mpsc::Sender<Self::FromJob>,
+	) -> Pin<Box<dyn Future<Output=Result<(), Self::Error>> + Send>>;
+
+	/// Name of the job, i.e. `CandidateBackingJob`
+	fn name() -> &'static str;
+}
+
+pub struct Jobs<Spawner, Job: JobTrait> {
+	spawner: Spawner,
+	running: HashMap<Hash, JobHandle<Job::ToJob>>,
+	outgoing_msgs: StreamUnordered<mpsc::Receiver<Job::FromJob>>,
+	job: std::marker::PhantomData<Job>,
+}
+
+impl<Spawner: Spawn, Job: JobTrait> Jobs<Spawner, Job> {
+	/// Create a new Jobs manager which handles spawning appropriate jobs.
+	pub fn new(spawner: Spawner) -> Self {
+		Self {
+			spawner,
+			running: HashMap::new(),
+			outgoing_msgs: StreamUnordered::new(),
+			job: std::marker::PhantomData,
+		}
+	}
+
+	/// Spawn a new job for this `parent_hash`, with whatever args are appropriate.
+	fn spawn_job(&mut self, parent_hash: Hash, run_args: Job::RunArgs) -> Result<(), Error> {
+		let (to_job_tx, to_job_rx) = mpsc::channel(JOB_CHANNEL_CAPACITY);
+		let (from_job_tx, from_job_rx) = mpsc::channel(JOB_CHANNEL_CAPACITY);
+		let (finished_tx, finished) = oneshot::channel();
+
+		let (future, abort_handle) = future::abortable(async move {
+			if let Err(e) = Job::run(parent_hash, run_args, to_job_rx, from_job_tx).await {
+				log::error!(
+					"{}({}) finished with an error {:?}",
+					Job::name(),
+					parent_hash,
+					e,
+				);
+			}
+		});
+
+		// discard output
+		let future = async move {
+			let _ = future.await;
+			let _ = finished_tx.send(());
+		};
+		self.spawner.spawn(future)?;
+
+		// this handle lets us remove the appropriate receiver from self.outgoing_msgs
+		// when it's time to stop the job.
+		let outgoing_msgs_handle = self.outgoing_msgs.push(from_job_rx);
+
+		let handle = JobHandle {
+			abort_handle,
+			to_job: to_job_tx,
+			finished,
+			outgoing_msgs_handle,
+		};
+
+		self.running.insert(parent_hash, handle);
+
+		Ok(())
+	}
+
+	/// Stop the job associated with this `parent_hash`.
+	pub async fn stop_job(&mut self, parent_hash: Hash) -> Result<(), Error> {
+		match self.running.remove(&parent_hash) {
+			Some(handle) => {
+				Pin::new(&mut self.outgoing_msgs).remove(handle.outgoing_msgs_handle);
+				handle.stop().await;
+				Ok(())
+			}
+			None => Err(Error::JobNotFound(parent_hash))
+		}
+	}
+
+	/// Send a message to the appropriate job for this `parent_hash`.
+	async fn send_msg(&mut self, parent_hash: Hash, msg: Job::ToJob) -> Result<(), Error> {
+		match self.running.get_mut(&parent_hash) {
+			Some(job) => job.send_msg(msg).await?,
+			None => return Err(Error::JobNotFound(parent_hash)),
+		}
+		Ok(())
+	}
+
+	/// Get the next message from any of the underlying jobs.
+	async fn next(&mut self) -> Option<Job::FromJob> {
+		self.outgoing_msgs.next().await.and_then(|(e, _)| match e {
+			StreamYield::Item(e) => Some(e),
+			_ => None,
+		})
+	}
+}
+
+impl<Spawner, Job: JobTrait> Drop for Jobs<Spawner, Job> {
+	fn drop(&mut self) {
+		for job_handle in self.running.values() {
+			job_handle.abort_handle.abort();
+		}
 	}
 }
