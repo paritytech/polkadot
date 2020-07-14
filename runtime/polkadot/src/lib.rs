c48106c3bb8c12d6dda75ee9846ed6deb897e300
Gav Wood <gavin@parity.io>
2020-07-14 18:39:48 +0200

    Bump Substrate & versions

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -93,7 +93,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 14,
+	spec_version: 15,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
