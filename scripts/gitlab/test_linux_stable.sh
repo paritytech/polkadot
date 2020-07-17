36802ad599504ac196881446f8b5811c6a9a7a2a
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-07-17 12:01:31 +0200

    Ignore build for companion PR

---
+++ scripts/gitlab/test_linux_stable.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+url="https://api.github.com/repos/paritytech/polkadot/pulls/${CI_COMMIT_REF_NAME}"
+echo "[+] API URL: $url"
+
+pr_title=$(curl -H "Authorization: token ${GITHUB_PR_TOKEN}" "$url" | jq -r .title)
+echo "[+] PR title: $pr_title"
+
+if grep -qi '^companion' <<< "$pr_title"; then
+  echo "[!] PR is a companion PR. Build is already done in substrate"
+  exit 0
+else
+  echo "[+] PR is not a companion PR. Proceeding test"
+  time cargo test --all --release --verbose --locked --features runtime-benchmarks
+fi
