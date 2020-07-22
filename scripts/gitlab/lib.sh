f0037fbe52c39188b793ee83a2f51ac6d203f895
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-07-22 10:02:51 +0200

    WIP
    Forked at: d390eb51791c612135f8612c40bd6f9f04789bcc
    Parent branch: origin/master

--- scripts/gitlab/lib.sh
+++ scripts/gitlab/lib.sh
@@ -97,13 +97,13 @@ curl -XPOST -d "$1" "https://matrix.parity.io/_matrix/client/r0/rooms/$2/send/m.
 boldprint () { printf "|\n| \033[1m%s\033[0m\n|\n" "${@}"; }
 boldcat () { printf "|\n"; while read -r l; do printf "| \033[1m%s\033[0m\n" "${l}"; done; printf "|\n" ; }
 
-prepare_git(){
+prepare_git() {
   # Set the user name and email to make merging work
   git config --global user.name 'CI system'
   git config --global user.email '<>'
 }
 
-prepare_substrate(){
+prepare_substrate() {
   pr_companion=$1
   boldprint "companion pr specified/detected: #${pr_companion}"
 
@@ -133,4 +133,42 @@ prepare_substrate(){
 
   mkdir -p target/debug/wbuild/.cargo
   cp .cargo/config target/debug/wbuild/.cargo/config
+}
+
+pull_companion_substrate() {
+  github_api_polkadot_pull_url="https://api.github.com/repos/paritytech/polkadot/pulls"
+  # use github api v3 in order to access the data without authentication
+  github_header="Authorization: token ${GITHUB_PR_TOKEN}"
+
+
+  # either it's a pull request then check for a companion otherwise use
+  # substrate:master
+  if expr match "${CI_COMMIT_REF_NAME}" '^[0-9]\+$' >/dev/null
+  then
+    boldprint "this is pull request no ${CI_COMMIT_REF_NAME}"
+
+    pr_data_file="$(mktemp)"
+    # get the last reference to a pr in substrate
+    curl -sSL -H "${github_header}" -o "${pr_data_file}" \
+      "${github_api_polkadot_pull_url}/${CI_COMMIT_REF_NAME}"
+
+    pr_body="$(sed -n -r 's/^[[:space:]]+"body": (".*")[^"]+$/\1/p' "${pr_data_file}")"
+
+    pr_companion="$(echo "${pr_body}" | sed -n -r \
+        -e 's;^.*substrate companion: paritytech/substrate#([0-9]+).*$;\1;p' \
+        -e 's;^.*substrate companion: https://github.com/paritytech/substrate/pull/([0-9]+).*$;\1;p' \
+      | tail -n 1)"
+
+    if [ "${pr_companion}" ]
+    then
+      echo Substrate path: $SUBSTRATE_PATH
+      prepare_git
+      prepare_substrate "$pr_companion"
+    else
+      boldprint "no companion branch found - building your Polkadot branch"
+    fi
+    rm -f "${pr_data_file}"
+  else
+    boldprint "this is not a pull request - building your Polkadot branch"
+  fi
 }
---
+++ scripts/gitlab/test_deterministic_wasm.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#shellcheck source=lib.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/lib.sh"
+
+pull_companion_substrate
+
+set -e
+
+# build runtime
+WASM_BUILD_NO_COLOR=1 cargo build --verbose --release -p kusama-runtime -p polkadot-runtime -p westend-runtime
+# make checksum
+sha256sum target/release/wbuild/target/wasm32-unknown-unknown/release/*.wasm > checksum.sha256
+# clean up – FIXME: can we reuse some of the artifacts?
+cargo clean
+# build again
+WASM_BUILD_NO_COLOR=1 cargo build --verbose --release -p kusama-runtime -p polkadot-runtime -p westend-runtime
+# confirm checksum
+sha256sum -c checksum.sha256
