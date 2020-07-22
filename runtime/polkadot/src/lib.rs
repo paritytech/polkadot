a1a6f527213e814e4a91e060516c8d3549508dbe
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-07-22 08:48:58 +0200

    Increase spec version

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -86,7 +86,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 18,
+	spec_version: 19,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
