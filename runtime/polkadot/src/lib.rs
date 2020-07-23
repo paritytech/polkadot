709a37396bc005982f7465488777326df53cbe5d
Robert Habermeier <rphmeier@gmail.com>
2020-07-23 13:26:35 -0400

    bump westend & polkadot versions

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -86,7 +86,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 19,
+	spec_version: 20,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
