a657c252fa0c9f3ea0dbe1f16f58a9ab47e12aa1
Martin Pugh <pugh@s3kr.it>
2020-07-28 18:13:55 +0200

    add runtime versions

--- scripts/github/generate_release_text.rb
+++ scripts/github/generate_release_text.rb
@@ -5,11 +5,17 @@ require 'git'
 require 'erb'
 require 'toml'
 
-polkadot_path = ENV['GITHUB_WORKSPACE'] + '/polkadot'
-pg = Git.open(polkadot_path)
-
 version = ENV['GITHUB_REF']
 token = ENV['GITHUB_TOKEN']
+
+polkadot_path = ENV['GITHUB_WORKSPACE'] + '/polkadot/'
+pg = Git.open(polkadot_path)
+
+# Generate an ERB renderer based on the template .erb file
+renderer = ERB.new(
+  File.read(ENV['GITHUB_WORKSPACE'] + '/polkadot/scripts/github/polkadot_release.erb'),
+  trim_mode: '<>'
+)
 
 # get last polkadot version. Use handy Gem::Version for sorting by version
 last_version = pg
@@ -17,13 +23,6 @@ last_version = pg
               .map(&:name)
               .grep(/^v\d+\.\d+\.\d+.*$/)
               .sort_by { |v| Gem::Version.new(v.slice(1...)) }[-2]
-
-# TODO: fill in these variables
-polkadot_runtime = '123'
-kusama_runtime = '123'
-westend_runtime = '123'
-rustc_stable = 'v1.2.3'
-rustc_nightly = 'v1.2.3'
 
 polkadot_cl = Changelog.new(
   's3krit/polkadot', version, last_version, token: token
@@ -50,14 +49,25 @@ substrate_cl = Changelog.new(
 
 all_changes = polkadot_cl.changes + substrate_cl.changes
 
+# Set all the variables needed for a release
+
 misc_changes = Changelog.changes_with_label(all_changes, 'B1-releasenotes')
 client_changes = Changelog.changes_with_label(all_changes, 'B5-clientnoteworthy')
 runtime_changes = Changelog.changes_with_label(all_changes, 'B7-runtimenoteworthy')
 
 release_priority = Changelog.highest_priority_for_changes(all_changes)
 
-renderer = ERB.new(
-  File.read(ENV['GITHUB_WORKSPACE'] + '/polkadot/scripts/github/polkadot_release.erb'),
-  trim_mode: '<>'
-)
+# Pulled from the previous Github step
+rustc_stable = 'v1.2.3'
+rustc_nightly = 'v1.2.3'
+
+polkadot_runtime = File.open(polkadot_path + '/runtime/polkadot/src/lib.rs')
+                       .find { |l| l =~ /spec_version/ }
+                       .match(/[0-9]+/)[0]
+kusama_runtime = File.open(polkadot_path + '/runtime/kusama/src/lib.rs')
+                       .find { |l| l =~ /spec_version/ }
+                       .match(/[0-9]+/)[0]
+westend_runtime = File.open(polkadot_path + '/runtime/westend/src/lib.rs')
+                       .find { |l| l =~ /spec_version/ }
+                       .match(/[0-9]+/)[0]
 puts renderer.result
