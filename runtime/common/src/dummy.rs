96e8b8ad46c4caa90ed1657b98d64e04057c0f5b
Robert Habermeier <rphmeier@gmail.com>
2020-07-30 18:06:01 -0400

    add a ZeroSizedTypeDifferentiator to dummy

--- runtime/common/src/dummy.rs
+++ runtime/common/src/dummy.rs
@@ -17,11 +17,49 @@
 //! A dummy module for holding place of modules in a runtime.
 
 use frame_support::{decl_module, decl_storage};
+use codec::{Encode, Decode};
 
 pub trait Trait<I: Instance = DefaultInstance>: system::Trait { }
 
+/// Workaround for the fact that the instanced `Call`s need not to have
+/// conflicting implementations.
+///
+/// The `Call` needs to reference the instance type parameter somehow in order to not
+/// conflict.
+#[derive(Encode, Decode)]
+pub struct ZeroSizedTypeDifferentiator<I>(sp_std::marker::PhantomData<I>);
+
+impl<I> Clone for ZeroSizedTypeDifferentiator<I> {
+	fn clone(&self) -> Self {
+		ZeroSizedTypeDifferentiator(sp_std::marker::PhantomData)
+	}
+}
+
+impl<I> PartialEq for ZeroSizedTypeDifferentiator<I> {
+	fn eq(&self, _other: &Self) -> bool { true }
+}
+
+impl<I> Eq for ZeroSizedTypeDifferentiator<I> { }
+
+#[cfg(feature = "std")]
+impl<I> std::fmt::Debug for ZeroSizedTypeDifferentiator<I> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		write!(f, "ZeroSizedTypeDifferentiator")
+	}
+}
+
+#[cfg(not(feature = "std"))]
+impl<I> core::fmt::Debug for ZeroSizedTypeDifferentiator<I> {
+	fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+		write!(f, "<wasm:stripped>")
+	}
+}
+
 decl_module! {
 	pub struct Module<T: Trait<I>, I: Instance = DefaultInstance> for enum Call where origin: T::Origin {
+		#[weight = 0]
+		fn dummy_call_invalid(origin, _x: ZeroSizedTypeDifferentiator<I>) {
+		}
 	}
 }
 
