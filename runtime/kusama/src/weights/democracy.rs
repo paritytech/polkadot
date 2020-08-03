c9f226ac881fa49e849cabe38b0e2de09e4613f7
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-08-04 00:52:14 +0200

    fix import

--- runtime/kusama/src/weights/democracy.rs
+++ runtime/kusama/src/weights/democracy.rs
@@ -20,7 +20,7 @@
 use frame_support::weights::{Weight, constants::RocksDbWeight as DbWeight};
 
 pub struct WeightInfo;
-impl pallet_democracy::WeightInfo for WeightInfo {
+impl democracy::WeightInfo for WeightInfo {
 	fn propose() -> Weight {
 		(49113000 as Weight)
 			.saturating_add(DbWeight::get().reads(2 as Weight))
