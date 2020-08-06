05a106f235283d3100d501019307bc02492e1ba7
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-08-06 16:10:58 +0200

    Revert "Revert "Ignore checks for companion PRs (#1455)""
    This reverts commit 4ac4a2b79a94a92e79699b034e36c245a4fd56f3.

---
+++ scripts/gitlab/check_web_wasm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#shellcheck source=lib.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/lib.sh"
+
+skip_if_companion_pr
+
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path runtime/polkadot/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path runtime/kusama/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path erasure-coding/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path parachain/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path primitives/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path rpc/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path statement-table/Cargo.toml
+time cargo build --locked --target=wasm32-unknown-unknown --manifest-path cli/Cargo.toml --no-default-features --features browser
