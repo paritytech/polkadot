c2eda36bf74a2b0318df01b8d14340582fc58774
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-08-06 16:11:49 +0200

    WIP
    Forked at: e91642361133dba084a9c9b3efa45fcb6cd3c5ca
    Parent branch: origin/rococo-branch

--- scripts/gitlab/test_deterministic_wasm.sh
+++ scripts/gitlab/test_deterministic_wasm.sh
@@ -3,8 +3,6 @@
 #shellcheck source=lib.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/lib.sh"
 
-skip_if_companion_pr
-
 # build runtime
 WASM_BUILD_NO_COLOR=1 cargo build --verbose --release -p kusama-runtime -p polkadot-runtime -p westend-runtime
 # make checksum
