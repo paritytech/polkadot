1628d7b4deacc5a80c71726d71c10fcab5af1809
Robert Habermeier <rphmeier@gmail.com>
2020-08-07 18:36:42 +0200

    port PoV distribution

--- node/network/pov-distribution/src/lib.rs
+++ node/network/pov-distribution/src/lib.rs
@@ -24,41 +24,26 @@ use polkadot_subsystem::{
 	ActiveLeavesUpdate, OverseerSignal, SubsystemContext, Subsystem, SubsystemResult, FromOverseer, SpawnedSubsystem,
 };
 use polkadot_subsystem::messages::{
-	PoVDistributionMessage, NetworkBridgeEvent, ReputationChange as Rep, PeerId,
-	RuntimeApiMessage, RuntimeApiRequest, AllMessages, NetworkBridgeMessage,
+	PoVDistributionMessage, RuntimeApiMessage, RuntimeApiRequest, AllMessages, NetworkBridgeMessage,
 };
-use node_primitives::{View, ProtocolId};
+use polkadot_node_network_protocol::{
+	v1 as protocol_v1, ReputationChange as Rep, NetworkBridgeEvent, PeerId, View,
+};
 
 use futures::prelude::*;
 use futures::channel::oneshot;
-use parity_scale_codec::{Encode, Decode};
 
 use std::collections::{hash_map::{Entry, HashMap}, HashSet};
 use std::sync::Arc;
 
 const COST_APPARENT_FLOOD: Rep = Rep::new(-500, "Peer appears to be flooding us with PoV requests");
 const COST_UNEXPECTED_POV: Rep = Rep::new(-500, "Peer sent us an unexpected PoV");
-const COST_MALFORMED_MESSAGE: Rep = Rep::new(-500, "Peer sent us a malformed message");
 const COST_AWAITED_NOT_IN_VIEW: Rep
 	= Rep::new(-100, "Peer claims to be awaiting something outside of its view");
 
 const BENEFIT_FRESH_POV: Rep = Rep::new(25, "Peer supplied us with an awaited PoV");
 const BENEFIT_LATE_POV: Rep = Rep::new(10, "Peer supplied us with an awaited PoV, \
 	but was not the first to do so");
-
-const PROTOCOL_V1: ProtocolId = *b"pvd1";
-
-#[derive(Encode, Decode)]
-enum WireMessage {
-    /// Notification that we are awaiting the given PoVs (by hash) against a
-	/// specific relay-parent hash.
-	#[codec(index = "0")]
-    Awaiting(Hash, Vec<Hash>),
-    /// Notification of an awaited PoV, in a given relay-parent context.
-    /// (relay_parent, pov_hash, pov)
-	#[codec(index = "1")]
-    SendPoV(Hash, Hash, PoV),
-}
 
 /// The PoV Distribution Subsystem.
 pub struct PoVDistribution;
@@ -96,6 +81,22 @@ struct BlockBasedState {
 struct PeerState {
 	/// A set of awaited PoV-hashes for each relay-parent in the peer's view.
 	awaited: HashMap<Hash, HashSet<Hash>>,
+}
+
+fn awaiting_message(relay_parent: Hash, awaiting: Vec<Hash>)
+	-> protocol_v1::ValidationProtocol
+{
+	protocol_v1::ValidationProtocol::PoVDistribution(
+		protocol_v1::PoVDistributionMessage::Awaiting(relay_parent, awaiting)
+	)
+}
+
+fn send_pov_message(relay_parent: Hash, pov_hash: Hash, pov: PoV)
+	-> protocol_v1::ValidationProtocol
+{
+	protocol_v1::ValidationProtocol::PoVDistribution(
+		protocol_v1::PoVDistributionMessage::SendPoV(relay_parent, pov_hash, pov)
+	)
 }
 
 /// Handles the signal. If successful, returns `true` if the subsystem should conclude,
@@ -169,11 +170,10 @@ async fn notify_all_we_are_awaiting(
 
 	if peers_to_send.is_empty() { return Ok(()) }
 
-	let payload = WireMessage::Awaiting(relay_parent, vec![pov_hash]).encode();
+	let payload = awaiting_message(relay_parent, vec![pov_hash]);
 
-	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendMessage(
+	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendValidationMessage(
 		peers_to_send,
-		PROTOCOL_V1,
 		payload,
 	))).await
 }
@@ -194,11 +194,10 @@ async fn notify_one_we_are_awaiting_many(
 
 	if awaiting_hashes.is_empty() { return Ok(()) }
 
-	let payload = WireMessage::Awaiting(relay_parent, awaiting_hashes).encode();
+	let payload = awaiting_message(relay_parent, awaiting_hashes);
 
-	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendMessage(
+	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendValidationMessage(
 		vec![peer.clone()],
-		PROTOCOL_V1,
 		payload,
 	))).await
 }
@@ -226,11 +225,10 @@ async fn distribute_to_awaiting(
 
 	if peers_to_send.is_empty() { return Ok(()) }
 
-	let payload = WireMessage::SendPoV(relay_parent, pov_hash, pov.clone()).encode();
+	let payload = send_pov_message(relay_parent, pov_hash, pov.clone());
 
-	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendMessage(
+	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendValidationMessage(
 		peers_to_send,
-		PROTOCOL_V1,
 		payload,
 	))).await
 }
@@ -361,11 +359,10 @@ async fn handle_awaiting(
 			// For all requested PoV hashes, if we have it, we complete the request immediately.
 			// Otherwise, we note that the peer is awaiting the PoV.
 			if let Some(pov) = relay_parent_state.known.get(&pov_hash) {
-				ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::SendMessage(
-					vec![peer.clone()],
-					PROTOCOL_V1,
-					WireMessage::SendPoV(relay_parent, pov_hash, (&**pov).clone()).encode(),
-				))).await?;
+				let payload = send_pov_message(relay_parent, pov_hash, (&**pov).clone());
+				ctx.send_message(AllMessages::NetworkBridge(
+					NetworkBridgeMessage::SendValidationMessage(vec![peer.clone()], payload)
+				)).await?;
 			} else {
 				peer_awaiting.insert(pov_hash);
 			}
@@ -449,7 +446,7 @@ async fn handle_incoming_pov(
 async fn handle_network_update(
 	state: &mut State,
 	ctx: &mut impl SubsystemContext<Message = PoVDistributionMessage>,
-	update: NetworkBridgeEvent,
+	update: NetworkBridgeEvent<protocol_v1::PoVDistributionMessage>,
 ) -> SubsystemResult<()> {
 	match update {
 		NetworkBridgeEvent::PeerConnected(peer, _observed_role) => {
@@ -483,17 +480,18 @@ async fn handle_network_update(
 
 			Ok(())
 		}
-		NetworkBridgeEvent::PeerMessage(peer, bytes) => {
-			match WireMessage::decode(&mut &bytes[..]) {
-				Ok(msg) => match msg {
-					WireMessage::Awaiting(relay_parent, pov_hashes) => handle_awaiting(
+		NetworkBridgeEvent::PeerMessage(peer, message) => {
+			match message {
+				protocol_v1::PoVDistributionMessage::Awaiting(relay_parent, pov_hashes)
+					=> handle_awaiting(
 						state,
 						ctx,
 						peer,
 						relay_parent,
 						pov_hashes,
 					).await,
-					WireMessage::SendPoV(relay_parent, pov_hash, pov) => handle_incoming_pov(
+				protocol_v1::PoVDistributionMessage::SendPoV(relay_parent, pov_hash, pov)
+					=> handle_incoming_pov(
 						state,
 						ctx,
 						peer,
@@ -501,11 +499,6 @@ async fn handle_network_update(
 						pov_hash,
 						pov,
 					).await,
-				},
-				Err(_) => {
-					report_peer(ctx, peer, COST_MALFORMED_MESSAGE).await?;
-					Ok(())
-				}
 			}
 		}
 		NetworkBridgeEvent::OurViewChange(view) => {
@@ -515,19 +508,9 @@ async fn handle_network_update(
 	}
 }
 
-fn network_update_message(update: NetworkBridgeEvent) -> AllMessages {
-	AllMessages::PoVDistribution(PoVDistributionMessage::NetworkBridgeUpdate(update))
-}
-
 async fn run(
 	mut ctx: impl SubsystemContext<Message = PoVDistributionMessage>,
 ) -> SubsystemResult<()> {
-	// startup: register the network protocol with the bridge.
-	ctx.send_message(AllMessages::NetworkBridge(NetworkBridgeMessage::RegisterEventProducer(
-		PROTOCOL_V1,
-		network_update_message,
-	))).await?;
-
 	let mut state = State {
 		relay_parent_state: HashMap::new(),
 		peer_state: HashMap::new(),
@@ -556,7 +539,7 @@ async fn run(
 						descriptor,
 						pov,
 					).await?,
-				PoVDistributionMessage::NetworkBridgeUpdate(event) =>
+				PoVDistributionMessage::NetworkBridgeUpdateV1(event) =>
 					handle_network_update(
 						&mut state,
 						&mut ctx,
@@ -661,10 +644,9 @@ mod tests {
 			assert_matches!(
 				handle.recv().await,
 				AllMessages::NetworkBridge(
-					NetworkBridgeMessage::SendMessage(peers, protocol, message)
+					NetworkBridgeMessage::SendValidationMessage(peers, message)
 				) => {
 					assert_eq!(peers, vec![peer_a.clone()]);
-					assert_eq!(protocol, PROTOCOL_V1);
 					assert_eq!(
 						message,
 						WireMessage::SendPoV(hash_a, pov_hash, pov.clone()).encode(),
@@ -737,10 +719,9 @@ mod tests {
 			assert_matches!(
 				handle.recv().await,
 				AllMessages::NetworkBridge(
-					NetworkBridgeMessage::SendMessage(peers, protocol, message)
+					NetworkBridgeMessage::SendValidationMessage(peers, protocol, message)
 				) => {
 					assert_eq!(peers, vec![peer_a.clone()]);
-					assert_eq!(protocol, PROTOCOL_V1);
 					assert_eq!(
 						message,
 						WireMessage::Awaiting(hash_a, vec![pov_hash]).encode(),
@@ -809,10 +790,9 @@ mod tests {
 			assert_matches!(
 				handle.recv().await,
 				AllMessages::NetworkBridge(
-					NetworkBridgeMessage::SendMessage(peers, protocol, message)
+					NetworkBridgeMessage::SendValidationMessage(peers, protocol, message)
 				) => {
 					assert_eq!(peers, vec![peer_a.clone()]);
-					assert_eq!(protocol, PROTOCOL_V1);
 					assert_eq!(
 						message,
 						WireMessage::Awaiting(hash_a, vec![pov_a_hash]).encode(),
@@ -1390,10 +1370,9 @@ mod tests {
 			assert_matches!(
 				handle.recv().await,
 				AllMessages::NetworkBridge(
-					NetworkBridgeMessage::SendMessage(peers, protocol, message)
+					NetworkBridgeMessage::SendValidationMessage(peers, protocol, message)
 				) => {
 					assert_eq!(peers, vec![peer_b.clone()]);
-					assert_eq!(protocol, PROTOCOL_V1);
 					assert_eq!(
 						message,
 						WireMessage::SendPoV(hash_a, pov_hash, pov.clone()).encode(),
