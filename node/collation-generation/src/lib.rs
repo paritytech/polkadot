ccaeee5f99830060c3f72c115c08888eb515db8d
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-08-14 12:17:03 +0200

    add (failing) test of collation distribution message sending

--- node/collation-generation/src/lib.rs
+++ node/collation-generation/src/lib.rs
@@ -282,7 +282,7 @@ mod tests {
 			subsystem_test_harness, TestSubsystemContextHandle,
 		};
 		use polkadot_primitives::v1::{
-			BlockData, CollatorPair, GlobalValidationData, LocalValidationData, PoV, ScheduledCore, Id as ParaId
+			BlockData, CollatorPair, GlobalValidationData, LocalValidationData, PoV, ScheduledCore, Id as ParaId, BlockNumber,
 		};
 		use std::pin::Pin;
 
@@ -415,9 +415,8 @@ mod tests {
 
 			let (tx, _rx) = mpsc::channel(0);
 
-			let subsystem_activated_hashes = activated_hashes.clone();
 			subsystem_test_harness(overseer, |mut ctx| async move {
-				handle_new_activations(test_config(16), &subsystem_activated_hashes, &mut ctx, &tx)
+				handle_new_activations(test_config(16), &activated_hashes, &mut ctx, &tx)
 					.await
 					.unwrap();
 			});
@@ -432,6 +431,82 @@ mod tests {
 			// given that the test configuration has a para_id of 16, there's only one way to get that value: with the 4
 			// hash.
 			assert_eq!(requested_local_validation_data, vec![[4; 32].into()]);
+		}
+
+		#[test]
+		fn sends_distribute_collation_message() {
+			let activated_hashes: Vec<Hash> = vec![
+				Hash::repeat_byte(1),
+				Hash::repeat_byte(4),
+				Hash::repeat_byte(9),
+				Hash::repeat_byte(16),
+			];
+
+			let overseer = |mut handle: TestSubsystemContextHandle<CollationGenerationMessage>| async move {
+				loop {
+					match handle.recv().await {
+						AllMessages::RuntimeApi(RuntimeApiMessage::Request(_hash, RuntimeApiRequest::GlobalValidationData(tx))) => {
+							tx.send(Ok(Default::default())).unwrap();
+						}
+						AllMessages::RuntimeApi(RuntimeApiMessage::Request(hash, RuntimeApiRequest::AvailabilityCores(tx))) => {
+							tx.send(Ok(vec![
+								CoreState::Free,
+								// this is weird, see explanation below
+								CoreState::Scheduled(scheduled_core_for((hash.as_fixed_bytes()[0] * 4) as u32)),
+								CoreState::Scheduled(scheduled_core_for((hash.as_fixed_bytes()[0] * 5) as u32)),
+							])).unwrap();
+						}
+						AllMessages::RuntimeApi(RuntimeApiMessage::Request(_hash, RuntimeApiRequest::LocalValidationData(_para_id, _occupied_core_assumption, tx))) => {
+							tx.send(Ok(Default::default())).unwrap();
+						}
+						msg => panic!("didn't expect any other overseer requests; got {:?}", msg),
+					}
+				}
+			};
+
+			let config = test_config(16);
+			let subsystem_config = config.clone();
+
+			let (tx, rx) = mpsc::channel(0);
+
+			// empty vec doesn't allocate on the heap, so it's ok we throw it away
+			let sent_messages = Arc::new(Mutex::new(Vec::new()));
+			let subsystem_sent_messages = sent_messages.clone();
+			subsystem_test_harness(overseer, |mut ctx| async move {
+				handle_new_activations(subsystem_config, &activated_hashes, &mut ctx, &tx)
+					.await
+					.unwrap();
+
+				std::mem::drop(tx);
+
+				// collect all sent messages
+				*subsystem_sent_messages.lock().await = rx.collect().await;
+			});
+
+			let sent_messages = Arc::try_unwrap(sent_messages).expect("subsystem should have shut down by now").into_inner();
+
+			// we expect a single message to be sent, containing a candidate receipt.
+			// we don't care too much about the commitments_hash right now, but let's ensure that we've calculated the
+			// correct descriptor
+			let expect_pov_hash = test_collation().proof_of_validity.hash();
+			let expect_validation_data_hash = validation_data_hash::<BlockNumber>(&Default::default(), &Default::default());
+			let expect_relay_parent = Hash::repeat_byte(16);
+			let expect_descriptor = CandidateDescriptor {
+				signature: config.key.sign(&collator_signature_payload(&expect_relay_parent, &config.para_id, &expect_validation_data_hash, &expect_pov_hash)),
+				para_id: config.para_id,
+				relay_parent: expect_relay_parent,
+				collator: config.key.public(),
+				validation_data_hash: expect_validation_data_hash,
+				pov_hash: expect_pov_hash,
+			};
+
+			assert_eq!(sent_messages.len(), 1);
+			match &sent_messages[0] {
+				AllMessages::CollatorProtocol(CollatorProtocolMessage::DistributeCollation(CandidateReceipt { descriptor, .. }, .. )) => {
+					assert_eq!(descriptor, &expect_descriptor);
+				},
+				_ => panic!("received wrong message type"),
+			}
 		}
 	}
 }
