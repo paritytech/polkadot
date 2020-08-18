62b9d4a575cc6844f589ccfbe3bcdc93a8b254a1
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-08-18 10:04:52 +0200

    Companion for Substrate 6868 (WeightInfo for System, Utility, and Timestamp) (#1606)
    * fix compile warning
    
    * remove tests for weights
    
    * add custom weights
    
    * "Update Substrate"
    
    Co-authored-by: parity-processbot <>

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -139,7 +139,7 @@ impl frame_system::Trait for Runtime {
 	type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
+	type SystemWeightInfo = weights::frame_system::WeightInfo;
 }
 
 impl pallet_scheduler::Trait for Runtime {
@@ -224,7 +224,7 @@ impl pallet_timestamp::Trait for Runtime {
 	type Moment = u64;
 	type OnTimestampSet = Babe;
 	type MinimumPeriod = MinimumPeriod;
-	type WeightInfo = ();
+	type WeightInfo = weights::pallet_timestamp::WeightInfo;
 }
 
 parameter_types! {
@@ -482,7 +482,7 @@ impl pallet_identity::Trait for Runtime {
 impl pallet_utility::Trait for Runtime {
 	type Event = Event;
 	type Call = Call;
-	type WeightInfo = ();
+	type WeightInfo = weights::pallet_utility::WeightInfo;
 }
 
 parameter_types! {
