de2e38815e63cca77e76e45181ce04a0c3af4dd5
RRTTI <raul@ost.com>
2020-08-19 10:47:31 +0200

    Update Kusama Council seats & Runners Up number
    Update Kusama council seats to 19 and DesiredRunnersUp to 19 as well, increase representation of passive stake holders in the council and giving more visibility to runners up in Kusama Network (from 7 to 19): this will be more visibility to new candidates to expand options for stakeholders when voting.

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -424,8 +424,8 @@ parameter_types! {
 	pub const VotingBond: Balance = 5 * CENTS;
 	/// Daily council elections.
 	pub const TermDuration: BlockNumber = 24 * HOURS;
-	pub const DesiredMembers: u32 = 17;
-	pub const DesiredRunnersUp: u32 = 7;
+	pub const DesiredMembers: u32 = 19;
+	pub const DesiredRunnersUp: u32 = 19;
 	pub const ElectionsPhragmenModuleId: LockIdentifier = *b"phrelect";
 }
 // Make sure that there are no more than MAX_MEMBERS members elected via phragmen.
