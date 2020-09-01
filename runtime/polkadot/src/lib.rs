f5839d4c7bc654bef86254d3a9d8895326c424b6
kianenigma <kian.peymani@gmail.com>
2020-09-01 09:15:45 +0200

    Add reward limits

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -326,7 +326,7 @@ parameter_types! {
 	pub const BondingDuration: pallet_staking::EraIndex = 28;
 	pub const SlashDeferDuration: pallet_staking::EraIndex = 27;
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
-	pub const MaxNominatorRewardedPerValidator: u32 = 64;
+	pub const MaxNominatorRewardedPerValidator: u32 = 256;
 	// last 15 minutes of the last session will be for election.
 	pub const ElectionLookahead: BlockNumber = EPOCH_DURATION_IN_BLOCKS / 16;
 	pub const MaxIterations: u32 = 10;
