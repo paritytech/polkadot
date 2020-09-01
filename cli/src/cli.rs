e2ebf25bb138137a2b6b6af177b643f5a8065ecb
Ashley <ashley.ruglys@gmail.com>
2020-09-01 11:27:24 +0200

    Companion PR for `Move subcommands from sc-cli to nodes #6948 ` (#1635)
    * Companion PR
    
    * Implement traits for Client
    
    * "Update Substrate"
    
    Co-authored-by: parity-processbot <>

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -21,9 +21,26 @@ use structopt::StructOpt;
 #[allow(missing_docs)]
 #[derive(Debug, StructOpt)]
 pub enum Subcommand {
-	#[allow(missing_docs)]
-	#[structopt(flatten)]
-	Base(sc_cli::Subcommand),
+	/// Build a chain specification.
+	BuildSpec(sc_cli::BuildSpecCmd),
+
+	/// Validate blocks.
+	CheckBlock(sc_cli::CheckBlockCmd),
+
+	/// Export blocks.
+	ExportBlocks(sc_cli::ExportBlocksCmd),
+
+	/// Export the state of a given block into a chain spec.
+	ExportState(sc_cli::ExportStateCmd),
+
+	/// Import blocks.
+	ImportBlocks(sc_cli::ImportBlocksCmd),
+
+	/// Remove the whole chain.
+	PurgeChain(sc_cli::PurgeChainCmd),
+
+	/// Revert the chain to a previous state.
+	Revert(sc_cli::RevertCmd),
 
 	#[allow(missing_docs)]
 	#[structopt(name = "validation-worker", setting = structopt::clap::AppSettings::Hidden)]
