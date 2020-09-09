7690d3f7727157e0897cf8d9f27f8676e8a2460b
thiolliere <gui.thiolliere@gmail.com>
2020-09-09 15:40:05 +0200

    use construct_runtime index

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -26,8 +26,6 @@ pub mod purchase;
 pub mod impls;
 pub mod paras_sudo_wrapper;
 
-pub mod dummy;
-
 use primitives::v1::{BlockNumber, ValidatorId};
 use sp_runtime::{Perquintill, Perbill, FixedPointNumber, traits::Saturating};
 use frame_support::{
