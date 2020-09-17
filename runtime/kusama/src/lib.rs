e077b51e97119b624a37b9bad1e7b3dcf9c30ccb
Andronik Ordian <write@reusable.software>
2020-09-17 17:00:31 +0200

    fix compilation

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1087,6 +1087,10 @@ sp_api::impl_runtime_apis! {
 		fn candidate_events() -> Vec<CandidateEvent<Hash>> {
 			Vec::new()
 		}
+
+		fn validator_discovery(_: Vec<ValidatorId>) -> Vec<Option<AuthorityDiscoveryId>> {
+			Vec::new()
+		}
 	}
 
 	impl fg_primitives::GrandpaApi<Block> for Runtime {
