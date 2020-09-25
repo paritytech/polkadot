f2288e1c5988061d3c32ee8a127f6e1f32b492bf
Martin Pugh <pugh@s3kr.it>
2020-09-25 17:40:04 +0200

    update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,11 +12,17 @@ fi
 
 if [[ ! "$version" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "Version should be in the format v1.2.3"
+  exit 1
 fi
 
+echo '[+] Checking out the release branch'
 git checkout release
+echo '[+] Pulling latest version of the release branch from github'
 git pull
+echo '[+] Attempting to merge the release-candidate branch to the release branch'
 git merge "$version"
+echo '[+] Tagging the release'
 git tag -s -m "$version" "$version"
+echo '[+] Pushing the release branch and tag to Github. A new release will be created shortly'
 git push origin release
 git push origin "refs/tags/$version"
