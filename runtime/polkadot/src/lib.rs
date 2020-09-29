a1193ffb14f018ed1238cc075c8be2a1cbc4762d
kianenigma <kian.peymani@gmail.com>
2020-09-29 20:08:10 +0200

    Companion for substrate/pull/7215

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -339,6 +339,13 @@ parameter_types! {
 	pub const ElectionLookahead: BlockNumber = EPOCH_DURATION_IN_BLOCKS / 16;
 	pub const MaxIterations: u32 = 10;
 	pub MinSolutionScoreBump: Perbill = Perbill::from_rational_approximation(5u32, 10_000);
+	// The unsigned solution is operational, so as long as the average on_initialize weights are
+	// less than `MaximumBlockWeight * (1 - T::AvailableRatio)`, it can consume at most most this
+	// amount.
+	pub OffchainSolutionWeightLimit: Weight =
+		MaximumBlockWeight::get()
+			.saturating_sub(BlockExecutionWeight::get())
+			.saturating_sub(ExtrinsicBaseWeight::get());
 }
 
 type SlashCancelOrigin = EnsureOneOf<
@@ -369,6 +376,7 @@ impl pallet_staking::Trait for Runtime {
 	type UnsignedPriority = StakingUnsignedPriority;
 	type MaxIterations = MaxIterations;
 	type MinSolutionScoreBump = MinSolutionScoreBump;
+	type OffchainSolutionWeightLimit = OffchainSolutionWeightLimit;
 	type WeightInfo = weights::pallet_staking::WeightInfo;
 }
 
@@ -620,7 +628,8 @@ parameter_types! {
 }
 
 parameter_types! {
-	pub const StakingUnsignedPriority: TransactionPriority = TransactionPriority::max_value() / 2;
+	pub StakingUnsignedPriority: TransactionPriority =
+		Perbill::from_percent(90) * TransactionPriority::max_value();
 	pub const ImOnlineUnsignedPriority: TransactionPriority = TransactionPriority::max_value();
 }
 
