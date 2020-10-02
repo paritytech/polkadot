95bbd18e559809ecc7fe8a6b815d5852dde8a244
Bryan Chen <xlchen1291@gmail.com>
2020-10-02 17:20:46 +1300

    add missing method

--- runtime/kusama/src/weights/pallet_utility.rs
+++ runtime/kusama/src/weights/pallet_utility.rs
@@ -32,4 +32,8 @@ impl pallet_utility::WeightInfo for WeightInfo {
 	fn as_derivative() -> Weight {
 		(4086000 as Weight)
 	}
+	fn batch_all(c: u32, ) -> Weight {
+		(21_104_000 as Weight)
+			.saturating_add((1_509_000 as Weight).saturating_mul(c as Weight))
+	}
 }
