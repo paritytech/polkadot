11830bdf2a20fe97efe936221e9d6bb33d914936
Sergei Shulepov <s.pepyakin@gmail.com>
2020-10-14 14:00:59 +0200

    test-runtime: use sp-std instead of rstd
    At some point we had aliases for all the substrate crates but at some point we've switched to the full names to avoid confusion. It seems that we missed this instance.

--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -20,7 +20,7 @@
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit="256"]
 
-use rstd::prelude::*;
+use sp_std::prelude::*;
 use codec::Encode;
 use polkadot_runtime_parachains::{
 	configuration,
