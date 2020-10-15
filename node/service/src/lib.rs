702fc9386fa2e5509e546cd3d02caebc04c58b16
André Silva <123550+andresilva@users.noreply.github.com>
2020-10-15 22:26:37 +0100

    runtime: remove finality-tracker (#1762)
    * runtime: remove finality-tracker
    
    * runtime: bump spec_version
    
    * update substrate

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -516,7 +516,6 @@ pub fn new_full<RuntimeApi, Executor>(
 			config,
 			link: link_half,
 			network: network.clone(),
-			inherent_data_providers: inherent_data_providers.clone(),
 			telemetry_on_connect: Some(telemetry_connection_sinks.on_connect_stream()),
 			voting_rule,
 			prometheus_registry: prometheus_registry.clone(),
@@ -528,11 +527,7 @@ pub fn new_full<RuntimeApi, Executor>(
 			grandpa::run_grandpa_voter(grandpa_config)?
 		);
 	} else {
-		grandpa::setup_disabled_grandpa(
-			client.clone(),
-			&inherent_data_providers,
-			network.clone(),
-		)?;
+		grandpa::setup_disabled_grandpa(network.clone())?;
 	}
 
 	if matches!(role, Role::Authority{..} | Role::Sentry{..}) {
