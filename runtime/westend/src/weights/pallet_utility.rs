fd7f24edffea38dd522db3ec78a7a9dadbe90792
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-10-15 11:12:03 +0200

    constant as_derivative

--- runtime/westend/src/weights/pallet_utility.rs
+++ runtime/westend/src/weights/pallet_utility.rs
@@ -29,7 +29,7 @@ impl<T: frame_system::Trait> pallet_utility::WeightInfo for WeightInfo<T> {
 		(18_589_000 as Weight)
 			.saturating_add((1_734_000 as Weight).saturating_mul(c as Weight))
 	}
-	fn as_derivative(_u: u32) -> Weight {
+	fn as_derivative() -> Weight {
 		(5_611_000 as Weight)
 	}
 }
