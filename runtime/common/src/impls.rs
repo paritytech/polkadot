f518000d2f4e5af73add41f4154f8e83867104f2
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-10-21 22:10:07 +0200

    add fee and tip test

--- runtime/common/src/impls.rs
+++ runtime/common/src/impls.rs
@@ -67,3 +67,157 @@ where
 		}
 	}
 }
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
+	use frame_support::traits::FindAuthor;
+	use sp_core::H256;
+	use sp_runtime::{
+		testing::Header, ModuleId,
+		traits::{BlakeTwo256, IdentityLookup},
+		Perbill,
+	};
+	use primitives::v1::AccountId;
+
+	#[derive(Clone, PartialEq, Eq, Debug)]
+	pub struct Test;
+
+	impl_outer_origin!{
+		pub enum Origin for Test {}
+	}
+
+	parameter_types! {
+		pub const BlockHashCount: u64 = 250;
+		pub const ExtrinsicBaseWeight: u64 = 100;
+		pub const MaximumBlockWeight: Weight = 1024;
+		pub const MaximumBlockLength: u32 = 2 * 1024;
+		pub const AvailableBlockRatio: Perbill = Perbill::one();
+	}
+
+	impl frame_system::Trait for Test {
+		type BaseCallFilter = ();
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = u64;
+		type Call = ();
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = AccountId;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = ();
+		type BlockHashCount = BlockHashCount;
+		type MaximumBlockWeight = MaximumBlockWeight;
+		type DbWeight = ();
+		type BlockExecutionWeight = ();
+		type ExtrinsicBaseWeight = ExtrinsicBaseWeight;
+		type MaximumExtrinsicWeight = MaximumBlockWeight;
+		type MaximumBlockLength = MaximumBlockLength;
+		type AvailableBlockRatio = AvailableBlockRatio;
+		type Version = ();
+		type PalletInfo = ();
+		type AccountData = pallet_balances::AccountData<u64>;
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+	}
+
+	impl pallet_balances::Trait for Test {
+		type Balance = u64;
+		type Event = ();
+		type DustRemoval = ();
+		type ExistentialDeposit = ();
+		type AccountStore = System;
+		type MaxLocks = ();
+		type WeightInfo = ();
+	}
+
+	pub struct Nobody;
+	impl frame_support::traits::Contains<AccountId> for Nobody {
+		fn contains(_: &AccountId) -> bool { false }
+		fn sorted_members() -> Vec<AccountId> { vec![] }
+		#[cfg(feature = "runtime-benchmarks")]
+		fn add(_: &AccountId) { unimplemented!() }
+	}
+	impl frame_support::traits::ContainsLengthBound for Nobody {
+		fn min_len() -> usize { 0 }
+		fn max_len() -> usize { 0 }
+	}
+
+	parameter_types! {
+		pub const TreasuryModuleId: ModuleId = ModuleId(*b"py/trsry");
+	}
+
+	impl pallet_treasury::Trait for Test {
+		type Currency = pallet_balances::Module<Test>;
+		type ApproveOrigin = frame_system::EnsureRoot<AccountId>;
+		type RejectOrigin = frame_system::EnsureRoot<AccountId>;
+		type Event = ();
+		type OnSlash = ();
+		type ProposalBond = ();
+		type ProposalBondMinimum = ();
+		type SpendPeriod = ();
+		type Burn = ();
+		type BurnDestination = ();
+		type Tippers = Nobody;
+		type TipCountdown = ();
+		type TipFindersFee = ();
+		type TipReportDepositBase = ();
+		type DataDepositPerByte = ();
+		type BountyDepositBase = ();
+		type BountyDepositPayoutDelay = ();
+		type BountyUpdatePeriod = ();
+		type MaximumReasonLength = ();
+		type BountyCuratorDeposit = ();
+		type BountyValueMinimum = ();
+		type ModuleId = TreasuryModuleId;
+		type WeightInfo = ();
+	}
+
+	pub struct OneAuthor;
+	impl FindAuthor<AccountId> for OneAuthor {
+		fn find_author<'a, I>(_: I) -> Option<AccountId>
+			where I: 'a,
+		{
+			Some(Default::default())
+		}
+	}
+	impl pallet_authorship::Trait for Test {
+		type FindAuthor = OneAuthor;
+		type UncleGenerations = ();
+		type FilterUncle = ();
+		type EventHandler = ();
+	}
+
+	type Treasury = pallet_treasury::Module<Test>;
+	type Balances = pallet_balances::Module<Test>;
+	type System = frame_system::Module<Test>;
+
+	pub fn new_test_ext() -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap();
+		// We use default for brevity, but you can configure as desired if needed.
+		pallet_balances::GenesisConfig::<Test>::default().assimilate_storage(&mut t).unwrap();
+		t.into()
+	}
+
+	#[test]
+	fn test_fees_and_tip_split() {
+		new_test_ext().execute_with(|| {
+			let fee = Balances::issue(10);
+			let tip = Balances::issue(20);
+
+			assert_eq!(Balances::free_balance(Treasury::account_id()), 0);
+			assert_eq!(Balances::free_balance(AccountId::default()), 0);
+
+			DealWithFees::on_unbalanceds(vec![fee, tip].into_iter());
+
+			// Author gets 100% of tip and 20% of fee = 22
+			assert_eq!(Balances::free_balance(AccountId::default()), 22);
+			// Treasury gets 80% of fee
+			//assert_eq!(Balances::free_balance(Treasury::account_id()), 8);
+		});
+	}
+}
