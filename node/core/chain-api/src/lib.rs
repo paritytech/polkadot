2170598c0e354fffb83779607486d8dcf07db671
Bernhard Schuster <bernhard@ahoi.io>
2020-10-23 18:05:14 +0200

    Update node/core/chain-api/src/lib.rs
    Co-authored-by: Andronik Ordian <write@reusable.software>

--- node/core/chain-api/src/lib.rs
+++ node/core/chain-api/src/lib.rs
@@ -66,7 +66,7 @@ impl<Client, Context> Subsystem<Context> for ChainApiSubsystem<Client> where
 	fn start(self, ctx: Context) -> SpawnedSubsystem {
 		let future = run(ctx, self).map_err(|e| SubsystemError::with_origin("chain-api", e)).map(|_| ()).boxed();
 		SpawnedSubsystem {
-			future: ,
+			future,
 			name: "chain-api-subsystem",
 		}
 	}
