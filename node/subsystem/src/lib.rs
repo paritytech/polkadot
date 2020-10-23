f1844c8225ba98c65035516ddfed56b32f660022
Bernhard Schuster <bernhard@ahoi.io>
2020-10-23 13:28:45 +0200

    Update node/subsystem/src/lib.rs
    Co-authored-by: Andronik Ordian <write@reusable.software>

--- node/subsystem/src/lib.rs
+++ node/subsystem/src/lib.rs
@@ -149,7 +149,7 @@ pub enum SubsystemError {
 impl SubsystemError {
 	/// Adds a `str` as `origin` to the given error `err`.
 	pub fn with_origin<E: 'static + Send + std::error::Error>(origin: &'static str, err: E) -> Self {
-		Self::FromOrigin{ origin, source: Box::new(err) }
+		Self::FromOrigin { origin, source: Box::new(err) }
 	}
 }
 
