41bedbed682c09915a6edfa5f760706547213538
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-10-27 20:44:29 +0100

    bump spec

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -88,7 +88,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 2026,
+	spec_version: 2027,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
