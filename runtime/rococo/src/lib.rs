6d39a0e63a3037b9102aed142a7f3ce40871235e
Bastian Köcher <git@kchr.de>
2020-10-28 13:08:11 +0100

    Downgrade transaction version

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -96,7 +96,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 2,
+	transaction_version: 0,
 };
 
 /// Native version.
