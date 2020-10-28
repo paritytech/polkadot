a676f431c54ee16041a0b2557b2d5548b6b0078f
s3krit <pugh@s3kr.it>
2020-10-28 13:18:41 +0100

    rollback spec_version and fix check_runtime (#1868)

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -90,7 +90,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 27,
+	spec_version: 26,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
