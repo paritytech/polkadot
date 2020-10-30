dcb8b4fede6c35ac40dd36863ae79d14d0bbda14
Fedor Sakharov <fedor.sakharov@gmail.com>
2020-10-31 00:30:35 +0300

    Fix build without feature

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -23,7 +23,7 @@ mod grandpa_support;
 mod client;
 
 use grandpa::{self, FinalityProofProvider as GrandpaFinalityProofProvider};
-#[cfg(all(feature = "full-node", feature = "real-overseer"))]
+#[cfg(all(feature = "full-node"))]
 use {
 	std::convert::TryInto,
 	std::time::Duration,
