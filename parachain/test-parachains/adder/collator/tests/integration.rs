7282e269181d2a5c6fe17819a5f608118fcd9b19
Bastian Köcher <git@kchr.de>
2020-11-08 10:10:01 +0100

    Make sure the integration test is run in CI

--- parachain/test-parachains/adder/collator/tests/integration.rs
+++ parachain/test-parachains/adder/collator/tests/integration.rs
@@ -17,7 +17,7 @@
 //! Integration test that ensures that we can build and include parachain
 //! blocks of the adder parachain.
 
-#[cfg(feature = "real-overseer")]
+// If this test is failing, make sure to run all tests with the `real-overseer` feature being enabled.
 #[substrate_test_utils::test]
 async fn collating_using_adder_collator(task_executor: sc_service::TaskExecutor) {
 	use sp_keyring::AccountKeyring::*;
