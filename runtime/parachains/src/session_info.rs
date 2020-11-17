4a5ca125c3616f6e177a50e43d5153f5aa9656f3
Andronik Ordian <write@reusable.software>
2020-11-17 16:41:25 +0100

    runtime/session_info: initial impl

--- runtime/parachains/src/session_info.rs
+++ runtime/parachains/src/session_info.rs
@@ -23,11 +23,14 @@ use primitives::v1::{SessionIndex, SessionInfo, ValidatorId};
 use frame_support::{
 	decl_storage, decl_module, decl_error,
 };
-use crate::configuration;
+use crate::{configuration, paras, scheduler};
 
 pub trait Trait:
 	frame_system::Trait
 	+ configuration::Trait
+	+ paras::Trait
+	+ scheduler::Trait
+	+ pallet_authority_discovery::Trait
 {
 }
 
@@ -65,13 +68,51 @@ impl<T: pallet_session::Trait + Trait> pallet_session::OneSessionHandler<T::Acco
 
 	}
 
-	fn on_new_session<'a, I: 'a>(_changed: bool, _validators: I, _queued: I)
+	fn on_new_session<'a, I: 'a>(_changed: bool, validators: I, _queued: I)
 		where I: Iterator<Item=(&'a T::AccountId, Self::Key)>
 	{
-		// TODO:
-		// fetch dispute_period from config
+		let config = <configuration::Module<T>>::config();
+
+		let dispute_period = config.dispute_period;
+		let n_parachains = <paras::Module<T>>::parachains().len() as u32;
+
+		// TODO: is is the same as <inclusion::Module<T>>::validators()?
+		let validators: Vec<_> = validators.map(|(_, key)| key).collect();
+		let discovery_keys = <pallet_authority_discovery::Module<T>>::authorities();
+		// FIXME:???
+		let approval_keys = Vec::new();
+		let validator_groups =  <scheduler::Module<T>>::validator_groups();
+		let n_cores = n_parachains + config.parathread_cores;
+		let zeroth_delay_tranche_width = config.zeroth_delay_tranche_width;
+		let relay_vrf_modulo_samples = config.relay_vrf_modulo_samples;
+		// FIXME: add n_delay_tranches to config?
+		let n_delay_tranches = 0u32;
+		let no_show_slots = config.no_show_slots;
+		let needed_approvals = config.needed_approvals;
+
 		let new_session_index = <pallet_session::Module<T>>::current_index();
-		todo!()
+		let old_earliest_stored_session = EarliestStoredSession::get();
+		let new_earliest_stored_session = new_session_index.checked_sub(dispute_period).unwrap_or(0);
+		// update `EarliestStoredSession` based on `config.dispute_period`
+		EarliestStoredSession::set(new_earliest_stored_session);
+		// remove all entries from `Sessions` from the previous value up to the new value
+		for idx in old_earliest_stored_session..new_earliest_stored_session {
+			Sessions::remove(&idx);
+		}
+		// create a new entry in `Sessions` with information about the current session
+		let new_session_info = SessionInfo {
+			validators,
+			discovery_keys,
+			approval_keys,
+			validator_groups,
+			n_cores,
+			zeroth_delay_tranche_width,
+			relay_vrf_modulo_samples,
+			n_delay_tranches,
+			no_show_slots,
+			needed_approvals,
+		};
+		Sessions::insert(&new_session_index, &new_session_info);
 	}
 
 	fn on_disabled(_i: usize) { }
@@ -80,7 +121,8 @@ impl<T: pallet_session::Trait + Trait> pallet_session::OneSessionHandler<T::Acco
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+	// use super::*;
+	#[test]
 	fn it_works() {
 		todo!()
 	}
