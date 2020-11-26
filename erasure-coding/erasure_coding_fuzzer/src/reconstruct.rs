55bf920ccf33d2c8c6adac5d7ff4fa8f7826e247
Andronik Ordian <write@reusable.software>
2020-11-26 18:53:04 +0100

    tabify fuzz tests

--- erasure-coding/erasure_coding_fuzzer/src/reconstruct.rs
+++ erasure-coding/erasure_coding_fuzzer/src/reconstruct.rs
@@ -4,17 +4,17 @@ use std::sync::Arc;
 use honggfuzz::fuzz;
 
 fn main(){
-    loop {
-        fuzz!(|data: (usize, Vec<(Vec<u8>, usize)>)| {
-            let (num_validators, chunk_input) = data;
-            if num_validators <= 1 || num_validators > 10_000 {
-                return;
-            }
-            let reconstructed: Result<AvailableData, _> = reconstruct_v1(
-                num_validators,
-                chunk_input.iter().map(|t| (&*t.0, t.1)).collect::<Vec<(&[u8], usize)>>()
-            );
-            println!("reconstructed {:?}", reconstructed);
-        });
-    }
+	loop {
+		fuzz!(|data: (usize, Vec<(Vec<u8>, usize)>)| {
+			let (num_validators, chunk_input) = data;
+			if num_validators <= 1 || num_validators > 10_000 {
+				return;
+			}
+			let reconstructed: Result<AvailableData, _> = reconstruct_v1(
+				num_validators,
+				chunk_input.iter().map(|t| (&*t.0, t.1)).collect::<Vec<(&[u8], usize)>>()
+			);
+			println!("reconstructed {:?}", reconstructed);
+		});
+	}
 }
