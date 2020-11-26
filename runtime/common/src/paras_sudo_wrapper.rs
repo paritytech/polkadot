518c8dab65a391141252b1e4f65dec7c9402b6c2
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-11-25 21:28:42 -0800

    Update paras_sudo_wrapper.rs

--- runtime/common/src/paras_sudo_wrapper.rs
+++ runtime/common/src/paras_sudo_wrapper.rs
@@ -74,22 +74,6 @@ decl_module! {
 			Ok(())
 		}
 
-		/// Send an opaque downward message to the given para.
-		///
-		/// The given parachain should exist and the payload should not exceed the preconfigured size
-		/// `config.max_downward_message_size`.
-		#[weight = (1_000, DispatchClass::Operational)]
-		pub fn sudo_queue_downward_message(origin, id: ParaId, payload: Vec<u8>) -> DispatchResult {
-			ensure_root(origin)?;
-			ensure!(<paras::Module<T>>::is_valid_para(id), Error::<T>::ParaDoesntExist);
-			let config = <configuration::Module<T>>::config();
-			<dmp::Module<T>>::queue_downward_message(&config, id, payload)
-				.map_err(|e| match e {
-					dmp::QueueDownwardMessageError::ExceedsMaxMessageSize =>
-						Error::<T>::ExceedsMaxMessageSize.into(),
-				})
-		}
-
 		/// Send a downward XCM to the given para.
 		///
 		/// The given parachain should exist and the payload should not exceed the preconfigured size
