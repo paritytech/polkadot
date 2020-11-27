4a4d552bc2973765ff0f9f4a01f87222ff659595
André Silva <andrerfosilva@gmail.com>
2020-11-27 17:49:35 +0000

    proposer: guard all provisioner data work with timeout

--- node/core/proposer/src/lib.rs
+++ node/core/proposer/src/lib.rs
@@ -141,20 +141,23 @@ where
 		let mut overseer = self.overseer.clone();
 		let parent_header_hash = self.parent_header_hash.clone();
 
-		let (sender, receiver) = futures::channel::oneshot::channel();
+		let pid = async {
+			let (sender, receiver) = futures::channel::oneshot::channel();
+			overseer.wait_for_activation(parent_header_hash, sender).await;
+			receiver.await.map_err(|_| Error::ClosedChannelAwaitingActivation)??;
 
-		overseer.wait_for_activation(parent_header_hash, sender).await;
-		receiver.await.map_err(|_| Error::ClosedChannelAwaitingActivation)??;
+			let (sender, receiver) = futures::channel::oneshot::channel();
+			overseer.send_msg(AllMessages::Provisioner(
+				ProvisionerMessage::RequestInherentData(parent_header_hash, sender),
+			)).await;
 
-		let (sender, receiver) = futures::channel::oneshot::channel();
-		overseer.send_msg(AllMessages::Provisioner(
-			ProvisionerMessage::RequestInherentData(parent_header_hash, sender),
-		)).await;
+			receiver.await.map_err(|_| Error::ClosedChannelAwaitingInherentData)
+		};
 
 		let mut timeout = futures_timer::Delay::new(PROPOSE_TIMEOUT).fuse();
 
 		select! {
-			pid = receiver.fuse() => pid.map_err(|_| Error::ClosedChannelAwaitingInherentData),
+			pid = pid.fuse() => pid,
 			_ = timeout => Err(Error::Timeout),
 		}
 	}
