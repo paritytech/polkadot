fecfc237602a0d3c8cdd0d95a5d6c374c30bbbe3
Bernhard Schuster <bernhard@ahoi.io>
2020-12-14 11:43:09 +0100

    make tests pass

--- node/network/availability-distribution/src/tests.rs
+++ node/network/availability-distribution/src/tests.rs
@@ -707,12 +707,13 @@ fn check_views() {
 			});
 			assert_eq!(view, view![test_state.relay_parent]);
 			assert_eq!(receipts, hashmap!{
-				test_state.ancestors[0] => hashset!{
+				test_state.relay_parent => hashset!{
 					candidate_hash_a,
 					candidate_hash_b,
 				},
-				test_state.relay_parent => hashset!{
+				test_state.ancestors[0] => hashset!{
 					candidate_hash_a,
+					candidate_hash_b,
 				},
 			});
 		}
@@ -746,6 +747,7 @@ fn reputation_verification() {
 	let pov_hash_a = pov_block_a.hash();
 	let pov_hash_b = pov_block_b.hash();
 	let pov_hash_c = pov_block_c.hash();
+
 
 	let expected_head_data = test_state.head_data.get(&test_state.chain_ids[0]).unwrap();
 
@@ -782,17 +784,61 @@ fn reputation_verification() {
 		.build(),
 	];
 
-	let rp0 = Hash::repeat_byte(0xFA);
+	let candidate_hash_a = candidates[0].hash();
+	let candidate_hash_b = candidates[1].hash();
 
 	let state = ProtocolState {
 		peer_views: hashmap!{
-			peer_a.clone() => view![rp0],
-		},
-		view: view![Hash::repeat_byte(0xFA)],
+				peer_b.clone() => view![
+					test_state.ancestors[0],
+				],
+				peer_a.clone() => view![
+					test_state.relay_parent,
+				],
+			},
+		view: view![test_state.relay_parent],
 		receipts: hashmap!{
-			rp0 => hashset!{ candidates[0].hash() },
+			test_state.ancestors[0] => hashset!{
+				candidate_hash_a,
+				candidate_hash_b,
+			},
+			test_state.relay_parent => hashset!{
+				candidate_hash_a,
+				candidate_hash_b,
+			},
 		},
-		.. Default::default()
+		per_candidate : hashmap!{
+			candidate_hash_a => PerCandidate {
+				descriptor: candidates[0].descriptor.clone(),
+				validators: test_state.validator_public.clone(),
+				validator_index: test_state.validator_index.clone(),
+				live_in: hashset!{test_state.relay_parent, test_state.ancestors[0]},
+				.. Default::default()
+			},
+			candidate_hash_b => PerCandidate {
+				descriptor: candidates[1].descriptor.clone(),
+				validators: test_state.validator_public.clone(),
+				validator_index: test_state.validator_index.clone(),
+				live_in: hashset!{test_state.relay_parent, test_state.ancestors[0]},
+				.. Default::default()
+			},
+		},
+		per_relay_parent: hashmap!{
+			test_state.relay_parent => PerRelayParent {
+				ancestors: vec![
+					test_state.ancestors[0],
+					test_state.ancestors[1],
+				],
+				live_candidates: hashset!{ candidate_hash_a, candidate_hash_b },
+			},
+			test_state.ancestors[0] => PerRelayParent {
+				ancestors: vec![
+					test_state.ancestors[1],
+					test_state.ancestors[2]
+				],
+				live_candidates: hashset!{ candidate_hash_a, candidate_hash_b },
+			},
+		}
 	};
 
 	let keystore = test_state.keystore.clone();
