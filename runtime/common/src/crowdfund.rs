ccb5aeff34291aca90b6575bae2c5769dda857ae
thiolliere <gui.thiolliere@gmail.com>
2020-12-16 12:23:00 +0100

    remove unused

--- runtime/common/src/crowdfund.rs
+++ runtime/common/src/crowdfund.rs
@@ -579,7 +579,7 @@ mod tests {
 	// The testing primitives are very useful for avoiding having to work with signatures
 	// or public keys. `u64` is used as the `AccountId` and no `Signature`s are requried.
 	use sp_runtime::{
-		Permill, Percent, testing::Header, DispatchResult,
+		Permill, testing::Header, DispatchResult,
 		traits::{BlakeTwo256, IdentityLookup},
 	};
 	use crate::slots::Registrar;
