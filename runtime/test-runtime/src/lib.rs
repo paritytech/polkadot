768286a859cbef607421f0ddf1750d8fcbe30284
Shawn Tabrizi <shawntabrizi@gmail.com>
2020-12-16 14:07:54 -0800

    copy pasta

--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -524,7 +524,7 @@ construct_runtime! {
 }
 
 /// The address format for describing accounts.
-pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
+pub type Address = sp_runtime::MultiAddress<AccountId, AccountIndex>;
 /// Block header type as expected by this runtime.
 pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
 /// Block type as expected by this runtime.
