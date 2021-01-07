4e3450af2fdad6f064b95bbe624a20125f2b67c6
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-07 14:43:13 +0100

    cleanup

--- statement-table/src/generic.rs
+++ statement-table/src/generic.rs
@@ -34,7 +34,7 @@ use parity_scale_codec::{Encode, Decode};
 
 /// Context for the statement table.
 pub trait Context {
-	/// A authority ID
+	/// An authority ID
 	type AuthorityId: Debug + Hash + Eq + Clone;
 	/// The digest (hash or other unique attribute) of a candidate.
 	type Digest: Debug + Hash + Eq + Clone;
@@ -62,18 +62,15 @@ pub trait Context {
 /// Statements circulated among peers.
 #[derive(PartialEq, Eq, Debug, Clone, Encode, Decode)]
 pub enum Statement<Candidate, Digest> {
-	/// Broadcast by an authority to indicate that this is his candidate for
-	/// inclusion.
+	/// Broadcast by an authority to indicate that this is its candidate for inclusion.
 	///
 	/// Broadcasting two different candidate messages per round is not allowed.
 	#[codec(index = "1")]
 	Candidate(Candidate),
-	/// Broadcast by a authority to attest that the candidate with given digest
-	/// is valid.
+	/// Broadcast by a authority to attest that the candidate with given digest is valid.
 	#[codec(index = "2")]
 	Valid(Digest),
-	/// Broadcast by a authority to attest that the candidate with given digest
-	/// is invalid.
+	/// Broadcast by a authority to attest that the candidate with given digest is invalid.
 	#[codec(index = "3")]
 	Invalid(Digest),
 }
@@ -145,7 +142,12 @@ pub enum Misbehavior<Candidate, Digest, AuthorityId, Signature> {
 }
 
 /// Type alias for misbehavior corresponding to context type.
-pub type MisbehaviorFor<Candidate> = Misbehavior<<Candidate as Context>::Candidate, <Candidate as Context>::Digest, <Candidate as Context>::AuthorityId, <Candidate as Context>::Signature>;
+pub type MisbehaviorFor<Ctx> = Misbehavior<
+	<Ctx as Context>::Candidate,
+	<Ctx as Context>::Digest,
+	<Ctx as Context>::AuthorityId,
+	<Ctx as Context>::Signature,
+>;
 
 // kinds of votes for validity
 #[derive(Clone, PartialEq, Eq)]
@@ -203,14 +205,14 @@ pub struct AttestedCandidate<Group, Candidate, AuthorityId, Signature> {
 }
 
 /// Stores votes and data about a candidate.
-pub struct CandidateData<Candidate: Context> {
-	group_id: Candidate::GroupId,
-	candidate: Candidate::Candidate,
-	validity_votes: HashMap<Candidate::AuthorityId, ValidityVote<Candidate::Signature>>,
-	indicated_bad_by: Vec<Candidate::AuthorityId>,
+pub struct CandidateData<Ctx: Context> {
+	group_id: Ctx::GroupId,
+	candidate: Ctx::Candidate,
+	validity_votes: HashMap<Ctx::AuthorityId, ValidityVote<Ctx::Signature>>,
+	indicated_bad_by: Vec<Ctx::AuthorityId>,
 }
 
-impl<Candidate: Context> CandidateData<Candidate> {
+impl<Ctx: Context> CandidateData<Ctx> {
 	/// whether this has been indicated bad by anyone.
 	pub fn indicated_bad(&self) -> bool {
 		!self.indicated_bad_by.is_empty()
@@ -220,7 +222,7 @@ impl<Candidate: Context> CandidateData<Candidate> {
 	/// If the candidate can be included, it will return `Some`.
 	pub fn attested(&self, validity_threshold: usize)
 		-> Option<AttestedCandidate<
-			Candidate::GroupId, Candidate::Candidate, Candidate::AuthorityId, Candidate::Signature,
+			Ctx::GroupId, Ctx::Candidate, Ctx::AuthorityId, Ctx::Signature,
 		>>
 	{
 		if self.can_be_included(validity_threshold) {
@@ -259,7 +261,7 @@ impl<Candidate: Context> CandidateData<Candidate> {
 		self.validity_votes.len() >= validity_threshold
 	}
 
-	fn summary(&self, digest: Candidate::Digest) -> Summary<Candidate::Digest, Candidate::GroupId> {
+	fn summary(&self, digest: Ctx::Digest) -> Summary<Ctx::Digest, Ctx::GroupId> {
 		Summary {
 			candidate: digest,
 			group_id: self.group_id.clone(),
@@ -270,11 +272,11 @@ impl<Candidate: Context> CandidateData<Candidate> {
 }
 
 // authority metadata
-struct AuthorityData<Candidate: Context> {
-	proposal: Option<(Candidate::Digest, Candidate::Signature)>,
+struct AuthorityData<Ctx: Context> {
+	proposal: Option<(Ctx::Digest, Ctx::Signature)>,
 }
 
-impl<Candidate: Context> Default for AuthorityData<Candidate> {
+impl<Ctx: Context> Default for AuthorityData<Ctx> {
 	fn default() -> Self {
 		AuthorityData {
 			proposal: None,
@@ -283,20 +285,20 @@ impl<Candidate: Context> Default for AuthorityData<Candidate> {
 }
 
 /// Type alias for the result of a statement import.
-pub type ImportResult<Candidate> = Result<
-	Option<Summary<<Candidate as Context>::Digest, <Candidate as Context>::GroupId>>,
-	MisbehaviorFor<Candidate>
+pub type ImportResult<Ctx> = Result<
+	Option<Summary<<Ctx as Context>::Digest, <Ctx as Context>::GroupId>>,
+	MisbehaviorFor<Ctx>
 >;
 
 /// Stores votes
-pub struct Table<Candidate: Context> {
-	authority_data: HashMap<Candidate::AuthorityId, AuthorityData<Candidate>>,
-	detected_misbehavior: HashMap<Candidate::AuthorityId, MisbehaviorFor<Candidate>>,
-	candidate_votes: HashMap<Candidate::Digest, CandidateData<Candidate>>,
-	includable_count: HashMap<Candidate::GroupId, usize>,
+pub struct Table<Ctx: Context> {
+	authority_data: HashMap<Ctx::AuthorityId, AuthorityData<Ctx>>,
+	detected_misbehavior: HashMap<Ctx::AuthorityId, MisbehaviorFor<Ctx>>,
+	candidate_votes: HashMap<Ctx::Digest, CandidateData<Ctx>>,
+	includable_count: HashMap<Ctx::GroupId, usize>,
 }
 
-impl<Candidate: Context> Default for Table<Candidate> {
+impl<Ctx: Context> Default for Table<Ctx> {
 	fn default() -> Self {
 		Table {
 			authority_data: HashMap::new(),
@@ -307,15 +309,15 @@ impl<Candidate: Context> Default for Table<Candidate> {
 	}
 }
 
-impl<Candidate: Context> Table<Candidate> {
+impl<Ctx: Context> Table<Ctx> {
 	/// Produce a set of proposed candidates.
 	///
 	/// This will be at most one per group, consisting of the
 	/// best candidate for each group with requisite votes for inclusion.
 	///
 	/// The vector is sorted in ascending order by group id.
-	pub fn proposed_candidates(&self, context: &Candidate) -> Vec<AttestedCandidate<
-		Candidate::GroupId, Candidate::Candidate, Candidate::AuthorityId, Candidate::Signature,
+	pub fn proposed_candidates(&self, context: &Ctx) -> Vec<AttestedCandidate<
+		Ctx::GroupId, Ctx::Candidate, Ctx::AuthorityId, Ctx::Signature,
 	>> {
 		use std::collections::BTreeMap;
 		use std::collections::btree_map::Entry as BTreeEntry;
@@ -354,7 +356,7 @@ impl<Candidate: Context> Table<Candidate> {
 	}
 
 	/// Whether a candidate can be included.
-	pub fn candidate_includable(&self, digest: &Candidate::Digest, context: &Candidate) -> bool {
+	pub fn candidate_includable(&self, digest: &Ctx::Digest, context: &Ctx) -> bool {
 		self.candidate_votes.get(digest).map_or(false, |data| {
 			let v_threshold = context.requisite_votes(&data.group_id);
 			data.can_be_included(v_threshold)
@@ -364,9 +366,9 @@ impl<Candidate: Context> Table<Candidate> {
 	/// Get the attested candidate for `digest`.
 	///
 	/// Returns `Some(_)` if the candidate exists and is includable.
-	pub fn attested_candidate(&self, digest: &Candidate::Digest, context: &Candidate)
+	pub fn attested_candidate(&self, digest: &Ctx::Digest, context: &Ctx)
 		-> Option<AttestedCandidate<
-			Candidate::GroupId, Candidate::Candidate, Candidate::AuthorityId, Candidate::Signature,
+			Ctx::GroupId, Ctx::Candidate, Ctx::AuthorityId, Ctx::Signature,
 		>>
 	{
 		self.candidate_votes.get(digest).and_then(|data| {
@@ -384,9 +386,9 @@ impl<Candidate: Context> Table<Candidate> {
 	/// If this returns `None`, the statement was either duplicate or invalid.
 	pub fn import_statement(
 		&mut self,
-		context: &Candidate,
-		statement: SignedStatement<Candidate::Candidate, Candidate::Digest, Candidate::AuthorityId, Candidate::Signature>,
-	) -> Option<Summary<Candidate::Digest, Candidate::GroupId>> {
+		context: &Ctx,
+		statement: SignedStatement<Ctx::Candidate, Ctx::Digest, Ctx::AuthorityId, Ctx::Signature>,
+	) -> Option<Summary<Ctx::Digest, Ctx::GroupId>> {
 		let SignedStatement { statement, signature, sender: signer } = statement;
 
 		let res = match statement {
@@ -422,13 +424,13 @@ impl<Candidate: Context> Table<Candidate> {
 	}
 
 	/// Get a candidate by digest.
-	pub fn get_candidate(&self, digest: &Candidate::Digest) -> Option<&Candidate::Candidate> {
+	pub fn get_candidate(&self, digest: &Ctx::Digest) -> Option<&Ctx::Candidate> {
 		self.candidate_votes.get(digest).map(|d| &d.candidate)
 	}
 
 	/// Access all witnessed misbehavior.
 	pub fn get_misbehavior(&self)
-		-> &HashMap<Candidate::AuthorityId, MisbehaviorFor<Candidate>>
+		-> &HashMap<Ctx::AuthorityId, MisbehaviorFor<Ctx>>
 	{
 		&self.detected_misbehavior
 	}
@@ -440,26 +442,26 @@ impl<Candidate: Context> Table<Candidate> {
 
 	fn import_candidate(
 		&mut self,
-		context: &Candidate,
-		from: Candidate::AuthorityId,
-		candidate: Candidate::Candidate,
-		signature: Candidate::Signature,
-	) -> ImportResult<Candidate> {
-		let group = Candidate::candidate_group(&candidate);
-		if !context.is_member_of(&from, &group) {
+		context: &Ctx,
+		authority: Ctx::AuthorityId,
+		candidate: Ctx::Candidate,
+		signature: Ctx::Signature,
+	) -> ImportResult<Ctx> {
+		let group = Ctx::candidate_group(&candidate);
+		if !context.is_member_of(&authority, &group) {
 			return Err(Misbehavior::UnauthorizedStatement(UnauthorizedStatement {
 				statement: SignedStatement {
 					signature,
 					statement: Statement::Candidate(candidate),
-					sender: from,
+					sender: authority,
 				},
 			}));
 		}
 
 		// check that authority hasn't already specified another candidate.
-		let digest = Candidate::candidate_digest(&candidate);
+		let digest = Ctx::candidate_digest(&candidate);
 
-		let new_proposal = match self.authority_data.entry(from.clone()) {
+		let new_proposal = match self.authority_data.entry(authority.clone()) {
 			Entry::Occupied(mut occ) => {
 				// if digest is different, fetch candidate and
 				// note misbehavior.
@@ -510,7 +512,7 @@ impl<Candidate: Context> Table<Candidate> {
 
 		self.validity_vote(
 			context,
-			from,
+			authority,
 			digest,
 			ValidityVote::Issued(signature),
 		)
@@ -518,11 +520,11 @@ impl<Candidate: Context> Table<Candidate> {
 
 	fn validity_vote(
 		&mut self,
-		context: &Candidate,
-		from: Candidate::AuthorityId,
-		digest: Candidate::Digest,
-		vote: ValidityVote<Candidate::Signature>,
-	) -> ImportResult<Candidate> {
+		context: &Ctx,
+		from: Ctx::AuthorityId,
+		digest: Ctx::Digest,
+		vote: ValidityVote<Ctx::Signature>,
+	) -> ImportResult<Ctx> {
 		let votes = match self.candidate_votes.get_mut(&digest) {
 			None => return Ok(None),
 			Some(votes) => votes,
