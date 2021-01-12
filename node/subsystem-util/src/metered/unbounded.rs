75220febfffb91906daf0babd500c8f5cddaa599
Bernhard Schuster <bernhard@ahoi.io>
2021-01-12 15:57:27 +0100

    Update node/subsystem-util/src/metered/unbounded.rs
    Co-authored-by: Andronik Ordian <write@reusable.software>

--- node/subsystem-util/src/metered/unbounded.rs
+++ node/subsystem-util/src/metered/unbounded.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Metered variant of bounded mpsc channels to be able to extract metrics.
+//! Metered variant of unbounded mpsc channels to be able to extract metrics.
 
 use super::*;
 
