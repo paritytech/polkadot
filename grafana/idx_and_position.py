4013f679e6d3dceb75a766f28309a5a0ef143b16
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-15 11:00:36 +0100

    never assign idx 0

--- grafana/idx_and_position.py
+++ grafana/idx_and_position.py
@@ -4,7 +4,7 @@ with open("grafana/dashboard.json") as dash_fp:
     dashboard = json.load(dash_fp)
 
 for (idx, panel) in enumerate(dashboard["panels"]):
-    panel["id"] = idx
+    panel["id"] = idx + 1
     panel["gridPos"]["y"] = idx * 8
 
 with open("grafana/dashboard.json", "w") as dash_fp:
