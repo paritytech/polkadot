0fb3d76e9546ef26d697078776abca88885b2477
Bastian Köcher <bkchr@users.noreply.github.com>
2021-01-26 23:56:48 +0100

    Rococo v1-1 (#2333)
    * Rococo V2
    
    * Do it again!
    
    * Rename

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -98,9 +98,9 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 /// Runtime version (Rococo).
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("rococo"),
-	impl_name: create_runtime_str!("parity-rococo-v1"),
+	impl_name: create_runtime_str!("parity-rococo-v1-1"),
 	authoring_version: 0,
-	spec_version: 19,
+	spec_version: 200,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
