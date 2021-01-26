21c3bdce7fd514aadbf233dfd416f81f279cc0fe
Robert Habermeier <rphmeier@gmail.com>
2021-01-25 19:50:35 -0500

    write a mock clock for testing

---
+++ node/core/approval-voting/src/tests.rs
@@ -0,0 +1,96 @@
+// Copyright 2021 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+use super::*;
+
+use parking_lot::Mutex;
+use std::pin::Pin;
+use std::sync::Arc;
+
+#[derive(Default)]
+struct MockClock {
+	inner: Arc<Mutex<MockClockInner>>,
+}
+
+impl Clock for MockClock {
+	fn tick_now(&self) -> Tick {
+		self.inner.lock().tick
+	}
+
+	fn wait(&self, tick: Tick) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
+		let rx = self.inner.lock().register_wakeup(tick, true);
+
+		Box::pin(async move {
+			rx.await.expect("i exist in a timeless void. yet, i remain");
+		})
+	}
+}
+
+// This mock clock allows us to manipulate the time and
+// be notified when wakeups have been triggered.
+#[derive(Default)]
+struct MockClockInner {
+	tick: Tick,
+	wakeups: Vec<(Tick, oneshot::Sender<()>)>,
+}
+
+impl MockClockInner {
+	fn set_tick(&mut self, tick: Tick) {
+		self.tick = tick;
+		self.wakeup_all(tick);
+	}
+
+	fn wakeup_all(&mut self, up_to: Tick) {
+		// This finds the position of the first wakeup after
+		// the given tick, or the end of the map.
+		let drain_up_to = self.wakeups.binary_search_by_key(
+			&(up_to + 1),
+			|w| w.0,
+		).unwrap_or_else(|i| i);
+
+		for (_, wakeup) in self.wakeups.drain(..drain_up_to) {
+			let _ = wakeup.send(());
+		}
+	}
+
+	// If `pre_emptive` is true, we compare the given tick to the internal
+	// tick of the clock for an early return.
+	//
+	// Otherwise, the wakeup will only trigger alongside another wakeup of
+	// equal or greater tick.
+	//
+	// When the pre-emptive wakeup is disabled, this can be used in combination with
+	// a preceding call to `set_tick` to wait until some other wakeup at that same tick
+	//  has been triggered.
+	fn register_wakeup(&mut self, tick: Tick, pre_emptive: bool) -> oneshot::Receiver<()> {
+		let (tx, rx) = oneshot::channel();
+
+		let pos = self.wakeups.binary_search_by_key(
+			&tick,
+			|w| w.0,
+		).unwrap_or_else(|i| i);
+
+		self.wakeups.insert(pos, (tick, tx));
+
+		if pre_emptive {
+			// if `tick > self.tick`, this won't wake up the new
+			// listener.
+			self.wakeup_all(self.tick);
+		}
+
+		rx
+	}
+}
