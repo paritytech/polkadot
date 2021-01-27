997141a8f284c55a322a927b6b418f5be5290d71
Martin Pugh <pugh@s3kr.it>
2021-01-27 14:08:15 +0100

    bump tx version for westend and test-runtime
    & bump substrate

--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -100,7 +100,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 1055,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
+	transaction_version: 2,
 };
 
 /// Native version.
