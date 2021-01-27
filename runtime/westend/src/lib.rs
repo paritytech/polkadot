231a5504fe4f4b729e787b5b2f203c9e30ecffab
Martin Pugh <pugh@s3kr.it>
2021-01-27 16:15:04 +0100

    Bump tx version for westend and test-runtime, bump substrate (#2337)
    * bump tx version for westend and test-runtime
    
    & bump substrate
    
    * Update runtime/test-runtime/src/lib.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -93,7 +93,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: version::create_apis_vec![[]],
-	transaction_version: 3,
+	transaction_version: 4,
 };
 
 /// Native version.
