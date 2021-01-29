a3ce46ac6691d099ff2ed99271d635efe620c1b8
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-01-29 13:22:06 +0100

    fix deterministic build test script

--- scripts/gitlab/test_deterministic_wasm.sh
+++ scripts/gitlab/test_deterministic_wasm.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 #shellcheck source=lib.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/lib.sh"
 
 # build runtime
 WASM_BUILD_NO_COLOR=1 cargo build --verbose --release -p runtime-kusama -p runtime-polkadot -p runtime-westend
 # make checksum
-sha256sum target/release/wbuild/*-runtime/target/wasm32-unknown-unknown/release/*.wasm > checksum.sha256
+sha256sum target/release/wbuild/runtime-*/target/wasm32-unknown-unknown/release/*.wasm > checksum.sha256
 # clean up - FIXME: can we reuse some of the artifacts?
 cargo clean
 # build again
