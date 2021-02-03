da4a605a0e6b48001c0e36fe2f8ee81a11b6aefe
Sergei Shulepov <sergei@parity.io>
2021-02-03 15:52:10 +0000

    Print para_id and candidate_hash upon receiving a collation

--- node/network/collator-protocol/src/validator_side.rs
+++ node/network/collator-protocol/src/validator_side.rs
@@ -386,6 +386,9 @@ where
 					tracing::debug!(
 						target: LOG_TARGET,
 						%request_id,
+						?para_id,
+						?relay_parent,
+						candidate_hash = ?receipt.hash(),
 						"Received collation",
 					);
 
