77ad3e498ef08beda0f37a40801e8bffb16655fc
Robert Klotzner <eskimor@users.noreply.github.com>
2021-02-03 10:25:53 +0100

    Update node/network/bridge/src/multiplexer.rs
    Remove redundant import.
    
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>

--- node/network/bridge/src/multiplexer.rs
+++ node/network/bridge/src/multiplexer.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::convert::From;
 use std::pin::Pin;
 use std::result::Result;
 
