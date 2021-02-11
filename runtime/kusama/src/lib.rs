57ea87e07a21742f3bffcac26de8dac1bdf03c22
shamb0 <r.raajey@gmail.com>
2021-02-11 14:11:31 +0530

    wk2106 subbounty integration updates

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -554,6 +554,7 @@ parameter_types! {
 	pub const MaximumReasonLength: u32 = 16384;
 	pub const BountyCuratorDeposit: Permill = Permill::from_percent(50);
 	pub const BountyValueMinimum: Balance = 2 * DOLLARS;
+	pub const MaxActiveSubBountyCount: u32 = 10;
 }
 
 type ApproveOrigin = EnsureOneOf<
@@ -587,6 +588,7 @@ impl pallet_bounties::Config for Runtime {
 	type BountyValueMinimum = BountyValueMinimum;
 	type DataDepositPerByte = DataDepositPerByte;
 	type MaximumReasonLength = MaximumReasonLength;
+	type MaxActiveSubBountyCount = MaxActiveSubBountyCount;
 	type WeightInfo = weights::pallet_bounties::WeightInfo<Runtime>;
 
 }
