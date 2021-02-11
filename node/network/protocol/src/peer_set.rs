9e0c4b88299ca63a02c0c439efa75bf4db2fc8bb
Sergei Shulepov <sergei@parity.io>
2021-02-11 14:04:29 +0000

    Introduce PerPeerSet utility that allows to segrate based on PeerSet

--- node/network/protocol/src/peer_set.rs
+++ node/network/protocol/src/peer_set.rs
@@ -17,7 +17,7 @@
 //! All peersets and protocols used for parachains.
 
 use sc_network::config::{NonDefaultSetConfig, SetConfig};
-use std::borrow::Cow;
+use std::{borrow::Cow, ops::{Index, IndexMut}};
 use strum::{EnumIter, IntoEnumIterator};
 
 /// The peer-sets and thus the protocols which are used for the network.
@@ -87,6 +87,32 @@ impl PeerSet {
 			_ => None,
 		}
 	}
+}
+
+/// A small and nifty collection that allows to store data pertaining to each peer set.
+#[derive(Debug, Default)]
+pub struct PerPeerSet<T> {
+	validation: T,
+	collation: T,
+}
+
+impl<T> Index<PeerSet> for PerPeerSet<T> {
+	type Output = T;
+	fn index(&self, index: PeerSet) -> &T {
+		match index {
+			PeerSet::Validation => &self.validation,
+			PeerSet::Collation => &self.collation,
+		}
+	}
+}
+
+impl<T> IndexMut<PeerSet> for PerPeerSet<T> {
+    fn index_mut(&mut self, index: PeerSet) -> &mut T {
+        match index {
+			PeerSet::Validation => &mut self.validation,
+			PeerSet::Collation => &mut self.collation,
+		}
+    }
 }
 
 /// Get `NonDefaultSetConfig`s for all available peer sets.
