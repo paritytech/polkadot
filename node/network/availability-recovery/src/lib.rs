21503479087ecb3af45084d7ba8fab47bcec4dc0
Robert Habermeier <rphmeier@gmail.com>
2021-02-17 12:31:28 -0600

    handle new messages

--- node/network/availability-recovery/src/lib.rs
+++ node/network/availability-recovery/src/lib.rs
@@ -662,6 +662,10 @@ async fn handle_network_update(
 						}
 					}
 				}
+				protocol_v1::AvailabilityRecoveryMessage::RequestFullData(_, _) |
+				protocol_v1::AvailabilityRecoveryMessage::FullData(_, _) => {
+					// handled in https://github.com/paritytech/polkadot/pull/2453
+				}
 			}
 		}
 		// We do not really need to track the peers' views in this subsystem
