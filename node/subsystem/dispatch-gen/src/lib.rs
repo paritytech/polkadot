1d586c4de594f960c800bc6a56b55d5d9b8c5cb1
Bernhard Schuster <bernhard@ahoi.io>
2021-02-24 16:58:09 +0100

    refactor

--- node/subsystem/dispatch-gen/src/lib.rs
+++ node/subsystem/dispatch-gen/src/lib.rs
@@ -1,11 +1,6 @@
 use proc_macro2::{Group, TokenStream, TokenTree};
 use quote::{quote, ToTokens};
-use syn::{
-	parse::ParseStream,
-	parse::{Parse, Parser},
-	punctuated::Punctuated,
-	Error, Fields, FieldsNamed, FieldsUnnamed, Ident, Result, Token, Type, Variant,
-};
+use syn::{parse2, Attribute, AttributeArgs, Error, TypePath, Meta, MetaList, Fields, FieldsNamed, FieldsUnnamed, Ident, ItemEnum, NestedMeta, Path, Result, Token, Type, Variant, parse::ParseStream, parse::{Parse, Parser}, punctuated::Punctuated};
 
 #[proc_macro_attribute]
 pub fn subsystem_dispatch_gen(attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -48,101 +43,93 @@ struct EnumVariantDispatch {
 	inner: Option<Type>,
 }
 
-impl Parse for EnumVariantDispatch {
-	fn parse(input: ParseStream) -> Result<Self> {
-		let variant = Variant::parse(input)?;
 
-		let skip = variant.attrs.iter().find(|attr| attr.path.is_ident("skip")).is_some();
+fn prepare_enum_variant(variant: Variant) -> Result<EnumVariantDispatch> {
+	let skip = variant.attrs.iter().find(|attr| attr.path.is_ident("skip")).is_some();
 
-		let inner =
-			match variant.fields {
-				// look for one called inner
-				Fields::Named(FieldsNamed { brace_token: _, named }) if !skip => named
-					.iter()
-					.filter(|field| {
-						if let Some(ident) = &field.ident {
-							ident.to_string() == "inner".to_owned()
-						} else {
-							false
-						}
-					})
-					.map(|field| Some(field.ty.clone()))
-					.next()
-					.ok_or_else(|| input.error("Missing field named `inner`"))?,
+	let inner =
+		match variant.fields {
+			// look for one called inner
+			Fields::Named(FieldsNamed { brace_token: _, named }) if !skip => named
+				.iter()
+				.filter(|field| {
+					if let Some(ident) = &field.ident {
+						ident.to_string() == "inner".to_owned()
+					} else {
+						false
+					}
+				})
+				.map(|field| Some(field.ty.clone()))
+				.next()
+				.unwrap(),
+				// .ok_or_else(|| input.error("Missing field named `inner`"))?,
 
-				// take the first one
-				Fields::Unnamed(FieldsUnnamed { paren_token: _, unnamed }) if !skip => unnamed
-					.first()
-					.map(|field| Some(field.ty.clone()))
-					.ok_or_else(|| input.error("Must at least have one inner type or be skipped"))?,
-				_ if skip => None,
-				_ => {
-					return Err(
-						input.error("Enum variant has either not skip and doesn't match anything, or somethin worse")
-					)
-				}
-			};
+			// take the first one
+			Fields::Unnamed(FieldsUnnamed { paren_token: _, unnamed }) if !skip => unnamed
+				.first()
+				.map(|field| Some(field.ty.clone())).unwrap(),
+				// .ok_or_else(|| input.error("Must at least have one inner type or be skipped"))?,
+			_ if skip => None,
+			_ => {
+				// return Err(
+				// 	input.error("Enum variant has either not skip and doesn't match anything, or somethin worse")
+				// )
+				unreachable!("fooo")
+			}
+		};
 
-		Ok(EnumVariantDispatch { name: variant.ident, inner })
-	}
-}
-
-fn extract_variants_inner_ty(group: Group) -> Result<Vec<EnumVariantDispatch>> {
-	let stream = group.stream();
-	dbg!(group);
-	let parser = Punctuated::<EnumVariantDispatch, Token![,]>::parse_terminated;
-	let variants: Vec<EnumVariantDispatch> =
-		parser.parse2(stream)?.into_iter().filter(|variant| variant.inner.is_some()).collect();
-
-	Ok(variants)
-}
-
-fn extract_inner_ty(stream: TokenStream) -> Result<TokenStream> {
-	Ok(stream)
+	Ok(EnumVariantDispatch { name: variant.ident, inner })
 }
 
 fn impl_subsystem_dispatch_gen2(attr: TokenStream, item: TokenStream) -> Result<proc_macro2::TokenStream> {
-	let mut attr = dbg!(attr.clone()).into_iter();
-	let event_ty = if let Some(TokenTree::Group(attr_group)) = attr.next() {
-		// includes the macro name
-		let mut attr = attr_group.stream().into_iter();
-		if let Some(x) = attr.next() {
-			dbg!(x);
-		}
-		let event_ty = if let Some(TokenTree::Group(attr_group)) = attr.next() {
-			attr_group.stream()
-		} else {
-			unreachable!("inner")
-		};
-		event_ty
-	} else {
-		unreachable!("outer")
-	};
+	let event_ty = dbg!(parse2::<Path>(dbg!(attr))?);
 
-	let mut iter = item.clone().into_iter();
-	match iter.next() {
-		Some(TokenTree::Ident(ident)) if ident.to_string() == "enum" => {}
-		Some(other) => return Err(Error::new(other.span(), "Only works on enums")),
-		None => unreachable!("z"),
-	}
-	let message_enum = match iter.next() {
-		Some(TokenTree::Ident(ident)) => ident,
-		Some(other) => return Err(Error::new(other.span(), "Expected an identifier as type name")),
-		None => unreachable!("f"),
-	};
+	// let attr = if attrs.len() != 1 {
+	// 	unreachable!("FIXME")
+	// } else {
+	// 	attrs.into_iter().next().expect("Just checked that this is true. qed")
+	// };
+	// let event_ty: Path = match attrs {
+	// 	NestedMeta::Lit(lit) => return Err(Error::new(lit.span(), "Requires exactly one type argument, not a literal")),
+	// 	NestedMeta::Meta(Meta::List(MetaList {
+	// 		path,
+	// 		paren_token: _,
+	// 		nested,
+	// 	})) => {
+	// 		if nested.len() != 2 {
+	// 			// return Err(Error::new(lit.span(), "Requires exactly one type argument"))
+	// 			unreachable!("ggggg")
+	// 		}
+	// 		let mut path = None;
+	// 		for item in nested {
+	// 			match dbg!(item) {
+	// 				NestedMeta::Lit(lit) => return Err(Error::new(lit.span(), "Requires exactly one type argument, not a literal")),
+	// 				NestedMeta::Meta(Meta::Path(path2)) => {
+	// 					path = Some(path2);
+	// 				}
+	// 				_ => unreachable!("foo_inner"),
+	// 			}
+	// 		}
+	// 		path.expect("Path is set at this point. qed")
+	// 	},
+	// 	_ => unreachable!("foo_outer"),
+	// };
 
-	let variants: Vec<_> = match iter.next() {
-		Some(TokenTree::Group(group)) => extract_variants_inner_ty(group)?
-			.into_iter()
-			.map(|variant| EnumVariantDispatchWithTy { ty: message_enum.clone(), variant })
-			.collect::<Vec<_>>(),
-		Some(other) => return Err(Error::new(other.span(), "Expected a group of items")),
-		None => unreachable!("yyyyyy"),
-	};
+	let ie = dbg!(parse2::<ItemEnum>(dbg!(item))?);
+
+	let message_enum = ie.ident;
+	let variants = ie.variants.into_iter()
+		.try_fold(Vec::new(), |mut acc: Vec<EnumVariantDispatchWithTy>, variant: Variant| {
+			let variant = prepare_enum_variant(variant)?;
+			if variant.inner.is_some() {
+				acc.push(EnumVariantDispatchWithTy { ty: message_enum.clone(), variant })
+			}
+			Ok::<_, syn::Error>(acc)
+		})?;
 
 	Ok(quote! {
 		impl #message_enum {
-			pub fn dispatch_iter(event: #event_ty) -> impl IntoIterator<Item=#message_enum> {
+			pub fn dispatch_iter(event: #event_ty) -> impl IntoIterator<Item=Self> {
 
 				let mut iter = None.into_iter();
 
@@ -151,7 +138,7 @@ fn impl_subsystem_dispatch_gen2(attr: TokenStream, item: TokenStream) -> Result<
 						#variants
 					})));
 
-				),*
+				)*
 				iter.filter_map(|x| x)
 			}
 		}
@@ -168,25 +155,31 @@ mod tests {
 
 	#[test]
 	fn pass() {
-		let attr = dbg!(quote! {
+		let attr = quote! {
 			NetEvent<foo::Bar>
-		});
+		};
 
-		let item = dbg!(quote! {
+		let item = quote! {
+			/// Documentation.
+			#[derive(Clone)]
 			enum AllMessages {
+
 				Sub1(Inner1),
 
 				#[skip]
+				/// D3
 				Sub3,
 
+				/// D4
 				#[skip]
 				Sub4(Inner2),
 
+				/// D2
 				Sub2(Inner2),
 			}
-		});
+		};
 
-		let output = dbg!(subsystem_dispatch_gen2(attr, item));
+		let output = dbg!(impl_subsystem_dispatch_gen2(attr, item).unwrap());
 		println!("//generated:");
 		println!("{}", output);
 	}
