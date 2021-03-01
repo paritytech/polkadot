c3f794874ed75ec89a3726b2c4e31f92f183c9ed
Robert Klotzner <robert.klotzner@gmx.at>
2021-03-01 20:47:42 +0100

    New tests compile.

--- node/network/availability-distribution/src/tests/mod.rs
+++ node/network/availability-distribution/src/tests/mod.rs
@@ -14,42 +14,20 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-
-use std::{collections::HashMap, sync::Arc, time::Duration};
-
 use futures::{executor, future, Future};
-use assert_matches::assert_matches;
-use maplit::hashmap;
-use smallvec::SmallVec;
 
-use sc_keystore::LocalKeystore;
-use sp_application_crypto::AppKey;
-use sp_keystore::{SyncCryptoStore, SyncCryptoStorePtr};
-use sp_keyring::Sr25519Keyring;
-use sc_network::PeerId;
+use sp_keystore::SyncCryptoStorePtr;
 
-
-use polkadot_erasure_coding::{branches, obtain_chunks_v1 as obtain_chunks};
-use polkadot_node_network_protocol::{view, ObservedRole, our_view};
-use polkadot_node_subsystem_util::TimeoutExt;
-use polkadot_node_jaeger as jaeger;
-use polkadot_primitives::v1::{AvailableData, BlockData, CandidateCommitments, CandidateDescriptor, CommittedCandidateReceipt, CoreState, GroupIndex, GroupRotationInfo, HeadData, Id as ParaId, OccupiedCore, PersistedValidationData, PoV, ScheduledCore, ValidatorId, ValidatorIndex, Hash};
-use polkadot_subsystem::messages::AllMessages;
-use polkadot_subsystem::ActiveLeavesUpdate;
 use polkadot_subsystem_testhelpers as test_helpers;
 
 use super::*;
 
-
 mod state;
 /// State for test harnesses.
-use state::TestState;
+use state::{TestState, TestHarness};
 
-
-struct TestHarness {
-	virtual_overseer: test_helpers::TestSubsystemContextHandle<AvailabilityDistributionMessage>,
-}
-
+/// Mock data useful for testing.
+pub(crate) mod mock;
 
 fn test_harness<T: Future<Output = ()>>(
 	keystore: SyncCryptoStorePtr,
@@ -64,7 +42,7 @@ fn test_harness<T: Future<Output = ()>>(
 	{
 		let subsystem = subsystem.run(context);
 
-		let test_fut = test_fx(TestHarness { virtual_overseer });
+		let test_fut = test_fx(TestHarness { virtual_overseer, pool });
 
 		futures::pin_mut!(test_fut);
 		futures::pin_mut!(subsystem);
@@ -73,106 +51,11 @@ fn test_harness<T: Future<Output = ()>>(
 	}
 }
 
-/// Tests
-///
-/// - No fetches from own backing group
-/// - Fetch succeeds as long as at least one validator can deliver the chunk.
-///
-/// Test with two backing groups, our own and another one which contains a couple of
-/// validators that fail in delivering the chunk for different reasons, except one. 
+/// Simple basic check, whether the subsystem works as expected.
 #[test]
-fn check_chunks_are_fetched() {
-	let test_state = TestState::default();
-
-	let peer_a = PeerId::random();
-	let peer_a_2 = peer_a.clone();
-	let peer_b = PeerId::random();
-	let peer_b_2 = peer_b.clone();
-	assert_ne!(&peer_a, &peer_b);
-
-	let keystore = test_state.keystore.clone();
-	let current = test_state.relay_parent;
-	let ancestors = test_state.ancestors.clone();
-
-	let state = test_harness(keystore, move |test_harness| async move {
-		let mut virtual_overseer = test_harness.virtual_overseer;
-
-		let TestState {
-			validator_public,
-			relay_parent: current,
-			ancestors,
-			candidates,
-			pov_blocks,
-			..
-		} = test_state.clone();
-
-		let genesis = Hash::repeat_byte(0x1);
-
-		send_active_leaves_update(&virtual_overseer, activate_leave(genesis)).await;
-
-
-		// setup peer a with interest in current
-		setup_peer_with_view(&mut virtual_overseer, peer_a.clone(), view![current]).await;
-
-		// setup peer b with interest in ancestor
-		setup_peer_with_view(&mut virtual_overseer, peer_b.clone(), view![ancestors[0]]).await;
+fn check_basic() {
+	let state = TestState::default();
+	test_harness(state.keystore.clone(), move |harness| {
+		state.run(harness)
 	});
-
-	assert_matches! {
-		state,
-		ProtocolState {
-			peer_views,
-			view,
-			..
-		} => {
-			assert_eq!(
-				peer_views,
-				hashmap! {
-					peer_a_2 => view![current],
-					peer_b_2 => view![ancestors[0]],
-				},
-			);
-			assert_eq!(view, our_view![current]);
-		}
-	};
 }
-
-
-
-/// Returns the number of chunks that the subsystem asked to store/fetched successfully.
-async fn send_active_leaves_update(
-	virtual_overseer: &mut test_helpers::TestSubsystemContextHandle<AvailabilityDistributionMessage>,
-	update: ActiveLeavesUpdate,
-	) -> u32 {
-		// Get fetcher started:
-		overseer_signal(
-			virtual_overseer,
-			OverseerSignal::ActiveLeaves(update)
-		).await;
-
-		let stored_chunks: u32 = 0;
-		// Handle requests:
-		loop {
-			match overseer_recv(virtual_overseer).await {
-				AllMessages::RuntimeApi(RuntimeApiMessage::Request(hash, RuntimeApiRequest::AvailabilityCores(sender)) => {
-				}
-			}
-		}
-}
-
-/// Create update with a single activated leaf.
-fn activate_leave(activated: Hash) -> ActiveLeavesUpdate {
-	ActiveLeavesUpdate {
-		activated: SmallVec::from[(activated, Arc::new(jaeger::Span::Disabled))],
-		deactivated: SmallVec::new(),
-	}
-}
-
-/// Create an update with a single deactivated leaf.
-fn deactivate_leave(deactivated: Hash) -> ActiveLeavesUpdate {
-	ActiveLeavesUpdate {
-		activated: SmallVec::new(),
-		deactivated: SmallVec::from([deactivated]),
-	}
-}
-
--- node/network/availability-distribution/src/tests/state.rs
+++ node/network/availability-distribution/src/tests/state.rs
@@ -14,15 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{collections::{HashMap, HashSet}, sync::Arc};
+use std::{collections::{HashMap, HashSet}, sync::Arc, time::Duration};
 
-use bitvec::vec::BitVec;
+use polkadot_node_subsystem_util::TimeoutExt;
 use polkadot_subsystem_testhelpers::TestSubsystemContextHandle;
-use smallvec::SmallVec;
+use smallvec::smallvec;
 
 use futures::{FutureExt, channel::oneshot};
 
-use polkadot_subsystem::{ActiveLeavesUpdate, OverseerSignal, messages::{AllMessages, AvailabilityDistributionMessage, AvailabilityStoreMessage, NetworkBridgeMessage, RuntimeApiMessage, RuntimeApiRequest}};
 use sc_keystore::LocalKeystore;
 use sp_application_crypto::AppKey;
 use sp_keystore::{SyncCryptoStore, SyncCryptoStorePtr};
@@ -31,12 +30,32 @@ use sp_core::{traits::SpawnNamed, testing::TaskExecutor};
 use sc_network as network;
 use sc_network::config as netconfig;
 
-use polkadot_erasure_coding::{branches, obtain_chunks_v1 as obtain_chunks};
-use polkadot_primitives::v1::{AvailableData, BlockData, CandidateCommitments, CandidateDescriptor, CandidateHash, CommittedCandidateReceipt, CoreState, ErasureChunk, GroupIndex, GroupRotationInfo, Hash, HeadData, Id as ParaId, OccupiedCore, PersistedValidationData, PoV, ScheduledCore, SessionInfo, ValidatorId, ValidatorIndex};
-use polkadot_node_network_protocol::{jaeger, request_response::{IncomingRequest, OutgoingRequest, Requests, v1}};
+use polkadot_subsystem::{ActiveLeavesUpdate, FromOverseer, OverseerSignal, messages::{AllMessages,
+	AvailabilityDistributionMessage, AvailabilityStoreMessage, NetworkBridgeMessage, RuntimeApiMessage,
+	RuntimeApiRequest}
+};
+use polkadot_primitives::v1::{CandidateHash, CoreState, ErasureChunk, GroupIndex, Hash, Id
+	as ParaId, ScheduledCore, SessionInfo, ValidatorId,
+	ValidatorIndex
+};
+use polkadot_node_network_protocol::{jaeger,
+	request_response::{IncomingRequest, OutgoingRequest, Requests, v1}
+};
+use polkadot_subsystem_testhelpers as test_helpers;
 
+use super::mock::{make_session_info, OccupiedCoreBuilder, };
 
+pub struct TestHarness {
+	pub virtual_overseer: test_helpers::TestSubsystemContextHandle<AvailabilityDistributionMessage>,
+	pub pool: TaskExecutor,
+}
 
+/// TestState for mocking execution of this subsystem.Clone
+///
+/// The `Default` instance provides data, which makes the system succeed by providing a couple of
+/// valid occupied cores. You can tune the data before calling `TestState::run`. E.g. modify some
+/// chunks to be invalid, the test will then still pass if you remove that chunk from
+/// `valid_chunks`.
 #[derive(Clone)]
 pub struct TestState {
 	// Simulated relay chain heads:
@@ -45,17 +64,9 @@ pub struct TestState {
 	/// All chunks that are valid and should be accepted.
 	pub valid_chunks: HashSet<(CandidateHash, ValidatorIndex)>,
 	pub session_info: SessionInfo,
-
-	pub chain_ids: Vec<ParaId>,
-	pub validators: Vec<Sr25519Keyring>,
-	pub validator_public: Vec<ValidatorId>,
-	pub validator_groups: (Vec<Vec<ValidatorIndex>>, GroupRotationInfo),
-	pub head_data: HashMap<ParaId, HeadData>,
+	/// Cores per relay chain block.
+	pub cores: HashMap<Hash, Vec<CoreState>>,
 	pub keystore: SyncCryptoStorePtr,
-	pub ancestors: Vec<Hash>,
-	pub persisted_validation_data: PersistedValidationData,
-	pub candidates: Vec<CommittedCandidateReceipt>,
-	pub pov_blocks: Vec<PoV>,
 }
 
 impl Default for TestState {
@@ -66,103 +77,73 @@ impl Default for TestState {
 
 		let chain_ids = vec![chain_a, chain_b];
 
-		let validators = vec![
-			Sr25519Keyring::Ferdie, // <- this node, role: validator
-			Sr25519Keyring::Alice,
-			Sr25519Keyring::Bob,
-			Sr25519Keyring::Charlie,
-			Sr25519Keyring::Dave,
-			Sr25519Keyring::Eve,
-			Sr25519Keyring::One,
-		];
-
 		let keystore: SyncCryptoStorePtr = Arc::new(LocalKeystore::in_memory());
+
+		let session_info = make_session_info();
 
 		SyncCryptoStore::sr25519_generate_new(
 			&*keystore,
 			ValidatorId::ID,
-			Some(&validators[0].to_seed()),
+			Some(&Sr25519Keyring::Ferdie.to_seed()),
 		)
 		.expect("Insert key into keystore");
 
-		let validator_public = validator_pubkeys(&validators);
+		let (cores, chunks) = {
+			let mut cores = HashMap::new();
+			let mut chunks = HashMap::new();
 
-		let validator_groups = vec![vec![5, 0], vec![1, 6], vec![3, 4, 2]]
-			.into_iter().map(|g| g.into_iter().map(ValidatorIndex).collect()).collect();
-		let group_rotation_info = GroupRotationInfo {
-			session_start_block: 0,
-			group_rotation_frequency: 100,
-			now: 1,
-		};
-		let validator_groups = (validator_groups, group_rotation_info);
+			cores.insert(relay_chain[0], 
+				vec![
+					CoreState::Scheduled(ScheduledCore {
+						para_id: chain_ids[0],
+						collator: None,
+					}),
+					CoreState::Scheduled(ScheduledCore {
+						para_id: chain_ids[1],
+						collator: None,
+					}),
+				]
+			);
 
-		let availability_cores = 
-		let mut head_data = HashMap::new();
-		head_data.insert(chain_a, HeadData(vec![4, 5, 6]));
-		head_data.insert(chain_b, HeadData(vec![7, 8, 9]));
-
-		let ancestors = vec![
-			Hash::repeat_byte(0x44),
-			Hash::repeat_byte(0x33),
-			Hash::repeat_byte(0x22),
-		];
-		let relay_parent = Hash::repeat_byte(0x05);
-
-		let persisted_validation_data = PersistedValidationData {
-			parent_head: HeadData(vec![7, 8, 9]),
-			relay_parent_number: Default::default(),
-			max_pov_size: 1024,
-			relay_parent_storage_root: Default::default(),
-		};
-
-		let pov_block_a = PoV {
-			block_data: BlockData(vec![42, 43, 44]),
-		};
-
-		let pov_block_b = PoV {
-			block_data: BlockData(vec![45, 46, 47]),
-		};
-
-		let candidates = vec![
-			TestCandidateBuilder {
-				para_id: chain_ids[0],
-				relay_parent: relay_parent,
-				pov_hash: pov_block_a.hash(),
-				erasure_root: make_erasure_root(persisted_validation_data.clone(), validators.len(), pov_block_a.clone()),
-				head_data: head_data.get(&chain_ids[0]).unwrap().clone(),
-				..Default::default()
+			let heads = relay_chain.iter().zip(relay_chain.iter().next());
+			for (relay_parent, relay_child) in heads {
+				let (p_cores, p_chunks): (Vec<_>, Vec<_>) = chain_ids.iter().enumerate()
+					.map(|(i, para_id)| {
+						let (core, chunk) = OccupiedCoreBuilder {
+							group_responsible: GroupIndex(i as _),
+							para_id: *para_id,
+							relay_parent: relay_parent.clone(),
+						}.build();
+						(CoreState::Occupied(core), chunk)
+					}
+					)
+					.unzip();
+				cores.insert(relay_child.clone(), p_cores);
+				for (validator_index, chunk) in p_chunks {
+					chunks.insert((validator_index, chunk.index), chunk);
+				}
 			}
-			.build(),
-			TestCandidateBuilder {
-				para_id: chain_ids[1],
-				relay_parent: relay_parent,
-				pov_hash: pov_block_b.hash(),
-				erasure_root: make_erasure_root(persisted_validation_data.clone(), validators.len(), pov_block_b.clone()),
-				head_data: head_data.get(&chain_ids[1]).unwrap().clone(),
-				..Default::default()
-			}
-			.build(),
-		];
-
-		let pov_blocks = vec![pov_block_a, pov_block_b];
-
+			(cores, chunks)
+		};
 		Self {
 			relay_chain,
-			chain_ids,
+			valid_chunks: chunks.clone().keys().map(Clone::clone).collect(),
+			chunks,
+			session_info,
+			cores,
 			keystore,
-			validators,
-			validator_public,
-			validator_groups,
-			head_data,
-			persisted_validation_data,
-			ancestors,
-			candidates,
-			pov_blocks,
 		}
 	}
 }
 
 impl TestState {
+	
+	/// Run, but fail after some timeout.
+	pub async fn run(self, harness: TestHarness) {
+		// Make sure test won't run forever.
+		let f = self.run_inner(harness.pool, harness.virtual_overseer).timeout(Duration::from_secs(2));
+		assert!(f.await.is_some(), "Test ran into timeout");
+	}
 
 	/// Run tests with the given mock values in `TestState`.
 	///
@@ -172,44 +153,55 @@ impl TestState {
 	/// - Fetches and stores valid chunks
 	/// - Delivers valid chunks
 	/// - Does not store invalid chunks.
-	pub async fn run(self, executor: TaskExecutor, virtual_overseer: &mut TestSubsystemContextHandle<AvailabilityDistributionMessage>) {
+	async fn run_inner(self, executor: TaskExecutor, mut virtual_overseer: TestSubsystemContextHandle<AvailabilityDistributionMessage>) {
 		// We skip genesis here (in reality ActiveLeavesUpdate can also skip a block:
-		let updates = self
+		let mut updates: Vec<_> = self
 			.relay_chain.iter().zip(self.relay_chain.iter().next())
 			.map(|(old, new)| ActiveLeavesUpdate {
-				activated: SmallVec::from[(new, Arc::new(jaeger::Span::Disabled))],
-				deactivated: SmallVec::from[old],
-			});
+				activated: smallvec![(new.clone(), Arc::new(jaeger::Span::Disabled))],
+				deactivated: smallvec![old.clone()],
+			}).collect();
+		updates.reverse();
 
-		for update in updates
+		// We should be storing all valid chunks during execution:
+		//
+		// Test will fail if this does not happen until timeout.
+		let mut remaining_stores = self.valid_chunks.len();
+
+		while remaining_stores > 0
 		{
-			// Advance subsystem by one block:
-			overseer_signal(
-				virtual_overseer,
-				OverseerSignal::ActiveLeaves(update)
-			).await;
-			let msg = overseer_recv(virtual_overseer).await;
+			if let Some(update) = updates.pop() {
+				// Advance subsystem by one block:
+				overseer_signal(
+					&mut virtual_overseer,
+					OverseerSignal::ActiveLeaves(update)
+				).await;
+			}
+			let msg = overseer_recv(&mut virtual_overseer).await;
 			match msg {
 				AllMessages::NetworkBridge(NetworkBridgeMessage::SendRequests(reqs)) => {
 					for req in reqs {
 						// Forward requests:
 						let in_req = to_incoming_req(&executor, req);
 						overseer_send(
-							virtual_overseer,
+							&mut virtual_overseer,
 							AvailabilityDistributionMessage::AvailabilityFetchingRequest(in_req)
 						).await;
 					}
 				}
 				AllMessages::AvailabilityStore(AvailabilityStoreMessage::QueryChunk(candidate_hash,	validator_index, tx)) => {
 					let chunk = self.chunks.get(&(candidate_hash, validator_index));
-					tx.send(chunk.map(Clone::clone));
+					tx.send(chunk.map(Clone::clone))
+						.expect("Receiver is expected to be alive");
 				}
 				AllMessages::AvailabilityStore(AvailabilityStoreMessage::StoreChunk{candidate_hash,	chunk, tx, ..}) => {
 					assert!(
 						self.valid_chunks.contains(&(candidate_hash, chunk.index)),
 						"Only valid chunks should ever get stored."
 					);
-					tx.send(Ok(()));
+					tx.send(Ok(()))
+						.expect("Receiver is expected to be alive");
+					remaining_stores -= 1;
 				}
 				AllMessages::RuntimeApi(RuntimeApiMessage::Request(hash, req)) => {
 					match req {
@@ -218,12 +210,12 @@ impl TestState {
 							tx.send(Ok(1))
 							.expect("Receiver should still be alive");
 						}
-						RuntimeApiRequest::SessionInfo(index, tx) => {
-							tx.send(Ok(Some(self.session_info)))
+						RuntimeApiRequest::SessionInfo(_, tx) => {
+							tx.send(Ok(Some(self.session_info.clone())))
 							.expect("Receiver should be alive.");
 						}
 						RuntimeApiRequest::AvailabilityCores(tx) => {
-							tx.send(Ok(self.get_cores(hash)))
+							tx.send(Ok(self.cores[&hash].clone()))
 							.expect("Receiver should still be alive");
 						}
 						_ => {
@@ -237,124 +229,9 @@ impl TestState {
 			}
 		}
 	}
-
-	/// Get test core states for a given relay parent.
-	fn get_cores(&self, hash: Hash) -> Vec<CoreState> {
-		if hash == Hash::repeat_byte(1u8) {
-			return vec![
-				CoreState::Scheduled(ScheduledCore {
-					para_id: self.chain_ids[0],
-					collator: None,
-				}),
-				CoreState::Scheduled(ScheduledCore {
-					para_id: self.chain_ids[1],
-					collator: None,
-				}),
-			]
-		}
-		// chain_id 0 becomes occupied:
-		if hash == Hash::repeat_byte(2u8) {
-			return vec![
-				CoreState::Occupied(OccupiedCoreBuilder{
-						group_responsible: GroupIndex(0),
-						candidate_hash:
-
-					}
-					.build()
-				),
-				CoreState::Scheduled(ScheduledCore {
-					para_id: self.chain_ids[1],
-					collator: None,
-				}),
-			];
-		}
-		// chain_id 1 becomes occupied:
-		if hash == Hash::repeat_byte(3u8) {
-			return vec![
-				CoreState::Occupied(OccupiedCoreBuilder {
-						group_responsible: GroupIndex(0),
-						candidate_hash:
-
-					}
-					.build()
-				),
-				CoreState::Scheduled(OccupiedCoreBuilder {
-					group_responsible: GroupIndex(0),
-					candidate_hash:
-				}),
-			];
-		}
-	}
 }
 
-#[derive(Default)]
-struct TestCandidateBuilder {
-	para_id: ParaId,
-	head_data: HeadData,
-	pov_hash: Hash,
-	relay_parent: Hash,
-	erasure_root: Hash,
-}
 
-impl TestCandidateBuilder {
-	fn build(self) -> CommittedCandidateReceipt {
-		CommittedCandidateReceipt {
-			descriptor: CandidateDescriptor {
-				para_id: self.para_id,
-				pov_hash: self.pov_hash,
-				relay_parent: self.relay_parent,
-				erasure_root: self.erasure_root,
-				..Default::default()
-			},
-			commitments: CandidateCommitments {
-				head_data: self.head_data,
-				..Default::default()
-			},
-		}
-	}
-}
-
-/// Builder for constructing occupied cores.
-///
-/// Takes all the values we care about and fills the rest with dummy values on `build`.
-struct OccupiedCoreBuilder {
-	group_responsible: GroupIndex,
-	candidate_hash: CandidateHash,
-	candidate_descriptor: CandidateDescriptor<Hash>,
-}
-
-impl OccupiedCoreBuilder {
-	fn build(self) -> OccupiedCore {
-		OccupiedCore {
-			next_up_on_available: None,
-			occupied_since: 0,
-			time_out_at: 0,
-			next_up_on_time_out: None,
-			availability: BitVec::new(),
-			group_responsible: self.group_responsible,
-			candidate_hash: self.candidate_hash,
-			candidate_descriptor: self.candidate_descriptor,
-		}
-	}
-}
-
-fn validator_pubkeys(val_ids: &[Sr25519Keyring]) -> Vec<ValidatorId> {
-	val_ids.iter().map(|v| v.public().into()).collect()
-}
-
-fn make_erasure_root(peristed: PersistedValidationData, validator_count: usize, pov: PoV) -> Hash {
-	let available_data = make_available_data(peristed, pov);
-
-	let chunks = obtain_chunks(validator_count, &available_data).unwrap();
-	branches(&chunks).root()
-}
-
-fn make_available_data(validation_data: PersistedValidationData, pov: PoV) -> AvailableData {
-	AvailableData {
-		validation_data,
-		pov: Arc::new(pov),
-	}
-}
 
 async fn overseer_signal(
 	overseer: &mut test_helpers::TestSubsystemContextHandle<AvailabilityDistributionMessage>,
@@ -395,7 +272,8 @@ fn to_incoming_req(
 			executor.spawn("Message forwarding", async {
 				let response = rx.await;
 				let payload = response.expect("Unexpected canceled request").result;
-				pending_response.send(payload.map_err(|_| network::RequestFailure::Refused));
+				pending_response.send(payload.map_err(|_| network::RequestFailure::Refused))
+					.expect("Sending response is expected to work");
 			}.boxed()
 			);
 
