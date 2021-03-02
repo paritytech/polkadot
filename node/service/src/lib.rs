c18c4721859f971f6413cb28e1ded21f23322c15
Tomasz Drwięga <tomasz@parity.io>
2021-03-02 14:48:07 +0100

    Fix telemetry_span issues.

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -678,7 +678,7 @@ pub fn new_full<RuntimeApi, Executor>(
 		remote_blockchain: None,
 		network_status_sinks: network_status_sinks.clone(),
 		system_rpc_tx,
-		telemetry_span: Some(telemetry_span),
+		telemetry_span: Some(telemetry_span.clone()),
 	})?;
 
 	let (block_import, link_half, babe_link) = import_setup;
@@ -999,7 +999,7 @@ fn new_light<Runtime, Dispatch>(mut config: Configuration) -> Result<(
 		network,
 		network_status_sinks,
 		system_rpc_tx,
-		telemetry_span: Some(telemetry_span),
+		telemetry_span: Some(telemetry_span.clone()),
 	})?;
 
 	network_starter.start_network();
