24d2cc2c0afda720abcc8abc6f2a75cb1d9da0c6
Martin Pugh <pugh@s3kr.it>
2021-03-03 18:30:10 +0100

    Revert "[release-branch bugfix] Bump substrate (#2399)"
    This reverts commit 99043f78c65119e9d121b5fc5af570d1942c6d60.

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -93,7 +93,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
 	spec_version: 28,
-	impl_version: 1,
+	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
