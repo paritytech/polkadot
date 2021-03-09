a6c637aeabc4f1556738fed8c82c84e4f197d730
Bastian Köcher <bkchr@users.noreply.github.com>
2021-03-09 20:43:31 +0100

    Substrate companion #8280 (#2578)
    * Substrate companion #8280
    
    https://github.com/paritytech/substrate/pull/8280
    
    * "Update Substrate"
    
    Co-authored-by: parity-processbot <>

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -843,6 +843,7 @@ pub fn new_full<RuntimeApi, Executor>(
 			backoff_authoring_blocks,
 			babe_link,
 			can_author_with,
+			block_proposal_slot_portion: babe::SlotProportion::new(2f32 / 3f32),
 		};
 
 		let babe = babe::start_babe(babe_config)?;
