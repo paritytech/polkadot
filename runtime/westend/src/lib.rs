c5a6cfbc4b96ed5b8f9276e855860f4e054982b7
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-03-10 10:12:24 +0100

    Resolve parantheses errors

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -796,7 +796,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllModules,
-	(BabeEpochConfigMigrations),
+	BabeEpochConfigMigrations,
 >;
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
