e4a5516fb5fe4e4f660e3aeee728868eef899536
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2021-03-13 19:51:22 +0100

    Apply suggestions from code review

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -315,7 +315,7 @@ parameter_types! {
 	pub const Fallback: pallet_election_provider_multi_phase::FallbackStrategy =
 		pallet_election_provider_multi_phase::FallbackStrategy::OnChain;
 
-	pub SolutionImprovementThreshold: Perbill = Perbill::from_rational_approximation(1u32, 10_000);
+	pub SolutionImprovementThreshold: Perbill = Perbill::from_rational_approximation(5u32, 10_000);
 
 	// miner configs
 	pub MultiPhaseUnsignedPriority: TransactionPriority = StakingUnsignedPriority::get() - 1u64;
