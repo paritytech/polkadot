d1e58a4722f149bf09d59b92b7541d7243be4101
Andronik Ordian <write@reusable.software>
2021-03-13 18:17:00 +0100

    cleanup and add futher TODOs

--- scripts/github/run_fuzzer.sh
+++ scripts/github/run_fuzzer.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-timeout --signal INT 20s cargo hfuzz run $1
+timeout --signal INT 5h cargo hfuzz run $1
 status=$?
 
 if [ $status -eq 0 ]; then
   echo "Found a panic!"
   # TODO: provide Minimal Reproducible Input
+  # TODO: message on Matrix
   exit 1
 else
   echo "Didn't find any problem in 5 hours of fuzzing"
