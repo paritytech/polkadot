e5e48d7fe71452ca185ef549cd548809d8d82683
Bastian Köcher <info@kchr.de>
2021-03-19 00:17:09 +0100

    Fix candidate timeout
    Candidates would not timeout when we are too far into a rotation. This
    pr fixes it by not checking how far we are into a rotation and instead
    only checking if `now - pending_since >= availability_period`.

--- runtime/parachains/src/inclusion_inherent.rs
+++ runtime/parachains/src/inclusion_inherent.rs
@@ -117,11 +117,7 @@ decl_module! {
 
 			// Handle timeouts for any availability core work.
 			let availability_pred = <scheduler::Module<T>>::availability_timeout_predicate();
-			let freed_timeout = if let Some(pred) = availability_pred {
-				<inclusion::Module<T>>::collect_pending(pred)
-			} else {
-				Vec::new()
-			};
+			let freed_timeout = <inclusion::Module<T>>::collect_pending(availability_pred);
 
 			// Schedule paras again, given freed cores, and reasons for freeing.
 			let freed = freed_concluded.into_iter().map(|c| (c, FreedReason::Concluded))
