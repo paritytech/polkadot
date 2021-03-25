f54fe41cc57a0200eea51a6fa9b76efc3e91e0c4
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-03-25 13:31:33 +0100

    Update Rococo v1 (#2705)
    * Fix Crowdloan Dissolve and Add Auction Cancel (#2665)
    
    * Check fund depositor calls dissolve
    
    * add auction cancel too
    
    * use drain api rather than `iter` + `take`
    
    * add test and benchmarks
    
    * cargo run --release --features=runtime-benchmarks -- benchmark --chain=westend-dev --steps=50 --repeat=20 --pallet=auctions --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./file_header.txt --output=./runtime/westend/src/weights/
    
    Co-authored-by: Parity Benchmarking Bot <admin@parity.io>
    
    * Allow Root to Edit a Crowdloan Configuration (#2666)
    
    * Allow Root to Edit a Crowdloan Configuration
    
    * Update crowdloan.rs
    
    * add test
    
    * cargo run --release --features=runtime-benchmarks -- benchmark --chain=westend-dev --steps=50 --repeat=20 --pallet=crowdloan --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./file_header.txt --output=./runtime/westend/src/weights/
    
    Co-authored-by: Parity Benchmarking Bot <admin@parity.io>
    
    * Sample Auction Winners Every `SampleLength` Blocks (#2670)
    
    * initial
    
    * add test
    
    * Update lib.rs
    
    * Update lib.rs
    
    * rococo: use insecure parent hash randomness (#2701)
    
    * bump spec +2
    
    * Fix check-labels.yml for forked repos (#2677)
    
    * fix check-labels.yml for forked repos
    
    * update labels
    
    Co-authored-by: Parity Benchmarking Bot <admin@parity.io>
    Co-authored-by: André Silva <123550+andresilva@users.noreply.github.com>
    Co-authored-by: Martin Pugh <pugh@s3kr.it>

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -103,7 +103,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("rococo"),
 	impl_name: create_runtime_str!("parity-rococo-v1.2"),
 	authoring_version: 0,
-	spec_version: 220,
+	spec_version: 222,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
@@ -612,33 +612,31 @@ impl paras_registrar::Config for Runtime {
 	type WeightInfo = paras_registrar::TestWeightInfo;
 }
 
-parameter_types! {
-	pub const EndingPeriod: BlockNumber = 15 * MINUTES;
+/// An insecure randomness beacon that uses the parent block hash as random material.
+///
+/// THIS SHOULD ONLY BE USED FOR TESTING PURPOSES.
+pub struct ParentHashRandomness;
+
+impl Randomness<Hash, BlockNumber> for ParentHashRandomness {
+	fn random(subject: &[u8]) -> (Hash, BlockNumber) {
+		(
+			(System::parent_hash(), subject).using_encoded(sp_io::hashing::blake2_256).into(),
+			System::block_number(),
+		)
+	}
 }
 
-// A wrapper around `babe::CurrentBlockRandomness` that does not return `Option<Random>`.
-pub struct CurrentBlockRandomness;
-
-impl Randomness<Hash, BlockNumber> for CurrentBlockRandomness {
-	fn random(subject: &[u8]) -> (Hash, BlockNumber) {
-		let (randomness, block_number) =
-			pallet_babe::CurrentBlockRandomness::<Runtime>::random(subject);
-
-		let randomness = randomness.expect(
-			"only returns None when secondary VRF slots are not enabled; \
-			 secondary VRF slots are enbaled for rococo runtime; \
-			 qed.",
-		);
-
-		(randomness, block_number)
-	}
+parameter_types! {
+	pub const EndingPeriod: BlockNumber = 15 * MINUTES;
+	pub const SampleLength: BlockNumber = 1;
 }
 
 impl auctions::Config for Runtime {
 	type Event = Event;
 	type Leaser = Slots;
 	type EndingPeriod = EndingPeriod;
-	type Randomness = CurrentBlockRandomness;
+	type SampleLength = SampleLength;
+	type Randomness = ParentHashRandomness;
 	type InitiateOrigin = EnsureRoot<AccountId>;
 	type WeightInfo = auctions::TestWeightInfo;
 }
