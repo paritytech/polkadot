ae1f9462d562933adf4393846ab8ce9b1d1c5a7b
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-03-27 10:12:49 +0100

    Restore the import 🤷‍♀️ I don't know why it compiles locally

--- node/network/bridge/src/validator_discovery.rs
+++ node/network/bridge/src/validator_discovery.rs
@@ -24,7 +24,7 @@ use std::sync::Arc;
 use async_trait::async_trait;
 use futures::channel::mpsc;
 
-use sc_network::{config::parse_addr, multiaddr::Multiaddr};
+use sc_network::{config::parse_addr, multiaddr::{Multiaddr, Protocol}};
 use sc_authority_discovery::Service as AuthorityDiscoveryService;
 use polkadot_node_network_protocol::PeerId;
 use polkadot_primitives::v1::{AuthorityDiscoveryId, Block, Hash};
