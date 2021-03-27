d86ab9d9c4aabec4f668446f4e4346688fabe8b9
Gav Wood <gavin@parity.io>
2021-03-27 18:59:13 +0100

    Fixes

--- xcm/xcm-builder/src/fungibles_adapter.rs
+++ xcm/xcm-builder/src/fungibles_adapter.rs
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use sp_std::{result, convert::TryFrom, marker::PhantomData, borrow::Borrow};
+use sp_std::{prelude::*, result, convert::TryFrom, marker::PhantomData, borrow::Borrow};
 use xcm::v0::{Error as XcmError, Result, MultiAsset, MultiLocation, Junction};
-use frame_support::traits::{Get, Fungibles};
+use frame_support::traits::{Get, tokens::fungibles::Mutate as Fungibles};
 use xcm_executor::traits::{LocationConversion, TransactAsset};
 
 /// Asset transaction errors.
