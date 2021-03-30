0c4cada410c96e479e886aec3fa7babaf3f3783d
Robert Klotzner <robert.klotzner@gmx.at>
2021-03-30 14:17:13 +0200

    Add stage as well.

--- node/network/availability-distribution/src/pov_requester/mod.rs
+++ node/network/availability-distribution/src/pov_requester/mod.rs
@@ -122,7 +122,8 @@ impl PoVRequester {
 		let span = jaeger::Span::new(candidate_hash, "fetch-pov")
 			.with_validator_index(from_validator)
 			.with_relay_parent(parent)
-			.with_candidate(candidate_hash);
+			.with_candidate(candidate_hash)
+			.with_stage(jaeger::Stage::PoVDistribution);
 		ctx.spawn("pov-fetcher", fetch_pov_job(pov_hash, pending_response.boxed(), span, tx).boxed())
 			.await
 			.map_err(|e| Error::SpawnTask(e))
