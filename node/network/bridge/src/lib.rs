7581e66dded92b787ae2e5e621f6c3f6c5dea587
Robert Habermeier <rphmeier@gmail.com>
2021-04-05 00:11:50 +0200

    Update node/network/bridge/src/lib.rs
    Co-authored-by: Andronik Ordian <write@reusable.software>

--- node/network/bridge/src/lib.rs
+++ node/network/bridge/src/lib.rs
@@ -213,7 +213,7 @@ impl metrics::Metrics for Metrics {
 			bytes_sent: prometheus::register(
 				prometheus::CounterVec::new(
 					prometheus::Opts::new(
-						"parachain_bytes_sent_total",
+						"parachain_notification_bytes_sent_total",
 						"The number of bytes sent on a parachain notification protocol",
 					),
 					&["protocol"]
