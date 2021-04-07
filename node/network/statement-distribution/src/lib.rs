095f78ed214f43f36f9e2504e31f9b98ed4d2067
Robert Klotzner <robert.klotzner@gmx.at>
2021-04-07 22:02:38 +0200

    Add lost copyright.

--- node/network/statement-distribution/src/lib.rs
+++ node/network/statement-distribution/src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
 // Polkadot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
