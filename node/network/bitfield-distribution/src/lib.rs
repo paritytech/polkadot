d83adb86ab7a7838b3a6a592955b3f6910eaf682
Andronik Ordian <write@reusable.software>
2021-04-09 23:11:47 +0200

    distribution: handle sqrt peer view updates

--- node/network/bitfield-distribution/src/lib.rs
+++ node/network/bitfield-distribution/src/lib.rs
@@ -599,7 +599,16 @@ where
 	Context: SubsystemContext<Message = BitfieldDistributionMessage>,
 {
 	let added = state.peer_views.entry(origin.clone()).or_default().replace_difference(view).cloned().collect::<Vec<_>>();
-
+	let lucky = util::gen_ratio_sqrt_subset(state.peer_views.len(), util::MIN_GOSSIP_PEERS);
+	if !lucky {
+		tracing::trace!(
+			target: LOG_TARGET,
+			?peerid,
+			?view,
+			"Peer view change is ignored",
+		);
+		return;
+	}
 	// Send all messages we've seen before and the peer is now interested
 	// in to that peer.
 
