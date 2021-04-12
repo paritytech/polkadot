c99fd07ba99887ef4ccb3a06407761cf3168149c
Denis P <denis.pisarev@parity.io>
2021-04-12 21:09:55 +0200

    mv

--- scripts/gitlab/trigger_pipeline.sh
+++ scripts/gitlab/trigger_pipeline.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # API trigger another projects' pipeline
-curl --silent
-    -X POST
-    -F token=${CI_JOB_TOKEN}
-    -F ref=master
-    -F "variables[TRGR_PROJECT]=${TRGR_PROJECT}"
-    -F "variables[TRGR_REF]=${TRGR_REF}"
-    -F "variables[IMAGE_NAME]=${IMAGE_NAME}"
-    -F "variables[IMAGE_TAG]=${IMAGE_TAG}"
-    -F "variables[COLLATOR_IMAGE_TAG]=${COLLATOR_IMAGE_TAG}"
-    "https://${CI_SERVER_HOST}/api/v4/projects/${DWNSTRM_ID}/trigger/pipeline" |
+curl --silent \
+    -X POST \
+    -F token=${CI_JOB_TOKEN} \
+    -F ref=master \
+    -F "variables[TRGR_PROJECT]=${TRGR_PROJECT}" \
+    -F "variables[TRGR_REF]=${TRGR_REF}" \
+    -F "variables[IMAGE_NAME]=${IMAGE_NAME}" \
+    -F "variables[IMAGE_TAG]=${IMAGE_TAG}" \
+    -F "variables[COLLATOR_IMAGE_TAG]=${COLLATOR_IMAGE_TAG}" \
+    "https://${CI_SERVER_HOST}/api/v4/projects/${DWNSTRM_ID}/trigger/pipeline" | \
         tee pipeline
 
 PIPELINE_ID=$(cat pipeline | jq ".id")
@@ -23,7 +23,10 @@ echo "Waiting on ${PIPELINE_ID} status..."
 # The timeout is 360 curls with 8 sec interval, roughly an hour.
 
 function get_status() {
-    curl --header "PRIVATE-TOKEN: ${TOKEN}" "https://${CI_SERVER_HOST}/api/v4/projects/${DWNSTRM_ID}/pipelines/${PIPELINE_ID}" | jq --raw-output ".status";
+    curl --silent \
+        --header "PRIVATE-TOKEN: ${TOKEN}" \
+        "https://${CI_SERVER_HOST}/api/v4/projects/${DWNSTRM_ID}/pipelines/${PIPELINE_ID}" | \
+            jq --raw-output ".status";
 }
 
 for i in $(seq 1 360); do
