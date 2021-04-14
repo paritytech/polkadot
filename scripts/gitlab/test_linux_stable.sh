a3808a427b2b17481510ba0416b21dee816d430e
Denis P <denis.pisarev@parity.io>
2021-04-14 20:48:21 +0200

    CI: workspace = all

--- scripts/gitlab/test_linux_stable.sh
+++ scripts/gitlab/test_linux_stable.sh
@@ -4,4 +4,4 @@ set -e
 #shellcheck source=../common/lib.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../common/lib.sh"
 
-time cargo test --all --release --verbose --locked --features=runtime-benchmarks
+time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks
