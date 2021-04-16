3f868ac25b655978fbfd125a5f5eabea53dd79d7
kianenigma <kian@parity.io>
2021-04-16 11:51:40 +0200

    Add missing types

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -883,6 +883,7 @@ impl pallet_membership::Config for Runtime {
 	type PrimeOrigin = EnsureRoot<AccountId>;
 	type MembershipInitialized = Collective;
 	type MembershipChanged = Collective;
+	type MaxMembers = MaxMembers;
 	type WeightInfo = ();
 }
 
