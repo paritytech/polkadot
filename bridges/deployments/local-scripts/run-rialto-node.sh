265eb8971052f990d87262950b5cc56d42834e94
Hernando Castano <hernando@hcastano.com>
2021-04-21 11:55:51 -0400

    Remove messed up bridges subtree

--- bridges/deployments/local-scripts/run-rialto-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run a development instance of the Rialto Substrate bridge node.
-
-RUST_LOG=runtime=trace \
-    ./target/debug/rialto-bridge-node --dev --tmp \
-    --rpc-cors=all --unsafe-rpc-external --unsafe-ws-external \
-    --port 33033 --rpc-port 9933 --ws-port 9944 \
