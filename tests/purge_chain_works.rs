f0bbe588e6af40f5e0a3571f8380ea20e441806d
hirschenberger <falco.hirschenberger@gmail.com>
2021-04-22 17:09:38 +0200

    fix test

--- tests/purge_chain_works.rs
+++ tests/purge_chain_works.rs
@@ -54,7 +54,7 @@ fn purge_chain_works() {
 		.unwrap();
 	assert!(status.success());
 
-	// Make sure that the `db` chain folder exists, but the `full` is deleted.
-	assert!(tmpdir.path().join("chains/dev/db").exists());
+	// Make sure that the chain folder exists, but `db/full` is deleted.
+	assert!(tmpdir.path().join("chains/dev").exists());
 	assert!(!tmpdir.path().join("chains/dev/db/full").exists());
 }
