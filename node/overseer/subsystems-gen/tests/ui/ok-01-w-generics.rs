65ff36bb60834f8cb6ce635218f8e8de42ca49b5
Bernhard Schuster <bernhard@ahoi.io>
2021-04-28 10:53:23 +0200

    feat: add proc macro to reduce boilerplate

---
+++ node/overseer/subsystems-gen/tests/ui/ok-01-w-generics.rs
@@ -0,0 +1,19 @@
+#![allow(dead_code)]
+
+use polkadot_procmacro_overseer_subsystems_gen::AllSubsystemsGen;
+
+#[derive(Clone, AllSubsystemsGen)]
+struct AllSubsystems<A> {
+	a: A,
+
+	#[sticky]
+	b: u16,
+}
+
+fn main() {
+	let all = AllSubsystems::<u8> {
+		a: 0u8,
+		b: 1u16,
+	};
+	let _all: AllSubsystems<u32> = all.replace_a::<u32>(777_777u32);
+}
