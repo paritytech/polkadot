965ff2d177293ebd8f50724bada41891855e29b9
Robert Klotzner <robert.klotzner@gmx.at>
2021-05-05 17:06:05 +0200

    Fix unused import.

--- node/network/collator-protocol/src/collator_side.rs
+++ node/network/collator-protocol/src/collator_side.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::collections::{HashMap, HashSet, hash_map::Entry};
+use std::collections::{HashMap, HashSet};
 
 use futures::{FutureExt, channel::oneshot, channel::mpsc};
 use sp_core::Pair;
