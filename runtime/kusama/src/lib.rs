51b4d78b2fe62dbebe54a8f65152160fe647d57d
Martin Pugh <martin@parity.io>
2021-05-11 15:27:52 +0200

    Bump version to v0.9.1 and spec_version to v9010 (#3001)
    * bump version and spec_version
    
    * fix generate_release_text.rb
    
    * Revert "fix generate_release_text.rb"
    
    This reverts commit 9e01e0ae4f6db2155d123f69063db7d7564588e0.
    
    * fix publish_draft_release
    
    * fix publish_draft_release
    
    * update target cache

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -115,7 +115,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 9000,
+	spec_version: 9010,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
