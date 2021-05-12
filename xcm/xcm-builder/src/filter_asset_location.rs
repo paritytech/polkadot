f4bf1211ed0c9db36e50fa5cefee9c615f14e3d3
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2021-05-12 11:34:56 +0200

    Update xcm/xcm-builder/src/filter_asset_location.rs
    Co-authored-by: Alexander Popiak <alexander.popiak@parity.io>

--- xcm/xcm-builder/src/filter_asset_location.rs
+++ xcm/xcm-builder/src/filter_asset_location.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! various implementations of `FilterAssetLocation`.
+//! Various implementations of `FilterAssetLocation`.
 
 use sp_std::marker::PhantomData;
 use xcm::v0::{MultiAsset, MultiLocation};
