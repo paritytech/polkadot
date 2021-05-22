ad4d9a137e998d540b6d4dd4d64b7f43a976a925
arkpar <arkady.paronyan@gmail.com>
2021-05-22 16:03:42 +0300

    Fixed build

--- node/core/chain-api/src/lib.rs
+++ node/core/chain-api/src/lib.rs
@@ -364,6 +364,7 @@ mod tests {
 				finalized_hash,
 				finalized_number,
 				number_leaves: 0,
+				finalized_state: None,
 			}
 		}
 		fn number(&self, hash: Hash) -> sp_blockchain::Result<Option<BlockNumber>> {
