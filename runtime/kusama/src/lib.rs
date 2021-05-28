f0ce0ca1f8b03d5ad3b80ce66793f18bde21cd14
Xiliang Chen <xlchen1291@gmail.com>
2021-05-28 20:52:32 +1200

    enable Config for Paras on Kusama & Westend (#3129)
    * enable Config for Paras
    
    * fix

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1452,7 +1452,7 @@ construct_runtime! {
 		ParasInclusion: parachains_inclusion::{Pallet, Call, Storage, Event<T>} = 53,
 		ParasInherent: parachains_paras_inherent::{Pallet, Call, Storage, Inherent} = 54,
 		ParasScheduler: parachains_scheduler::{Pallet, Call, Storage} = 55,
-		Paras: parachains_paras::{Pallet, Call, Storage, Event} = 56,
+		Paras: parachains_paras::{Pallet, Call, Storage, Event, Config<T>} = 56,
 		ParasInitializer: parachains_initializer::{Pallet, Call, Storage} = 57,
 		ParasDmp: parachains_dmp::{Pallet, Call, Storage} = 58,
 		ParasUmp: parachains_ump::{Pallet, Call, Storage} = 59,
