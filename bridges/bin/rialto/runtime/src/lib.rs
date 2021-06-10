0488104a4ee6c44ada7b00d59a702b775ce78e6c
koushiro <koushiro.cqx@gmail.com>
2021-06-10 14:38:25 +0800

    some nits
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- bridges/bin/rialto/runtime/src/lib.rs
+++ bridges/bin/rialto/runtime/src/lib.rs
@@ -211,6 +211,7 @@ impl frame_system::Config for Runtime {
 	/// The set code logic, just the default since we're not a parachain.
 	type OnSetCode = ();
 }
+
 impl pallet_randomness_collective_flip::Config for Runtime {}
 
 impl pallet_aura::Config for Runtime {
