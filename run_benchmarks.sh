277950dfb6e42d95737097c7f54b64662f4cc4c2
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-06-18 14:07:16 +0100

    Create run_benchmarks.sh

---
+++ run_benchmarks.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+steps=50
+repeat=20
+
+output=./runtime/polkadot/src/weights
+chain=polkadot-dev
+
+pallets=(
+	runtime_common::claims
+	pallet_balances
+	pallet_bounties
+	pallet_collective
+	pallet_democracy
+	pallet_elections_phragmen
+	pallet_election_provider_multi_phase
+	pallet_identity
+	pallet_im_online
+	pallet_indices
+	pallet_membership
+	pallet_multisig
+	pallet_offences
+	pallet_proxy
+	pallet_scheduler
+	pallet_session
+	pallet_staking
+	frame_system
+	pallet_timestamp
+	pallet_tips
+	pallet_treasury
+	pallet_utility
+	pallet_vesting
+)
+
+for p in ${pallets[@]}
+do
+	./target/release/polkadot benchmark \
+		--chain=$chain \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--pallet=$p  \
+		--extrinsic='*' \
+		--steps=$steps  \
+		--repeat=$repeat \
+		--heap-pages=4096 \
+		--header=./file_header.txt \
+		--output=$output
+
+done
