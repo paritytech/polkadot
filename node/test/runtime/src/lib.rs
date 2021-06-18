f84bf59dfed4ab1cef4a1c3c0d5931205e1f4775
Seun Lanlege <seun@parity.io>
2021-06-18 10:34:46 +0100

    switch branches

--- node/test/runtime/src/lib.rs
+++ node/test/runtime/src/lib.rs
@@ -388,37 +388,11 @@ pub fn dispatch_with_pallet_democracy<T>(call: <T::Runtime as frame_system::Conf
 #[cfg(test)]
 mod tests {
     use super::*;
-    use test_runner::NodeConfig;
-    use log::LevelFilter;
     use sp_runtime::{MultiSigner, traits::IdentifyAccount};
 
     #[test]
     fn test_runner() {
-        let config = NodeConfig {
-            log_targets: vec![
-                ("yamux", LevelFilter::Off),
-                ("multistream_select", LevelFilter::Off),
-                ("libp2p", LevelFilter::Off),
-                ("jsonrpc_client_transports", LevelFilter::Off),
-                ("sc_network", LevelFilter::Off),
-                ("tokio_reactor", LevelFilter::Off),
-                ("parity-db", LevelFilter::Off),
-                ("sub-libp2p", LevelFilter::Off),
-                ("sync", LevelFilter::Off),
-                ("peerset", LevelFilter::Off),
-                ("ws", LevelFilter::Off),
-                ("sc_network", LevelFilter::Off),
-                ("sc_service", LevelFilter::Off),
-                ("sc_basic_authorship", LevelFilter::Off),
-                ("telemetry-logger", LevelFilter::Off),
-                ("sc_peerset", LevelFilter::Off),
-                ("rpc", LevelFilter::Off),
-
-                ("runtime", LevelFilter::Trace),
-                ("babe", LevelFilter::Debug)
-            ],
-        };
-        let mut node = Node::<PolkadotChainInfo>::new(config).unwrap();
+        let mut node = Node::<PolkadotChainInfo>::new().unwrap();
         // seals blocks
         node.seal_blocks(1);
         // submit extrinsics
