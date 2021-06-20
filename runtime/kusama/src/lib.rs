5ce1893015706c6183aff7b530448dff6fc55b97
Andronik Ordian <write@reusable.software>
2021-06-20 18:15:13 +0200

    downgrade a warn log to debug (#3319)
    * downgrade to debug
    
    * another thick log
    
    * bump spec versions in kusama, polkadot and westend again IV

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 9050,
+	spec_version: 9051,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
