0fce17c4d08e8f6648dc04cc01110f50dd4ce063
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-06-21 16:58:35 +0100

    Update node/primitives/src/lib.rs

--- node/primitives/src/lib.rs
+++ node/primitives/src/lib.rs
@@ -48,8 +48,8 @@ pub use polkadot_parachain::primitives::BlockData;
 
 pub mod approval;
 
-/// The bomb limit for decompressing code blobs. (8x growth)
-pub const VALIDATION_CODE_BOMB_LIMIT: usize = (MAX_CODE_SIZE * 8u32) as usize;
+/// The bomb limit for decompressing code blobs.
+pub const VALIDATION_CODE_BOMB_LIMIT: usize = (MAX_CODE_SIZE * 4u32) as usize;
 
 /// The bomb limit for decompressing PoV blobs.
 pub const POV_BOMB_LIMIT: usize = (MAX_POV_SIZE * 4u32) as usize;
