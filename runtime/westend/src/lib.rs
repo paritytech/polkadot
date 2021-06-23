b19f7e1dcaae9a042615c2698d569b9809571704
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2021-06-23 17:16:22 +0200

    demo calculating an appropriate fee for the signed reward
    Unfortunately, this doesn't work: it needs to be a constant function,
    and AFAIK there's no way to make a trait method constant.

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -36,6 +36,7 @@ use runtime_common::{
 	impls::ToAuthor,
 	BlockHashCount, BlockWeights, BlockLength, RocksDbWeight,
 	OffchainSolutionWeightLimit, OffchainSolutionLengthLimit,
+	elections::fee_for_weight,
 };
 
 use runtime_parachains::origin as parachains_origin;
@@ -343,7 +344,11 @@ parameter_types! {
 	// A typical solution occupies 20kb. This formula is currently adjusted such that a typical
 	// solution will spend approximately equal amounts on the base and per-byte deposits.
 	pub const SignedDepositByte: Balance = deposit(1, 0) / (20 * 1024 * 1024);
-	pub const SignedRewardBase: Balance = 1 * CENTS;
+	pub const SignedRewardBase: Balance = fee_for_weight::<
+		Runtime,
+		crate::constants::fee::WeightToFee,
+		crate::weights::pallet_election_provider_multi_phase::WeightInfo<Runtime>,
+	>(Perbill::from_perthousand(3500));
 
 	// fallback: emergency phase.
 	pub const Fallback: pallet_election_provider_multi_phase::FallbackStrategy =
