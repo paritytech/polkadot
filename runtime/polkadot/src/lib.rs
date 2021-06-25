8a766cd67db822496c3290fa27f43beef76b9859
Martin Pugh <martin@parity.io>
2021-06-25 10:45:15 +0200

    bump versions & spec_versions

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -95,7 +95,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 9051,
+	spec_version: 9070,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
