77cce3e12eb4eb8be499a24f0b5eb46d78facbac
Andrew Jones <ascjones@gmail.com>
2021-07-06 10:13:25 +0100

    Add recursion_limit for runtime-parachains

--- runtime/parachains/src/lib.rs
+++ runtime/parachains/src/lib.rs
@@ -22,6 +22,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
+#![recursion_limit = "256"]
+
 pub mod configuration;
 pub mod shared;
 pub mod inclusion;
