7dd86d720744e414c9c20384acc6cb9a28e878c0
Bernhard Schuster <bernhard@ahoi.io>
2021-07-08 12:23:41 +0200

    more tabs

--- node/overseer/all-subsystems-gen/tests/ui/ok-01-w-generics.rs
+++ node/overseer/all-subsystems-gen/tests/ui/ok-01-w-generics.rs
@@ -4,14 +4,14 @@ use polkadot_overseer_all_subsystems_gen::AllSubsystemsGen;
 
 #[derive(Clone, AllSubsystemsGen)]
 struct AllSubsystems<A, B> {
-    a: A,
-    b: B,
+	a: A,
+	b: B,
 }
 
 fn main() {
-    let all = AllSubsystems::<u8, u16> {
-        a: 0u8,
-        b: 1u16,
-    };
-    let _all: AllSubsystems<_,_> = all.replace_a::<u32>(777_777u32);
+	let all = AllSubsystems::<u8, u16> {
+		a: 0u8,
+		b: 1u16,
+	};
+	let _all: AllSubsystems<_,_> = all.replace_a::<u32>(777_777u32);
 }
