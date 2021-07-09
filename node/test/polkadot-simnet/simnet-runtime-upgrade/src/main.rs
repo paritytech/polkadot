16344f9b3191f96a31999b4ee978f198c87d4de5
Seun Lanlege <seun@parity.io>
2021-07-09 08:58:38 +0100

    adds post upgrade test

--- node/test/polkadot-simnet/simnet-runtime-upgrade/src/main.rs
+++ node/test/polkadot-simnet/simnet-runtime-upgrade/src/main.rs
@@ -14,13 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 //! Attempts to upgrade the polkadot runtime, in a simnet environment
-use std::error::Error;
+use std::{error::Error, str::FromStr};
 
 use test_runner::{Node, client_parts, ConfigOrChainSpec, task_executor};
 use polkadot_runtime_test::{PolkadotChainInfo, dispatch_with_root};
 use sc_cli::{build_runtime, SubstrateCli, CliConfiguration, print_node_infos};
 use polkadot_cli::Cli;
 use structopt::StructOpt;
+use sp_core::crypto::AccountId32;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut tokio_runtime = build_runtime()?;
@@ -39,11 +40,25 @@ fn main() -> Result<(), Box<dyn Error>> {
         client_parts::<PolkadotChainInfo>(ConfigOrChainSpec::Config(config))?;
     let node = Node::<PolkadotChainInfo>::new(rpc, task_manager, client, pool, command_sink, backend);
 
-    // start runtime upgrade
     let wasm_binary = polkadot_runtime::WASM_BINARY
         .ok_or("Polkadot development wasm not available")?
         .to_vec();
-    tokio_runtime.block_on(dispatch_with_root(system::Call::set_code(wasm_binary).into(), &node))?;
+
+    tokio_runtime.block_on(async {
+        // start runtime upgrade
+        dispatch_with_root(system::Call::set_code(wasm_binary).into(), &node).await?;
+
+        let (from, dest, balance) = (
+            AccountId32::from_str("15j4dg5GzsL1bw2U2AWgeyAk6QTxq43V7ZPbXdAmbVLjvDCK")?,
+            AccountId32::from_str("1rvXMZpAj9nKLQkPFCymyH7Fg3ZyKJhJbrc7UtHbTVhJm1A")?,
+            10_000_000_000_000 // 10 dots
+        );
+
+        // post upgrade tests, a simple balance transfer
+        node.submit_extrinsic(balances::Call::transfer(dest.into(), balance), from).await?;
+
+        Ok::<_, Box<dyn Error>>(())
+    })?;
 
     // done upgrading runtime, drop node.
     drop(node);
