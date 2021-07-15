5c35143bf29ed4139a7fda1e51a88b46e77d0be9
Tomasz Drwięga <tomasz@parity.io>
2021-07-15 16:32:00 +0200

    Add version docs.

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -843,7 +843,19 @@ impl pallet_beefy_mmr::ParachainHeadsProvider for ParasProvider {
 }
 
 parameter_types! {
-	/// TODO [ToDr] Docs!
+	/// Version of the produced MMR leaf.
+	///
+	/// The version consists of two parts;
+	/// - `major` (3 bits)
+	/// - `minor` (5 bits)
+	///
+	/// `major` should be updated only if decoding the previous MMR Leaf format from the payload
+	/// is not possible (i.e. backward incompatible change).
+	/// `minor` should be updated if fields are added to the previous MMR Leaf, which given SCALE
+	/// encoding does not prevent old leafs from being decoded.
+	///
+	/// Hence we expect `major` to be changed really rarely (think never).
+	/// See [MmrLeafVersion] type documentation for more details.
 	pub LeafVersion: MmrLeafVersion = MmrLeafVersion::new(0, 0);
 }
 
