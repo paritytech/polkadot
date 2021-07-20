85f19a54bf2a757c5dcbcd0e6bf688aee20e8584
Bastian Köcher <info@kchr.de>
2021-07-20 11:25:49 +0200

    Update secp256k1 and remove unrequired usage

--- runtime/common/src/claims.rs
+++ runtime/common/src/claims.rs
@@ -618,19 +618,18 @@ impl<T: Config + Send + Sync> SignedExtension for PrevalidateAttests<T> where
 #[cfg(any(test, feature = "runtime-benchmarks"))]
 mod secp_utils {
 	use super::*;
-	use secp256k1;
 
-	pub fn public(secret: &secp256k1::SecretKey) -> secp256k1::PublicKey {
-		secp256k1::PublicKey::from_secret_key(secret)
+	pub fn public(secret: &libsecp256k1::SecretKey) -> libsecp256k1::PublicKey {
+		libsecp256k1::PublicKey::from_secret_key(secret)
 	}
-	pub fn eth(secret: &secp256k1::SecretKey) -> EthereumAddress {
+	pub fn eth(secret: &libsecp256k1::SecretKey) -> EthereumAddress {
 		let mut res = EthereumAddress::default();
 		res.0.copy_from_slice(&keccak_256(&public(secret).serialize()[1..65])[12..]);
 		res
 	}
-	pub fn sig<T: Config>(secret: &secp256k1::SecretKey, what: &[u8], extra: &[u8]) -> EcdsaSignature {
+	pub fn sig<T: Config>(secret: &libsecp256k1::SecretKey, what: &[u8], extra: &[u8]) -> EcdsaSignature {
 		let msg = keccak_256(&<super::Pallet<T>>::ethereum_signable_message(&to_ascii_hex(what)[..], extra));
-		let (sig, recovery_id) = secp256k1::sign(&secp256k1::Message::parse(&msg), secret);
+		let (sig, recovery_id) = libsecp256k1::sign(&libsecp256k1::Message::parse(&msg), secret);
 		let mut r = [0u8; 65];
 		r[0..64].copy_from_slice(&sig.serialize()[..]);
 		r[64] = recovery_id.serialize();
@@ -640,7 +639,6 @@ mod secp_utils {
 
 #[cfg(test)]
 mod tests {
-	use secp256k1;
 	use hex_literal::hex;
 	use super::*;
 	use secp_utils::*;
@@ -751,20 +749,20 @@ mod tests {
 		type WeightInfo = TestWeightInfo;
 	}
 
-	fn alice() -> secp256k1::SecretKey {
-		secp256k1::SecretKey::parse(&keccak_256(b"Alice")).unwrap()
+	fn alice() -> libsecp256k1::SecretKey {
+		libsecp256k1::SecretKey::parse(&keccak_256(b"Alice")).unwrap()
 	}
-	fn bob() -> secp256k1::SecretKey {
-		secp256k1::SecretKey::parse(&keccak_256(b"Bob")).unwrap()
+	fn bob() -> libsecp256k1::SecretKey {
+		libsecp256k1::SecretKey::parse(&keccak_256(b"Bob")).unwrap()
 	}
-	fn dave() -> secp256k1::SecretKey {
-		secp256k1::SecretKey::parse(&keccak_256(b"Dave")).unwrap()
+	fn dave() -> libsecp256k1::SecretKey {
+		libsecp256k1::SecretKey::parse(&keccak_256(b"Dave")).unwrap()
 	}
-	fn eve() -> secp256k1::SecretKey {
-		secp256k1::SecretKey::parse(&keccak_256(b"Eve")).unwrap()
+	fn eve() -> libsecp256k1::SecretKey {
+		libsecp256k1::SecretKey::parse(&keccak_256(b"Eve")).unwrap()
 	}
-	fn frank() -> secp256k1::SecretKey {
-		secp256k1::SecretKey::parse(&keccak_256(b"Frank")).unwrap()
+	fn frank() -> libsecp256k1::SecretKey {
+		libsecp256k1::SecretKey::parse(&keccak_256(b"Frank")).unwrap()
 	}
 
 	// This function basically just builds a genesis storage key/value store according to
