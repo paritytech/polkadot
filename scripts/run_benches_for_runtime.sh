385a5cc671a20a11b251c2bb53dc5e5b0808e52f
Martin Pugh <martin@parity.io>
2021-07-23 15:18:57 +0200

    fix chevdor comments

--- scripts/run_benches_for_runtime.sh
+++ scripts/run_benches_for_runtime.sh
@@ -5,13 +5,16 @@
 # current reference machine: https://github.com/paritytech/substrate/pull/5848
 
 runtime="$1"
+standard_args="--release --locked --features=runtime-benchmarks"
 
 echo "[+] Running all benchmarks for $runtime"
 
-cargo +nightly run --release --features=runtime-benchmarks --locked benchmark --chain "${runtime}-dev" --execution=wasm --wasm-execution=compiled --pallet "*" --extrinsic "*" --repeat 0 | sed -r -e 's/Pallet: "([a-z_:]+)".*/\1/' | uniq | grep -v frame_system > "${runtime}_pallets"
+# shellcheck disable=SC2086
+cargo +nightly run $standard_args benchmark --chain "${runtime}-dev" --execution=wasm --wasm-execution=compiled --pallet "*" --extrinsic "*" --repeat 0 | sed -r -e 's/Pallet: "([a-z_:]+)".*/\1/' | uniq | grep -v frame_system > "${runtime}_pallets"
 while read -r line; do
   pallet="$(echo "$line" | cut -d' ' -f1)";
   echo "Runtime: $runtime. Pallet: $pallet";
-  cargo +nightly run --release --features=runtime-benchmarks -- benchmark --chain="${runtime}-dev" --steps=50 --repeat=20 --pallet="$pallet" --extrinsic="*" --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./file_header.txt --output="./runtime/${runtime}/src/weights/${pallet/::/_}.rs"
+# shellcheck disable=SC2086
+  cargo +nightly run $standard_args -- benchmark --chain="${runtime}-dev" --steps=50 --repeat=20 --pallet="$pallet" --extrinsic="*" --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./file_header.txt --output="./runtime/${runtime}/src/weights/${pallet/::/_}.rs"
 done < "${runtime}_pallets"
 rm "${runtime}_pallets"
