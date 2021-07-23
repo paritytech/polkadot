a96e7f7eb447cd624c05b26a3f4df581138e69e5
ferrell-code <automatedcharlesferrell@gmail.com>
2021-07-23 17:11:28 -0400

    genesis build correctly

--- runtime/parachains/src/configuration.rs
+++ runtime/parachains/src/configuration.rs
@@ -307,6 +307,7 @@ pub mod pallet {
 	#[pallet::genesis_build]
 	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
 		fn build(&self) {
+			ActiveConfig::<T>::put(&self.config);
 			self.config.check_consistency();
 		}
 	}
