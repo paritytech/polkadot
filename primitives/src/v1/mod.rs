4dec4e9964c5ec24bb1d453780d3613d79c13f62
Andrew Jones <ascjones@gmail.com>
2021-07-29 13:02:28 +0100

    Add missing TypeInfo impl

--- primitives/src/v1/mod.rs
+++ primitives/src/v1/mod.rs
@@ -1203,7 +1203,7 @@ pub struct DisputeStatementSet {
 pub type MultiDisputeStatementSet = Vec<DisputeStatementSet>;
 
 /// The entire state of a dispute.
-#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq)]
+#[derive(Encode, Decode, Clone, RuntimeDebug, PartialEq, TypeInfo)]
 pub struct DisputeState<N = BlockNumber> {
 	/// A bitfield indicating all validators for the candidate.
 	pub validators_for: BitVec<bitvec::order::Lsb0, u8>, // one bit per validator.
