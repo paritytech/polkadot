4767814e3d6eb8f95f2673dbe3802034d0858124
Squirrel <giles.cope@gmail.com>
2021-07-30 15:02:43 +0100

    Companion for substrate #9319 (#3456)
    * move client consensus code out of primitives
    
    * merging crates
    
    * import tweak
    
    * Fixing build: ServiceFactory is a word...
    
    * updating lock file
    
    * Fixed typo in error message
    
    (to bump build)
    
    * update Substrate
    
    Co-authored-by: parity-processbot <>

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -93,10 +93,10 @@ pub use polkadot_client::{
 	RuntimeApiCollection,
 };
 pub use chain_spec::{PolkadotChainSpec, KusamaChainSpec, WestendChainSpec, RococoChainSpec};
-pub use consensus_common::{Proposal, SelectChain, BlockImport, block_validation::Chain};
+pub use consensus_common::{Proposal, SelectChain, block_validation::Chain};
 pub use polkadot_primitives::v1::{Block, BlockId, CollatorPair, Hash, Id as ParaId};
 pub use sc_client_api::{Backend, ExecutionStrategy, CallExecutor};
-pub use sc_consensus::LongestChain;
+pub use sc_consensus::{BlockImport, LongestChain};
 pub use sc_executor::NativeExecutionDispatch;
 pub use service::{
 	Role, PruningMode, TransactionPoolOptions, Error as SubstrateServiceError, RuntimeGenesis,
@@ -324,7 +324,7 @@ fn new_partial<RuntimeApi, Executor>(
 ) -> Result<
 	service::PartialComponents<
 		FullClient<RuntimeApi, Executor>, FullBackend, FullSelectChain,
-		consensus_common::DefaultImportQueue<Block, FullClient<RuntimeApi, Executor>>,
+		sc_consensus::DefaultImportQueue<Block, FullClient<RuntimeApi, Executor>>,
 		sc_transaction_pool::FullPool<Block, FullClient<RuntimeApi, Executor>>,
 		(
 			impl Fn(
@@ -1214,7 +1214,7 @@ pub fn new_chain_ops(
 	(
 		Arc<Client>,
 		Arc<FullBackend>,
-		consensus_common::import_queue::BasicQueue<Block, PrefixedMemoryDB<BlakeTwo256>>,
+		sc_consensus::BasicQueue<Block, PrefixedMemoryDB<BlakeTwo256>>,
 		TaskManager,
 	),
 	Error
