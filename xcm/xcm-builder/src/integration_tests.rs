29b7b0434255784fbcdebde4ccbd4c93a8d813f4
Alexander Popiak <alexander.popiak@parity.io>
2021-07-31 14:06:37 +0200

    add an integration test for reserve_transfer_assets

--- xcm/xcm-builder/src/integration_tests.rs
+++ xcm/xcm-builder/src/integration_tests.rs
@@ -1,4 +1,4 @@
-use frame_support::{construct_runtime, parameter_types, traits::{All, AllowAll}, weights::Weight};
+use frame_support::{assert_ok, construct_runtime, parameter_types, traits::{All, AllowAll}, weights::Weight};
 use sp_core::H256;
 use sp_runtime::{testing::Header, traits::IdentityLookup, AccountId32};
 use sp_runtime::traits::AccountIdConversion;
@@ -251,5 +251,44 @@ fn withdraw_and_deposit_works() {
 		assert_eq!(r, Outcome::Complete(3_000));
 		let other_para_acc: AccountId = ParaId::from(other_para_id).into_account();
 		assert_eq!(Balances::free_balance(other_para_acc), amount);
+	});
+}
+
+#[test]
+fn reserve_transfer_assets_works() {
+	use xcm::v0::{Xcm, Junction};
+	use xcm::opaque::v0::prelude::*;
+	use MultiLocation::*;
+
+	kusama_like_ext().execute_with(|| {
+		let amount = 5_000_000;
+		let dest_weight = 2_000_000;
+		assert_ok!(XcmPallet::reserve_transfer_assets(
+			Origin::signed(ALICE),
+			Parachain(PARA_ID).into(),
+			Junction::AccountId32 { network: NetworkId::Kusama, id: ALICE.into() }.into(),
+			vec![ConcreteFungible { id: Null, amount }],
+			dest_weight
+		));
+
+		assert_eq!(Balances::free_balance(ALICE), INITIAL_BALANCE - amount);
+		let para_acc: AccountId = ParaId::from(PARA_ID).into_account();
+		assert_eq!(Balances::free_balance(para_acc), INITIAL_BALANCE + amount);
+		assert_eq!(
+			crate::mock::sent_xcm(),
+			vec![(Parachain(PARA_ID).into(), Xcm::ReserveAssetDeposit {
+				assets: vec![ConcreteFungible { id: Parent.into(), amount }],
+				effects: vec![
+					BuyExecution {
+						fees: All,
+						weight: 0,
+						debt: dest_weight,
+						halt_on_error: false,
+						xcm: vec![],
+					},
+					DepositAsset { assets: vec![ All ], dest: Junction::AccountId32 { network: NetworkId::Kusama, id: ALICE.into() }.into() },
+				]
+			})]
+		);
 	});
 }
