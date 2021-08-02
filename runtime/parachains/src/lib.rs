03c1a50bb85080b3fcfe7c2e288b2f4ec4c7f8d4
Andrew Jones <ascjones@gmail.com>
2021-08-02 17:38:48 +0100

    Remove recursion limit

--- runtime/parachains/src/lib.rs
+++ runtime/parachains/src/lib.rs
@@ -21,8 +21,6 @@
 //! of the other modules.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
-#![recursion_limit = "256"]
 
 pub mod configuration;
 pub mod disputes;
