39afc030bcbbaaec4078abea89699f9fd5a6600d
Bastian Köcher <info@kchr.de>
2021-08-02 12:10:13 +0200

    :facepalm:

--- node/test/service/src/chain_spec.rs
+++ node/test/service/src/chain_spec.rs
@@ -174,7 +174,7 @@ fn polkadot_testnet_genesis(
 		},
 		vesting: runtime::VestingConfig { vesting: vec![] },
 		sudo: runtime::SudoConfig { key: root_key },
-		configuration: runtime::ParachainsConfigurationConfig {
+		configuration: runtime::ConfigurationConfig {
 			config: polkadot_runtime_parachains::configuration::HostConfiguration {
 				validation_upgrade_frequency: 10u32,
 				validation_upgrade_delay: 5,
