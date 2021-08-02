e70b2a3a3e961898b7abe76d3662519d00ed535a
Andrew Jones <ascjones@gmail.com>
2021-08-02 17:38:33 +0100

    More call variant structs

--- node/test/service/tests/call-function.rs
+++ node/test/service/tests/call-function.rs
@@ -22,10 +22,10 @@ use sp_keyring::Sr25519Keyring::{Alice, Bob};
 async fn call_function_actually_work(task_executor: TaskExecutor) {
 	let alice = run_validator_node(task_executor, Alice, || {}, Vec::new(), None);
 
-	let function = polkadot_test_runtime::Call::Balances(pallet_balances::Call::transfer(
-		Default::default(),
-		1,
-	));
+	let function = polkadot_test_runtime::Call::Balances(pallet_balances::Call::transfer {
+		dest: Default::default(),
+		value: 1,
+	});
 	let output = alice.send_extrinsic(function, Bob).await.unwrap();
 
 	let res = output.result.expect("return value expected");
