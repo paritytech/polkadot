7727c4b09d235dfe0c68d7342404ccf0ddeefa32
Keith Yeung <kungfukeith11@gmail.com>
2021-08-08 07:56:48 -0700

    Fix merge

--- runtime/test-runtime/src/xcm_config.rs
+++ runtime/test-runtime/src/xcm_config.rs
@@ -16,8 +16,8 @@
 
 use frame_support::{parameter_types, traits::Everything, weights::Weight};
 use xcm::latest::{
-	Error as XcmError, Junction::*, MultiAsset, MultiLocation, MultiLocation::*, NetworkId,
-	Result as XcmResult, SendXcm, Xcm,
+	Error as XcmError, Junction::*, MultiAsset, MultiLocation, NetworkId, Result as XcmResult,
+	SendXcm, Xcm,
 };
 use xcm_builder::{AllowUnpaidExecutionFrom, FixedWeightBounds, SignedToAccountId32};
 use xcm_executor::{
@@ -52,7 +52,7 @@ impl TransactAsset for DummyAssetTransactor {
 	}
 
 	fn withdraw_asset(_what: &MultiAsset, _who: &MultiLocation) -> Result<Assets, XcmError> {
-		let asset: MultiAsset = (X1(Parent), 100_000).into();
+		let asset: MultiAsset = (MultiLocation::parent(), 100_000).into();
 		Ok(asset.into())
 	}
 }
