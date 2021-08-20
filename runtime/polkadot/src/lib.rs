026cf2f82aed01b012dafdd9cd46ca444ee4e5b4
thiolliere <gui.thiolliere@gmail.com>
2021-08-20 13:19:13 +0200

    add description for technical committee

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -1448,6 +1448,7 @@ sp_api::impl_runtime_apis! {
 			list_benchmark!(list, extra, pallet_balances, Balances);
 			list_benchmark!(list, extra, pallet_bounties, Bounties);
 			list_benchmark!(list, extra, pallet_collective, Council);
+			list_benchmark!(list, extra, pallet_collective, TechnicalCommittee);
 			list_benchmark!(list, extra, pallet_democracy, Democracy);
 			list_benchmark!(list, extra, pallet_elections_phragmen, PhragmenElection);
 			list_benchmark!(list, extra, pallet_election_provider_multi_phase, ElectionProviderMultiPhase);
