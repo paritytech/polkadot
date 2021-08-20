9f6d1bb01ddd09a0f1d855e0265e8d0666e7ebca
Robert Habermeier <rphmeier@gmail.com>
2021-08-20 19:52:24 +0200

    remove trait implementation and fix errors

--- node/service/src/relay_chain_selection.rs
+++ node/service/src/relay_chain_selection.rs
@@ -182,7 +182,7 @@ where
 			self.fallback.finality_target(target_hash, maybe_max_number).await?;
 
 		if self.selection.overseer.is_disconnected() {
-			return longest_chain_best
+			return Ok(longest_chain_best)
 		}
 		self.selection
 			.finality_target_with_fallback(target_hash, longest_chain_best, maybe_max_number)
@@ -273,8 +273,7 @@ impl OverseerHandleT for Handle {
 	}
 }
 
-#[async_trait::async_trait]
-impl<B, OH> SelectChain<PolkadotBlock> for SelectRelayChain<B, OH>
+impl<B, OH> SelectRelayChain<B, OH>
 where
 	B: HeaderProviderProvider<PolkadotBlock>,
 	OH: OverseerHandleT,
@@ -309,20 +308,6 @@ where
 		self.block_header(best_leaf)
 	}
 
-	async fn finality_target(
-		&self,
-		_target_hash: Hash,
-		_maybe_max_number: Option<BlockNumber>,
-	) -> Result<Option<Hash>, ConsensusError> {
-		unreachable!("Must call finality_target_with_fallback instead. qed");
-	}
-}
-
-impl<B, OH> SelectRelayChain<B, OH>
-where
-	B: HeaderProviderProvider<PolkadotBlock>,
-	OH: OverseerHandleT,
-{
 	/// Get the best descendant of `target_hash` that we should attempt to
 	/// finalize next, if any. It is valid to return the `target_hash` if
 	/// no better block exists.
@@ -457,7 +442,7 @@ where
 					std::any::type_name::<Self>(),
 				)
 				.await;
-			let (subchain_number, subchain_head) = rx
+			let (subchain_number, _subchain_head) = rx
 				.await
 				.map_err(Error::OverseerDisconnected)
 				.map_err(|e| ConsensusError::Other(Box::new(e)))?
