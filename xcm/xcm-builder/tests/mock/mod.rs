8e25edc0f55dd40fc2a92339dbe3c8ea773b2453
Gav Wood <gavin@parity.io>
2021-08-27 15:26:04 -0500

    Fixes

--- xcm/xcm-builder/tests/mock/mod.rs
+++ xcm/xcm-builder/tests/mock/mod.rs
@@ -167,6 +167,8 @@ impl xcm_executor::Config for XcmConfig {
 	type Weigher = FixedWeightBounds<BaseXcmWeight, Call, MaxInstructions>;
 	type Trader = FixedRateOfFungible<KsmPerSecond, ()>;
 	type ResponseHandler = ();
+	type AssetTrap = XcmPallet;
+	type AssetClaims = XcmPallet;
 }
 
 pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, KusamaNetwork>;
