ef7db6906e27adb34e634fdc6c6adbe656b41f57
Bryan Chen <xlchen1291@gmail.com>
2021-08-28 16:55:15 +1200

    update

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -526,6 +526,7 @@ impl pallet_democracy::Config for Runtime {
 	type Event = Event;
 	type Currency = Balances;
 	type EnactmentPeriod = EnactmentPeriod;
+	type LockVotingPeriod = EnactmentPeriod;
 	type LaunchPeriod = LaunchPeriod;
 	type VotingPeriod = VotingPeriod;
 	type MinimumDeposit = MinimumDeposit;
