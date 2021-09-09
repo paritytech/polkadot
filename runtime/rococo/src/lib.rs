deed3bcea4ddca5c6b80d7a1eff65778c3bd58e1
Ricardo Rius <ricardo@parity.io>
2021-09-09 14:29:01 +0200

    Add Canvas

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -628,6 +628,7 @@ parameter_types! {
 	pub const RococoForTrick: (MultiAssetFilter, MultiLocation) = (Rococo::get(), Parachain(110).into());
 	pub const RococoForTrack: (MultiAssetFilter, MultiLocation) = (Rococo::get(), Parachain(120).into());
 	pub const RococoForStatemint: (MultiAssetFilter, MultiLocation) = (Rococo::get(), Parachain(1001).into());
+	pub const RococoForCanvas: (MultiAssetFilter, MultiLocation) = (Rococo::get(), Parachain(1002).into());
 	pub const MaxInstructions: u32 = 100;
 }
 pub type TrustedTeleporters = (
@@ -635,6 +636,7 @@ pub type TrustedTeleporters = (
 	xcm_builder::Case<RococoForTrick>,
 	xcm_builder::Case<RococoForTrack>,
 	xcm_builder::Case<RococoForStatemint>,
+	xcm_builder::Case<RococoForCanvas>,
 );
 
 parameter_types! {
@@ -644,6 +646,7 @@ parameter_types! {
 			Parachain(110).into(),
 			Parachain(120).into(),
 			Parachain(1001).into(),
+			Parachain(1002).into(),
 		];
 }
 
