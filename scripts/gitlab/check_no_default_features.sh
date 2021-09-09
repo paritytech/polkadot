4f8230835f2be02a93fe3016347da58ba588c8a0
Andronik Ordian <write@reusable.software>
2021-09-09 13:57:11 +0200

    set -e

--- scripts/gitlab/check_no_default_features.sh
+++ scripts/gitlab/check_no_default_features.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-#shellcheck source=../common/lib.sh
-source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../common/lib.sh"
+set -e
 
 time cargo check --no-default-features -p polkadot-service
 time cargo check --no-default-features -p polkadot-cli
