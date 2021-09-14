5051e3aab8b982f9fe4b2587c586294118a12961
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-09-13 20:44:55 -0400

    add force_unreserve weight

--- runtime/kusama/src/weights/pallet_balances.rs
+++ runtime/kusama/src/weights/pallet_balances.rs
@@ -72,4 +72,10 @@ impl<T: frame_system::Config> pallet_balances::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
+	// Storage: System Account (r:1 w:1)
+	fn force_unreserve() -> Weight {
+		(27_766_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+	}
 }
