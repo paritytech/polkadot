fc9204beed1b20cbb621e47b1af6dd87766f5b65
Martin Pugh <martin@parity.io>
2021-09-16 15:53:25 +0200

    bump transaction version

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -118,7 +118,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: version::create_apis_vec![[]],
-	transaction_version: 5,
+	transaction_version: 6,
 };
 
 /// The BABE epoch configuration at genesis.
