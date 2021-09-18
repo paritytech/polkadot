264bb0760548b3e141956c28a4102483342e6b35
Keith Yeung <kungfukeith11@gmail.com>
2021-09-17 22:13:09 -0700

    Use autogenerated WeightInfos for kusama and westend

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -810,7 +810,7 @@ impl parachains_inclusion::Config for Runtime {
 impl parachains_paras::Config for Runtime {
 	type Origin = Origin;
 	type Event = Event;
-	type WeightInfo = parachains_paras::weights::WeightInfo<Runtime>;
+	type WeightInfo = weights::runtime_parachains_paras::WeightInfo<Runtime>;
 }
 
 parameter_types! {
