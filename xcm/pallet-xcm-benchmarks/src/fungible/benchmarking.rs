a73f703444f2a90f93db78d15b3125f9a0023184
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-09-21 03:33:06 -0400

    fmt

--- xcm/pallet-xcm-benchmarks/src/fungible/benchmarking.rs
+++ xcm/pallet-xcm-benchmarks/src/fungible/benchmarking.rs
@@ -16,7 +16,9 @@
 
 use super::*;
 use crate::{account_and_location, new_executor, worst_case_holding, AssetTransactorOf, XcmCallOf};
-use frame_benchmarking::{benchmarks_instance_pallet, impl_benchmark_test_suite, BenchmarkError, BenchmarkResult};
+use frame_benchmarking::{
+	benchmarks_instance_pallet, impl_benchmark_test_suite, BenchmarkError, BenchmarkResult,
+};
 use frame_support::{pallet_prelude::Get, traits::fungible::Inspect};
 use sp_runtime::traits::Zero;
 use sp_std::{convert::TryInto, prelude::*, vec};
