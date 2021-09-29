7f53ddb861aac066ae4a2c392483e0631114d247
Bernhard Schuster <bernhard@ahoi.io>
2021-09-29 12:59:17 +0200

    assure disputes are checked too

--- scripts/gitlab/test_linux_stable.sh
+++ scripts/gitlab/test_linux_stable.sh
@@ -4,4 +4,6 @@ set -e
 #shellcheck source=../common/lib.sh
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../common/lib.sh"
 
+time cargo t --features=disputes -p polkadot-node-core-dispute-coordinator
+
 time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks
