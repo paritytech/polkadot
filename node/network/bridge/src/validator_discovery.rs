75e7f2a02d425d01d136e3950dc34c5182c2c611
Andronik Ordian <write@reusable.software>
2021-10-07 19:00:53 +0200

    validator-discovery: remove from peer set before inserting

--- node/network/bridge/src/validator_discovery.rs
+++ node/network/bridge/src/validator_discovery.rs
@@ -69,6 +69,14 @@ impl<N: Network, AD: AuthorityDiscovery> Service<N, AD> {
 			removed = multiaddr_to_remove.len(),
 			"New ConnectToValidators resolved request",
 		);
+
+		// we remove before inserting because there might be same PeerIds
+		// in both lists
+		// the addresses are known to be valid
+		let _ = network_service
+			.remove_from_peers_set(peer_set.into_protocol_name(), multiaddr_to_remove)
+			.await;
+
 		// ask the network to connect to these nodes and not disconnect
 		// from them until removed from the set
 		if let Err(e) = network_service
@@ -77,10 +85,6 @@ impl<N: Network, AD: AuthorityDiscovery> Service<N, AD> {
 		{
 			tracing::warn!(target: LOG_TARGET, err = ?e, "AuthorityDiscoveryService returned an invalid multiaddress");
 		}
-		// the addresses are known to be valid
-		let _ = network_service
-			.remove_from_peers_set(peer_set.into_protocol_name(), multiaddr_to_remove)
-			.await;
 
 		network_service
 	}
