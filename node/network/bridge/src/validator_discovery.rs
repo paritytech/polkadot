ffb18ac348af958c683dcaf47726214e62b6e19c
Andronik Ordian <write@reusable.software>
2021-10-07 21:22:16 +0200

    one more try

--- node/network/bridge/src/validator_discovery.rs
+++ node/network/bridge/src/validator_discovery.rs
@@ -155,7 +155,7 @@ impl<N: Network, AD: AuthorityDiscovery> Service<N, AD> {
 
 fn extract_peer_ids(multiaddr: impl Iterator<Item = Multiaddr>) -> HashSet<PeerId> {
 	multiaddr
-		.filter_map(|addr| match addr.pop() {
+		.filter_map(|mut addr| match addr.pop() {
 			Some(multiaddr::Protocol::P2p(key)) => PeerId::from_multihash(key).ok(),
 			_ => None,
 		})
