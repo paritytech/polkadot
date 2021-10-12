7fb5ec582f5c6a1991f12ab368eca80f5a5a7cec
Keith Yeung <kungfukeith11@gmail.com>
2021-10-12 01:45:52 -0700

    Typo

--- xcm/src/v2/mod.rs
+++ xcm/src/v2/mod.rs
@@ -17,7 +17,7 @@
 
 //! # XCM Version 2
 //! Version 2 of the Cross-Consensus Message format data structures. The comprehensive list of
-// !changes can be found in
+//! changes can be found in
 //! [this PR description](https://github.com/paritytech/polkadot/pull/3629#issue-968428279).
 //! 
 //! ## Changes to be aware of
