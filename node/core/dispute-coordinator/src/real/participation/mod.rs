595c41084acfe59e51d258c86dd0541a967566db
Robert Klotzner <robert.klotzner@gmx.at>
2021-10-15 18:32:48 +0200

    Participation.

--- node/core/dispute-coordinator/src/real/participation/mod.rs
+++ node/core/dispute-coordinator/src/real/participation/mod.rs
@@ -17,15 +17,39 @@
 use std::collections::HashSet;
 
 mod queues;
+use futures::channel::mpsc;
+use polkadot_node_subsystem::SubsystemContext;
 use polkadot_primitives::v1::CandidateHash;
 use queues::Queues;
 
 /// Keep track of disputes we need to participate in.
 ///
 /// - Prioritize and queue participations
-/// - Dequeue participation requests in order and send them to the participation subsystem.
+/// - Dequeue participation requests in order and launch participation worker.
 struct Participation {
 	/// Participations currently being processed.
 	running_participations: HashSet<CandidateHash>,
+	/// Priority and best effort queues.
 	queue: Queues,
+	/// Sender to be passed to worker tasks.
+	worker_sender: mpsc::Sender<WorkerMessage>,
 }
+
+enum WorkerMessage {
+}
+
+impl Participation {
+	/// Get ready for managing dispute participation requests.
+	///
+	/// The passed in sender will be used by background workers to communicate back their results.
+	/// The calling context should make sure to call `Participation::on_worker_message()` for the
+	/// received messages.
+	pub fn new(sender: mpsc::Sender<WorkerMessage>) -> Self {}
+
+	/// Queue a dispute for the node to participate in:
+	async fn queue_participation<Context: SubsystemContext>(&mut self, ctx: &mut Context, ordering: &mut OrderingProvider, candidate: CandidateReceipt, session: SessionIndex)  {}
+
+	async fn on_worker_message(&mut self, ctx: &mut Context, msg: WorkerMessage) {}
+}
+
+
