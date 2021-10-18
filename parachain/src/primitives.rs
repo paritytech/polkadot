5c00b94586df1a41e6156cc30843a8fb65ef092a
emostov <32168567+emostov@users.noreply.github.com>
2021-10-18 13:34:09 -0700

    Small fixes to make CI syntax checks happier

--- parachain/src/primitives.rs
+++ parachain/src/primitives.rs
@@ -41,9 +41,22 @@ pub use polkadot_core_primitives::BlockNumber as RelayChainBlockNumber;
 
 /// Parachain head data included in the chain.
 #[derive(
-	PartialEq, Eq, Clone, PartialOrd, Ord, Encode, Decode, Default, RuntimeDebug, derive_more::From, TypeInfo,
+	PartialEq,
+	Eq,
+	Clone,
+	PartialOrd,
+	Ord,
+	Encode,
+	Decode,
+	Default,
+	RuntimeDebug,
+	derive_more::From,
+	TypeInfo,
 )]
-#[cfg_attr(any(feature = "runtime-benchmarks", feature = "std"), derive(Serialize, Deserialize, Hash, MallocSizeOf))]
+#[cfg_attr(
+	any(feature = "runtime-benchmarks", feature = "std"),
+	derive(Serialize, Deserialize, Hash, MallocSizeOf)
+)]
 pub struct HeadData(#[cfg_attr(feature = "std", serde(with = "bytes"))] pub Vec<u8>);
 
 impl HeadData {
