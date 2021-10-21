398d125c39207fb20e152479f36d71601ee241a7
Cheng JIANG <alex_cj96@foxmail.com>
2021-10-21 11:16:55 +0800

    run format
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -558,7 +558,7 @@ fn westend_staging_testnet_config_genesis(wasm_binary: &[u8]) -> westend::Genesi
 		registrar: westend_runtime::RegistrarConfig {
 			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
 		},
-        xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
+		xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
 	}
 }
 
@@ -756,7 +756,7 @@ fn kusama_staging_testnet_config_genesis(wasm_binary: &[u8]) -> kusama::GenesisC
 		},
 		gilt: Default::default(),
 		paras: Default::default(),
-        xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
+		xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
 	}
 }
 
@@ -1056,7 +1056,7 @@ fn rococo_staging_testnet_config_genesis(wasm_binary: &[u8]) -> rococo_runtime::
 		registrar: rococo_runtime::RegistrarConfig {
 			next_free_para_id: polkadot_primitives::v1::LOWEST_PUBLIC_ID,
 		},
-        xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
+		xcm_pallet: kusama::XcmPalletConfig { safe_xcm_version: Some(2) },
 		// bridge_rococo_grandpa: rococo_runtime::BridgeRococoGrandpaConfig {
 		// 	owner: Some(endowed_accounts[0].clone()),
 		// 	..Default::default()
