d4a81143bdd31807eaacfb97e3fa6e790a0c01f1
Cheng JIANG <alex_cj96@foxmail.com>
2021-10-22 10:25:57 +0800

    export xcm_pallet config (#4116)
    * export xcm_pallet config
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>
    
    * run format
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>
    
    * fix typo
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>
    
    * add generic parameter to support different runtimes
    
    * Revert "add generic parameter to support different runtimes"
    
    This reverts commit 4405ea9fab24307318ffc20a833010fe864b3be3.

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -255,7 +255,7 @@ construct_runtime! {
 		Multisig: pallet_multisig::{Pallet, Call, Storage, Event<T>},
 
 		// Pallet for sending XCM.
-		XcmPallet: pallet_xcm::{Pallet, Call, Storage, Event<T>, Origin} = 99,
+		XcmPallet: pallet_xcm::{Pallet, Call, Storage, Event<T>, Origin, Config} = 99,
 
 	}
 }
