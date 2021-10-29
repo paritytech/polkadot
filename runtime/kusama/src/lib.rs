dd2d603f24b9629daa865aebae005a372fc719df
Bastian Köcher <info@kchr.de>
2021-10-29 14:51:27 +0200

    Feedback

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -212,9 +212,9 @@ impl PrivilegeCmp<OriginCaller> for OriginPrivilegeCmp {
 			(OriginCaller::system(frame_system::RawOrigin::Root), _) => Some(Ordering::Greater),
 			// Check which one has more yes votes.
 			(
-				OriginCaller::Council(pallet_collective::RawOrigin::Members(l_yes_votes, _)),
-				OriginCaller::Council(pallet_collective::RawOrigin::Members(r_yes_votes, _)),
-			) => Some(l_yes_votes.cmp(r_yes_votes)),
+				OriginCaller::Council(pallet_collective::RawOrigin::Members(l_yes_votes, l_count)),
+				OriginCaller::Council(pallet_collective::RawOrigin::Members(r_yes_votes, r_count)),
+			) => Some((l_yes_votes * r_count).cmp(&(r_yes_votes * l_count))),
 			// For every other origin we don't care, as they are not used for `ScheduleOrigin`.
 			_ => None,
 		}
