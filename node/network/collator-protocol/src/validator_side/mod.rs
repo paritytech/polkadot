b5edafcbf3fdd924fd1b4ea3b6d867767a969c97
Bernhard Schuster <bernhard@ahoi.io>
2021-11-04 11:58:21 +0100

    Revert "chore: fmt"
    This reverts commit 94d39bab5d1553d082a032ab6e31f579316dfa40.

--- node/network/collator-protocol/src/validator_side/mod.rs
+++ node/network/collator-protocol/src/validator_side/mod.rs
@@ -1455,7 +1455,7 @@ async fn poll_collation_response(
 				);
 
 				CollationFetchResult::Error(COST_WRONG_PARA)
-			},
+			}
 			Ok(CollationFetchingResponse::Collation(receipt, pov)) => {
 				tracing::debug!(
 					target: LOG_TARGET,
