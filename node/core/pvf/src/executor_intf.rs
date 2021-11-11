9e8b26fe41771ee35a4b63e488e0a9f785fa61bf
sandreim <54316454+sandreim@users.noreply.github.com>
2021-11-11 20:53:37 +0200

    Per subsystem CPU usage tracking (#4239)
    * SubsystemContext: add subsystem name str
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Overseer builder proc macro changes
    
    * initilize SubsystemContext name field.
    * Add subsystem name in TaskKind::launch_task()
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Update ToOverseer enum
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Assign subsystem names to orphan tasks
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * cargo fmt
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * SubsystemContext: add subsystem name str
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Overseer builder proc macro changes
    
    * initilize SubsystemContext name field.
    * Add subsystem name in TaskKind::launch_task()
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Update ToOverseer enum
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * Assign subsystem names to orphan tasks
    
    Signed-off-by: Andrei Sandu <sandu.andrei@gmail.com>
    
    * cargo fmt
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Rebase changes for new spawn() group param
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Add subsystem constat in JobTrait
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Add subsystem string
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fix tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fix spawn() calls
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * cargo fmt
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fix
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fix tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * fix
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fix more tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Address PR review feedback #1
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Address PR review round 2
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * Fixes
    - remove JobTrait::Subsystem
    - fix tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    * update Cargo.lock
    
    Co-authored-by: Andronik Ordian <write@reusable.software>

--- node/core/pvf/src/executor_intf.rs
+++ node/core/pvf/src/executor_intf.rs
@@ -278,11 +278,21 @@ impl TaskExecutor {
 }
 
 impl sp_core::traits::SpawnNamed for TaskExecutor {
-	fn spawn_blocking(&self, _: &'static str, future: futures::future::BoxFuture<'static, ()>) {
+	fn spawn_blocking(
+		&self,
+		_task_name: &'static str,
+		_subsystem_name: Option<&'static str>,
+		future: futures::future::BoxFuture<'static, ()>,
+	) {
 		self.0.spawn_ok(future);
 	}
 
-	fn spawn(&self, _: &'static str, future: futures::future::BoxFuture<'static, ()>) {
+	fn spawn(
+		&self,
+		_task_name: &'static str,
+		_subsystem_name: Option<&'static str>,
+		future: futures::future::BoxFuture<'static, ()>,
+	) {
 		self.0.spawn_ok(future);
 	}
 }
