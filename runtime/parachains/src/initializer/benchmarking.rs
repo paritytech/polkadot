91ce4d95a1c678b29fe610b8822831d764dc845c
arkpar <arkady.paronyan@gmail.com>
2021-11-12 14:28:39 +0100

    Fixed benches

--- runtime/parachains/src/initializer/benchmarking.rs
+++ runtime/parachains/src/initializer/benchmarking.rs
@@ -15,8 +15,9 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use super::*;
+use sp_runtime::DigestItem;
 use frame_benchmarking::benchmarks;
-use frame_system::{DigestItemOf, RawOrigin};
+use frame_system::RawOrigin;
 use primitives::v1::ConsensusLog;
 
 // Random large number for the digest
@@ -32,7 +33,7 @@ benchmarks! {
 	verify {
 		assert_eq!(
 			<frame_system::Pallet<T>>::digest().logs.last().unwrap(),
-			&<DigestItemOf<T>>::from(ConsensusLog::ForceApprove(d + 1)),
+			&DigestItem::from(ConsensusLog::ForceApprove(d + 1)),
 		);
 	}
 
