46510b1281f5d36352837388a15ff722df0a4a11
david <wizdave97@gmail.com>
2021-11-12 07:50:13 +0100

    fmt

--- node/test/polkadot-simnet/common/src/lib.rs
+++ node/test/polkadot-simnet/common/src/lib.rs
@@ -181,8 +181,12 @@ where
 		let (index, hash): (u32, H256) = events
 			.iter()
 			.filter_map(|event| match event.event {
-				Event::Council(CouncilCollectiveEvent::Proposed { account: _, proposal_index, ref proposal_hash, threshold: _}) =>
-					Some((proposal_index, proposal_hash.clone())),
+				Event::Council(CouncilCollectiveEvent::Proposed {
+					account: _,
+					proposal_index,
+					ref proposal_hash,
+					threshold: _,
+				}) => Some((proposal_index, proposal_hash.clone())),
 				_ => None,
 			})
 			.next()
@@ -212,12 +216,16 @@ where
 			.events()
 			.into_iter()
 			.filter(|event| match event.event {
-				Event::Council(CouncilCollectiveEvent::Closed { proposal_hash, yes: _, no: _ }) if hash == proposal_hash =>
-					true,
-				Event::Council(CouncilCollectiveEvent::Approved { proposal_hash }) if hash == proposal_hash => true,
-				Event::Council(CouncilCollectiveEvent::Executed { proposal_hash, result: Ok(()) })
+				Event::Council(CouncilCollectiveEvent::Closed { proposal_hash, yes: _, no: _ })
 					if hash == proposal_hash =>
 					true,
+				Event::Council(CouncilCollectiveEvent::Approved { proposal_hash })
+					if hash == proposal_hash =>
+					true,
+				Event::Council(CouncilCollectiveEvent::Executed {
+					proposal_hash,
+					result: Ok(()),
+				}) if hash == proposal_hash => true,
 				_ => false,
 			})
 			.collect::<Vec<_>>();
@@ -289,15 +297,17 @@ where
 			.events()
 			.into_iter()
 			.filter(|event| match event.event {
-				Event::TechnicalCommittee(TechnicalCollectiveEvent::Closed { proposal_hash, .. })
-					if hash == _hash =>
-					true,
+				Event::TechnicalCommittee(TechnicalCollectiveEvent::Closed {
+					proposal_hash,
+					..
+				}) if hash == _hash => true,
 				Event::TechnicalCommittee(TechnicalCollectiveEvent::Approved { proposal_hash })
 					if hash == _hash =>
 					true,
-				Event::TechnicalCommittee(TechnicalCollectiveEvent::Executed { proposal_hash, result: Ok(()) })
-					if hash == _hash =>
-					true,
+				Event::TechnicalCommittee(TechnicalCollectiveEvent::Executed {
+					proposal_hash,
+					result: Ok(()),
+				}) if hash == _hash => true,
 				_ => false,
 			})
 			.collect::<Vec<_>>();
@@ -344,11 +354,14 @@ where
 		.events()
 		.into_iter()
 		.filter(|event| match event.event {
-			Event::Democracy(democracy::Event::Passed { ref_index: _index }) if _index == ref_index => true,
+			Event::Democracy(democracy::Event::Passed { ref_index: _index })
+				if _index == ref_index =>
+				true,
 			Event::Democracy(democracy::Event::PreimageUsed { proposal_hash: _hash, .. })
 				if _hash == proposal_hash =>
 				true,
-			Event::Democracy(democracy::Event::Executed { ref_index: _index, result: Ok(()) }) if _index == ref_index =>
+			Event::Democracy(democracy::Event::Executed { ref_index: _index, result: Ok(()) })
+				if _index == ref_index =>
 				true,
 			_ => false,
 		})
