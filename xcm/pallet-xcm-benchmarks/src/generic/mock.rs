30c13a2435b0b79b82d678385b94aef4cb59b3b3
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-11-15 15:07:28 -0400

    fix spell check

--- xcm/pallet-xcm-benchmarks/src/generic/mock.rs
+++ xcm/pallet-xcm-benchmarks/src/generic/mock.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! A mock runtime for xcm benchmarking.
+//! A mock runtime for XCM benchmarking.
 
 use crate::{generic, mock::*, *};
 use frame_support::{
