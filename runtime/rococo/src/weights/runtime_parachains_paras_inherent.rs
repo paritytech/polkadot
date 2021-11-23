2f0d61294260eb8ca9be25720699cd236a99a701
emostov <32168567+emostov@users.noreply.github.com>
2021-11-23 11:19:02 -0800

    Make it compile

--- runtime/rococo/src/weights/runtime_parachains_paras_inherent.rs
+++ runtime/rococo/src/weights/runtime_parachains_paras_inherent.rs
@@ -133,7 +133,7 @@ impl<T: frame_system::Config> runtime_parachains::paras_inherent::WeightInfo for
 	// Storage: ParasDisputes Included (r:0 w:1)
 	// Storage: Hrmp HrmpWatermarks (r:0 w:1)
 	// Storage: Paras Heads (r:0 w:1)
-	fn enter_backed_candidates_variable(_v) -> Weight {
+	fn enter_backed_candidates_variable(_v: u32) -> Weight {
 		(442_472_000 as Weight)
 			.saturating_add(T::DbWeight::get().reads(25 as Weight))
 			.saturating_add(T::DbWeight::get().writes(14 as Weight))
