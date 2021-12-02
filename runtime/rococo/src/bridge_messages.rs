d5de62d5b961a21f31a2b78482c5dcb8bad8f13b
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-12-02 17:37:32 +0300

    accept messages from lane 00000000 only

--- runtime/rococo/src/bridge_messages.rs
+++ runtime/rococo/src/bridge_messages.rs
@@ -240,8 +240,22 @@ where
 		proof: Self::MessagesProof,
 		messages_count: u32,
 	) -> Result<ProvedMessages<Message<crate::Balance>>, Self::Error> {
-		messages_target::verify_messages_proof::<B, crate::Runtime, GI>(proof, messages_count)
+		messages_target::verify_messages_proof::<B, crate::Runtime, GI>(proof, messages_count).and_then(verify_inbound_messages_lane)
 	}
+}
+
+/// Error that happens when we are receiving incoming message via unexpected lane.
+const INBOUND_LANE_DISABLED: &str = "The inbound message lane is disabled.";
+
+/// Verify that lanes of inbound messages are enabled.
+fn verify_inbound_messages_lane(
+	messages: ProvedMessages<Message<crate::Balance>>,
+) -> Result<ProvedMessages<Message<crate::Balance>>, &'static str> {
+	let allowed_incoming_lanes = [[0, 0, 0, 0]];
+	if messages.keys().any(|lane_id| !allowed_incoming_lanes.contains(lane_id)) {
+		return Err(INBOUND_LANE_DISABLED)
+	}
+	Ok(messages)
 }
 
 /// The cost of delivery confirmation transaction.
@@ -360,6 +374,7 @@ mod at_wococo {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use bp_messages::{target_chain::ProvedLaneMessages, MessageData, MessageKey};
 	use bridge_runtime_common::messages;
 	use parity_scale_codec::Encode;
 
@@ -381,6 +396,7 @@ mod tests {
 			ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT,
 			MAX_SINGLE_MESSAGE_DELIVERY_CONFIRMATION_TX_WEIGHT,
 			PAY_INBOUND_DISPATCH_FEE_WEIGHT,
+			crate::RocksDbWeight::get(),
 		);
 
 		let max_incoming_message_proof_size = bp_rococo::EXTRA_STORAGE_PROOF_SIZE.saturating_add(
@@ -408,6 +424,7 @@ mod tests {
 			max_incoming_inbound_lane_data_proof_size,
 			bp_rococo::MAX_UNREWARDED_RELAYER_ENTRIES_AT_INBOUND_LANE,
 			bp_rococo::MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE,
+			crate::RocksDbWeight::get(),
 		);
 	}
 
@@ -443,5 +460,42 @@ mod tests {
 			TX_EXTRA_BYTES,
 			extra_bytes_in_transaction,
 		);
+	}
+
+	fn proved_messages(lane_id: LaneId) -> ProvedMessages<Message<crate::Balance>> {
+		vec![(
+			lane_id,
+			ProvedLaneMessages {
+				lane_state: None,
+				messages: vec![Message {
+					key: MessageKey { lane_id, nonce: 0 },
+					data: MessageData { payload: vec![], fee: 0 },
+				}],
+			},
+		)]
+		.into_iter()
+		.collect()
+	}
+
+	#[test]
+	fn verify_inbound_messages_lane_succeeds() {
+		assert_eq!(
+			verify_inbound_messages_lane(proved_messages([0, 0, 0, 0])),
+			Ok(proved_messages([0, 0, 0, 0])),
+		);
+	}
+
+	#[test]
+	fn verify_inbound_messages_lane_fails() {
+		assert_eq!(
+			verify_inbound_messages_lane(proved_messages([0, 0, 0, 1])),
+			Err(INBOUND_LANE_DISABLED),
+		);
+
+		let proved_messages = proved_messages([0, 0, 0, 0])
+			.into_iter()
+			.chain(proved_messages([0, 0, 0, 1]))
+			.collect();
+		assert_eq!(verify_inbound_messages_lane(proved_messages), Err(INBOUND_LANE_DISABLED),);
 	}
 }
