b3be1161ca8477a41157dc3a5453d5d5cf332b8d
Javier Viola <pepoviola@gmail.com>
2021-12-07 14:27:50 -0300

    refactor to use ws tx/rx

--- node/zombienet-backchannel/src/lib.rs
+++ node/zombienet-backchannel/src/lib.rs
@@ -19,59 +19,90 @@
 //! values in the test specifications, through a bidirectional message passing
 //! implemented as a `backchannel`.
 
-use futures_util::{stream::SplitStream, StreamExt};
-use lazy_static::lazy_static;
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
 use parity_scale_codec as codec;
+use serde::{Deserialize, Serialize};
 use std::env;
 use tokio::net::TcpStream;
-use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+use tokio::sync::broadcast;
+use tokio_tungstenite::{
+	connect_async, tungstenite::protocol::Message, MaybeTlsStream, WebSocketStream,
+};
 
 mod errors;
 use errors::BackchannelError;
 
 #[derive(Debug)]
 pub struct ZombienetBackchannel {
-	pub inner: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+	broadcast_tx: broadcast::Sender<BackchannelItem>,
+	ws_tx: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct BackchannelItem {
+	key: String,
+	value: String,
 }
 
 pub const ZOMBIENET: &str = "ZOMBIENET_BACKCHANNEL🧟🧟🧟";
 
-lazy_static! {
-	static ref BACKCHANNEL_HOST: String =
-		env::var("BACKCHANNEL_HOST").unwrap_or_else(|_| "backchannel".to_string());
-	static ref BACKCHANNEL_PORT: String =
-		env::var("BACKCHANNEL_PORT").unwrap_or_else(|_| "3000".to_string());
-}
-
 impl ZombienetBackchannel {
-	pub async fn connect() -> Result<ZombienetBackchannel, BackchannelError> {
-		let ws_url = format!("ws://{}:{}/ws", *BACKCHANNEL_HOST, *BACKCHANNEL_PORT);
+	pub async fn init() -> Result<ZombienetBackchannel, BackchannelError> {
+		let backchannel_host =
+			env::var("BACKCHANNEL_HOST").unwrap_or_else(|_| "backchannel".to_string());
+		let backchannel_port = env::var("BACKCHANNEL_PORT").unwrap_or_else(|_| "3000".to_string());
+		let ws_url = format!("ws://{}:{}/ws", backchannel_host, backchannel_port);
 		tracing::debug!(target = ZOMBIENET, "Connecting to : {}", &ws_url);
 		let (ws_stream, _) =
 			connect_async(ws_url).await.map_err(|_| BackchannelError::CantConnectToWS)?;
-		let (_write, read) = ws_stream.split();
+		let (write, mut read) = ws_stream.split();
 
-		Ok(ZombienetBackchannel { inner: read })
+		let (tx, _rx) = broadcast::channel(256);
+
+		let tx1 = tx.clone();
+		tokio::spawn(async move {
+			while let Some(Ok(Message::Text(text))) = read.next().await {
+				println!("es {}", &text);
+
+				match serde_json::from_str::<BackchannelItem>(&text) {
+					Ok(backchannel_item) => {
+						if tx1.send(backchannel_item).is_err() {
+							tracing::error!("Error sending through the channel");
+							return;
+						}
+					}
+					Err(_) => {
+						tracing::error!("Invalid payload received");
+					}
+				}
+			}
+		});
+
+		Ok(ZombienetBackchannel { broadcast_tx: tx, ws_tx: write })
 	}
 
-	pub async fn send(key: &'static str, val: impl codec::Encode) -> Result<(), BackchannelError> {
-		let client = reqwest::Client::new();
-		let encoded = val.encode();
-		let body = String::from_utf8_lossy(&encoded);
-		let zombienet_endpoint =
-			format!("http://{}:{}/{}", *BACKCHANNEL_HOST, *BACKCHANNEL_PORT, key);
+	pub fn subscribe(&self) -> broadcast::Receiver<BackchannelItem> {
+		self.broadcast_tx.subscribe()
+	}
 
-		tracing::debug!(target = ZOMBIENET, "Sending to: {}", &zombienet_endpoint);
-		let _res =
-			client
-				.post(zombienet_endpoint)
-				.body(body.to_string())
-				.send()
-				.await
-				.map_err(|e| {
-					tracing::error!(target = ZOMBIENET, "Error sending new item: {}", e);
-					BackchannelError::SendItemFail
-				})?;
+	pub async fn send(
+		&mut self,
+		key: &'static str,
+		val: impl codec::Encode,
+	) -> Result<(), BackchannelError> {
+		let encoded = val.encode();
+		let backchannel_item = BackchannelItem {
+			key: key.to_string(),
+			value: String::from_utf8_lossy(&encoded).to_string(),
+		};
+
+		self.ws_tx
+			.send(Message::Text(serde_json::to_string(&backchannel_item).unwrap()))
+			.await
+			.map_err(|e| {
+				tracing::error!(target = ZOMBIENET, "Error sending new item: {}", e);
+				BackchannelError::SendItemFail
+			})?;
 
 		Ok(())
 	}
