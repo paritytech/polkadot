90dd6fbe6944601f686d5f82b6aa1690d7345c59
Andrei Sandu <andrei-mihail@parity.io>
2021-12-14 16:54:08 +0000

    gitlab script update
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- scripts/gitlab/test_linux_stable.sh
+++ scripts/gitlab/test_linux_stable.sh
@@ -5,4 +5,4 @@ set -e
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../common/lib.sh"
 
 time cargo test --release --locked -p polkadot-node-core-dispute-coordinator --features disputes
-time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks
+time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks,runtime-metrics
