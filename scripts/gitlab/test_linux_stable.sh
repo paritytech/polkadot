0b6f0f276abb1e7f9bbee0c6c36239b1e7235584
Andrei Sandu <andrei-mihail@parity.io>
2021-12-15 13:13:21 +0000

    Run tests twice
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- scripts/gitlab/test_linux_stable.sh
+++ scripts/gitlab/test_linux_stable.sh
@@ -5,4 +5,6 @@ set -e
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../common/lib.sh"
 
 time cargo test --release --locked -p polkadot-node-core-dispute-coordinator --features disputes
-time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks,runtime-metrics
+time cargo test --workspace --release --verbose --locked --features=runtime-benchmarks
+# Builds with the runtime metrics feature are only to be used for testing.
+time cargo test --workspace --release --verbose --locked --features=runtime-metrics
