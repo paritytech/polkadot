d0037cda27ac7e6ebd7dfd3d46bfe319b1749473
Keith Yeung <kungfukeith11@gmail.com>
2021-12-15 19:11:06 -0800

    Use AllPalletsWithSystem

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -1408,7 +1408,7 @@ impl xcm_executor::Config for XcmConfig {
 	type AssetTrap = XcmPallet;
 	type AssetClaims = XcmPallet;
 	type SubscriptionService = XcmPallet;
-	type PalletInstancesInfo = AllPallets;
+	type PalletInstancesInfo = AllPalletsWithSystem;
 	type MaxAssetsIntoHolding = MaxAssetsIntoHolding;
 }
 
