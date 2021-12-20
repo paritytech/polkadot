1b758c7e5eeeeea3e898e2d763ac1d36860332c2
Bernhard Schuster <bernhard@ahoi.io>
2021-12-20 12:10:18 +0100

    improved log

--- node/overseer/overseer-gen/proc-macro/src/impl_builder.rs
+++ node/overseer/overseer-gen/proc-macro/src/impl_builder.rs
@@ -340,7 +340,7 @@ pub(crate) fn impl_builder(info: &OverseerInfo) -> proc_macro2::TokenStream {
 						FieldInitMethod::Fn(func) => func(handle.clone())?,
 						FieldInitMethod::Value(val) => val,
 						FieldInitMethod::Uninitialized =>
-							panic!("All subsystems must exist with the builder pattern."),
+							panic!("All subsystems must exist with the builder pattern. Missing initilization for subsystem: {}", stringify!(#subsystem_name)),
 					};
 
 					let unbounded_meter = #channel_name_unbounded_rx.meter().clone();
