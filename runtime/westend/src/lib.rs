6b65f7dff42b16b3325efc02eea18594f12f4870
Wilfried Kopp <wilfried@parity.io>
2022-01-15 11:05:55 +0100

    Bump spec_version for Polkadot, Kusama and Westend

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -116,7 +116,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 9150,
+	spec_version: 9151,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
