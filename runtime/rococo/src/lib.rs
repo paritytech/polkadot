d80f22ab20f8433c6a8a6b9d88c7baeac64fb429
Santi Balaguer <santibalaguer@Santis-MacBook-Pro.local>
2022-01-19 20:05:54 +0100

    changed 356 to 365 days to mimic one year

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -662,7 +662,7 @@ impl parachains_initializer::Config for Runtime {
 impl paras_sudo_wrapper::Config for Runtime {}
 
 parameter_types! {
-	pub const PermanentSlotLeasePeriodLength: u32 = 356;
+	pub const PermanentSlotLeasePeriodLength: u32 = 365;
 	pub const TemporarySlotLeasePeriodLength: u32 = 3;
 	pub const MaxPermanentSlots: u32 = 25;
 	pub const MaxTemporarySlots: u32 = 20;
