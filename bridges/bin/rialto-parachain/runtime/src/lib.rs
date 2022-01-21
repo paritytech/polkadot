aacb8f6eab83fdb42ee13ed57046bb4e30ad6066
Keith Yeung <kungfukeith11@gmail.com>
2022-01-21 03:03:39 -0800

    Remove `Default` bound on `AccountId` types under the xcm directory (#4712)
    * Refactor ParentIsDefault to ParentIsAllZeroes
    
    * Remove Default bound on all AccountId types under the xcm directory
    
    * Change to ParentIs<A: Get<AccountId>, AccountId>
    
    * Provide a better account for ParentIs
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Update xcm/xcm-builder/src/currency_adapter.rs
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>
    
    * Use preset account ID value for parent MultiLocations
    
    Co-authored-by: Bastian Köcher <bkchr@users.noreply.github.com>

--- bridges/bin/rialto-parachain/runtime/src/lib.rs
+++ bridges/bin/rialto-parachain/runtime/src/lib.rs
@@ -70,7 +70,7 @@ use xcm::latest::prelude::*;
 use xcm_builder::{
 	AccountId32Aliases, AllowTopLevelPaidExecutionFrom, AllowUnpaidExecutionFrom, CurrencyAdapter,
 	EnsureXcmOrigin, FixedWeightBounds, IsConcrete, LocationInverter, NativeAsset,
-	ParentAsSuperuser, ParentIsDefault, RelayChainAsNative, SiblingParachainAsNative,
+	ParentAsSuperuser, ParentIsPreset, RelayChainAsNative, SiblingParachainAsNative,
 	SiblingParachainConvertsVia, SignedAccountId32AsNative, SignedToAccountId32,
 	SovereignSignedViaLocation, TakeWeightCredit, UsingComponents,
 };
@@ -294,8 +294,8 @@ parameter_types! {
 /// when determining ownership of accounts for asset transacting and when attempting to use XCM
 /// `Transact` in order to determine the dispatch Origin.
 pub type LocationToAccountId = (
-	// The parent (Relay-chain) origin converts to the default `AccountId`.
-	ParentIsDefault<AccountId>,
+	// The parent (Relay-chain) origin converts to the parent `AccountId`.
+	ParentIsPreset<AccountId>,
 	// Sibling parachain origins convert to AccountId via the `ParaId::into`.
 	SiblingParachainConvertsVia<Sibling, AccountId>,
 	// Straight up local `AccountId32` origins just alias directly to `AccountId`.
