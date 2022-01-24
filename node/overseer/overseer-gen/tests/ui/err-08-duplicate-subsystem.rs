8ecc081e3b2bc50d3a783c49108f2adbe015fa9d
Vsevolod Stakhov <vsevolod.stakhov@parity.io>
2022-01-24 13:32:09 +0000

    Fix identation in the tests

--- node/overseer/overseer-gen/tests/ui/err-08-duplicate-subsystem.rs
+++ node/overseer/overseer-gen/tests/ui/err-08-duplicate-subsystem.rs
@@ -6,9 +6,9 @@ use polkadot_overseer_gen::*;
 struct AwesomeSubSys;
 
 impl ::polkadot_overseer_gen::Subsystem<OverseerSubsystemContext<MsgStrukt>, OverseerError> for AwesomeSubSys {
-    fn start(self, _ctx: OverseerSubsystemContext<MsgStrukt>) -> SpawnedSubsystem<OverseerError> {
-        unimplemented!("starting yay!")
-    }
+	fn start(self, _ctx: OverseerSubsystemContext<MsgStrukt>) -> SpawnedSubsystem<OverseerError> {
+		unimplemented!("starting yay!")
+	}
 }
 
 #[derive(Clone, Debug)]
@@ -21,42 +21,42 @@ pub struct MsgStrukt(u8);
 
 #[overlord(signal=SigSigSig, error=OverseerError, event=Event, gen=AllMessages)]
 struct Overseer {
-    #[subsystem(no_dispatch, MsgStrukt)]
-    sub0: AwesomeSubSys,
-    i_like_pie: f64,
+	#[subsystem(no_dispatch, MsgStrukt)]
+	sub0: AwesomeSubSys,
+	i_like_pie: f64,
 }
 
 #[derive(Debug, Clone)]
 pub struct DummySpawner;
 
 impl SpawnNamed for DummySpawner {
-    fn spawn_blocking(
-        &self,
-        task_name: &'static str,
-        subsystem_name: Option<&'static str>,
-        _future: futures::future::BoxFuture<'static, ()>,
-    ) {
-        unimplemented!("spawn blocking {} {}", task_name, subsystem_name.unwrap_or("default"))
-    }
+	fn spawn_blocking(
+		&self,
+		task_name: &'static str,
+		subsystem_name: Option<&'static str>,
+		_future: futures::future::BoxFuture<'static, ()>,
+	) {
+		unimplemented!("spawn blocking {} {}", task_name, subsystem_name.unwrap_or("default"))
+	}
 
-    fn spawn(
-        &self,
-        task_name: &'static str,
-        subsystem_name: Option<&'static str>,
-        _future: futures::future::BoxFuture<'static, ()>,
-    ) {
-        unimplemented!("spawn {} {}", task_name, subsystem_name.unwrap_or("default"))
-    }
+	fn spawn(
+		&self,
+		task_name: &'static str,
+		subsystem_name: Option<&'static str>,
+		_future: futures::future::BoxFuture<'static, ()>,
+	) {
+		unimplemented!("spawn {} {}", task_name, subsystem_name.unwrap_or("default"))
+	}
 }
 
 struct DummyCtx;
 
 fn main() {
-    let _ = Overseer::builder()
-        .sub0(AwesomeSubSys::default())
-        .sub0(AwesomeSubSys::default()) // Duplicate subsystem
-        .i_like_pie(std::f64::consts::PI)
-        .spawner(DummySpawner)
-        .build()
-        .unwrap();
+	let _ = Overseer::builder()
+		.sub0(AwesomeSubSys::default())
+		.sub0(AwesomeSubSys::default()) // Duplicate subsystem
+		.i_like_pie(std::f64::consts::PI)
+		.spawner(DummySpawner)
+		.build()
+		.unwrap();
 }
--- node/overseer/overseer-gen/tests/ui/err-09-uninit_generic_baggage.rs
+++ node/overseer/overseer-gen/tests/ui/err-09-uninit_generic_baggage.rs
@@ -6,9 +6,9 @@ use polkadot_overseer_gen::*;
 struct AwesomeSubSys;
 
 impl ::polkadot_overseer_gen::Subsystem<OverseerSubsystemContext<MsgStrukt>, OverseerError> for AwesomeSubSys {
-    fn start(self, _ctx: OverseerSubsystemContext<MsgStrukt>) -> SpawnedSubsystem<OverseerError> {
-        unimplemented!("starting yay!")
-    }
+	fn start(self, _ctx: OverseerSubsystemContext<MsgStrukt>) -> SpawnedSubsystem<OverseerError> {
+		unimplemented!("starting yay!")
+	}
 }
 
 #[derive(Clone, Debug)]
@@ -21,41 +21,41 @@ pub struct MsgStrukt(u8);
 
 #[overlord(signal=SigSigSig, error=OverseerError, event=Event, gen=AllMessages)]
 struct Overseer<T> {
-    #[subsystem(no_dispatch, MsgStrukt)]
-    sub0: AwesomeSubSys,
-    i_like_pie: T,
+	#[subsystem(no_dispatch, MsgStrukt)]
+	sub0: AwesomeSubSys,
+	i_like_pie: T,
 }
 
 #[derive(Debug, Clone)]
 pub struct DummySpawner;
 
 impl SpawnNamed for DummySpawner {
-    fn spawn_blocking(
-        &self,
-        task_name: &'static str,
-        subsystem_name: Option<&'static str>,
-        _future: futures::future::BoxFuture<'static, ()>,
-    ) {
-        unimplemented!("spawn blocking {} {}", task_name, subsystem_name.unwrap_or("default"))
-    }
+	fn spawn_blocking(
+		&self,
+		task_name: &'static str,
+		subsystem_name: Option<&'static str>,
+		_future: futures::future::BoxFuture<'static, ()>,
+	) {
+		unimplemented!("spawn blocking {} {}", task_name, subsystem_name.unwrap_or("default"))
+	}
 
-    fn spawn(
-        &self,
-        task_name: &'static str,
-        subsystem_name: Option<&'static str>,
-        _future: futures::future::BoxFuture<'static, ()>,
-    ) {
-        unimplemented!("spawn {} {}", task_name, subsystem_name.unwrap_or("default"))
-    }
+	fn spawn(
+		&self,
+		task_name: &'static str,
+		subsystem_name: Option<&'static str>,
+		_future: futures::future::BoxFuture<'static, ()>,
+	) {
+		unimplemented!("spawn {} {}", task_name, subsystem_name.unwrap_or("default"))
+	}
 }
 
 struct DummyCtx;
 
 fn main() {
-    let (_, _): (Overseer<_, f64>, _) = Overseer::builder()
-        .sub0(AwesomeSubSys::default())
-        //.i_like_pie(std::f64::consts::PI) // The filed is not initialised
-        .spawner(DummySpawner)
-        .build()
-        .unwrap();
+	let (_, _): (Overseer<_, f64>, _) = Overseer::builder()
+		.sub0(AwesomeSubSys::default())
+		//.i_like_pie(std::f64::consts::PI) // The filed is not initialised
+		.spawner(DummySpawner)
+		.build()
+		.unwrap();
 }
