396cb8a2477a12ec2bb125f99cfe7cefa8c61271
Lldenaurois <Ljdenaurois@gmail.com>
2022-01-25 14:34:54 +0100

    Update Versi chainspec to make use of most recent Rococo runtime

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -157,7 +157,24 @@ pub fn rococo_config() -> Result<RococoChainSpec, String> {
 }
 
 pub fn versi_config() -> Result<VersiChainSpec, String> {
-	VersiChainSpec::from_json_bytes(&include_bytes!("../res/versi.json")[..])
+	let wasm_binary = rococo::WASM_BINARY.ok_or("Versi development wasm not available")?;
+
+	Ok(RococoChainSpec::from_genesis(
+		"Versi",
+		"versi_v1_3",
+		ChainType::Live,
+		move || RococoGenesisExt {
+			runtime_genesis_config: rococo_development_config_genesis(wasm_binary),
+			// Use 1 minute session length.
+			session_length_in_blocks: Some(100),
+		},
+		vec![],
+		None,
+		Some("versi"),
+		None,
+		None,
+		Default::default(),
+	))
 }
 
 /// This is a temporary testnet that uses the same runtime as rococo.
