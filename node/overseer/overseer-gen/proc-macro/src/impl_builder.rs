fe8faf6b1a9c98e856fc5a65f84de32e58c8afbc
Vsevolod Stakhov <vsevolod.stakhov@parity.io>
2022-01-27 21:29:39 +0000

    Rename variable to reflect it's meaning better

--- node/overseer/overseer-gen/proc-macro/src/impl_builder.rs
+++ node/overseer/overseer-gen/proc-macro/src/impl_builder.rs
@@ -231,14 +231,14 @@ pub(crate) fn impl_builder(info: &OverseerInfo) -> proc_macro2::TokenStream {
 		post_setter_generics[idx] = parse_quote! { Init<#field_type> };
 
 		// Baggage can also be generic, so we need to include that to a signature
-		let additional_baggage_generic = if bag_field.generic {
+		let preserved_baggage_generic = if bag_field.generic {
 			quote! {#field_type,}
 		} else {
 			TokenStream::new()
 		};
 
 		quote! {
-			impl <InitStateSpawner, #additional_baggage_generic #( #subsystem_passthrough_state_generics, )* #( #impl_baggage_state_generics, )* >
+			impl <InitStateSpawner, #preserved_baggage_generic #( #subsystem_passthrough_state_generics, )* #( #impl_baggage_state_generics, )* >
 			#builder <InitStateSpawner, #( #subsystem_passthrough_state_generics, )* #( #pre_setter_generics, )* >
 			{
 				/// Specify the baggage in the builder when it was not initialized before
@@ -257,7 +257,7 @@ pub(crate) fn impl_builder(info: &OverseerInfo) -> proc_macro2::TokenStream {
 					}
 				}
 			}
-			impl <InitStateSpawner, #additional_baggage_generic #( #subsystem_passthrough_state_generics, )* #( #impl_baggage_state_generics, )* >
+			impl <InitStateSpawner, #preserved_baggage_generic #( #subsystem_passthrough_state_generics, )* #( #impl_baggage_state_generics, )* >
 			#builder <InitStateSpawner, #( #subsystem_passthrough_state_generics, )* #( #post_setter_generics, )* > {
 				/// Specify the baggage in the builder when it has been previously initialized
 				pub fn #fname (self, var: #field_type ) ->
