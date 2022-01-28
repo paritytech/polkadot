90816b09ff1f996ca138870f90c9200cecb132ea
Robert Habermeier <rphmeier@gmail.com>
2022-01-27 19:49:10 -0600

    docs & more TODOs

--- node/subsystem-util/src/candidate_context.rs
+++ node/subsystem-util/src/candidate_context.rs
@@ -15,35 +15,52 @@
 use polkadot_primitives::v1::Id as ParaId;
 use std::collections::HashMap;
 
+/// Limitations on inbound HRMP channels.
 #[derive(Debug, Clone, PartialEq)]
-pub struct InboundHrmpChannelContext {
-	messages_remaining: usize,
+pub struct InboundHrmpChannelLimitations {
+	/// The number of messages remaining to be processed.
+	pub messages_remaining: usize,
 }
 
+/// Limitations on outbound HRMP channels.
 #[derive(Debug, Clone, PartialEq)]
-pub struct OutboundHrmpChannelContext {
-	bytes_remaining: usize,
-	messages_remaining: usize,
+pub struct OutboundHrmpChannelLimitations {
+	/// The maximum bytes that can be written to the channel.
+	pub bytes_remaining: usize,
+	/// The maximum messages that can be written to the channel.
+	pub messages_remaining: usize,
 }
 
+/// An update to inbound HRMP channels.
 #[derive(Debug, Clone, PartialEq)]
 pub struct InboundHrmpChannelUpdate {
-	messages_consumed: usize,
+	/// The number of messages consumed from the channel.
+	pub messages_consumed: usize,
 }
 
+/// An update to outbound HRMP channels.
 #[derive(Debug, Clone, PartialEq)]
 pub struct OutboundHrmpChannelUpdate {
-	bytes_submitted: usize,
-	messages_submitted: usize,
+	/// The number of bytes submitted to the channel.
+	pub bytes_submitted: usize,
+	/// The number of messages submitted to the channel.
+	pub messages_submitted: usize,
 }
 
+/// Limitations on the actions that can be taken by a new parachain
+/// block.
 #[derive(Debug, Clone, PartialEq)]
 pub struct ContextLimitations {
-	ump_remaining: usize,
-	ump_rmaining_bytes: usize,
-	dmp_remaining_messages: usize,
-	hrmp_channels_in: HashMap<ParaId, InboundHrmpChannelContext>,
-	hrmp_channels_out: HashMap<ParaId, OutboundHrmpChannelContext>,
+	/// The amount of UMP messages remaining.
+	pub ump_remaining: usize,
+	/// The amount of UMP bytes remaining.
+	pub ump_remaining_bytes: usize,
+	/// The amount of remaining DMP messages.
+	pub dmp_remaining_messages: usize,
+	/// The limitations of all registered inbound HRMP channels.
+	pub hrmp_channels_in: HashMap<ParaId, InboundHrmpChannelLimitations>,
+	/// The limitations of all registered outbound HRMP channels.
+	pub hrmp_channels_out: HashMap<ParaId, OutboundHrmpChannelLimitations>,
 	// TODO [now]: some session-wide config members like maximums?
 	// Other expected criteria like the DMP advancement rule?
 	// TODO [now]: validation code hash & allowed code upgrade.
@@ -52,6 +69,10 @@ pub struct ContextLimitations {
 // TODO [now]
 pub struct Error;
 
+/// A context used for judging parachain candidate validity.
+///
+/// This is a combination of base limitations, which come from a
+/// base relay-chain state and a series of updates to those limitations.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Context {
 	base: ContextLimitations,
@@ -62,6 +83,7 @@ pub struct Context {
 }
 
 impl Context {
+	/// Create a context from a given base.
 	pub fn from_base(base: ContextLimitations) -> Self {
 		Context { base: base.clone(), updates: Vec::new(), cumulative: base }
 	}
@@ -78,18 +100,22 @@ impl Context {
 		Ok(context)
 	}
 
+	/// Push an update onto a context.
 	pub fn push(&mut self, update: ContextUpdate) -> Result<(), Error> {
 		unimplemented!()
 	}
 
+	/// Get the limitations associated with this context.
 	pub fn limitations(&self) -> &ContextLimitations {
 		&self.cumulative
 	}
 
+	/// Get all updates associated with this context.
 	pub fn updates(&self) -> &[ContextUpdate] {
 		&self.updates[..]
 	}
 
+	/// Rebase this context onto a new base.
 	pub fn rebase(&self, new_base: ContextLimitations) -> Result<Self, Error> {
 		unimplemented!()
 
@@ -98,16 +124,29 @@ impl Context {
 	}
 }
 
+// TODO [now]: this needs 2 parts: what we take away from the limitations,
+// and what we add to the limitations.
+//
+// The first is the change from the previous relay-parent to the current state.
+// And the second is based on the outputs of the candidate.
+/// An update to a context.
 #[derive(Debug, Clone, PartialEq)]
 pub struct ContextUpdate {
-	ump_messages_submitted: usize,
-	ump_bytes_submitted: usize,
-	dmp_messages_consumed: usize,
-	hrmp_in: HashMap<ParaId, InboundHrmpChannelUpdate>,
-	hrmp_out: HashMap<ParaId, OutboundHrmpChannelUpdate>,
+	// TODO [now] : relay-parent?
+	/// The number of messages submitted to UMP
+	pub ump_messages_submitted: usize,
+	/// The number of message-bytes submitted to UMP
+	pub ump_bytes_submitted: usize,
+	/// The number of DMP messages consumed.
+	pub dmp_messages_consumed: usize,
+	/// Updates to inbound HRMP channels.
+	pub hrmp_in: HashMap<ParaId, InboundHrmpChannelUpdate>,
+	/// Updates to outbound HRMP channels.
+	pub hrmp_out: HashMap<ParaId, OutboundHrmpChannelUpdate>,
 }
 
 impl ContextUpdate {
+	/// Create a blank context update.
 	pub fn blank() -> Self {
 		ContextUpdate {
 			ump_messages_submitted: 0,
