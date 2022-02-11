f3623e45722aee2c639e937443fee8cb88cbb690
Mara Robin B <mara@broda.me>
2022-02-11 20:43:39 +0100

    Bump version (#4904)
    * Bump spec_version
    
    * Bump version to 0.9.17

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 9160,
+	spec_version: 9170,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
