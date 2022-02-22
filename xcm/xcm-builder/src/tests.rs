8f41fbe7e2947ea6e971a8802db1e3f7517f7387
Keith Yeung <kungfukeith11@gmail.com>
2022-02-21 17:00:34 -0800

    Fixes

--- xcm/xcm-builder/src/tests.rs
+++ xcm/xcm-builder/src/tests.rs
@@ -24,7 +24,6 @@ fn basic_setup_works() {
 	assert!(<TestConfig as Config>::IsReserve::filter_asset_location(
 		&(Parent, 100).into(),
 		&Parent.into(),
-		XcmContext::with_message_hash([0; 32]),
 	));
 
 	assert_eq!(to_account(Parachain(1)), Ok(1001));
