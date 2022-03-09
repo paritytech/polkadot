3c1bdaf15d61a5b7e5896c74270808418a6da398
Mara Broda <mara@broda.me>
2022-03-09 20:47:10 +0100

    westend: bump transaction_version

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: version::create_apis_vec![[]],
-	transaction_version: 10,
+	transaction_version: 11,
 	state_version: 0,
 };
 
