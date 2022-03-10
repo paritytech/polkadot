a2a83af848fa80ca99d6b1875821379034931e48
Dan Shields <nukemandan@protonmail.com>
2022-03-10 14:17:28 -0700

    bump all spec version to 9180 and versions to 0.9.18

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -110,7 +110,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kusama"),
 	impl_name: create_runtime_str!("parity-kusama"),
 	authoring_version: 2,
-	spec_version: 9170,
+	spec_version: 9180,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
