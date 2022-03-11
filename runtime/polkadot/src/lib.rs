1cb013cc824fa0402e6ae5ec63a198aa826042da
Dan Shields <35669742+NukeManDan@users.noreply.github.com>
2022-03-11 02:19:47 -0700

    Bump all spec version to `9180` and versions to `0.9.18` (#5083)
    * bump all spec version to 9180 and versions to 0.9.18
    
    * all to 0.9.18
    
    * lock update

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -107,7 +107,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 9170,
+	spec_version: 9180,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
