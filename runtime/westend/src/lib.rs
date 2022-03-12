969bfed53b43accf5bf114079757650cad1c20a6
Georges Dib <georges.dib@gmail.com>
2022-03-12 07:28:01 +0000

    forget to commit one change

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -364,7 +364,7 @@ parameter_types! {
 	pub const VoterSnapshotPerBlock: u32 = 22_500;
 }
 
-sp_npos_elections::generate_solution_type!(
+frame_npos_elections_solution_type::generate_solution_type!(
 	#[compact]
 	pub struct NposCompactSolution16::<
 		VoterIndex = u32,
