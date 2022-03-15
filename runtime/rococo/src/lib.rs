8baaeb200c19e299912bc01f6b7362fbffea49a3
Dan Shields <nukemandan@protonmail.com>
2022-03-14 18:41:04 -0600

    Randomness rm

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -24,7 +24,7 @@ use authority_discovery_primitives::AuthorityId as AuthorityDiscoveryId;
 use beefy_primitives::{crypto::AuthorityId as BeefyId, mmr::MmrLeafVersion};
 use frame_support::{
 	construct_runtime, parameter_types,
-	traits::{Contains, InstanceFilter, KeyOwnerProofSystem, Randomness},
+	traits::{Contains, InstanceFilter, KeyOwnerProofSystem},
 	PalletId,
 };
 use frame_system::EnsureRoot;
