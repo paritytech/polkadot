42d83e66a989de8d5b0463a911e57b5bae305630
Andrei Sandu <andrei-mihail@parity.io>
2022-03-17 15:11:30 +0000

    Revert "strengthen signals-received atomics (#5132)"
    This reverts commit 3f2095fe346e0243f734a12b2bc90141b2d4fa52.

--- node/overseer/overseer-gen/src/lib.rs
+++ node/overseer/overseer-gen/src/lib.rs
@@ -196,15 +196,13 @@ pub struct SignalsReceived(Arc<AtomicUsize>);
 impl SignalsReceived {
 	/// Load the current value of received signals.
 	pub fn load(&self) -> usize {
-		// It's imperative that we prevent reading a stale value from memory because of reordering.
-		// Memory barrier to ensure that no reads or writes in the current thread before this load are reordered.
-		// All writes in other threads using release semantics become visible to the current thread.
-		self.0.load(atomic::Ordering::Acquire)
+		// off by a few is ok
+		self.0.load(atomic::Ordering::Relaxed)
 	}
 
 	/// Increase the number of signals by one.
 	pub fn inc(&self) {
-		self.0.fetch_add(1, atomic::Ordering::AcqRel);
+		self.0.fetch_add(1, atomic::Ordering::Acquire);
 	}
 }
 
