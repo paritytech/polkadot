af0da5403af019755b20f3c2de3f60768f4a23d9
Andrei Sandu <andrei-mihail@parity.io>
2022-03-18 11:42:39 +0000

    Revert "typo"
    This reverts commit c94a8de98d31489798f6224918b9e1a8a505b322.

--- scripts/andrei/polkadot-launch-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ROOT=$(git rev-parse --show-toplevel)
-
-podman run -it \
-    -v $ROOT/target/release/polkadot:/polkadot/polkadot \
-    -v $ROOT/target/release/adder-collator:/polkadot-collator/adder-collator \
-    --network host \
-    polkadot-launch /bin/bash
--- zombienet_tests/scale-net/0001-scale-net.feature
+++
@@ -1,9 +0,0 @@
-Description: Smoke Test scale
-Network: ./0001-scale-net.toml
-Creds: config
-
-
-a: is up
-b: is up
-
-sleep 100000 seconds
