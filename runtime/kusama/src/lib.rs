a7a5387a6720bebe7b336358ee2891654016f61f
Keith Yeung <kungfukeith11@gmail.com>
2022-03-25 07:58:09 -0700

    Add Configuration::ActiveConfig storage key to the whitelist

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -2047,6 +2047,8 @@ sp_api::impl_runtime_apis! {
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef780d41e5e16056765bc8461851072c9d7").to_vec().into(),
 				// Treasury Account
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da95ecffd7b6c0f78751baa9d281e0bfa3a6d6f646c70792f74727372790000000000000000000000000000000000000000").to_vec().into(),
+				// Configuration ActiveConfig
+				hex_literal::hex!("06de3d8a54d27e44a9d5ce189618f22db4b49d95320d9021994c850f25b8e385").to_vec().into(),
 			];
 
 			let mut batches = Vec::<BenchmarkBatch>::new();
