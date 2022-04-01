649e56d23dcc7d3515373f94829307cb578ad9ed
Robert Habermeier <rphmeier@gmail.com>
2022-04-01 22:04:31 +0200

    add more metrics about apd messages

--- node/network/approval-distribution/src/metrics.rs
+++ node/network/approval-distribution/src/metrics.rs
@@ -15,6 +15,7 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use polkadot_node_subsystem_util::metrics::{prometheus, Metrics as MetricsTrait};
+use super::SentMessagesStats;
 
 /// Approval Distribution metrics.
 #[derive(Default, Clone)]
@@ -31,6 +32,19 @@ struct MetricsInner {
 	time_unify_with_peer: prometheus::Histogram,
 	time_import_pending_now_known: prometheus::Histogram,
 	time_awaiting_approval_voting: prometheus::Histogram,
+
+	// TODO [now]: these metrics are (for the most part) temporary for figuring
+	// out why there are so many messages.
+	basic_circulation_messages_total: prometheus::CounterVec<prometheus::U64>,
+	basic_circulation_packets_total: prometheus::CounterVec<prometheus::U64>,
+	unify_with_peer_messages_total: prometheus::CounterVec<prometheus::U64>,
+	unify_with_peer_packets_total: prometheus::CounterVec<prometheus::U64>,
+	resend_messages_total: prometheus::CounterVec<prometheus::U64>,
+	resend_packets_total: prometheus::CounterVec<prometheus::U64>,
+	aggression_messages_total: prometheus::CounterVec<prometheus::U64>,
+	aggression_packets_total: prometheus::CounterVec<prometheus::U64>,
+	new_topology_messages_total: prometheus::CounterVec<prometheus::U64>,
+	new_topology_packets_total: prometheus::CounterVec<prometheus::U64>,
 }
 
 impl Metrics {
@@ -83,6 +97,68 @@ impl Metrics {
 			metrics.aggression_l2_messages_total.inc();
 		}
 	}
+
+	pub(crate) fn note_basic_circulation_stats(&self, stats: SentMessagesStats) {
+		if let Some(metrics) = &self.0 {
+			note_sent_message_stats(
+				stats,
+				&metrics.basic_circulation_messages_total,
+				&metrics.basic_circulation_packets_total,
+			)
+		}
+	}
+
+	pub(crate) fn note_unify_with_peer_stats(&self, stats: SentMessagesStats) {
+		if let Some(metrics) = &self.0 {
+			note_sent_message_stats(
+				stats,
+				&metrics.unify_with_peer_messages_total,
+				&metrics.unify_with_peer_packets_total,
+			)
+		}
+	}
+
+	pub(crate) fn note_resend_stats(&self, stats: SentMessagesStats) {
+		if let Some(metrics) = &self.0 {
+			note_sent_message_stats(
+				stats,
+				&metrics.resend_messages_total,
+				&metrics.resend_packets_total,
+			)
+		}
+	}
+
+	pub(crate) fn note_aggression_stats(&self, stats: SentMessagesStats) {
+		if let Some(metrics) = &self.0 {
+			note_sent_message_stats(
+				stats,
+				&metrics.aggression_messages_total,
+				&metrics.aggression_packets_total,
+			)
+		}
+	}
+
+	pub(crate) fn note_new_topology_stats(&self, stats: SentMessagesStats) {
+		if let Some(metrics) = &self.0 {
+			note_sent_message_stats(
+				stats,
+				&metrics.new_topology_messages_total,
+				&metrics.new_topology_packets_total,
+			)
+		}
+	}
+}
+
+fn note_sent_message_stats(
+	stats: SentMessagesStats,
+	message_counters: &prometheus::CounterVec<prometheus::U64>,
+	packet_counters: &prometheus::CounterVec<prometheus::U64>,
+) {
+	message_counters.with_label_values(&["assignments"]).inc_by(stats.assignments as u64);
+	message_counters.with_label_values(&["approvals"]).inc_by(stats.approvals as u64);
+
+	packet_counters.with_label_values(&["assignments"]).inc_by(stats.assignment_packets as u64);
+	packet_counters.with_label_values(&["approvals"]).inc_by(stats.approval_packets as u64);
 }
 
 impl MetricsTrait for Metrics {
@@ -142,6 +218,106 @@ impl MetricsTrait for Metrics {
 					"polkadot_parachain_time_awaiting_approval_voting",
 					"Time spent awaiting a reply from the Approval Voting Subsystem.",
 				))?,
+				registry,
+			)?,
+			basic_circulation_messages_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_basic_circulation_messages_total",
+						"Number of assignments and approvals sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			basic_circulation_packets_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_basic_circulation_packets_total",
+						"Number of packets sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			unify_with_peer_messages_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_unify_with_peer_messages_total",
+						"Number of assignments and approvals sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			unify_with_peer_packets_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_unify_with_peer_packets_total",
+						"Number of packets sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			resend_messages_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_resend_messages_total",
+						"Number of assignments and approvals sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			resend_packets_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_resend_packets_total",
+						"Number of packets sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			aggression_messages_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_aggression_messages_total",
+						"Number of assignments and approvals sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			aggression_packets_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_aggression_packets_total",
+						"Number of packets sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			new_topology_messages_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_new_topology_messages_total",
+						"Number of assignments and approvals sent by basic circulation",
+					),
+					&["kind"]
+				)?,
+				registry,
+			)?,
+			new_topology_packets_total: prometheus::register(
+				prometheus::CounterVec::new(
+					prometheus::Opts::new(
+						"polkadot_parachain_approval_new_topology_packets_total",
+						"Number of packets sent by basic circulation",
+					),
+					&["kind"]
+				)?,
 				registry,
 			)?,
 		};
