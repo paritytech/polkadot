6d29474d4dc67610c168bd1e0e028670920a0a19
Andrei Sandu <andrei-mihail@parity.io>
2022-04-13 10:07:18 +0000

    write everything to db once every 1000 votes
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    add logs and fix tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    this works
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    fix
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    
    buffer approval voting and fix tests
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/core/dispute-coordinator/src/real/db/v1.rs
+++ node/core/dispute-coordinator/src/real/db/v1.rs
@@ -278,7 +278,7 @@ mod tests {
 	fn overlay_pre_and_post_commit_consistency() {
 		let mut backend = make_db();
 
-		let mut overlay_db = OverlayedBackend::new(&backend);
+		let mut overlay_db = OverlayedBackend::new(&mut backend);
 
 		overlay_db.write_earliest_session(0);
 		overlay_db.write_earliest_session(1);
@@ -369,7 +369,7 @@ mod tests {
 	fn overlay_preserves_candidate_votes_operation_order() {
 		let mut backend = make_db();
 
-		let mut overlay_db = OverlayedBackend::new(&backend);
+		let mut overlay_db = OverlayedBackend::new(&mut backend);
 		overlay_db.delete_candidate_votes(1, CandidateHash(Hash::repeat_byte(1)));
 
 		overlay_db.write_candidate_votes(
@@ -396,7 +396,7 @@ mod tests {
 			ParaId::from(1),
 		);
 
-		let mut overlay_db = OverlayedBackend::new(&backend);
+		let mut overlay_db = OverlayedBackend::new(&mut backend);
 		overlay_db.write_candidate_votes(
 			1,
 			CandidateHash(Hash::repeat_byte(1)),
@@ -446,7 +446,7 @@ mod tests {
 			invalid: Vec::new(),
 		};
 
-		let mut overlay_db = OverlayedBackend::new(&backend);
+		let mut overlay_db = OverlayedBackend::new(&mut backend);
 		overlay_db.write_earliest_session(prev_earliest_session);
 		overlay_db.write_recent_disputes(
 			vec![
@@ -470,7 +470,7 @@ mod tests {
 		let write_ops = overlay_db.into_write_ops();
 		backend.write(write_ops).unwrap();
 
-		let mut overlay_db = OverlayedBackend::new(&backend);
+		let mut overlay_db = OverlayedBackend::new(&mut backend);
 		note_current_session(&mut overlay_db, current_session).unwrap();
 
 		assert_eq!(overlay_db.load_earliest_session().unwrap(), Some(new_earliest_session));
