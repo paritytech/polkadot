9dde4cefe0cb17b91fef99b7013ff1f68ded5d51
Chris Sosnin <chris125_@live.com>
2022-04-21 12:11:16 +0300

    spellcheck

--- node/subsystem-types/src/messages.rs
+++ node/subsystem-types/src/messages.rs
@@ -210,7 +210,7 @@ pub enum CollatorProtocolMessage {
 	///
 	/// The hash is the relay parent.
 	Seconded(Hash, SignedFullStatement),
-	/// Issue a (pre)connect request for the given relay parent, i.e. get the peer set ready
+	/// Issue a preconnect request for the given relay parent, i.e. get the peer set ready
 	/// for collating on it or its child.
 	PreConnectAsCollator(Hash),
 }
