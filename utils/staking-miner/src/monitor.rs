df316d1df9046f29715c96f91eec9e31b3180d14
kianenigma <kian@parity.io>
2022-05-03 09:43:02 +0100

    checkpoints

--- utils/staking-miner/src/monitor.rs
+++ utils/staking-miner/src/monitor.rs
@@ -1,10 +1,17 @@
-use crate::{prelude::*, MonitorConfig, SubmissionStrategy};
+use crate::{
+	prelude::*,
+	runtime::runtime_types::{
+		pallet_election_provider_multi_phase::{self, RoundSnapshot},
+		sp_core::crypto::AccountId32,
+	},
+	BalanceIterations, Balancing, MonitorConfig, Solver, SubmissionStrategy,
+};
 use codec::Encode;
 use jsonrpsee::core::Error as RpcError;
 use sc_transaction_pool_api::TransactionStatus;
 use sp_runtime::Perbill;
 use std::sync::Arc;
-use subxt::sp_core::storage::StorageKey;
+use subxt::{sp_core::storage::StorageKey, sp_runtime::Perbill};
 use tokio::sync::mpsc;
 
 /// Ensure that now is the signed phase.
@@ -39,7 +46,7 @@ async fn ensure_no_previous_solution(
 
 		if let Some(submission) = submission {
 			if &submission.who == us {
-				return Err(anyhow::anyhow!("Already submitted"));
+				return Err(anyhow::anyhow!("Already submitted"))
 			}
 		}
 	}
@@ -75,7 +82,7 @@ async fn ensure_no_better_solution(
 		};
 
 		if !score.strict_threshold_better(other_score, epsilon) {
-			return Err(anyhow::anyhow!("better score already exist"));
+			return Err(anyhow::anyhow!("better score already exist"))
 		}
 	}
 
@@ -138,6 +145,93 @@ pub(crate) async fn run_cmd(
 	}
 }
 
+mod mock_runtime {
+	use super::*;
+	use crate::runtime::runtime_types::primitive_types::H256;
+	use core_primitives::BlakeTwo256;
+	use pallet_election_provider_multi_phase as multi_phase;
+	use sp_runtime::traits::Header;
+
+	pub type Block = sp_runtime::generic::Block<Header, UncheckedExtrinsic>;
+	pub type UncheckedExtrinsic = sp_runtime::generic::UncheckedExtrinsic<AccountId, Call, (), ()>;
+
+	frame_support::construct_runtime!(
+		pub enum Runtime where
+			Block = Block,
+			NodeBlock = Block,
+			UncheckedExtrinsic = UncheckedExtrinsic
+		{
+			System: frame_system,
+			MultiPhase: multi_phase::{Pallet, Call, Event<T>},
+		}
+	);
+
+	// IMPORTANT: should be same as real runtime
+	pub(crate) type Balance = u32;
+	pub(crate) type BlockNumber = u32;
+	pub(crate) type VoterIndex = u32;
+	pub(crate) type TargetIndex = u16;
+
+	impl frame_system::Config for Runtime {
+		type SS58Prefix = ();
+		type BaseCallFilter = frame_support::traits::Everything;
+		type Origin = Origin;
+		type Index = u64;
+		type BlockNumber = BlockNumber;
+		type Call = Call;
+		type Hash = H256;
+		type Hashing = BlakeTwo256;
+		type AccountId = AccountId;
+		type Lookup = IdentityLookup<Self::AccountId>;
+		type Header = Header;
+		type Event = Event;
+		type BlockHashCount = ();
+		type DbWeight = ();
+		type BlockLength = ();
+		type BlockWeights = BlockWeights;
+		type Version = ();
+		type PalletInfo = PalletInfo;
+		type AccountData = pallet_balances::AccountData<u64>;
+		type OnNewAccount = ();
+		type OnKilledAccount = ();
+		type SystemWeightInfo = ();
+		type OnSetCode = ();
+		type MaxConsumers = ConstU32<16>;
+	}
+	impl pallet_election_provider_multi_phase::Config for MockRuntime {
+		type Event = Event;
+		type Currency = Balances;
+		type EstimateCallFee = frame_support::traits::ConstU32<8>;
+		type SignedPhase = SignedPhase;
+		type UnsignedPhase = UnsignedPhase;
+		type BetterUnsignedThreshold = BetterUnsignedThreshold;
+		type BetterSignedThreshold = BetterSignedThreshold;
+		type OffchainRepeat = OffchainRepeat;
+		type MinerMaxWeight = MinerMaxWeight;
+		type MinerMaxLength = MinerMaxLength;
+		type MinerTxPriority = MinerTxPriority;
+		type SignedRewardBase = SignedRewardBase;
+		type SignedDepositBase = SignedDepositBase;
+		type SignedDepositByte = ();
+		type SignedDepositWeight = ();
+		type SignedMaxWeight = SignedMaxWeight;
+		type SignedMaxSubmissions = SignedMaxSubmissions;
+		type SignedMaxRefunds = SignedMaxRefunds;
+		type SlashHandler = ();
+		type RewardHandler = ();
+		type DataProvider = StakingMock;
+		type WeightInfo = DualMockWeightInfo;
+		type BenchmarkingConfig = TestBenchmarkingConfig;
+		type Fallback = MockFallback;
+		type GovernanceFallback = NoFallback<Self>;
+		type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+		type Solution = TestNposSolution;
+		type MaxElectingVoters = MaxElectingVoters;
+		type MaxElectableTargets = MaxElectableTargets;
+		type Solver = SequentialPhragmen<AccountId, SolutionAccuracyOf<Runtime>, Balancing>;
+	}
+}
+
 /// Construct extrinsic at given block and watch it.
 async fn send_and_watch_extrinsic(
 	tx: tokio::sync::mpsc::UnboundedSender<()>,
@@ -160,19 +254,39 @@ async fn send_and_watch_extrinsic(
 	let api: RuntimeApi = client.to_runtime_api();
 
 	if ensure_signed_phase(&api, hash).await.is_err() {
-		return;
+		return
 	}
 
 	if ensure_no_previous_solution(&api, hash, signer.account_id()).await.is_err() {
-		return;
+		return
 	}
 
-	let mut ext = match crate::create_election_ext(&api, Some(hash), vec![]).await {
-		Ok(ext) => ext,
-		Err(err) => {
-			log::debug!(target: LOG_TARGET, "Skipping block {}; {}", at.number, err);
-			return;
-		},
+	let raw_solution = {
+		let RoundSnapshot { voters, targets } = api
+			.storage()
+			.election_provider_multi_phase()
+			.snapshot(Some(hash))
+			.await?
+			.unwrap();
+		let desired_targets = api
+			.storage()
+			.election_provider_multi_phase()
+			.desired_targets(Some(hash))
+			.await?
+			.unwrap();
+
+		use frame_election_provider_support::{PhragMMS, SequentialPhragmen};
+
+		match config.solver {
+			Solver::SeqPhragmen { iterations } => {
+				BalanceIterations::set(*iterations);
+				SequentialPhragmen::<AccountId32, Perbill, Balancing>::solve(todo!())
+			},
+			Solver::PhragMMS { iterations } => {
+				BalanceIterations::set(*iterations);
+				PhragMMS::<AccountId32, Perbill, Balancing>::solve();
+			},
+		}
 	};
 
 	/*
