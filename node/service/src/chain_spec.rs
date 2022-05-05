1d832a5df0059827ba8bcf7b2a3de5fa73be6e26
Giles Cope <gilescope@gmail.com>
2022-05-05 09:26:43 +0100

    Westend like Polkadot now has 10 decimal places

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -1158,7 +1158,7 @@ pub fn westend_staging_testnet_config() -> Result<WestendChainSpec, String> {
 		),
 		Some(DEFAULT_PROTOCOL_ID),
 		None,
-		None,
+		Some(polkadot_chain_spec_properties()),
 		Default::default(),
 	))
 }
@@ -1909,7 +1909,7 @@ pub fn westend_local_testnet_config() -> Result<WestendChainSpec, String> {
 		None,
 		Some(DEFAULT_PROTOCOL_ID),
 		None,
-		None,
+		Some(polkadot_chain_spec_properties()),
 		Default::default(),
 	))
 }
