cac02e0dec59db5cb9539768479d0f19caa571bd
Jun Jiang <jasl9187@hotmail.com>
2022-05-05 22:39:30 +0800

    add `force_batch` weight entry to runtimes (#5452)

--- runtime/rococo/src/weights/pallet_utility.rs
+++ runtime/rococo/src/weights/pallet_utility.rs
@@ -63,4 +63,11 @@ impl<T: frame_system::Config> pallet_utility::WeightInfo for WeightInfo<T> {
 	fn dispatch_as() -> Weight {
 		(9_357_000 as Weight)
 	}
+	fn force_batch(c: u32, ) -> Weight {
+		(12_912_000 as Weight)
+			// Standard Error: 2_000
+			.saturating_add((4_632_000 as Weight).saturating_mul(c as Weight))
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+	}
 }
