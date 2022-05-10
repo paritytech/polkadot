8c41516984eab0e26b760a5d68505698a7f96ddb
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-05-10 11:09:22 +0200

    cargo fmt

--- tests/benchmark_block_works.rs
+++ tests/benchmark_block_works.rs
@@ -22,7 +22,12 @@ use nix::{
 	sys::signal::{kill, Signal::SIGINT},
 	unistd::Pid,
 };
-use std::{path::Path, process::{self, Command}, result::Result, time::Duration};
+use std::{
+	path::Path,
+	process::{self, Command},
+	result::Result,
+	time::Duration,
+};
 use tempfile::tempdir;
 
 pub mod common;
