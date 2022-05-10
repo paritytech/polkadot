c1075f010513629a7a20b9bbe273b41ef52296ca
kianenigma <kian@parity.io>
2022-05-10 16:35:50 +0100

    add weights

--- runtime/westend/src/weights/pallet_nomination_pools.rs
+++ runtime/westend/src/weights/pallet_nomination_pools.rs
@@ -250,5 +250,12 @@ impl<T: frame_system::Config> pallet_nomination_pools::WeightInfo for WeightInfo
 		(2_793_000 as Weight)
 			.saturating_add(T::DbWeight::get().writes(5 as Weight))
 	}
+
+	// Storage: NominationPools BondedPools (r:1 w:1)
+	fn update_roles() -> Weight {
+		(18_608_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
+			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+	}
 }
 
