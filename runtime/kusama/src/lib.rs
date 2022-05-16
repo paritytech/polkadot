d7f9ccf66f5cd9e68c3fedf7fbf0e3016ce7145c
kianenigma <kian@parity.io>
2022-05-16 20:03:09 +0100

    fmt

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1622,7 +1622,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(InitiatePoolConfigs, pallet_nomination_pools::migration::v1::MigrateToV1<Runtime>) ,
+	(InitiatePoolConfigs, pallet_nomination_pools::migration::v1::MigrateToV1<Runtime>),
 >;
 /// The payload being signed in the transactions.
 pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
