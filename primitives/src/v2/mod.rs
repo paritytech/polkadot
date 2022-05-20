6ed1f84f613d0ce3782a71ae6147e199177c3c19
Boluwatife Bakre <tifebakre@yahoo.co.uk>
2022-05-20 19:03:51 +0100

    FromVec update
    Co-authored-by: Bernhard Schuster <bernhard@ahoi.io>

--- primitives/src/v2/mod.rs
+++ primitives/src/v2/mod.rs
@@ -1627,7 +1627,7 @@ impl<K: From<usize>, V: Clone> TypeVec<K, V> {
 impl<K: From<usize>, V: Clone> From<Vec<V>> for TypeVec<K, V> {
 	fn from(vec: Vec<V>) -> Self {
 		let mut type_vec: TypeVec<K, V> = TypeVec::new();
-		for value in vec.into_iter() {
+		for value in vec {
 			type_vec.0.push(value)
 		}
 		type_vec
