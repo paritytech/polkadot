760124789d4b1eb6f8fbbf0d12a52528edf5e656
Robert Klotzner <robert.klotzner@gmx.at>
2022-05-25 12:49:49 +0200

    Fix cleanup of old votes.

--- node/core/dispute-coordinator/src/db/v1.rs
+++ node/core/dispute-coordinator/src/db/v1.rs
@@ -31,12 +31,24 @@ use crate::{
 	backend::{Backend, BackendWriteOp, OverlayedBackend},
 	error::{FatalError, FatalResult},
 	status::DisputeStatus,
-	DISPUTE_WINDOW,
+	DISPUTE_WINDOW, LOG_TARGET,
 };
 
 const RECENT_DISPUTES_KEY: &[u8; 15] = b"recent-disputes";
 const EARLIEST_SESSION_KEY: &[u8; 16] = b"earliest-session";
 const CANDIDATE_VOTES_SUBKEY: &[u8; 15] = b"candidate-votes";
+/// Until what session have votes been cleaned up already?
+const CLEANED_VOTES_WATERMARK_KEY: &[u8; 24] = b"earliest-cleaned-session";
+
+/// Restrict number of cleanup operations.
+///
+/// On the first run we are starting at session 0 going up all the way to the current session -
+/// this should not be done at once, but rather in smaller batches so nodes won't get stalled by
+/// this.
+#[cfg(test)]
+const MAX_CLEAN_BATCH_SIZE: u32 = 10;
+#[cfg(not(test))]
+const MAX_CLEAN_BATCH_SIZE: u32 = 1000;
 
 pub struct DbBackend {
 	inner: Arc<dyn Database>,
@@ -71,6 +83,8 @@ impl Backend for DbBackend {
 
 	/// Atomically writes the list of operations, with later operations taking precedence over
 	/// prior.
+	///
+	/// This also takes care of purging old votes (of obsolete sessions).
 	fn write<I>(&mut self, ops: I) -> FatalResult<()>
 	where
 		I: IntoIterator<Item = BackendWriteOp>,
@@ -79,6 +93,28 @@ impl Backend for DbBackend {
 		for op in ops {
 			match op {
 				BackendWriteOp::WriteEarliestSession(session) => {
+					// Cleanup old votes in db:
+					let watermark =
+						load_cleaned_votes_watermark(&*self.inner, &self.config)?.unwrap_or(0);
+					let clean_until = if session.saturating_sub(watermark) > MAX_CLEAN_BATCH_SIZE {
+						watermark + MAX_CLEAN_BATCH_SIZE
+					} else {
+						session
+					};
+					for index in watermark..clean_until {
+						tx.delete_prefix(
+							self.config.col_data,
+							&candidate_votes_session_prefix(index),
+						);
+					}
+					// New watermark:
+					tx.put_vec(
+						self.config.col_data,
+						CLEANED_VOTES_WATERMARK_KEY,
+						clean_until.encode(),
+					);
+
+					// Actually write the earliest session.
 					tx.put_vec(self.config.col_data, EARLIEST_SESSION_KEY, session.encode());
 				},
 				BackendWriteOp::WriteRecentDisputes(recent_disputes) => {
@@ -108,7 +144,23 @@ fn candidate_votes_key(session: SessionIndex, candidate_hash: &CandidateHash) ->
 	// big-endian encoding is used to ensure lexicographic ordering.
 	buf[15..][..4].copy_from_slice(&session.to_be_bytes());
 	candidate_hash.using_encoded(|s| buf[(15 + 4)..].copy_from_slice(s));
+	gum::warn!(
+		target: LOG_TARGET,
+		?session,
+		?candidate_hash,
+		?buf,
+		"Buf for given session and candidate hash"
+	);
 
+	buf
+}
+
+fn candidate_votes_session_prefix(session: SessionIndex) -> [u8; 15 + 4] {
+	let mut buf = [0u8; 15 + 4];
+	buf[..15].copy_from_slice(CANDIDATE_VOTES_SUBKEY);
+
+	// big-endian encoding is used to ensure lexicographic ordering.
+	buf[15..][..4].copy_from_slice(&session.to_be_bytes());
 	buf
 }
 
@@ -244,9 +296,7 @@ pub(crate) fn note_current_session(
 
 				if pruned_disputes.len() != 0 {
 					overlay_db.write_recent_disputes(new_recent_disputes);
-					for ((session, candidate_hash), _) in pruned_disputes {
-						overlay_db.delete_candidate_votes(session, candidate_hash);
-					}
+					// Note: Deleting old candidate votes is handled in `write` based on the earliest session.
 				}
 			}
 		},
@@ -258,6 +308,18 @@ pub(crate) fn note_current_session(
 	Ok(())
 }
 
+/// Until what session votes have been cleaned up already.
+///
+/// That is the db has already been purged of votes for sessions older than the returned
+/// `SessionIndex`.
+fn load_cleaned_votes_watermark(
+	db: &dyn Database,
+	config: &ColumnConfiguration,
+) -> FatalResult<Option<SessionIndex>> {
+	load_decode(db, config.col_data, CLEANED_VOTES_WATERMARK_KEY)
+		.map_err(|e| FatalError::DbReadFailed(e))
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -266,7 +328,7 @@ mod tests {
 
 	fn make_db() -> DbBackend {
 		let db = kvdb_memorydb::create(1);
-		let db = polkadot_node_subsystem_util::database::kvdb_impl::DbAdapter::new(db, &[]);
+		let db = polkadot_node_subsystem_util::database::kvdb_impl::DbAdapter::new(db, &[0]);
 		let store = Arc::new(db);
 		let config = ColumnConfiguration { col_data: 0 };
 		DbBackend::new(store, config)
@@ -368,13 +430,24 @@ mod tests {
 		let mut backend = make_db();
 
 		let mut overlay_db = OverlayedBackend::new(&backend);
-		overlay_db.delete_candidate_votes(1, CandidateHash(Hash::repeat_byte(1)));
 
 		overlay_db.write_candidate_votes(
 			1,
 			CandidateHash(Hash::repeat_byte(1)),
 			CandidateVotes {
-				candidate_receipt: dummy_candidate_receipt(dummy_hash()),
+				candidate_receipt: dummy_candidate_receipt(Hash::random()),
+				valid: Vec::new(),
+				invalid: Vec::new(),
+			},
+		);
+
+		let receipt = dummy_candidate_receipt(dummy_hash());
+
+		overlay_db.write_candidate_votes(
+			1,
+			CandidateHash(Hash::repeat_byte(1)),
+			CandidateVotes {
+				candidate_receipt: receipt.clone(),
 				valid: Vec::new(),
 				invalid: Vec::new(),
 			},
@@ -388,37 +461,9 @@ mod tests {
 				.load_candidate_votes(1, &CandidateHash(Hash::repeat_byte(1)))
 				.unwrap()
 				.unwrap()
-				.candidate_receipt
-				.descriptor
-				.para_id,
-			ParaId::from(1),
+				.candidate_receipt,
+			receipt,
 		);
-
-		let mut overlay_db = OverlayedBackend::new(&backend);
-		overlay_db.write_candidate_votes(
-			1,
-			CandidateHash(Hash::repeat_byte(1)),
-			CandidateVotes {
-				candidate_receipt: {
-					let mut receipt = dummy_candidate_receipt(dummy_hash());
-					receipt.descriptor.para_id = ParaId::from(5_u32);
-
-					receipt
-				},
-				valid: Vec::new(),
-				invalid: Vec::new(),
-			},
-		);
-
-		overlay_db.delete_candidate_votes(1, CandidateHash(Hash::repeat_byte(1)));
-
-		let write_ops = overlay_db.into_write_ops();
-		backend.write(write_ops).unwrap();
-
-		assert!(backend
-			.load_candidate_votes(1, &CandidateHash(Hash::repeat_byte(1)))
-			.unwrap()
-			.is_none());
 	}
 
 	#[test]
@@ -482,6 +527,12 @@ mod tests {
 			.into_iter()
 			.collect(),
 		);
+
+		// Votes are only cleaned up after actual write:
+		let write_ops = overlay_db.into_write_ops();
+		backend.write(write_ops).unwrap();
+
+		let overlay_db = OverlayedBackend::new(&backend);
 
 		assert!(overlay_db.load_candidate_votes(very_old, &hash_a).unwrap().is_none());
 		assert!(overlay_db.load_candidate_votes(slightly_old, &hash_b).unwrap().is_none());
