40b499703b32d77de4ea0253c27c0d5e147da848
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-06-01 18:27:53 +0200

    Fixup
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -538,15 +538,16 @@ pub fn run() -> Result<()> {
 								unwrap_client!(
 									client,
 									cmd.run(client.clone(), inherent_data, &ext_factory)
+										.map_err(Error::SubstrateCli)
 								)
 							},
 							BenchmarkCmd::Overhead(cmd) => unwrap_client!(
 								client,
 								cmd.run(config, client.clone(), inherent_data, &remark_builder)
+									.map_err(Error::SubstrateCli)
 							),
 							_ => unreachable!("Ensured by the outside match; qed"),
 						}
-						.map_err(Error::SubstrateCli)
 					})
 				},
 				BenchmarkCmd::Pallet(cmd) => {
