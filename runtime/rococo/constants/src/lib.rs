e074eb0d592341ce4e3d1aa45bd2cb74404a16f3
al3mart <alejandro@parity.io>
2022-06-16 19:07:00 +0200

    money constatns to mirror Kusama

--- runtime/rococo/constants/src/lib.rs
+++ runtime/rococo/constants/src/lib.rs
@@ -26,12 +26,13 @@ pub mod currency {
 	pub const EXISTENTIAL_DEPOSIT: Balance = 1 * CENTS;
 
 	pub const UNITS: Balance = 1_000_000_000_000;
-	pub const CENTS: Balance = UNITS / 100;
+	pub const CENTS: Balance = UNITS / 30_000;
 	pub const MILLICENTS: Balance = CENTS / 1_000;
+	pub const GRAND: Balance = CENTS * 100_000;
 
 	pub const fn deposit(items: u32, bytes: u32) -> Balance {
-		items as Balance * 1 * UNITS + (bytes as Balance) * 5 * MILLICENTS
-	}
+		items as Balance * 2_000 * CENTS + (bytes as Balance) * 100 * MILLICENTS
+}
 }
 
 /// Time and blocks.
