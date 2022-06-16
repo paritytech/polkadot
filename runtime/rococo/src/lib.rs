92d8e9e379f245ae0e3a8ac8d897b7acb2ae3de1
Mara Broda <mara@broda.me>
2022-06-16 13:05:50 +0200

    rococo: bump spec_version to 9250

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -100,7 +100,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("rococo"),
 	impl_name: create_runtime_str!("parity-rococo-v2.0"),
 	authoring_version: 0,
-	spec_version: 9230,
+	spec_version: 9250,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
