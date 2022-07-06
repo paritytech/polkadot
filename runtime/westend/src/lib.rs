904763781fd1ee87483e20b29a76a5543e164d80
kianenigma <kian@parity.io>
2022-07-06 11:12:04 +0100

    fix

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -1173,7 +1173,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(),
+	pallet_nomination_pools::migration::v2::MigrateToV2<Runtime>,
 >;
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<Call, SignedExtra>;
