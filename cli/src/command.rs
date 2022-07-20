a02a42778edb665800cf76f1e52e0deae3a5f6ec
Bernhard Schuster <bernhard@ahoi.io>
2022-07-20 10:28:05 +0200

    fixup removed trailing ;

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -247,11 +247,11 @@ macro_rules! unwrap_client {
 /// Should only be used in release build since the check would take too much time otherwise.
 fn host_perf_check() -> Result<()> {
 	#[cfg(not(build_type = "release"))]
-	return Err(PerfCheckError::WrongBuildType.into())
+	return Err(PerfCheckError::WrongBuildType.into());
 
 	#[allow(unreachable_code)]
 	#[cfg(not(feature = "hostperfcheck"))]
-	return Err(PerfCheckError::FeatureNotEnabled { feature: "hostperfcheck" }.into())
+	return Err(PerfCheckError::FeatureNotEnabled { feature: "hostperfcheck" }.into());
 
 	#[cfg(all(build_type = "release", feature = "hostperfcheck"))]
 	{
