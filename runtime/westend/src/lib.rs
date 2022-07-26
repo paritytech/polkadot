284054e37e9d3f26ba702939840d9051217be963
Szegoo <sakacszergej@gmail.com>
2022-07-26 10:41:10 +0200

    update runtimes

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -1574,7 +1574,7 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 
-	impl pallet_nomination_pools_rpc_runtime_api::NominationPoolsApi<
+	impl pallet_nomination_pools_runtime_api::NominationPoolsApi<
 		Block,
 		AccountId,
 		Balance,
