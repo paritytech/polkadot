05eb694a36311ea65e1f4435e99ff15957e55d58
Mara Robin B <mara@broda.me>
2022-08-01 15:13:21 +0200

    update weights (#5844)
    * westend: update weights
    
    * kusama: update weights
    
    * polkadot: update weights
    
    * rococo: update weights
    
    * update BlockExecutionWeight
    
    * kusama: readd phragmen remove_member_wrong_refund weight
    
    * polkadot: readd phragmen remove_member_wrong_refund weight

--- runtime/westend/constants/src/weights/block_weights.rs
+++ runtime/westend/constants/src/weights/block_weights.rs
@@ -16,8 +16,8 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2022-07-08 (Y/M/D)
-//! HOSTNAME: `bm4`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
+//! DATE: 2022-07-29 (Y/M/D)
+//! HOSTNAME: `bm5`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
@@ -45,16 +45,16 @@ parameter_types! {
 	/// Calculated by multiplying the *Average* with `1` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 4_863_227, 5_077_270
-	///   Average:  4_930_882
-	///   Median:   4_919_682
-	///   Std-Dev:  44924.76
+	///   Min, Max: 5_164_333, 5_384_350
+	///   Average:  5_251_804
+	///   Median:   5_253_341
+	///   Std-Dev:  53972.35
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 5_042_706
-	///   95th: 5_016_556
-	///   75th: 4_958_742
-	pub const BlockExecutionWeight: Weight = 4_930_882 * WEIGHT_PER_NANOS;
+	///   99th: 5_376_408
+	///   95th: 5_346_393
+	///   75th: 5_286_569
+	pub const BlockExecutionWeight: Weight = 5_251_804 * WEIGHT_PER_NANOS;
 }
 
 #[cfg(test)]
