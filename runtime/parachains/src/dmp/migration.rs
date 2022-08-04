2e9a8b2c946347eb7240443a4410701343d4fa0d
Andrei Sandu <andrei-mihail@parity.io>
2022-08-04 13:56:13 +0000

    v1 migration test
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- runtime/parachains/src/dmp/migration.rs
+++ runtime/parachains/src/dmp/migration.rs
@@ -16,38 +16,41 @@
 
 //! A module that is responsible for migration of DMP storage.
 
-use frame_support::{storage_alias, traits::StorageVersion, Twox64Concat};
+use frame_support::{
+	ensure,
+	pallet_prelude::ValueQuery,
+	storage::migration::clear_storage_prefix,
+	storage_alias,
+	traits::{Get, PalletInfoAccess, StorageVersion},
+	Twox64Concat,
+};
+
+use primitives::v2::BlockNumber;
+use sp_std::vec::Vec;
 
 /// The current storage version.
 pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
-pub mod v1 {
+pub mod v0 {
 	use super::*;
-	use crate::{
-		configuration, dmp,
-		dmp::{Config, InboundDownwardMessage},
-		ParaId,
-	};
-	use frame_support::{
-		ensure,
-		pallet_prelude::ValueQuery,
-		storage::migration::clear_storage_prefix,
-		traits::{Get, PalletInfoAccess},
-	};
-	use primitives::v2::BlockNumber;
-	use sp_std::vec::Vec;
+
+	pub use primitives::v2::{Id as ParaId, InboundDownwardMessage};
 
 	#[storage_alias]
-	pub(crate) type DownwardMessageQueues<T: Config> = StorageMap<
-		dmp::Pallet<T>,
+	pub(crate) type DownwardMessageQueues<T: crate::dmp::Config> = StorageMap<
+		crate::dmp::Pallet<T>,
 		Twox64Concat,
 		ParaId,
 		Vec<InboundDownwardMessage<BlockNumber>>,
 		ValueQuery,
 	>;
+}
+pub mod v1 {
+	use super::*;
+	use crate::{configuration, dmp, dmp::Config};
 
 	pub fn pre_migrate<T: Config>() -> Result<(), &'static str> {
-		let count = DownwardMessageQueues::<T>::iter()
+		let count = v0::DownwardMessageQueues::<T>::iter()
 			.filter(|(para_id, messages)| {
 				if messages.len() > 0 {
 					log::debug!(
@@ -84,7 +87,7 @@ pub mod v1 {
 		);
 
 		if version < STORAGE_VERSION {
-			let weight = DownwardMessageQueues::<T>::iter()
+			let weight = v0::DownwardMessageQueues::<T>::iter()
 				.map(|(para_id, messages)| {
 					let mut weight = 0u64;
 					for message in messages {
@@ -118,9 +121,75 @@ pub mod v1 {
 
 	pub fn post_migrate<T: Config>() -> Result<(), &'static str> {
 		let old_storage_clean =
-			DownwardMessageQueues::<T>::iter().any(|(_, messages)| messages.len() > 0);
+			v0::DownwardMessageQueues::<T>::iter().any(|(_, messages)| messages.len() > 0);
 
 		ensure!(!old_storage_clean, "Upgrade did not cleanup old storage");
 		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::mock::{new_test_ext, MockGenesisConfig, Test};
+	use parity_scale_codec::{Decode, Encode};
+	use primitives::v2::{Id as ParaId, MessageQueueChain};
+	use sp_runtime::traits::{BlakeTwo256, Hash};
+
+	fn default_genesis_config() -> MockGenesisConfig {
+		MockGenesisConfig {
+			configuration: crate::configuration::GenesisConfig {
+				config: crate::configuration::HostConfiguration {
+					max_downward_message_size: 1024,
+					..Default::default()
+				},
+			},
+			..Default::default()
+		}
+	}
+
+	#[test]
+	fn test_migrate_to_v1() {
+		let messages = (0..100)
+			.map(|v| v0::InboundDownwardMessage {
+				sent_at: v,
+				msg: format!("message {}", v).encode(),
+			})
+			.collect::<Vec<_>>();
+
+		let mut mqc = MessageQueueChain::default();
+
+		for (index, msg) in messages.iter().enumerate() {
+			mqc = mqc.extend(index as u32, BlakeTwo256::hash_of(&msg.msg));
+		}
+
+		new_test_ext(default_genesis_config()).execute_with(|| {
+			// Write queues for a few parachains.
+			for para_id in 0..40 {
+				frame_support::storage::unhashed::put_raw(
+					&v0::DownwardMessageQueues::<Test>::hashed_key_for(v0::ParaId::from(para_id)),
+					&messages.encode(),
+				);
+			}
+
+			v1::migrate::<Test>();
+			v1::post_migrate::<Test>().unwrap();
+
+			// Iterate all paras after migration and check mqc of all messages.
+			for para_id in 0..40 {
+				// Get the first 100 pages of messages.
+				let all_messages =
+					<crate::dmp::Pallet<Test>>::dmq_contents_bounded(ParaId::from(para_id), 0, 100);
+
+				let mut migrated_mqc = MessageQueueChain::default();
+
+				for (index, msg) in all_messages.into_iter().enumerate() {
+					migrated_mqc =
+						migrated_mqc.extend(index as u32, BlakeTwo256::hash_of(&msg.msg));
+				}
+
+				assert_eq!(migrated_mqc.head(), mqc.head())
+			}
+		});
 	}
 }
