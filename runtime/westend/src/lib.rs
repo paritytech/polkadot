107456be0f461bb3ef50daf9bc4053782a537646
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2022-08-11 07:24:11 +0100

    add unit tests to run runtime migrations (#5865)
    * add unit tests to run runtime migrations
    
    * Update runtime/westend/src/lib.rs
    
    Co-authored-by: Mara Robin B. <mara@broda.me>
    
    * fix
    
    * fmt
    
    * Update runtime/westend/src/lib.rs
    
    Co-authored-by: Mara Robin B. <mara@broda.me>

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -1748,3 +1748,38 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 }
+
+#[cfg(all(test, feature = "try-runtime"))]
+mod remote_tests {
+	use super::*;
+	use frame_try_runtime::runtime_decl_for_TryRuntime::TryRuntime;
+	use remote_externalities::{
+		Builder, Mode, OfflineConfig, OnlineConfig, SnapshotConfig, Transport,
+	};
+	use std::env::var;
+
+	#[tokio::test]
+	async fn run_migrations() {
+		sp_tracing::try_init_simple();
+		let transport: Transport =
+			var("WS").unwrap_or("wss://westend-rpc.polkadot.io:443".to_string()).into();
+		let maybe_state_snapshot: Option<SnapshotConfig> = var("SNAP").map(|s| s.into()).ok();
+		let mut ext = Builder::<Block>::default()
+			.mode(if let Some(state_snapshot) = maybe_state_snapshot {
+				Mode::OfflineOrElseOnline(
+					OfflineConfig { state_snapshot: state_snapshot.clone() },
+					OnlineConfig {
+						transport,
+						state_snapshot: Some(state_snapshot),
+						..Default::default()
+					},
+				)
+			} else {
+				Mode::Online(OnlineConfig { transport, ..Default::default() })
+			})
+			.build()
+			.await
+			.unwrap();
+		ext.execute_with(|| Runtime::on_runtime_upgrade());
+	}
+}
