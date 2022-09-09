2fff1fa4123b635ff83eb7b4d927792b785ac7c6
Mara Broda <mara@broda.me>
2022-09-09 12:30:35 +0200

    kusama: update weights

--- runtime/kusama/constants/src/weights/block_weights.rs
+++ runtime/kusama/constants/src/weights/block_weights.rs
@@ -16,13 +16,13 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2022-08-19 (Y/M/D)
+//! DATE: 2022-09-08 (Y/M/D)
 //! HOSTNAME: `bm4`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
 //! WEIGHT-PATH: `runtime/kusama/constants/src/weights/`
-//! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1`, WEIGHT-ADD: `0`
+//! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
 //   ./target/production/polkadot
@@ -42,23 +42,24 @@ use frame_support::{
 
 parameter_types! {
 	/// Time to execute an empty block.
-	/// Calculated by multiplying the *Average* with `1` and adding `0`.
+	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 6_094_385, 6_351_993
-	///   Average:  6_192_341
-	///   Median:   6_193_838
-	///   Std-Dev:  63893.84
+	///   Min, Max: 350_097, 393_199
+	///   Average:  356_760
+	///   Median:   353_859
+	///   Std-Dev:  8772.35
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 6_332_047
-	///   95th: 6_308_225
-	///   75th: 6_236_204
-	pub const BlockExecutionWeight: Weight = WEIGHT_PER_NANOS.saturating_mul(6_192_341);
+	///   99th: 390_162
+	///   95th: 383_320
+	///   75th: 356_698
+	pub const BlockExecutionWeight: Weight = WEIGHT_PER_NANOS.saturating_mul(356_760);
 }
 
 #[cfg(test)]
 mod test_weights {
+	use super::*;
 	use frame_support::weights::constants;
 
 	/// Checks that the weight exists and is sane.
@@ -69,8 +70,8 @@ mod test_weights {
 		let w = super::BlockExecutionWeight::get();
 
 		// At least 100 µs.
-		assert!(w >= 100u64 * constants::WEIGHT_PER_MICROS, "Weight should be at least 100 µs.");
+		assert!(w >= Weight::from_ref_time(100 * constants::WEIGHT_PER_MICROS), "Weight should be at least 100 µs.");
 		// At most 50 ms.
-		assert!(w <= 50u64 * constants::WEIGHT_PER_MILLIS, "Weight should be at most 50 ms.");
+		assert!(w <= Weight::from_ref_time(50 * constants::WEIGHT_PER_MILLIS), "Weight should be at most 50 ms.");
 	}
 }
