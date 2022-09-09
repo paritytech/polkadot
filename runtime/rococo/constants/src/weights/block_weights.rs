cec048131bfef65aeab991194cd4f7885a1d24d4
Mara Broda <mara@broda.me>
2022-09-09 12:30:43 +0200

    rococo: update weights

--- runtime/rococo/constants/src/weights/block_weights.rs
+++ runtime/rococo/constants/src/weights/block_weights.rs
@@ -16,13 +16,13 @@
 // limitations under the License.
 
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2022-08-19 (Y/M/D)
-//! HOSTNAME: `bm6`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
+//! DATE: 2022-09-08 (Y/M/D)
+//! HOSTNAME: `bm5`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
 //!
 //! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Development`
 //! WARMUPS: `10`, REPEAT: `100`
 //! WEIGHT-PATH: `runtime/rococo/constants/src/weights/`
-//! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1`, WEIGHT-ADD: `0`
+//! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
 //   ./target/production/polkadot
@@ -42,23 +42,24 @@ use frame_support::{
 
 parameter_types! {
 	/// Time to execute an empty block.
-	/// Calculated by multiplying the *Average* with `1` and adding `0`.
+	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 4_039_227, 4_394_160
-	///   Average:  4_084_738
-	///   Median:   4_077_180
-	///   Std-Dev:  44325.29
+	///   Min, Max: 372_293, 413_365
+	///   Average:  379_926
+	///   Median:   375_815
+	///   Std-Dev:  10896.39
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 4_189_094
-	///   95th: 4_152_261
-	///   75th: 4_098_529
-	pub const BlockExecutionWeight: Weight = WEIGHT_PER_NANOS.saturating_mul(4_084_738);
+	///   99th: 413_324
+	///   95th: 411_769
+	///   75th: 378_515
+	pub const BlockExecutionWeight: Weight = WEIGHT_PER_NANOS.saturating_mul(379_926);
 }
 
 #[cfg(test)]
 mod test_weights {
+	use super::*;
 	use frame_support::weights::constants;
 
 	/// Checks that the weight exists and is sane.
@@ -69,8 +70,8 @@ mod test_weights {
 		let w = super::BlockExecutionWeight::get();
 
 		// At least 100 µs.
-		assert!(w >= 100u64 * constants::WEIGHT_PER_MICROS, "Weight should be at least 100 µs.");
+		assert!(w >= Weight::from_ref_time(100 * constants::WEIGHT_PER_MICROS), "Weight should be at least 100 µs.");
 		// At most 50 ms.
-		assert!(w <= 50u64 * constants::WEIGHT_PER_MILLIS, "Weight should be at most 50 ms.");
+		assert!(w <= Weight::from_ref_time(50 * constants::WEIGHT_PER_MILLIS), "Weight should be at most 50 ms.");
 	}
 }
