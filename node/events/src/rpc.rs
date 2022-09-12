e4fd75d9e894680780735cf17790bc8d6a18712e
Andrei Sandu <andrei-mihail@parity.io>
2022-09-12 09:24:39 +0000

    remove some leftovers
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/events/src/rpc.rs
+++ node/events/src/rpc.rs
@@ -85,14 +85,14 @@ pub trait NodeEventsApi<Notification> {
 	fn subscribe_node_events(&self);
 }
 
-/// Implements the BeefyApi RPC trait for interacting with BEEFY.
+/// Implements the NodeEventsApi RPC trait for receiving node events.
 pub struct NodeEventsBackend {
 	event_stream: NodeEventsStream,
 	executor: SubscriptionTaskExecutor,
 }
 
 impl NodeEventsBackend {
-	/// Creates a new Beefy Rpc handler instance.
+	/// Creates a new Node event Rpc handler instance.
 	pub fn new(
 		event_stream: NodeEventsStream,
 		executor: SubscriptionTaskExecutor,
@@ -115,142 +115,3 @@ impl<NodeEvents> NodeEventsApiServer<NodeEvents> for NodeEventsBackend {
 		Ok(())
 	}
 }
-
-// #[cfg(test)]
-// mod tests {
-// 	use super::*;
-
-// 	use beefy_gadget::{
-// 		justification::BeefyVersionedFinalityProof,
-// 		notification::{BeefyBestBlockStream, BeefyVersionedFinalityProofSender},
-// 	};
-// 	use beefy_primitives::{known_payload_ids, Payload, SignedCommitment};
-// 	use codec::{Decode, Encode};
-// 	use jsonrpsee::{types::EmptyParams, RpcModule};
-// 	use sp_runtime::traits::{BlakeTwo256, Hash};
-// 	use substrate_test_runtime_client::runtime::Block;
-
-// 	fn setup_io_handler() -> (RpcModule<Beefy<Block>>, BeefyVersionedFinalityProofSender<Block>) {
-// 		let (_, stream) = BeefyBestBlockStream::<Block>::channel();
-// 		setup_io_handler_with_best_block_stream(stream)
-// 	}
-
-// 	fn setup_io_handler_with_best_block_stream(
-// 		best_block_stream: BeefyBestBlockStream<Block>,
-// 	) -> (RpcModule<Beefy<Block>>, BeefyVersionedFinalityProofSender<Block>) {
-// 		let (finality_proof_sender, finality_proof_stream) =
-// 			BeefyVersionedFinalityProofStream::<Block>::channel();
-
-// 		let handler =
-// 			Beefy::new(finality_proof_stream, best_block_stream, sc_rpc::testing::test_executor())
-// 				.expect("Setting up the BEEFY RPC handler works");
-
-// 		(handler.into_rpc(), finality_proof_sender)
-// 	}
-
-// 	#[tokio::test]
-// 	async fn uninitialized_rpc_handler() {
-// 		let (rpc, _) = setup_io_handler();
-// 		let request = r#"{"jsonrpc":"2.0","method":"beefy_getFinalizedHead","params":[],"id":1}"#;
-// 		let expected_response = r#"{"jsonrpc":"2.0","error":{"code":1,"message":"BEEFY RPC endpoint not ready"},"id":1}"#.to_string();
-// 		let (response, _) = rpc.raw_json_request(&request).await.unwrap();
-
-// 		assert_eq!(expected_response, response.result);
-// 	}
-
-// 	#[tokio::test]
-// 	async fn latest_finalized_rpc() {
-// 		let (sender, stream) = BeefyBestBlockStream::<Block>::channel();
-// 		let (io, _) = setup_io_handler_with_best_block_stream(stream);
-
-// 		let hash = BlakeTwo256::hash(b"42");
-// 		let r: Result<(), ()> = sender.notify(|| Ok(hash));
-// 		r.unwrap();
-
-// 		// Verify RPC `beefy_getFinalizedHead` returns expected hash.
-// 		let request = r#"{"jsonrpc":"2.0","method":"beefy_getFinalizedHead","params":[],"id":1}"#;
-// 		let expected = "{\
-// 			\"jsonrpc\":\"2.0\",\
-// 			\"result\":\"0x2f0039e93a27221fcf657fb877a1d4f60307106113e885096cb44a461cd0afbf\",\
-// 			\"id\":1\
-// 		}"
-// 		.to_string();
-// 		let not_ready = "{\
-// 			\"jsonrpc\":\"2.0\",\
-// 			\"error\":{\"code\":1,\"message\":\"BEEFY RPC endpoint not ready\"},\
-// 			\"id\":1\
-// 		}"
-// 		.to_string();
-
-// 		let deadline = std::time::Instant::now() + std::time::Duration::from_secs(2);
-// 		while std::time::Instant::now() < deadline {
-// 			let (response, _) = io.raw_json_request(request).await.expect("RPC requests work");
-// 			if response.result != not_ready {
-// 				assert_eq!(response.result, expected);
-// 				// Success
-// 				return
-// 			}
-// 			std::thread::sleep(std::time::Duration::from_millis(50))
-// 		}
-
-// 		panic!(
-// 			"Deadline reached while waiting for best BEEFY block to update. Perhaps the background task is broken?"
-// 		);
-// 	}
-
-// 	#[tokio::test]
-// 	async fn subscribe_and_unsubscribe_with_wrong_id() {
-// 		let (rpc, _) = setup_io_handler();
-// 		// Subscribe call.
-// 		let _sub = rpc
-// 			.subscribe("beefy_subscribeJustifications", EmptyParams::new())
-// 			.await
-// 			.unwrap();
-
-// 		// Unsubscribe with wrong ID
-// 		let (response, _) = rpc
-// 			.raw_json_request(
-// 				r#"{"jsonrpc":"2.0","method":"beefy_unsubscribeJustifications","params":["FOO"],"id":1}"#,
-// 			)
-// 			.await
-// 			.unwrap();
-// 		let expected = r#"{"jsonrpc":"2.0","result":false,"id":1}"#;
-
-// 		assert_eq!(response.result, expected);
-// 	}
-
-// 	fn create_finality_proof() -> BeefyVersionedFinalityProof<Block> {
-// 		let payload = Payload::new(known_payload_ids::MMR_ROOT_ID, "Hello World!".encode());
-// 		BeefyVersionedFinalityProof::<Block>::V1(SignedCommitment {
-// 			commitment: beefy_primitives::Commitment {
-// 				payload,
-// 				block_number: 5,
-// 				validator_set_id: 0,
-// 			},
-// 			signatures: vec![],
-// 		})
-// 	}
-
-// 	#[tokio::test]
-// 	async fn subscribe_and_listen_to_one_justification() {
-// 		let (rpc, finality_proof_sender) = setup_io_handler();
-
-// 		// Subscribe
-// 		let mut sub = rpc
-// 			.subscribe("beefy_subscribeJustifications", EmptyParams::new())
-// 			.await
-// 			.unwrap();
-
-// 		// Notify with finality_proof
-// 		let finality_proof = create_finality_proof();
-// 		let r: Result<(), ()> = finality_proof_sender.notify(|| Ok(finality_proof.clone()));
-// 		r.unwrap();
-
-// 		// Inspect what we received
-// 		let (bytes, recv_sub_id) = sub.next::<sp_core::Bytes>().await.unwrap().unwrap();
-// 		let recv_finality_proof: BeefyVersionedFinalityProof<Block> =
-// 			Decode::decode(&mut &bytes[..]).unwrap();
-// 		assert_eq!(&recv_sub_id, sub.subscription_id());
-// 		assert_eq!(recv_finality_proof, finality_proof);
-// 	}
-// }
