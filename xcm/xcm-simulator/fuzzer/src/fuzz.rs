cf49ffc0f36f111d636ad27c4e9986615d5de09e
Andrei Sandu <andrei-mihail@parity.io>
2022-09-20 12:58:44 +0000

    another fix for something not touched. why ?
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- xcm/xcm-simulator/fuzzer/src/fuzz.rs
+++ xcm/xcm-simulator/fuzzer/src/fuzz.rs
@@ -19,7 +19,7 @@ mod relay_chain;
 
 use codec::DecodeLimit;
 use polkadot_parachain::primitives::Id as ParaId;
-use sp_runtime::traits::AccountIdConversion;
+use sp_runtime::traits::{AccountIdConversion, Bounded};
 use xcm_simulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain, TestExt};
 
 use frame_support::assert_ok;
