b79bb143273e390557c9b25443f0bebf1ca4de0d
Ankan <10196091+Ank4n@users.noreply.github.com>
2022-09-21 17:24:14 +0200

    [Companion] Get rid of HistoryDepth storage (#5996)
    * remove history depth from storage and add it as config for pallet staking
    
    * add migration to v11 for staking pallet
    
    * revert local substrate dependency patch
    
    * remove history depth from storage and add it as config for pallet staking
    
    * add migration to v11 for staking pallet
    
    * revert local substrate dependency patch
    
    * update substrate dependencies
    
    * update cargo lock
    
    * update to latest substrate
    
    * revert all changes to Cargo.lock
    
    * cargo update -p sp-io
    
    * mark migration job as failable
    
    Co-authored-by: kianenigma <kian@parity.io>

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -560,6 +560,7 @@ impl pallet_staking::Config for Runtime {
 	type VoterList = VoterList;
 	type TargetList = UseValidatorsMap<Self>;
 	type MaxUnlockingChunks = frame_support::traits::ConstU32<32>;
+	type HistoryDepth = frame_support::traits::ConstU32<84>;
 	type BenchmarkingConfig = runtime_common::StakingBenchmarkingConfig;
 	type OnStakerSlash = NominationPools;
 	type WeightInfo = weights::pallet_staking::WeightInfo<Runtime>;
@@ -1543,6 +1544,7 @@ pub type Executive = frame_executive::Executive<
 			VoterList,
 			StakingMigrationV11OldPallet,
 		>,
+		pallet_staking::migrations::v12::MigrateToV12<Runtime>,
 	),
 >;
 
