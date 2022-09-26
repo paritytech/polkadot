ff3f093b0df177a2ff39a03e289951cc0d1df6a3
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2022-09-26 09:37:13 +0100

    Change suggest garbage candidate logic so it only triggers with some probability drawn from a Bernoulli distribution

--- node/malus/src/variants/suggest_garbage_candidate.rs
+++ node/malus/src/variants/suggest_garbage_candidate.rs
@@ -37,6 +37,8 @@ use polkadot_primitives::v2::{CandidateDescriptor, CandidateHash};
 use polkadot_node_subsystem_util::request_validators;
 use sp_core::traits::SpawnNamed;
 
+use rand::distributions::{Bernoulli, Distribution};
+
 // Filter wrapping related types.
 use crate::{
 	interceptor::*,
@@ -97,122 +99,139 @@ where
 					"Received request to second candidate"
 				);
 
-				let pov = PoV { block_data: BlockData(MALICIOUS_POV.into()) };
+				// Need to draw value from Bernoulli distribution with given probability of success defined by the Clap parameter.
+				// Note that clap parameter must be f64 since this is expected by the Bernoulli::new() function.
+				let distribution = Bernoulli::new(self.percentage_param).unwrap();
+				
+				// Draw a random value from the distribution, where T: bool, using rng as the source of randomness.
+				let t_or_f = distribution.sample(&mut rand::thread_rng());
 
-				let (sender, receiver) = std::sync::mpsc::channel();
-				let mut new_sender = subsystem_sender.clone();
-				let _candidate = candidate.clone();
-				self.spawner.spawn_blocking(
-					"malus-get-validation-data",
-					Some("malus"),
-					Box::pin(async move {
-						gum::trace!(target: MALUS, "Requesting validators");
-						let n_validators = request_validators(relay_parent, &mut new_sender)
-							.await
-							.await
-							.unwrap()
-							.unwrap()
-							.len();
-						gum::trace!(target: MALUS, "Validators {}", n_validators);
-						match find_validation_data(&mut new_sender, &_candidate.descriptor()).await
-						{
-							Ok(Some((validation_data, validation_code))) => {
-								sender
-									.send((validation_data, validation_code, n_validators))
-									.expect("channel is still open");
-							},
-							_ => {
-								panic!("Unable to fetch validation data");
-							},
-						}
-					}),
-				);
+				// Manipulate the message if sampled value is true
+				if t_or_f == true  {
+					let pov = PoV { block_data: BlockData(MALICIOUS_POV.into()) };
 
-				let (validation_data, validation_code, n_validators) = receiver.recv().unwrap();
-
-				let validation_data_hash = validation_data.hash();
-				let validation_code_hash = validation_code.hash();
-				let validation_data_relay_parent_number = validation_data.relay_parent_number;
-
-				gum::trace!(
-					target: MALUS,
-					candidate_hash = ?candidate.hash(),
-					?relay_parent,
-					?n_validators,
-					?validation_data_hash,
-					?validation_code_hash,
-					?validation_data_relay_parent_number,
-					"Fetched validation data."
-				);
-
-				let malicious_available_data =
-					AvailableData { pov: Arc::new(pov.clone()), validation_data };
-
-				let pov_hash = pov.hash();
-				let erasure_root = {
-					let chunks =
-						erasure::obtain_chunks_v1(n_validators as usize, &malicious_available_data)
-							.unwrap();
-
-					let branches = erasure::branches(chunks.as_ref());
-					branches.root()
-				};
-
-				let (collator_id, collator_signature) = {
-					use polkadot_primitives::v2::CollatorPair;
-					use sp_core::crypto::Pair;
-
-					let collator_pair = CollatorPair::generate().0;
-					let signature_payload = polkadot_primitives::v2::collator_signature_payload(
-						&relay_parent,
-						&candidate.descriptor().para_id,
-						&validation_data_hash,
-						&pov_hash,
-						&validation_code_hash,
+					let (sender, receiver) = std::sync::mpsc::channel();
+					let mut new_sender = subsystem_sender.clone();
+					let _candidate = candidate.clone();
+					self.spawner.spawn_blocking(
+						"malus-get-validation-data",
+						Some("malus"),
+						Box::pin(async move {
+							gum::trace!(target: MALUS, "Requesting validators");
+							let n_validators = request_validators(relay_parent, &mut new_sender)
+								.await
+								.await
+								.unwrap()
+								.unwrap()
+								.len();
+							gum::trace!(target: MALUS, "Validators {}", n_validators);
+							match find_validation_data(&mut new_sender, &_candidate.descriptor()).await
+							{
+								Ok(Some((validation_data, validation_code))) => {
+									sender
+										.send((validation_data, validation_code, n_validators))
+										.expect("channel is still open");
+								},
+								_ => {
+									panic!("Unable to fetch validation data");
+								},
+							}
+						}),
 					);
 
-					(collator_pair.public(), collator_pair.sign(&signature_payload))
-				};
+					let (validation_data, validation_code, n_validators) = receiver.recv().unwrap();
 
-				let malicious_commitments =
-					create_fake_candidate_commitments(&malicious_available_data.validation_data);
+					let validation_data_hash = validation_data.hash();
+					let validation_code_hash = validation_code.hash();
+					let validation_data_relay_parent_number = validation_data.relay_parent_number;
 
-				let malicious_candidate = CandidateReceipt {
-					descriptor: CandidateDescriptor {
-						para_id: candidate.descriptor().para_id,
-						relay_parent,
-						collator: collator_id,
-						persisted_validation_data_hash: validation_data_hash,
-						pov_hash,
-						erasure_root,
-						signature: collator_signature,
-						para_head: malicious_commitments.head_data.hash(),
-						validation_code_hash,
-					},
-					commitments_hash: malicious_commitments.hash(),
-				};
-				let malicious_candidate_hash = malicious_candidate.hash();
+					gum::trace!(
+						target: MALUS,
+						candidate_hash = ?candidate.hash(),
+						?relay_parent,
+						?n_validators,
+						?validation_data_hash,
+						?validation_code_hash,
+						?validation_data_relay_parent_number,
+						"Fetched validation data."
+					);
 
-				gum::debug!(
-					target: MALUS,
-					candidate_hash = ?candidate.hash(),
-					?malicious_candidate_hash,
-					"Created malicious candidate"
-				);
+					let malicious_available_data =
+						AvailableData { pov: Arc::new(pov.clone()), validation_data };
 
-				// Map malicious candidate to the original one. We need this mapping to send back the correct seconded statement
-				// to the collators.
-				self.inner
-					.lock()
-					.expect("bad lock")
-					.map
-					.insert(malicious_candidate_hash, candidate.hash());
+					let pov_hash = pov.hash();
+					let erasure_root = {
+						let chunks =
+							erasure::obtain_chunks_v1(n_validators as usize, &malicious_available_data)
+								.unwrap();
 
-				let message = FromOrchestra::Communication {
-					msg: CandidateBackingMessage::Second(relay_parent, malicious_candidate, pov),
-				};
+						let branches = erasure::branches(chunks.as_ref());
+						branches.root()
+					};
 
-				Some(message)
+					let (collator_id, collator_signature) = {
+						use polkadot_primitives::v2::CollatorPair;
+						use sp_core::crypto::Pair;
+
+						let collator_pair = CollatorPair::generate().0;
+						let signature_payload = polkadot_primitives::v2::collator_signature_payload(
+							&relay_parent,
+							&candidate.descriptor().para_id,
+							&validation_data_hash,
+							&pov_hash,
+							&validation_code_hash,
+						);
+
+						(collator_pair.public(), collator_pair.sign(&signature_payload))
+					};
+									
+					let malicious_commitments =
+						create_fake_candidate_commitments(&malicious_available_data.validation_data);
+
+					let malicious_candidate = CandidateReceipt {
+						descriptor: CandidateDescriptor {
+							para_id: candidate.descriptor().para_id,
+							relay_parent,
+							collator: collator_id,
+							persisted_validation_data_hash: validation_data_hash,
+							pov_hash,
+							erasure_root,
+							signature: collator_signature,
+							para_head: malicious_commitments.head_data.hash(),
+							validation_code_hash,
+						},
+						commitments_hash: malicious_commitments.hash(),
+					};
+					let malicious_candidate_hash = malicious_candidate.hash();
+
+					gum::debug!(
+						target: MALUS,
+						candidate_hash = ?candidate.hash(),
+						?malicious_candidate_hash,
+						"Created malicious candidate"
+					);
+
+					// Map malicious candidate to the original one. We need this mapping to send back the correct seconded statement
+					// to the collators.
+					self.inner
+						.lock()
+						.expect("bad lock")
+						.map
+						.insert(malicious_candidate_hash, candidate.hash());
+
+					let message = FromOrchestra::Communication {
+						msg: CandidateBackingMessage::Second(relay_parent, malicious_candidate, pov),
+					};
+
+					Some(message)
+				} else {
+					gum::debug!(
+						target: MALUS,
+						candidate_hash = ?candidate.hash(),
+						?relay_parent,
+						"Skipped creation of malicious candidate"
+					);
+				}
 			},
 			FromOrchestra::Communication { msg } => Some(FromOrchestra::Communication { msg }),
 			FromOrchestra::Signal(signal) => Some(FromOrchestra::Signal(signal)),
