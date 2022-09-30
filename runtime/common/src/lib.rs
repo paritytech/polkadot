7b29e359ca5cfbac4132adfac91bc057c3b89252
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-09-30 14:35:06 +0200

    Unlimited Proof size per block
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -69,7 +69,9 @@ pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(1);
 /// by  Operational  extrinsics.
 pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 /// We allow for 2 seconds of compute with a 6 second average block time.
-pub const MAXIMUM_BLOCK_WEIGHT: Weight = WEIGHT_PER_SECOND.saturating_mul(2);
+/// The storage proof size is not limited so far.
+pub const MAXIMUM_BLOCK_WEIGHT: Weight =
+	WEIGHT_PER_SECOND.saturating_mul(2).set_proof_size(u64::MAX);
 
 const_assert!(NORMAL_DISPATCH_RATIO.deconstruct() >= AVERAGE_ON_INITIALIZE_RATIO.deconstruct());
 
