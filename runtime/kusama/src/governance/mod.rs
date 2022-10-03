1c804698831956f42a7327dd5b4bccb4c5644e85
Gavin Wood <gavin@parity.io>
2022-10-03 17:33:46 +0100

    Governance v2 (Kusama only) (#5205)
    * First bits
    
    * Create new origins
    
    * Repot governance configs
    
    * Tests for treasury spends
    
    * Integrate treasury spend threshold origins
    
    * Bump deps
    
    * Add whitelist, configure treasury spend origins
    
    * Add governance curves
    
    * Enable curve's ceil to be configured
    
    * Make compile
    
    * Rest of new governance params
    
    * Build fixes
    
    * Fixes
    
    * Fix warning
    
    * Use new EnsureOrigin::try_successful_origin
    
    * Some repotting
    
    * Add files
    
    * Ready for initial review
    
    * Fixes
    
    * Formatting
    
    * Bump
    
    * rename
    
    * Derive MEL for Origin enum
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Add benchmarks
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_conviction_voting
    
    * Update Cargo.lock
    
    * Update runtime/kusama/src/governance/origins.rs
    
    Co-authored-by: Xiliang Chen <xlchen1291@gmail.com>
    
    * allow root to demote and promote, fixing benchmarks
    
    * fix whitelist origin benchmarks
    
    * typo
    
    * cargo update -p sp-io
    
    * add second referenda instance to benchmarks
    
    * allow root to act as a 9th dan in track
    
    * feature flag it
    
    * cargo update -p sp-io
    
    * fix merge
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_referenda
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_conviction_voting
    
    * allow root to create auction
    
    * Update mod.rs
    
    * more origin fixes
    
    * root isnt needed actually
    
    * cargo update -p sp-io
    
    * allow root to cancel slash
    
    * initial stuff
    
    * make copy data from kusama
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_referenda
    
    * Update Cargo.lock
    
    * commit new weight files
    
    * more files
    
    * use weight files
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_referenda
    
    * Update Cargo.lock
    
    * Update to latest master
    
    * Formatting
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_ranked_collective
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_conviction_voting
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_conviction_voting
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_whitelist
    
    * Rename Origin -> RuntimeOrigin
    
    This was done in Substrate and is now needed here as well.
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * cargo update -p sp-io
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Tech Committee can promote to VI Dan
    
    * Update runtime/kusama/src/governance/mod.rs
    
    Co-authored-by: Amar Singh <asinghchrony@protonmail.com>
    
    * Formatting
    
    * Update runtime/kusama/src/governance/fellowship.rs
    
    Co-authored-by: Amar Singh <asinghchrony@protonmail.com>
    
    * Fixes
    
    * Avoid updating extraneous deps
    
    * Fix broken stuff
    
    * ".git/.scripts/bench-bot.sh" runtime kusama-dev pallet_referenda
    
    * Update runtime/kusama/src/governance/fellowship.rs
    
    * Reorder EitherOf origins
    
    Zero logic change to make the benchmarking work.
    It uses the first successful origin which can only promote to rank 6.
    By swapping the order we use Root which can always promote.
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    
    * Revert param change and update doc
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: command-bot <>
    Co-authored-by: Shawn Tabrizi <shawntabrizi@gmail.com>
    Co-authored-by: Xiliang Chen <xlchen1291@gmail.com>
    Co-authored-by: Keith Yeung <kungfukeith11@gmail.com>
    Co-authored-by: Amar Singh <asinghchrony@protonmail.com>

--- runtime/kusama/src/governance/mod.rs
+++ runtime/kusama/src/governance/mod.rs
@@ -14,7 +14,81 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot. If not, see <http://www.gnu.org/licenses/>.
 
-//! Governance configurations for the Kusama runtime.
+//! New governance configurations for the Kusama runtime.
+
+use super::*;
+use frame_support::{
+	parameter_types,
+	traits::{ConstU16, EitherOf},
+};
+use frame_system::EnsureRootWithSuccess;
 
 // Old governance configurations.
 pub mod old;
+
+mod origins;
+pub use origins::{
+	pallet_custom_origins, AuctionAdmin, Fellows, FellowshipAdmin, FellowshipExperts,
+	FellowshipInitiates, FellowshipMasters, GeneralAdmin, LeaseAdmin, ReferendumCanceller,
+	ReferendumKiller, Spender, StakingAdmin, WhitelistedCaller,
+};
+mod tracks;
+pub use tracks::TracksInfo;
+mod fellowship;
+pub use fellowship::{FellowshipCollectiveInstance, FellowshipReferendaInstance};
+
+parameter_types! {
+	pub const VoteLockingPeriod: BlockNumber = 7 * DAYS;
+}
+
+impl pallet_conviction_voting::Config for Runtime {
+	type WeightInfo = weights::pallet_conviction_voting::WeightInfo<Self>;
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type VoteLockingPeriod = VoteLockingPeriod;
+	type MaxVotes = ConstU32<512>;
+	type MaxTurnout = frame_support::traits::TotalIssuanceOf<Balances, Self::AccountId>;
+	type Polls = Referenda;
+}
+
+parameter_types! {
+	pub const AlarmInterval: BlockNumber = 1;
+	pub const SubmissionDeposit: Balance = 100 * UNITS;
+	pub const UndecidingTimeout: BlockNumber = 28 * DAYS;
+}
+
+parameter_types! {
+	pub const MaxBalance: Balance = Balance::max_value();
+}
+pub type TreasurySpender = EitherOf<EnsureRootWithSuccess<AccountId, MaxBalance>, Spender>;
+
+impl origins::pallet_custom_origins::Config for Runtime {}
+
+impl pallet_whitelist::Config for Runtime {
+	type WeightInfo = weights::pallet_whitelist::WeightInfo<Self>;
+	type RuntimeCall = RuntimeCall;
+	type RuntimeEvent = RuntimeEvent;
+	type WhitelistOrigin =
+		EitherOf<EnsureRootWithSuccess<Self::AccountId, ConstU16<65535>>, Fellows>;
+	type DispatchWhitelistedOrigin = EitherOf<EnsureRoot<Self::AccountId>, WhitelistedCaller>;
+	type PreimageProvider = Preimage;
+}
+
+impl pallet_referenda::Config for Runtime {
+	type WeightInfo = weights::pallet_referenda_referenda::WeightInfo<Self>;
+	type RuntimeCall = RuntimeCall;
+	type RuntimeEvent = RuntimeEvent;
+	type Scheduler = Scheduler;
+	type Currency = Balances;
+	type SubmitOrigin = frame_system::EnsureSigned<AccountId>;
+	type CancelOrigin = ReferendumCanceller;
+	type KillOrigin = ReferendumKiller;
+	type Slash = Treasury;
+	type Votes = pallet_conviction_voting::VotesOf<Runtime>;
+	type Tally = pallet_conviction_voting::TallyOf<Runtime>;
+	type SubmissionDeposit = SubmissionDeposit;
+	type MaxQueued = ConstU32<100>;
+	type UndecidingTimeout = UndecidingTimeout;
+	type AlarmInterval = AlarmInterval;
+	type Tracks = TracksInfo;
+}
