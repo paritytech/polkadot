cba43a871505459a145860154cf51affb2009c04
Serban Iorga <serban@parity.io>
2022-10-03 11:55:40 +0300

    beefy-mmr: Simplify hashing

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1257,7 +1257,7 @@ impl BeefyDataProvider<H256> for ParasProvider {
 			.filter_map(|id| Paras::para_head(&id).map(|head| (id.into(), head.0)))
 			.collect();
 		para_heads.sort();
-		beefy_merkle_tree::merkle_root::<pallet_beefy_mmr::Pallet<Runtime>, _, _>(
+		beefy_merkle_tree::merkle_root::<<Runtime as pallet_mmr::Config>::Hashing, _>(
 			para_heads.into_iter().map(|pair| pair.encode()),
 		)
 		.into()
