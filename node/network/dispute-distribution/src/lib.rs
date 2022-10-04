cd8dedaafa859f0ce6c3ab2f3ef1c76877e3be99
Sebastian Kunert <skunert49@gmail.com>
2022-10-04 12:01:34 +0200

    Add expects instead of unwrap
    Co-authored-by: Bastian Köcher <info@kchr.de>

--- node/network/dispute-distribution/src/lib.rs
+++ node/network/dispute-distribution/src/lib.rs
@@ -147,7 +147,7 @@ where
 	) -> Self {
 		let runtime = RuntimeInfo::new_with_config(runtime::Config {
 			keystore: Some(keystore),
-			session_cache_lru_size: NonZeroUsize::new(DISPUTE_WINDOW.get() as usize).unwrap(),
+			session_cache_lru_size: NonZeroUsize::new(DISPUTE_WINDOW.get() as usize).expect("`DISPUTE_WINDOW` should not be 0"),
 		});
 		let (tx, sender_rx) = mpsc::channel(1);
 		let disputes_sender = DisputeSender::new(tx, metrics.clone());
--- node/network/dispute-distribution/src/receiver/mod.rs
+++ node/network/dispute-distribution/src/receiver/mod.rs
@@ -147,7 +147,7 @@ where
 	) -> Self {
 		let runtime = RuntimeInfo::new_with_config(runtime::Config {
 			keystore: None,
-			session_cache_lru_size: NonZeroUsize::new(DISPUTE_WINDOW.get() as usize).unwrap(),
+			session_cache_lru_size: NonZeroUsize::new(DISPUTE_WINDOW.get() as usize).expect("`DISPUTE_WINDOW` should not be 0"),
 		});
 		Self {
 			runtime,
