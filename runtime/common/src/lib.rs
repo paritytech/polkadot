ba1abb90e520af31d3efd3130ceac5e004d944d4
Szegoo <sakacszergej@gmail.com>
2022-10-06 17:59:24 +0200

    fix typo

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -88,7 +88,7 @@ parameter_types! {
 	/// that combined with `AdjustmentVariable`, we can recover from the minimum.
 	/// See `multiplier_can_grow_from_zero`.
 	pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 10u128);
-	/// The maximum amouf of the multiplier.
+	/// The maximum amount of the multiplier.
 	pub MaximumMultiplier: Multiplier = Bounded::max_value();
 	/// Maximum length of block. Up to 5MB.
 	pub BlockLength: limits::BlockLength =
