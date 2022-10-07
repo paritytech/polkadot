c6e727d91f88c37e6d7c8e52a40d3829565c830e
Andrei Sandu <andrei-mihail@parity.io>
2022-10-07 13:02:04 +0000

    fix loose ends
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -944,6 +944,7 @@ where
 
 	let approval_voting_config = ApprovalVotingConfig {
 		col_data: parachains_db::REAL_COLUMNS.col_approval_data,
+		col_session_data: parachains_db::REAL_COLUMNS.col_session_window_data,
 		slot_duration_millis: slot_duration.as_millis() as u64,
 	};
 
@@ -967,6 +968,7 @@ where
 
 	let dispute_coordinator_config = DisputeCoordinatorConfig {
 		col_data: parachains_db::REAL_COLUMNS.col_dispute_coordinator_data,
+		col_session_data: parachains_db::REAL_COLUMNS.col_session_window_data,
 	};
 
 	let rpc_handlers = service::spawn_tasks(service::SpawnTasksParams {
@@ -1536,6 +1538,7 @@ fn revert_chain_selection(db: Arc<dyn Database>, hash: Hash) -> sp_blockchain::R
 fn revert_approval_voting(db: Arc<dyn Database>, hash: Hash) -> sp_blockchain::Result<()> {
 	let config = approval_voting_subsystem::Config {
 		col_data: parachains_db::REAL_COLUMNS.col_approval_data,
+		col_session_data: parachains_db::REAL_COLUMNS.col_session_window_data,
 		slot_duration_millis: Default::default(),
 	};
 
