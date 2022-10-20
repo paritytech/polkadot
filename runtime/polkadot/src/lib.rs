cbd4044dd7bc022142d84255d7708c44d0280c2f
Shawn Tabrizi <shawntabrizi@gmail.com>
2022-10-20 17:39:08 -0400

    u16 -> u32

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -1018,7 +1018,7 @@ parameter_types! {
 	pub const DepositBase: Balance = deposit(1, 88);
 	// Additional storage item size of 32 bytes.
 	pub const DepositFactor: Balance = deposit(0, 32);
-	pub const MaxSignatories: u16 = 100;
+	pub const MaxSignatories: u32 = 100;
 }
 
 impl pallet_multisig::Config for Runtime {
