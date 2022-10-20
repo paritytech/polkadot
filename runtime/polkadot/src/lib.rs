50c541c3efc0eb74a1ccfc01e1702fea3addeb2b
Mara Robin B <mara@broda.me>
2022-10-20 17:16:27 +0200

    bump transaction_version (0.9.31) (#6171)
    * Bump transaction_version for polkadot
    
    * Bump transaction_version for kusama
    
    * Bump transaction_version for rococo
    
    * Bump transaction_version for westend
    
    * Bump transaction_version for polkadot
    
    * Bump transaction_version for kusama
    
    * Bump transaction_version for rococo
    
    * Bump transaction_version for westend

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -119,7 +119,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 14,
+	transaction_version: 16,
 	state_version: 0,
 };
 
