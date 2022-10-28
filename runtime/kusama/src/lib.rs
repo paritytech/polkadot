a88f3136013edb4bae247786406783f267cc1f83
Mara Broda <mara@broda.me>
2022-10-28 11:41:08 +0200

    kusama: clean up executed migrations

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1465,12 +1465,6 @@ pub type Executive = frame_executive::Executive<
 	Runtime,
 	AllPalletsWithSystem,
 	(
-		pallet_staking::migrations::v11::MigrateToV11<
-			Runtime,
-			VoterList,
-			StakingMigrationV11OldPallet,
-		>,
-		pallet_staking::migrations::v12::MigrateToV12<Runtime>,
 		// "Bound uses of call" <https://github.com/paritytech/polkadot/pull/5729>
 		pallet_preimage::migration::v1::Migration<Runtime>,
 		pallet_scheduler::migration::v3::MigrateToV4<Runtime>,
