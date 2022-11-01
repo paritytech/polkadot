71766709fc8ba8abf7dab0b79540887a2fb7a901
alexgparity <115470171+alexgparity@users.noreply.github.com>
2022-11-01 18:34:16 +0100

    Replace parachain/parathread boolean by enum (#6198)
    * Replace parachain/parathread boolean by enum
    
    * Address PR comments
    
    * Update dependencies
    
    * ParaType -> ParaKind
    
    * Swap enum field order to avoid migration
    
    * Rename paratype field to parakind
    
    * Manual en-/decocing of Parakind
    
    * Manual TypeInfo for ParaKind
    
    * rename field back to parachain
    
    * minor
    
    * Update runtime/parachains/src/paras/mod.rs
    
    Co-authored-by: Andrei Sandu <54316454+sandreim@users.noreply.github.com>
    
    * Manual serde Serialize and Deserialize for ParaKind
    
    * cargo fmt
    
    * Update runtime/parachains/src/paras/mod.rs
    
    Co-authored-by: Andronik <write@reusable.software>
    
    * Add test for serde_json encoding/decoding
    
    * Move serde_json dep to dev-deps
    
    Co-authored-by: Andrei Sandu <54316454+sandreim@users.noreply.github.com>
    Co-authored-by: Andronik <write@reusable.software>

--- runtime/parachains/src/hrmp/benchmarking.rs
+++ runtime/parachains/src/hrmp/benchmarking.rs
@@ -17,7 +17,7 @@
 use crate::{
 	configuration::Pallet as Configuration,
 	hrmp::{Pallet as Hrmp, *},
-	paras::{Pallet as Paras, ParachainsCache},
+	paras::{Pallet as Paras, ParaKind, ParachainsCache},
 	shared::Pallet as Shared,
 };
 use frame_support::{assert_ok, traits::Currency};
@@ -31,7 +31,7 @@ fn register_parachain_with_balance<T: Config>(id: ParaId, balance: BalanceOf<T>)
 		&mut parachains,
 		id,
 		&crate::paras::ParaGenesisArgs {
-			parachain: true,
+			para_kind: ParaKind::Parachain,
 			genesis_head: vec![1].into(),
 			validation_code: vec![1].into(),
 		},
