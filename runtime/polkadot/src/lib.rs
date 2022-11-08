9309ac60cc734d2b243b78e4afac481e1a863314
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2022-11-08 08:03:54 +0000

    Increase max rewardable nominators (#6230)
    * Increase max rewardable nominators
    
    * update kusama as well

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -525,7 +525,7 @@ parameter_types! {
 	pub const BondingDuration: sp_staking::EraIndex = 28;
 	pub const SlashDeferDuration: sp_staking::EraIndex = 27;
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
-	pub const MaxNominatorRewardedPerValidator: u32 = 256;
+	pub const MaxNominatorRewardedPerValidator: u32 = 512;
 	pub const OffendingValidatorsThreshold: Perbill = Perbill::from_percent(17);
 	// 16
 	pub const MaxNominations: u32 = <NposCompactSolution16 as frame_election_provider_support::NposSolution>::LIMIT as u32;
