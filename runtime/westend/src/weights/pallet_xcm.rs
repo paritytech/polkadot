a5620b44ee84246685a7568e960644a6073c8d3b
Keith Yeung <kungfukeith11@gmail.com>
2022-11-09 15:39:45 +0900

    Fixes

--- runtime/westend/src/weights/pallet_xcm.rs
+++ runtime/westend/src/weights/pallet_xcm.rs
@@ -104,7 +104,7 @@ impl<T: frame_system::Config> pallet_xcm::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().reads(6 as u64))
 			.saturating_add(T::DbWeight::get().writes(5 as u64))
 	}
-	fn execute() -> Weights {
+	fn execute() -> Weight {
 		Weight::MAX
 	}
 }
