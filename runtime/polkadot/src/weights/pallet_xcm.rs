0da0af1bed5e6833c7c1bf5b9b519e84ba7d8a78
Keith Yeung <kungfukeith11@gmail.com>
2022-11-09 15:01:19 +0900

    Fixes

--- runtime/polkadot/src/weights/pallet_xcm.rs
+++ runtime/polkadot/src/weights/pallet_xcm.rs
@@ -107,4 +107,7 @@ impl<T: frame_system::Config> pallet_xcm::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().reads(7 as u64))
 			.saturating_add(T::DbWeight::get().writes(5 as u64))
 	}
+	fn execute() -> Weight {
+		Weight::MAX
+	}
 }
