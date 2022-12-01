911e90b04b0426311e4f283c4df82c73807aa16d
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2022-12-01 16:59:01 +0000

    typo
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- tests/benchmark_storage_works.rs
+++ tests/benchmark_storage_works.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-#![cfg(features = "runtime-benchmarks")]
+#![cfg(feature = "runtime-benchmarks")]
 
 use assert_cmd::cargo::cargo_bin;
 use std::{
