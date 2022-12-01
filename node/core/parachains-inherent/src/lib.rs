8b9555dc2451acfabab173d259e00da2728b7aa2
alexgparity <115470171+alexgparity@users.noreply.github.com>
2022-12-01 10:05:38 +0100

    Update node/core/parachains-inherent/src/lib.rs
    Co-authored-by: Bastian Köcher <info@kchr.de>

--- node/core/parachains-inherent/src/lib.rs
+++ node/core/parachains-inherent/src/lib.rs
@@ -129,11 +129,7 @@ impl<C: sp_blockchain::HeaderBackend<Block>> sp_inherents::InherentDataProvider
 		&self,
 		dst_inherent_data: &mut sp_inherents::InherentData,
 	) -> Result<(), sp_inherents::Error> {
-		let inherent_data = ParachainsInherentDataProvider::create(
-			self.client.clone(),
-			self.overseer.clone(),
-			self.parent,
-		)
+		let inherent_data = self.create()
 		.await
 		.map_err(|e| sp_inherents::Error::Application(Box::new(e)))?;
 
