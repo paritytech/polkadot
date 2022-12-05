83f23b6fb9ee6cfabc180158404982be063d2f78
Bastian Köcher <info@kchr.de>
2022-12-06 00:08:59 +0100

    crowdloan: Fix migration.
    The migration would not have been run because of the `current_version ==
    1` check.

--- runtime/common/src/crowdloan/mod.rs
+++ runtime/common/src/crowdloan/mod.rs
@@ -184,9 +184,13 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::{ensure_root, ensure_signed, pallet_prelude::*};
 
+	/// The current storage version.
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::without_storage_info]
+	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
