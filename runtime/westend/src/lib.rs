f9c7796b68cf20f73d1d5cf9db9e8c83e549b8f0
cheme <emericchevalier.pro@gmail.com>
2022-12-07 15:07:22 +0100

    correct fix

--- runtime/westend/src/lib.rs
+++ runtime/westend/src/lib.rs
@@ -1864,6 +1864,7 @@ mod init_state_migration {
 	use super::Runtime;
 	use frame_support::traits::OnRuntimeUpgrade;
 	use pallet_state_trie_migration::{AutoLimits, MigrationLimits, MigrationProcess};
+	#[cfg(not(feature = "std"))]
 	use sp_std::prelude::*;
 
 	/// Initialize an automatic migration process.
