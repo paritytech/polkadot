d2cf59dcef30273eaa2a03774adbf457e446461a
Robert Hambrock <roberthambrock@gmail.com>
2022-12-13 11:35:33 +0100

    add pallet-mmr migration logic to runtime upgrade

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1456,7 +1456,9 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(),
+	(
+		pallet_mmr::migrations::v1::MigrateToV1<Runtime>,
+	),
 >;
 /// The payload being signed in transactions.
 pub type SignedPayload = generic::SignedPayload<RuntimeCall, SignedExtra>;
