4ccf61a6f867a82100c1054a4e52f1b145d954c8
Keith Yeung <kungfukeith11@gmail.com>
2022-12-22 22:26:07 +0900

    Fixes

--- runtime/kusama/src/xcm_config.rs
+++ runtime/kusama/src/xcm_config.rs
@@ -17,8 +17,9 @@
 //! XCM configurations for the Kusama runtime.
 
 use super::{
-	parachains_origin, AccountId, AllPalletsWithSystem, Balances, CouncilCollective, ParaId,
-	Runtime, RuntimeCall, RuntimeEvent, RuntimeOrigin, StakingAdmin, WeightToFee, XcmPallet,
+	parachains_origin, AccountId, AllPalletsWithSystem, Balances, CouncilCollective, Fellows,
+	ParaId, Runtime, RuntimeCall, RuntimeEvent, RuntimeOrigin, StakingAdmin, WeightToFee,
+	XcmPallet,
 };
 use frame_support::{
 	match_types, parameter_types,
