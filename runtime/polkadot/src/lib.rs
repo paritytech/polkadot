d287c5b0ac33642f6d8a1aa8c9f9142a39fed647
Ross Bulat <ross@jkrbinvestments.com>
2022-12-23 23:38:38 +0700

    fix

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -1430,6 +1430,7 @@ parameter_types! {
 impl pallet_nomination_pools::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Currency = Balances;
+	type RewardCounter = FixedU128;
 	type BalanceToU256 = runtime_common::BalanceToU256;
 	type U256ToBalance = runtime_common::U256ToBalance;
 	type Staking = Staking;
