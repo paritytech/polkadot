1254626c0f0fb75b461047d8c0d4441994b215ff
Ross Bulat <ross@parity.io>
2022-12-28 22:38:02 +0700

    + as u64

--- runtime/polkadot/src/weights/pallet_nomination_pools.rs
+++ runtime/polkadot/src/weights/pallet_nomination_pools.rs
@@ -100,9 +100,9 @@ impl<T: frame_system::Config> pallet_nomination_pools::WeightInfo for WeightInfo
 	// Storage: NominationPools GlobalMaxCommission (r:1 w:0)
 	fn claim_payout() -> Weight {
 		// Minimum execution time: 64_000 nanoseconds.
-		Weight::from_ref_time(65_000_000)
-			.saturating_add(T::DbWeight::get().reads(5))
-			.saturating_add(T::DbWeight::get().writes(4))
+		Weight::from_ref_time(65_000_000 as u64)
+			.saturating_add(T::DbWeight::get().reads(5 as u64))
+			.saturating_add(T::DbWeight::get().writes(4 as u64))
 	}
 	// Storage: NominationPools PoolMembers (r:1 w:1)
 	// Storage: NominationPools BondedPools (r:1 w:1)
@@ -290,22 +290,22 @@ impl<T: frame_system::Config> pallet_nomination_pools::WeightInfo for WeightInfo
 	// Storage: NominationPools BondedPools (r:1 w:1)
 	fn set_commission() -> Weight {
 		// Minimum execution time: 24_000 nanoseconds.
-		Weight::from_ref_time(24_000_000)
-			.saturating_add(T::DbWeight::get().reads(1))
-			.saturating_add(T::DbWeight::get().writes(1))
+		Weight::from_ref_time(24_000_000 as u64)
+			.saturating_add(T::DbWeight::get().reads(1 as u64))
+			.saturating_add(T::DbWeight::get().writes(1 as u64))
 	}
 	// Storage: NominationPools BondedPools (r:1 w:1)
 	fn set_commission_max() -> Weight {
 		// Minimum execution time: 22_000 nanoseconds.
-		Weight::from_ref_time(24_000_000)
-			.saturating_add(T::DbWeight::get().reads(1))
-			.saturating_add(T::DbWeight::get().writes(1))
+		Weight::from_ref_time(24_000_000 as u64)
+			.saturating_add(T::DbWeight::get().reads(1 as u64))
+			.saturating_add(T::DbWeight::get().writes(1 as u64))
 	}
 	// Storage: NominationPools BondedPools (r:1 w:1)
 	fn set_commission_change_rate() -> Weight {
 		// Minimum execution time: 21_000 nanoseconds.
-		Weight::from_ref_time(23_000_000)
-			.saturating_add(T::DbWeight::get().reads(1))
-			.saturating_add(T::DbWeight::get().writes(1))
+		Weight::from_ref_time(23_000_000 as u64)
+			.saturating_add(T::DbWeight::get().reads(1 as u64))
+			.saturating_add(T::DbWeight::get().writes(1 as u64))
 	}
 }
