b2779834189e00075483aee4b0eaa9389ae05a57
alexgparity <alex.gremm@parity.io>
2023-01-09 17:19:33 +0100

    fix test-runtime

--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -29,8 +29,8 @@ use polkadot_runtime_parachains::{
 	dmp as parachains_dmp, hrmp as parachains_hrmp, inclusion as parachains_inclusion,
 	initializer as parachains_initializer, origin as parachains_origin, paras as parachains_paras,
 	paras_inherent as parachains_paras_inherent, runtime_api_impl::v2 as runtime_impl,
-	scheduler as parachains_scheduler, session_info as parachains_session_info,
-	shared as parachains_shared, ump as parachains_ump,
+	scheduler as parachains_scheduler, scheduler_parachains, scheduler_parathreads,
+	session_info as parachains_session_info, shared as parachains_shared, ump as parachains_ump,
 };
 
 use authority_discovery_primitives::AuthorityId as AuthorityDiscoveryId;
@@ -572,7 +572,12 @@ impl parachains_hrmp::Config for Runtime {
 	type WeightInfo = parachains_hrmp::TestWeightInfo;
 }
 
-impl parachains_scheduler::Config for Runtime {}
+impl scheduler_parathreads::Config for Runtime {}
+
+impl parachains_scheduler::Config for Runtime {
+	type CoreAssigners<T: parachains_scheduler::Config> =
+		(scheduler_parachains::ParachainsScheduler, scheduler_parathreads::ParathreadsScheduler);
+}
 
 impl paras_sudo_wrapper::Config for Runtime {}
 
