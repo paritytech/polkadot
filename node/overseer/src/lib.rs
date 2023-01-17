3b6795577654523ce1a327e69277fa757b9faa77
Andrei Sandu <andrei-mihail@parity.io>
2023-01-17 13:46:05 +0000

    Bump message channel capacity.
    Only needed for approval voting, but no way to do it granularly.
    Good enough for a test.
    
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/overseer/src/lib.rs
+++ node/overseer/src/lib.rs
@@ -451,7 +451,7 @@ pub async fn forward_events<P: BlockchainEvents<Block>>(client: Arc<P>, mut hand
 	event=Event,
 	signal=OverseerSignal,
 	error=SubsystemError,
-	message_capacity=2048,
+	message_capacity=4096,
 )]
 pub struct Overseer<SupportsParachains> {
 	#[subsystem(blocking, CandidateValidationMessage, sends: [
