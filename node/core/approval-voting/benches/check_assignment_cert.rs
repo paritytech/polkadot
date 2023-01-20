af0c26abac8a14a2c26d53bdc49b30932174521c
Andrei Sandu <andrei-mihail@parity.io>
2023-01-20 19:41:20 +0000

    Update benchmark
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/core/approval-voting/benches/check_assignment_cert.rs
+++ node/core/approval-voting/benches/check_assignment_cert.rs
@@ -15,78 +15,22 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use merlin::Transcript;
-use parity_scale_codec::{Decode, Encode};
-use polkadot_primitives::v2::Hash;
 use schnorrkel::{
 	vrf::{VRFOutput, VRFProof},
 	Keypair,
 };
 use std::time::Duration;
 
-use polkadot_node_primitives::{
-	approval::{
-		AssignmentCert, AssignmentCertKind, DelayTranche, RelayVRFStory, RELAY_VRF_MODULO_CONTEXT,
-	},
-	AvailableData, BlockData, PoV,
-};
-use polkadot_node_subsystem::{
-	messages::{
-		AllMessages, ApprovalVotingMessage, AssignmentCheckResult, AvailabilityRecoveryMessage,
-	},
-	ActivatedLeaf, ActiveLeavesUpdate, LeafStatus,
-};
-use polkadot_node_subsystem_test_helpers as test_helpers;
-use polkadot_node_subsystem_util::TimeoutExt;
-use polkadot_overseer::HeadSupportsParachains;
-use polkadot_primitives::v2::{
-	CandidateCommitments, CandidateEvent, CoreIndex, GroupIndex, Header, Id as ParaId, IndexedVec,
-	ValidationCode, ValidatorSignature,
-};
-
-use assert_matches::assert_matches;
-use async_trait::async_trait;
-use parking_lot::Mutex;
-use sp_keyring::sr25519::Keyring as Sr25519Keyring;
-use sp_keystore::CryptoStore;
-use std::{
-	pin::Pin,
-	sync::{
-		atomic::{AtomicBool, Ordering},
-		Arc,
-	},
-};
-
-// use crate::{
-// 	approval_db::v1::StoredBlockRange,
-// 	backend::BackendWriteOp,
-// 	import::tests::{
-// 		garbage_vrf, AllowedSlots, BabeEpoch, BabeEpochConfiguration, CompatibleDigestItem, Digest,
-// 		DigestItem, PreDigest, SecondaryVRFPreDigest,
-// 	},
-// };
-
-use ::test_helpers::{dummy_candidate_receipt, dummy_candidate_receipt_bad_sig};
+use futures::{channel::oneshot, Stream, StreamExt, TryStreamExt};
 
 fn vrf_sign(keypair: &Keypair, transcript: Transcript, extra: Transcript) -> (VRFOutput, VRFProof) {
-	let ctx = schnorrkel::signing_context(RELAY_VRF_MODULO_CONTEXT);
-
 	let (vrf_in_out, vrf_proof, _) = keypair.vrf_sign_extra(transcript, extra);
 
 	(vrf_in_out.to_output(), vrf_proof)
 }
 
-// fn relay_vrf_modulo_transcript(relay_vrf_story: RelayVRFStory, sample: u32) -> Transcript {
-// 	// combine the relay VRF story with a sample number.
-// 	let mut t = Transcript::new(approval_types::RELAY_VRF_MODULO_CONTEXT);
-// 	t.append_message(b"RC-VRF", &relay_vrf_story.0);
-// 	sample.using_encoded(|s| t.append_message(b"sample", s));
-
-// 	t
-// }
-
-fn check_assignment(c: &mut Criterion) {
+fn time_1000_vrf_signatures(c: &mut Criterion) {
 	let mut group = c.benchmark_group("check");
-	let input = 0;
 	let mut transcript = Transcript::new(b"dummy");
 	transcript.append_message(b"label", b"message");
 
@@ -95,34 +39,117 @@ fn check_assignment(c: &mut Criterion) {
 
 	let mut prng = rand_core::OsRng;
 	let keypair = schnorrkel::Keypair::generate_with(&mut prng);
+
 	let (output, proof) = vrf_sign(&keypair, transcript.clone(), extra.clone());
 
-	group.bench_with_input(BenchmarkId::from_parameter(0), &input, |b, &n| {
+	group.throughput(Throughput::Elements(1000));
+
+	group.bench_with_input(BenchmarkId::from_parameter("no-pool"), &keypair, |b, _n| {
+		let public = keypair.public;
+		let mut responses = Vec::new();
 		b.iter(|| {
-			let public = &keypair.public;
-			let mut transcript = Transcript::new(b"dummy");
-			transcript.append_message(b"label", b"message");
+			for _ in 0..1000 {
+				let mut transcript = Transcript::new(b"dummy");
+				transcript.append_message(b"label", b"message");
 
-			let mut extra = Transcript::new(b"extra");
-			extra.append_message(b"label", b"message");
+				let mut extra = Transcript::new(b"extra");
+				extra.append_message(b"label", b"message");
+				let output = output.clone();
+				let proof = proof.clone();
 
-			let (vrf_in_out, _) =
-				public.vrf_verify_extra(transcript, &output, &proof, extra).unwrap();
+				let (vrf_in_out, _) =
+					public.vrf_verify_extra(transcript, &output, &proof, extra).unwrap();
+				responses.push(vrf_in_out);
+			}
 		});
 	});
+
+	group.finish();
+}
+
+fn time_1000_vrf_signatures_pool(c: &mut Criterion) {
+	let mut group = c.benchmark_group("check");
+	let mut transcript = Transcript::new(b"dummy");
+	transcript.append_message(b"label", b"message");
+
+	let mut extra = Transcript::new(b"extra");
+	extra.append_message(b"label", b"message");
+
+	let mut prng = rand_core::OsRng;
+	let keypair = schnorrkel::Keypair::generate_with(&mut prng);
+
+	let (output, proof) = vrf_sign(&keypair, transcript.clone(), extra.clone());
+
+	for i in 0..5 {
+		group.throughput(Throughput::Elements(1000));
+
+		group.bench_with_input(
+			BenchmarkId::from_parameter(format!("pool_size_{}", 2u32.pow(i))),
+			&keypair,
+			|b, _n| {
+				let public = keypair.public;
+				b.iter(|| {
+					let mut futures = futures::stream::FuturesUnordered::new();
+					let proof = proof.clone();
+
+					let main_runtime = tokio::runtime::Runtime::new().unwrap();
+					let cpu_pool = tokio::runtime::Builder::new_multi_thread()
+						.max_blocking_threads(2usize.pow(i))
+						.build()
+						.unwrap();
+					let cpu_pool = cpu_pool.handle().clone();
+
+					main_runtime.block_on(async move {
+						for _ in 0..1000 {
+							let mut transcript = Transcript::new(b"dummy");
+							transcript.append_message(b"label", b"message");
+
+							let mut extra = Transcript::new(b"extra");
+							extra.append_message(b"label", b"message");
+							let (tx, rx) = oneshot::channel();
+							let output = output.clone();
+							let proof = proof.clone();
+
+							cpu_pool.spawn_blocking(move || {
+								let (vrf_in_out, _) = public
+									.vrf_verify_extra(transcript, &output, &proof, extra)
+									.unwrap();
+								let _ = tx.send(vrf_in_out);
+							});
+							futures.push(rx);
+						}
+
+						while !futures.is_empty() {
+							futures::select! {
+								m = futures.select_next_some() => {
+									match m {
+										Ok(vrf_in_out) => {
+
+										},
+										Err(err) => panic!("Err {:?}", err)
+									}
+								}
+							}
+						}
+					});
+				});
+			},
+		);
+	}
+
 	group.finish();
 }
 
 fn criterion_config() -> Criterion {
 	Criterion::default()
-		.sample_size(15)
-		.warm_up_time(Duration::from_millis(1000))
-		.measurement_time(Duration::from_secs(5))
+		.sample_size(10)
+		.warm_up_time(Duration::from_millis(3000))
+		.measurement_time(Duration::from_secs(30))
 }
 
 criterion_group!(
-	name = check;
+	name = check_1000_vrf_signatures;
 	config = criterion_config();
-	targets = check_assignment,
+	targets = time_1000_vrf_signatures, time_1000_vrf_signatures_pool
 );
-criterion_main!(check);
+criterion_main!(check_1000_vrf_signatures);
