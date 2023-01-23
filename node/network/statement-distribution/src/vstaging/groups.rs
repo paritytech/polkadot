a0c80c87cb3e18904dfcb184339746ef585e7a04
Marcin S <marcin@realemail.net>
2023-01-23 17:47:23 +0100

    Fix compile error

--- node/network/statement-distribution/src/vstaging/groups.rs
+++ node/network/statement-distribution/src/vstaging/groups.rs
@@ -69,7 +69,7 @@ impl Groups {
 		&self,
 		group_index: GroupIndex,
 	) -> Option<(usize, usize)> {
-		self.get(group_index).map(|g| (g.len(), super::minimum_votes(g.len())))
+		self.get(group_index).map(|g| (g.len(), minimum_votes(g.len())))
 	}
 
 	/// Get the group index for a validator by index.
