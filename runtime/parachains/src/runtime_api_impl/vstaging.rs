6f2c0c98b115807890daf9ad29587fc15ca2cf5b
Dmitry Sinyavin <dmitry.sinyavin@parity.io>
2023-02-13 21:06:39 +0100

    Add paritytech/substrate#9997 workaround

--- runtime/parachains/src/runtime_api_impl/vstaging.rs
+++ runtime/parachains/src/runtime_api_impl/vstaging.rs
@@ -30,5 +30,13 @@ pub fn get_session_disputes<T: disputes::Config>(
 pub fn session_executor_params<T: session_info::Config>(
 	session_index: SessionIndex,
 ) -> Option<ExecutorParams> {
-	<session_info::Pallet<T>>::session_executor_params(session_index)
+	// This is to bootstrap the storage working around the runtime migration issue:
+	// https://github.com/paritytech/substrate/issues/9997
+	// After the bootstrap is complete (no less than 7 session passed with the runtime)
+	// this code should be replaced with a pure
+	// <session_info::Pallet<T>>::session_executor_params(session_index) call.
+	match <session_info::Pallet<T>>::session_executor_params(session_index) {
+		Some(ep) => Some(ep),
+		None => Some(ExecutorParams::default())
+	}
 }
