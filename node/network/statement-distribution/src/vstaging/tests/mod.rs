12947b7f961cdb8718c56718b35dc6cd7d511b79
Robert Habermeier <rphmeier@gmail.com>
2023-02-17 18:19:28 -0600

    create a test state struct

--- node/network/statement-distribution/src/vstaging/tests/mod.rs
+++ node/network/statement-distribution/src/vstaging/tests/mod.rs
@@ -16,9 +16,13 @@
 
 use super::*;
 use crate::*;
+use polkadot_primitives::vstaging::{ValidatorPair, AssignmentPair, AssignmentId, IndexedVec};
 use polkadot_node_subsystem_test_helpers as test_helpers;
 use polkadot_node_network_protocol::request_response::ReqProtocolNames;
 use sc_keystore::LocalKeystore;
+use sp_authority_discovery::AuthorityPair as AuthorityDiscoveryPair;
+use sp_application_crypto::Pair as PairT;
+use sp_keyring::Sr25519Keyring;
 
 use futures::Future;
 use rand::{Rng, SeedableRng};
@@ -32,16 +36,89 @@ const GENESIS_HASH: Hash = Hash::repeat_byte(0xff);
 
 struct TestConfig {
 	validator_count: usize,
+	// how many validators to place in each group.
 	group_size: usize,
-	max_groups: Option<usize>,
 	// whether the local node should be a validator
 	local_validator: bool,
 	rng_seed: u64,
 }
 
+struct TestLocalValidator {
+	validator_id: ValidatorId,
+	validator_index: ValidatorIndex,
+	group_index: GroupIndex,
+}
+
+struct TestState {
+	config: TestConfig,
+	local: Option<TestLocalValidator>,
+	validators: Vec<ValidatorPair>,
+	discovery_keys: Vec<AuthorityDiscoveryId>,
+	assignment_keys: Vec<AssignmentId>,
+	validator_groups: IndexedVec<GroupIndex, Vec<ValidatorIndex>>,
+}
+
+impl TestState {
+	fn from_config(config: TestConfig, rng: &mut impl Rng) -> Self {
+		if config.group_size == 0 {
+			panic!("group size cannot be 0");
+		}
+
+		let mut validators = Vec::new();
+		let mut discovery_keys = Vec::new();
+		let mut assignment_keys = Vec::new();
+		let mut validator_groups = Vec::new();
+
+		let local_validator_pos = if config.local_validator {
+			Some(rng.gen_range(0..config.validator_count))
+		} else {
+			None
+		};
+
+		for i in 0..config.validator_count {
+			let validator_pair = if Some(i) == local_validator_pos {
+				Sr25519Keyring::Ferdie.pair().into()
+			} else {
+				ValidatorPair::generate().0
+			};
+			let assignment_id = AssignmentPair::generate().0.public();
+			let discovery_id = AuthorityDiscoveryPair::generate().0.public();
+
+			let group_index = i / config.group_size;
+			validators.push(validator_pair);
+			discovery_keys.push(discovery_id);
+			assignment_keys.push(assignment_id);
+			if validator_groups.len() == group_index {
+				validator_groups.push(vec![ValidatorIndex(i as _)]);
+			} else {
+				validator_groups.last_mut().unwrap().push(ValidatorIndex(i as _));
+			}
+		}
+
+		let local = if let Some(local_pos) = local_validator_pos {
+			Some(TestLocalValidator {
+				validator_id: validators[local_pos].public().clone(),
+				validator_index: ValidatorIndex(local_pos as _),
+				group_index: GroupIndex((local_pos / config.group_size) as _),
+			})
+		} else {
+			None
+		};
+
+		TestState {
+			config,
+			local,
+			validators,
+			discovery_keys,
+			assignment_keys,
+			validator_groups: IndexedVec::from(validator_groups),
+		}
+	}
+}
+
 fn test_harness<T: Future<Output = VirtualOverseer>>(
 	config: TestConfig,
-	test: impl FnOnce(VirtualOverseer) -> T,
+	test: impl FnOnce(TestState, VirtualOverseer) -> T,
 ) {
 	let pool = sp_core::testing::TaskExecutor::new();
 	let keystore = if config.local_validator {
@@ -52,7 +129,9 @@ fn test_harness<T: Future<Output = VirtualOverseer>>(
 	let req_protocol_names = ReqProtocolNames::new(&GENESIS_HASH, None);
 	let (statement_req_receiver, _) = IncomingRequest::get_config_receiver(&req_protocol_names);
 	let (candidate_req_receiver, _) = IncomingRequest::get_config_receiver(&req_protocol_names);
-	let rng = rand_chacha::ChaCha8Rng::seed_from_u64(config.rng_seed);
+	let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(config.rng_seed);
+
+	let test_state = TestState::from_config(config, &mut rng);
 
 	let (context, virtual_overseer) = test_helpers::make_subsystem_context(pool.clone());
 	let subsystem = async move {
@@ -69,7 +148,7 @@ fn test_harness<T: Future<Output = VirtualOverseer>>(
 		}
 	};
 
-	let test_fut = test(virtual_overseer);
+	let test_fut = test(test_state, virtual_overseer);
 
 	futures::pin_mut!(test_fut);
 	futures::pin_mut!(subsystem);
