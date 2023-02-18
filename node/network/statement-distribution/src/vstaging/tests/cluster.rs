d62e5dc72f68291b56446bd6f267789795202c0b
Robert Habermeier <rphmeier@gmail.com>
2023-02-17 18:31:52 -0600

    some TODOs for cluster test suite

--- node/network/statement-distribution/src/vstaging/tests/cluster.rs
+++ node/network/statement-distribution/src/vstaging/tests/cluster.rs
@@ -13,3 +13,28 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+
+// TODO [now]: shared seconded statement is circulated to all cluster peers with relay-parent
+//             in view
+
+// TODO [now]: cluster 'valid' statement without prior seconded is ignored
+
+// TODO [now]: statement with invalid signature leads to report
+
+// TODO [now]: cluster statement from non-cluster peer is rejected
+
+// TODO [now]: statement from non-cluster originator is rejected
+
+// TODO [now]: cluster statement for unknown candidate leads to request
+
+// TODO [now]: cluster statements are shared with `Seconded` first for all cluster peers
+//             with relay-parent in view
+
+// TODO [now]: cluster statements not re-shared on view update
+
+// TODO [now]: cluster statements shared on first time cluster peer gets relay-parent in view.
+
+// TODO [now]: confirmed cluster statement does not import statements until candidate in hypothetical frontier
+
+// TODO [now]: shared valid statement after confirmation sent to all cluster peers with relay-parent
