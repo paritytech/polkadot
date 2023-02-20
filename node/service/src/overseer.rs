5effdfc87225a349c629fc243dcec86c5fff6fc5
Aaro Altonen <aaro.altonen@parity.io>
2023-02-20 22:23:49 +0200

    Pass sync service

--- node/service/src/overseer.rs
+++ node/service/src/overseer.rs
@@ -234,7 +234,7 @@ where
 		.availability_store(AvailabilityStoreSubsystem::new(
 			parachains_db.clone(),
 			availability_config,
-			Box::new(network_service.clone()),
+			Box::new(sync_service.clone()),
 			Metrics::register(registry)?,
 		))
 		.bitfield_distribution(BitfieldDistributionSubsystem::new(Metrics::register(registry)?))
