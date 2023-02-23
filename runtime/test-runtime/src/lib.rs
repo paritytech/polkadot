0713f409d844707431312cfc0918db171f9cad94
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-02-23 15:28:51 +0100

    Fix test runtime
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- runtime/test-runtime/src/lib.rs
+++ runtime/test-runtime/src/lib.rs
@@ -521,6 +521,7 @@ impl parachains_paras::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = parachains_paras::TestWeightInfo;
 	type UnsignedPriority = ParasUnsignedPriority;
+	type UmpQueueTracker = ParaInclusion;
 	type NextSessionRotation = Babe;
 }
 
