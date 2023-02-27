d42010eb52030e9a243b530cc7026e77b70dda91
Mara Broda <mara@broda.me>
2023-02-27 15:17:56 +0100

    Bump transaction_version for polkadot

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -120,7 +120,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 20,
+	transaction_version: 21,
 	state_version: 0,
 };
 
