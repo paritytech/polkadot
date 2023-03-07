4bededa6ebb38f923e64182d5e2c9ea7191853ce
Vivek Pandya <vivekvpandya@gmail.com>
2023-03-07 18:31:22 +0530

    Remove Store trait usage from xcm directory

--- xcm/pallet-xcm/src/lib.rs
+++ xcm/pallet-xcm/src/lib.rs
@@ -157,7 +157,6 @@ pub mod pallet {
 	}
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::storage_version(migration::STORAGE_VERSION)]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
