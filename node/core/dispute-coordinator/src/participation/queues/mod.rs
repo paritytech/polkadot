27fd59fe6ef2c3fc6206a83fa97573e3acadc373
BradleyOlson64 <lotrftw9@gmail.com>
2023-03-14 16:33:04 -0700

    fmt

--- node/core/dispute-coordinator/src/participation/queues/mod.rs
+++ node/core/dispute-coordinator/src/participation/queues/mod.rs
@@ -171,13 +171,10 @@ impl Clone for ParticipationRequest {
 		}
 	}
 
-	fn clone_from(&mut self, source: &Self)
-	{
+	fn clone_from(&mut self, source: &Self) {
 		*self = source.clone();
 	}
 }
-
-
 
 impl Queues {
 	/// Create new `Queues`.
