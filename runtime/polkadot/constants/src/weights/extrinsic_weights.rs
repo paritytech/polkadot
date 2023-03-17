1f89e100f8d00aa3ca114736366025d8266718b6
Mara Robin B <mara@broda.me>
2023-03-17 11:09:21 +0100

    update weights (#6897)
    * polkadot: update weights
    
    * westend: update weights
    
    * kusama: update weights
    
    * rococo: update weights

--- runtime/polkadot/constants/src/weights/extrinsic_weights.rs
+++ runtime/polkadot/constants/src/weights/extrinsic_weights.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 //! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2023-02-28 (Y/M/D)
+//! DATE: 2023-03-16 (Y/M/D)
 //! HOSTNAME: `bm4`, CPU: `Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz`
 //!
 //! SHORT-NAME: `extrinsic`, LONG-NAME: `ExtrinsicBase`, RUNTIME: `Development`
@@ -42,17 +42,17 @@ parameter_types! {
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 106_370, 108_817
-	///   Average:  107_284
-	///   Median:   107_211
-	///   Std-Dev:  420.86
+	///   Min, Max: 103_764, 105_485
+	///   Average:  104_371
+	///   Median:   104_324
+	///   Std-Dev:  336.27
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 108_588
-	///   95th: 108_172
-	///   75th: 107_386
+	///   99th: 105_475
+	///   95th: 105_028
+	///   75th: 104_509
 	pub const ExtrinsicBaseWeight: Weight =
-		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(107_284), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(104_371), 0);
 }
 
 #[cfg(test)]
