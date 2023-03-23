fdd144949e4ac7c9d56934e11d086eec8b56d180
Tsvetomir Dimitrov <tsvetomir@parity.io>
2023-03-23 15:47:43 +0200

    Final buckets adjustment

--- primitives/src/v4/metrics.rs
+++ primitives/src/v4/metrics.rs
@@ -194,6 +194,6 @@ pub mod metric_definitions {
 		HistogramDefinition {
 			name: "polkadot_parachain_verify_dispute_signature",
 			description: "How much time does it take to verify a single validator signature of a dispute statement, in microseconds",
-			buckets: &[0.0, 0.00005, 0.00006, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.3, 0.5, 1.0, 2.0, 3.0, 4.0],
+			buckets: &[0.0, 0.00005, 0.00006, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.3, 0.5, 1.0],
 	};
 }
