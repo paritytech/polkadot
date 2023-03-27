352a607af5cec59e8d7fba9b2dca2eb677ef61bd
Chris Sosnin <chris125_@live.com>
2023-03-27 19:32:27 +0400

    migration follow-up

--- runtime/parachains/src/configuration/migration.rs
+++ runtime/parachains/src/configuration/migration.rs
@@ -19,6 +19,7 @@
 use crate::configuration::{self, ActiveConfig, Config, Pallet, PendingConfigs, MAX_POV_SIZE};
 use frame_support::{pallet_prelude::*, traits::StorageVersion, weights::Weight};
 use frame_system::pallet_prelude::BlockNumberFor;
+use primitives::vstaging::AsyncBackingParameters;
 use sp_std::vec::Vec;
 
 /// The current storage version.
@@ -226,6 +227,9 @@ ump_max_individual_weight                : pre.ump_max_individual_weight,
 pvf_checking_enabled                     : pre.pvf_checking_enabled,
 pvf_voting_ttl                           : pre.pvf_voting_ttl,
 minimum_validation_upgrade_delay         : pre.minimum_validation_upgrade_delay,
+
+// Default values are zeroes, thus it's ensured allowed ancestry never crosses the upgrade block.
+async_backing_parameters                 : AsyncBackingParameters { max_candidate_depth: 0, allowed_ancestry_len: 0 },
 		}
 	};
 
@@ -383,6 +387,10 @@ mod tests {
 					assert_eq!(v4.pvf_voting_ttl                           , v5.pvf_voting_ttl);
 					assert_eq!(v4.minimum_validation_upgrade_delay         , v5.minimum_validation_upgrade_delay);
 				}; // ; makes this a statement. `rustfmt::skip` cannot be put on an expression.
+
+				// additional checks for async backing.
+				assert_eq!(v5.async_backing_parameters.allowed_ancestry_len, 0);
+				assert_eq!(v5.async_backing_parameters.max_candidate_depth, 0);
 			}
 		});
 	}
