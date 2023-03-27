95fe4c8862810bffd68343231a517e62689c05c0
Mara Broda <mara@broda.me>
2023-03-27 21:44:05 +0200

    fixup rococo

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1471,7 +1471,7 @@ pub type UncheckedExtrinsic =
 ///
 /// This contains the combined migrations of the last 10 releases. It allows to skip runtime
 /// upgrades in case governance decides to do so.
-pub type Migrations = {};
+pub type Migrations = ();
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
