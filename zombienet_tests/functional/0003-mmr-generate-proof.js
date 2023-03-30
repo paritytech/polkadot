064b089f68922c67695131ac5de89a51141f36d2
Robert Hambrock <roberthambrock@gmail.com>
2023-03-30 12:28:13 +0200

    verify proof result on all unpaused nodes

--- zombienet_tests/functional/0003-mmr-generate-proof.js
+++ zombienet_tests/functional/0003-mmr-generate-proof.js
@@ -1,10 +1,19 @@
-async function run(nodeName, networkInfo, _) {
+async function run(nodeName, networkInfo, args) {
   const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
   const api = await zombie.connect(wsUri, userDefinedTypes);
 
   const proof = await api.rpc.mmr.generateProof([1]);
 
-  if (await api.rpc.mmr.verifyProof(proof)) {
+  const proofVerifications = await Promise.all(
+    args.map(async (nodeName) => {
+      const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
+      const api = await zombie.connect(wsUri, userDefinedTypes);
+      return api.rpc.mmr.verifyProof(proof);
+    })
+  );
+
+  // check that all nodes accepted the proof
+  if (proofVerifications.every((proofVerification) => proofVerification)) {
     return 1;
   } else {
     return 0;
