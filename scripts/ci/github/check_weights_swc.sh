ff1d4f38b54b21dfd7d625c6503c496a1d8e2d6a
Martin Pugh <martin@parity.io>
2023-04-04 14:47:40 -0700

    fix comments, remove artifact. I hate CI

--- scripts/ci/github/check_weights_swc.sh
+++ scripts/ci/github/check_weights_swc.sh
@@ -3,7 +3,7 @@
 RUNTIME=$1
 # Get last release from github
 # Remove before merging, and uncomment the line below
-RELEASE="v0.9.39"
+RELEASE="v0.9.38"
 # RELEASE=$(curl -s https://api.github.com/repos/paritytech/polkadot/releases/latest | jq -r .tag_name)
 
 swc compare commits --method asymptotic --path-pattern "./runtime/$RUNTIME/src/weights/**/*.rs" "$RELEASE"
