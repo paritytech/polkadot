3dac9a08a21bcf9c1efdf6e2a304bb0b2bdde3fe
Robert Hambrock <roberthambrock@gmail.com>
2023-04-06 11:48:53 +0200

    test beefy rpc: finalized heads match

---
+++ zombienet_tests/functional/0003-beefy-finalized-heads.js
@@ -0,0 +1,23 @@
+async function run(nodeName, networkInfo, nodeNames) {
+  const apis = await Promise.all(
+    nodeNames.map(async (nodeName) => {
+      const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
+      return await zombie.connect(wsUri, userDefinedTypes);
+    })
+  );
+
+  const finalizedHeads = await Promise.all(
+    apis.map(async (api) => {
+      return api.rpc.beefy.getFinalizedHead();
+    })
+  );
+
+  // check that the finalized heads are the same
+  if (finalizedHeads.every((finalizedHead) => finalizedHead.toString() === finalizedHeads[0].toString())) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+module.exports = { run };
