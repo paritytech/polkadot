a9a06aa53cedfdde420707ddee9d35e9516e0707
Chris Sosnin <48099298+slumber@users.noreply.github.com>
2023-04-10 17:39:03 +0300

    scheduler: update storage migration (#6963)
    * update scheduler migration
    
    * Adjust weight to account for storage read

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1495,7 +1495,11 @@ pub type UncheckedExtrinsic =
 ///
 /// This contains the combined migrations of the last 10 releases. It allows to skip runtime
 /// upgrades in case governance decides to do so.
-pub type Migrations = parachains_configuration::migration::v5::MigrateToV5<Runtime>;
+pub type Migrations = (
+	parachains_configuration::migration::v5::MigrateToV5<Runtime>,
+	/* Asynchronous backing mirgration */
+	parachains_scheduler::migration::v1::MigrateToV1<Runtime>,
+);
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
