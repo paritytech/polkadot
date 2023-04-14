53a42fe6b0f91bbb0cdc4596d0b550be28a20dc2
Mara Broda <mara@broda.me>
2023-04-14 14:14:32 +0200

    polkadot: sync spec with current release

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -121,13 +121,13 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 9390,
+	spec_version: 9410,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 21,
+	transaction_version: 22,
 	state_version: 0,
 };
 
