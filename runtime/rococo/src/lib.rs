a5e253df10059a8fddf159fd5b49d94585aa2d79
Keith Yeung <kungfukeith11@gmail.com>
2023-04-20 19:04:33 +0800

    XCM: Properly set the pricing for the DMP router (#6843)
    * Properly set the pricing for the DMP router
    
    * Publicize price types
    
    * Use FixedU128 instead of Percent
    
    * Add sp-arithmetic as a dependency for rococo runtime
    
    * Add sp-arithmetic as a dependency to all runtimes
    
    * Remove duplicate import
    
    * Add missing import
    
    * Fix tests
    
    * Create an appropriate QueueDownwardMessageError variant
    
    * Recalculate delivery fee factor based on past queue sizes
    
    * Remove unused error variant
    
    * Fixes
    
    * Fixes
    
    * Remove unused imports
    
    * Rewrite fee factor update mechanism
    
    * Remove unused imports
    
    * Fixes
    
    * Update runtime/parachains/src/dmp.rs
    
    Co-authored-by: Squirrel <gilescope@gmail.com>
    
    * Make DeliveryFeeFactor be a StorageMap keyed on ParaIds
    
    * Fixes
    
    * introduce limit for fee increase on dmp queue
    
    * add message_size based fee factor to increment_fee_factor
    
    * change message_size fee rate to correct value
    
    * fix div by 0 error
    
    * bind limit to variable
    
    * fix message_size_factor and add DeliveryFeeFactor test
    
    * add test for ExponentialPrice implementation
    
    * make test formula based
    
    * make delivery fee factor test formula based
    
    * add max value test for DeliveryFeeFactor and move limit to config
    
    * change threshold back to dynamic value and fix tests
    
    * fmt
    
    * suggested changes and fmt
    
    * small stylistic change
    
    * fmt
    
    * change to tokenlocation
    
    * small fixes
    
    * fmt
    
    * remove sp_arithmetic dependency
    
    * Update runtime/parachains/src/dmp.rs
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Squirrel <gilescope@gmail.com>
    Co-authored-by: Just van Stam <just.van.stam@gmail.com>
    Co-authored-by: Just van Stam <vstam1@users.noreply.github.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1412,7 +1412,7 @@ construct_runtime! {
 		ParaScheduler: parachains_scheduler::{Pallet, Storage} = 55,
 		Paras: parachains_paras::{Pallet, Call, Storage, Event, Config, ValidateUnsigned} = 56,
 		Initializer: parachains_initializer::{Pallet, Call, Storage} = 57,
-		Dmp: parachains_dmp::{Pallet, Call, Storage} = 58,
+		Dmp: parachains_dmp::{Pallet, Storage} = 58,
 		Ump: parachains_ump::{Pallet, Call, Storage, Event} = 59,
 		Hrmp: parachains_hrmp::{Pallet, Call, Storage, Event<T>, Config} = 60,
 		ParaSessionInfo: parachains_session_info::{Pallet, Storage} = 61,
