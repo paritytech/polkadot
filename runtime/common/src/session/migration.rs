b31686ff9d94d2be0f30973ea08482eb505b5fa8
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2023-04-26 09:29:34 +0530

    Moves migration to polkadot

---
+++ runtime/common/src/session/migration.rs
@@ -0,0 +1,122 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+use pallet_session::{Pallet, Config};
+use frame_support::{
+    traits::{Get, OnRuntimeUpgrade},
+    dispatch::GetStorageVersion,
+    storage::storage_prefix,
+    weights::Weight,
+};
+use sp_io::{storage::clear_prefix, KillStorageResult};
+
+const LOG_TARGET: &str = "runtime::session";
+
+pub struct MigrateToV2<T>(sp_std::marker::PhantomData<T>);
+impl<T: Config> OnRuntimeUpgrade for MigrateToV2<T> {
+    #[cfg(feature = "try-runtime")]
+    fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+        let onchain = Pallet::<T>::on_chain_storage_version();
+        ensure!(onchain < 2, "pallet_session::MigrateToV2 migration can be deleted");
+        Ok(Vec::new())
+    }
+
+    fn on_runtime_upgrade() -> Weight {
+        let current = Pallet::<T>::current_storage_version();
+        let onchain = Pallet::<T>::on_chain_storage_version();
+
+        if onchain > 1 {
+            log::info!(target: LOG_TARGET, "pallet_session::MigrateToV2 should be removed");
+            return T::DbWeight::get().reads(1)
+        }
+
+        let prefix = storage_prefix(b"Session", b"StoredRange");
+        let keys_removed_stored_range = match clear_prefix(&prefix, None) {
+			KillStorageResult::AllRemoved(value) => value,
+			KillStorageResult::SomeRemaining(value) => {
+				log::error!(
+					"`clear_prefix` failed to remove all keys. THIS SHOULD NEVER HAPPEN! 🚨",
+				);
+				value
+			},
+		} as u64;
+
+        let prefix = storage_prefix(b"Session", b"HistoricalSessions");
+        let keys_removed_historical_sessions = match clear_prefix(&prefix, None) {
+			KillStorageResult::AllRemoved(value) => value,
+			KillStorageResult::SomeRemaining(value) => {
+				log::error!(
+					"`clear_prefix` failed to remove all keys. THIS SHOULD NEVER HAPPEN! 🚨",
+				);
+				value
+			},
+		} as u64;
+
+        let keys_removed = keys_removed_stored_range + keys_removed_historical_sessions;
+		log::info!("Removed {} keys 🧹", keys_removed);
+
+        current.put::<Pallet<T>>();
+
+        T::DbWeight::get().reads_writes(keys_removed, keys_removed)
+    }
+
+    #[cfg(feature = "try-runtime")]
+    fn post_upgrade(_state: Vec<u8>) -> Result<(), &'static str> {
+        let onchain = Pallet::<T>::on_chain_storage_version();
+        ensure!(onchain == 2, "pallet_session::MigrateToV2 needs to be run");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+    use crate::mock::{new_test_ext, Test as T};
+    use frame_support::{weights::RuntimeDbWeight, Twox64Concat};
+
+    #[test]
+	fn migration_to_v1_works() {
+        #[frame_support::storage_alias]
+        type HistoricalSessions = StorageMap<Session, Twox64Concat, u64, u64>;
+
+        #[frame_support::storage_alias]
+        type StoredRange = StorageValue<Session, u64>;
+
+		let mut ext = new_test_ext();
+
+		ext.execute_with(|| {
+			HistoricalSessions::insert(1, 10);
+            StoredRange::set(Some(5));
+
+			assert!(HistoricalSessions::iter_keys().collect::<Vec<_>>().len() > 0);
+            assert_eq!(StoredRange::get(), Some(5));
+		});
+
+		ext.commit_all().unwrap();
+
+		ext.execute_with(|| {
+            let weight: RuntimeDbWeight = <T as frame_system::Config>::DbWeight::get();
+
+			assert_eq!(
+				MigrateToV2::<T>::on_runtime_upgrade(),
+				weight.reads_writes(2, 2),
+			);
+
+			assert!(HistoricalSessions::iter_keys().collect::<Vec<_>>().len() == 0);
+            assert_eq!(StoredRange::get(), None);
+		})
+	}
+}
