324cb73e77b0bef7b728a9212aa1fa2c106a7967
Robert Hambrock <roberthambrock@gmail.com>
2023-05-01 11:41:39 +0200

    verify finalized heads share same canonical chain
    picks the node with the highest finalized head, gets its blockchain
    headers since genesis, and verifies that all other nodes' finalized
    heads are in said blockchain.

--- zombienet_tests/functional/0003-beefy-finalized-heads.js
+++ zombienet_tests/functional/0003-beefy-finalized-heads.js
@@ -5,18 +5,31 @@ async function run(_, networkInfo, nodeNames) {
 
   const finalizedHeads = await Promise.all(
     Object.entries(apis).map(async ([nodeName, api]) => {
-      return { nodeName, finalizedHead: await api.rpc.beefy.getFinalizedHead() };
+      const finalizedHead = await api.rpc.beefy.getFinalizedHead();
+      return { nodeName, finalizedHead, finalizedHeight: await api.rpc.chain.getHeader(finalizedHead).then((header) => header.number) };
     })
   );
 
-  const finalizedHeadsHeight = await Promise.all(
-    finalizedHeads.map(async ({ nodeName, finalizedHead }) => {
-      return apis[nodeName].rpc.chain.getHeader(finalizedHead).then((header) => header.number);
-    })
+  // select the node with the highest finalized height
+  const highestFinalizedHeight = finalizedHeads.reduce(
+    (acc, { nodeName, finalizedHeight }) =>
+      finalizedHeight >= acc.finalizedHeight
+        ? { nodeName, finalizedHeight }
+        : acc,
+    { nodeName: 'validator', finalizedHeight: 0 }
   );
+
+  // get all block hashes up until the highest finalized height
+  const blockHashes = [];
+  for (let blockNumber = 0; blockNumber <= highestFinalizedHeight.finalizedHeight; blockNumber++) {
+    const blockHash = await apis[highestFinalizedHeight.nodeName].rpc.chain.getBlockHash(blockNumber);
+    blockHashes.push(blockHash);
+  }
 
   // verify that height(finalized_head) is at least as high as the substrate_beefy_best_block test already verified
-  return finalizedHeadsHeight.every((height) => height >= 21)
+  return finalizedHeads.every(({ finalizedHead, finalizedHeight }) =>
+    finalizedHeight >= 21 && finalizedHead.toHex() === blockHashes[finalizedHeight].toHex()
+  )
 }
 
 module.exports = { run };
