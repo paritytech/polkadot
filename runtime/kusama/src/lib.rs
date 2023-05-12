925074a072f24aa2d63ba878b6cffafb9709b607
cheme <emericchevalier.pro@gmail.com>
2023-05-12 11:49:18 +0200

    fmt?

--- runtime/kusama/src/lib.rs
+++ runtime/kusama/src/lib.rs
@@ -1484,12 +1484,9 @@ impl Get<Perbill> for NominationPoolsMigrationV4OldPallet {
 ///
 /// This contains the combined migrations of the last 10 releases. It allows to skip runtime
 /// upgrades in case governance decides to do so. THE ORDER IS IMPORTANT.
-pub type Migrations = (
-	migrations::V0940,
-	migrations::V0941,
-	migrations::V0942,
-	migrations::Unreleased,
-);
+pub type Migrations =
+		(migrations::V0940, migrations::V0941,
+	migrations::V0942, migrations::Unreleased);
 
 /// The runtime migrations per release.
 #[allow(deprecated, missing_docs)]
