506fb3131945b121c448d08d4af2dd043d62ad43
kianenigma <kian@parity.io>
2023-05-15 08:37:42 +0200

    try and enable logs..

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -17,7 +17,6 @@
 //! The Polkadot runtime. This can be compiled with `#[no_std]`, ready for Wasm.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
 
 use pallet_transaction_payment::CurrencyAdapter;
