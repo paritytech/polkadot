4b5db237d14d07005aed70e3b5f4f29da490bdf9
Sergej Sakac <sakacszergej@gmail.com>
2023-05-22 21:39:03 +0200

    use ensure

--- runtime/parachains/src/configuration/migration_ump.rs
+++ runtime/parachains/src/configuration/migration_ump.rs
@@ -23,7 +23,7 @@ use parity_scale_codec::{Decode, Encode};
 
 pub mod latest {
 	use super::*;
-	use frame_support::{pallet_prelude::Weight, traits::OnRuntimeUpgrade};
+	use frame_support::{ensure, pallet_prelude::Weight, traits::OnRuntimeUpgrade};
 
 	/// Force update the UMP limits in the parachain host config.
 	// NOTE: `OnRuntimeUpgrade` does not have a `self`, so everything must be compile time.
@@ -99,9 +99,8 @@ pub mod latest {
 				"Last pending HostConfig upgrade:\n\n{:#?}\n",
 				pending.last()
 			);
-			assert_eq!(
-				pending.len(),
-				old_pending as usize + 1,
+			ensure!(
+				pending.len() == old_pending as usize + 1,
 				"There must be a new pending upgrade enqueued"
 			);
 
