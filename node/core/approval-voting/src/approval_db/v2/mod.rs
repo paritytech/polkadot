0f9e1eccd91382cc1ee6edbf89895ccb3a531ad1
Andrei Sandu <andrei-mihail@parity.io>
2023-05-24 15:12:41 +0000

    fix comment
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- node/core/approval-voting/src/approval_db/v2/mod.rs
+++ node/core/approval-voting/src/approval_db/v2/mod.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Version 1 of the DB schema.
+//! Version 2 of the DB schema.
 
 use parity_scale_codec::{Decode, Encode};
 use polkadot_node_primitives::approval::{
