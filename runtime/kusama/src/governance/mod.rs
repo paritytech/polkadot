d1cf215d31b61663c9fb499e5578e8a1163c6374
Liam Aharon <liam.aharon@hotmail.com>
2023-05-31 12:25:05 +0400

    restore old gov configs

--- runtime/kusama/src/governance/mod.rs
+++ runtime/kusama/src/governance/mod.rs
@@ -23,6 +23,9 @@ use frame_support::{
 };
 use frame_system::EnsureRootWithSuccess;
 
+// Old governance configurations.
+pub mod old;
+
 mod origins;
 pub use origins::{
 	pallet_custom_origins, AuctionAdmin, Fellows, FellowshipAdmin, FellowshipExperts,
