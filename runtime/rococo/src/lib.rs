8f22808b26118cb7e7506282df2717fd2b1cfdd6
Anton Kaliaev <anton.kalyaev@gmail.com>
2023-05-31 11:23:30 +0400

    remove MaxPeerDataEncodingSize

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -552,7 +552,6 @@ parameter_types! {
 	pub const MaxAuthorities: u32 = 100_000;
 	pub const MaxKeys: u32 = 10_000;
 	pub const MaxPeerInHeartbeats: u32 = 10_000;
-	pub const MaxPeerDataEncodingSize: u32 = 1_000;
 }
 
 type ApproveOrigin = EitherOfDiverse<
@@ -652,7 +651,6 @@ impl pallet_im_online::Config for Runtime {
 	type WeightInfo = weights::pallet_im_online::WeightInfo<Runtime>;
 	type MaxKeys = MaxKeys;
 	type MaxPeerInHeartbeats = MaxPeerInHeartbeats;
-	type MaxPeerDataEncodingSize = MaxPeerDataEncodingSize;
 }
 
 parameter_types! {
