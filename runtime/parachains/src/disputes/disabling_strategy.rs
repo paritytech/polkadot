d0629068f25473d26b5cf9487d98a983052a996a
Tsvetomir Dimitrov <tsvetomir@parity.io>
2023-06-05 14:47:36 +0300

    Disabling strategy modulo random selection

--- runtime/parachains/src/disputes/disabling_strategy.rs
+++ runtime/parachains/src/disputes/disabling_strategy.rs
@@ -27,6 +27,10 @@ use super::slashing::ValidatorDisablingHandler;
 use frame_support::weights::Weight;
 use primitives::{vstaging::slashing::SlashingOffenceKind, SessionIndex, ValidatorIndex};
 use sp_std::prelude::*;
+
+// TODO: set this based on the number of validators
+const MAX_DISABLED_VALIDATORS_COUNT: usize = 10;
+
 pub struct ValidatorDisabling<C> {
 	_phantom: sp_std::marker::PhantomData<C>,
 }
@@ -36,16 +40,29 @@ where
 	T: pallet::Config,
 {
 	fn report_offender(
-		_validators: impl IntoIterator<Item = ValidatorIndex>,
-		_offence_kind: SlashingOffenceKind,
+		validators: impl IntoIterator<Item = ValidatorIndex>,
+		offence_kind: SlashingOffenceKind,
 	) {
-		// Add to offenders
+		// Add all new offenders to `Offenders`
+		for validator_idx in validators.into_iter() {
+			<Offenders<T>>::mutate(validator_idx, |current_offence| {
+				let current_offence = current_offence.get_or_insert(offence_kind);
+				if *current_offence == SlashingOffenceKind::ForInvalid &&
+					offence_kind == SlashingOffenceKind::ForInvalid
+				{
+					*current_offence = offence_kind;
+				}
+			});
+		}
 
 		// Add to disabled validators, if there is enough space
+		let disabled = if <Offenders<T>>::iter().count() <= MAX_DISABLED_VALIDATORS_COUNT {
+			<Offenders<T>>::iter().map(|offender| offender.0).collect()
+		} else {
+			Self::reshuffle_disabled_validators()
+		};
 
-		// Else - reshuffle
-
-		todo!()
+		<DisabledValidators<T>>::set(disabled);
 	}
 
 	fn initializer_initialize(now: T::BlockNumber) -> Weight {
@@ -58,6 +75,57 @@ where
 
 	fn initializer_on_new_session(session_index: SessionIndex) {
 		Pallet::<T>::initializer_on_new_session(session_index)
+	}
+}
+
+impl<T> ValidatorDisabling<Pallet<T>>
+where
+	T: pallet::Config,
+{
+	fn reshuffle_disabled_validators() -> Vec<ValidatorIndex> {
+		let mut disabled_capacity = MAX_DISABLED_VALIDATORS_COUNT;
+		let mut disabled = Vec::new();
+
+		// First partition the validators by their offence. At the moment there are only two offence
+		// types so a simple partition can do the job.
+		let (big_offenders, small_offenders): (
+			Vec<(ValidatorIndex, SlashingOffenceKind)>,
+			Vec<(ValidatorIndex, SlashingOffenceKind)>,
+		) = <Offenders<T>>::iter().partition(|offender| offender.1 == SlashingOffenceKind::ForInvalid);
+
+		// Strip `SlashingOffenceKind`
+		let mut big_offenders =
+			big_offenders.into_iter().map(|offender| offender.0).collect::<Vec<_>>();
+
+		// If there are too many `big_offenders` pick offenders randomly until `disabled is out of
+		// capacity.
+		if big_offenders.len() > disabled_capacity {
+			return Self::random_selection(&mut big_offenders, disabled_capacity)
+		}
+
+		// If the `big_offenders` fits - add all validators from the disabled set
+		disabled_capacity -= big_offenders.len();
+		disabled.extend(big_offenders.into_iter());
+
+		// Strip `SlashingOffenceKind`
+		let mut small_offenders =
+			small_offenders.into_iter().map(|offender| offender.0).collect::<Vec<_>>();
+
+		// Next try to extend the disabled list with small offenders
+		if small_offenders.len() > disabled_capacity {
+			return Self::random_selection(&mut small_offenders, disabled_capacity)
+		}
+
+		// All small offenders can be inserted.
+		disabled.extend(small_offenders.into_iter());
+
+		disabled
+	}
+
+	fn random_selection(input: &mut Vec<ValidatorIndex>, capacity: usize) -> Vec<ValidatorIndex> {
+		debug_assert!(input.len() > capacity);
+		// TODO: source of randomness?!
+		input.split_off(capacity)
 	}
 }
 
@@ -124,11 +192,11 @@ pub mod pallet {
 	// Disabled validators at the moment
 	#[pallet::storage]
 	#[pallet::getter(fn disabled_validators)]
-	type DisabledValidators<T> = StorageValue<_, Vec<ValidatorIndex>, ValueQuery>;
+	pub type DisabledValidators<T> = StorageValue<_, Vec<ValidatorIndex>, ValueQuery>;
 
 	// All offenders reported during the current session
 	#[pallet::storage]
-	type Offenders<T> = StorageMap<_, Twox64Concat, ValidatorIndex, SlashingOffenceKind>;
+	pub type Offenders<T> = StorageMap<_, Twox64Concat, ValidatorIndex, SlashingOffenceKind>;
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
