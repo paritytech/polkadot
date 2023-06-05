0948738f856fd18bd4ba3241124a2fa2d41a1a01
Gav <gavin@parity.io>
2023-06-05 07:29:24 +0100

    No need for aliasing

--- xcm/xcm-builder/src/location_conversion.rs
+++ xcm/xcm-builder/src/location_conversion.rs
@@ -18,7 +18,7 @@ use crate::universal_exports::ensure_is_remote;
 use frame_support::traits::Get;
 use parity_scale_codec::{Compact, Decode, Encode};
 use sp_io::hashing::blake2_256;
-use sp_runtime::traits::{AccountIdConversion, Convert as SimpleConvert, TrailingZeroInput};
+use sp_runtime::traits::{AccountIdConversion, Convert, TrailingZeroInput};
 use sp_std::{marker::PhantomData, prelude::*};
 use xcm::latest::prelude::*;
 use xcm_executor::traits::ConvertLocation;
@@ -321,7 +321,7 @@ impl<Network: Get<Option<NetworkId>>, AccountId: From<[u8; 32]> + Into<[u8; 32]>
 /// network (provided by `Network`) and the `AccountId`'s `[u8; 32]` datum for the `id`.
 pub struct AliasesIntoAccountId32<Network, AccountId>(PhantomData<(Network, AccountId)>);
 impl<'a, Network: Get<Option<NetworkId>>, AccountId: Clone + Into<[u8; 32]> + Clone>
-	SimpleConvert<&'a AccountId, MultiLocation> for AliasesIntoAccountId32<Network, AccountId>
+	Convert<&'a AccountId, MultiLocation> for AliasesIntoAccountId32<Network, AccountId>
 {
 	fn convert(who: &AccountId) -> MultiLocation {
 		AccountId32 { network: Network::get(), id: who.clone().into() }.into()
