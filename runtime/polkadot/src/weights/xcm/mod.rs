8ef3669183c75350eab8c9ed82e8869ab1734a13
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-06-23 09:30:56 -0300

    Underscore unused parameter

--- runtime/polkadot/src/weights/xcm/mod.rs
+++ runtime/polkadot/src/weights/xcm/mod.rs
@@ -90,7 +90,7 @@ impl<RuntimeCall> XcmWeightInfo<RuntimeCall> for PolkadotXcmWeight<RuntimeCall> 
 	fn withdraw_asset(assets: &MultiAssets) -> Weight {
 		assets.weigh_multi_assets(XcmBalancesWeight::<Runtime>::withdraw_asset())
 	}
-	fn reserve_asset_deposited(assets: &MultiAssets) -> Weight {
+	fn reserve_asset_deposited(_assets: &MultiAssets) -> Weight {
 		// assets.weigh_multi_assets(XcmBalancesWeight::<Runtime>::reserve_asset_deposited())
 		Weight::MAX
 	}
