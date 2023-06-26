fb20b1a11b32a7243c1cfedc63c338f9a498cada
Tsvetomir Dimitrov <tsvetomir@parity.io>
2023-06-26 14:14:18 +0300

    fmt

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -1530,7 +1530,10 @@ pub mod migrations {
 	);
 
 	/// Unreleased migrations. Add new ones here:
-	pub type Unreleased = (pallet_im_online::migration::v1::Migration<Runtime>, parachains_configuration::migration::v7::MigrateToV7<Runtime>);
+	pub type Unreleased = (
+		pallet_im_online::migration::v1::Migration<Runtime>,
+		parachains_configuration::migration::v7::MigrateToV7<Runtime>,
+	);
 
 	/// Migrations that set `StorageVersion`s we missed to set.
 	pub struct SetStorageVersions;
