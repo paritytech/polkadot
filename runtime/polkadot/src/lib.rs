1eeffe74cce1687ed084efe212a0dab8d7c3c7ac
Mara Broda <mara@broda.me>
2023-07-11 13:05:15 +0200

    bump spec_version (1.0.0)

--- runtime/polkadot/src/lib.rs
+++ runtime/polkadot/src/lib.rs
@@ -126,7 +126,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkadot"),
 	impl_name: create_runtime_str!("parity-polkadot"),
 	authoring_version: 0,
-	spec_version: 9440,
+	spec_version: 10000,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
