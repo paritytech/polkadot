fa4fa5a1f65ae62cb6c20c5e271551e74e91e5a4
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-07-12 14:36:01 +0200

    Rococo is at .42
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1514,13 +1514,7 @@ pub type UncheckedExtrinsic =
 ///
 /// This contains the combined migrations of the last 10 releases. It allows to skip runtime
 /// upgrades in case governance decides to do so. THE ORDER IS IMPORTANT.
-pub type Migrations = (
-	migrations::V0940,
-	migrations::V0941,
-	migrations::V0942,
-	migrations::V0943,
-	migrations::Unreleased,
-);
+pub type Migrations = (migrations::V0943, migrations::Unreleased);
 
 /// The runtime migrations per release.
 #[allow(deprecated, missing_docs)]
@@ -1528,12 +1522,6 @@ pub mod migrations {
 	use super::*;
 	use frame_support::traits::{GetStorageVersion, OnRuntimeUpgrade, StorageVersion};
 
-	pub type V0940 = ();
-	pub type V0941 = (); // Node only release - no migrations.
-	pub type V0942 = (
-		parachains_configuration::migration::v5::MigrateToV5<Runtime>,
-		pallet_offences::migration::v1::MigrateToV1<Runtime>,
-	);
 	pub type V0943 = (
 		SetStorageVersions,
 		// Remove UMP dispatch queue <https://github.com/paritytech/polkadot/pull/6271>
