14e263bdfa97e1efada3d8cf7460d19a43a10987
PraetorP <praetorian281@gmail.com>
2023-07-14 19:21:58 +0700

    feat(kusama-runtime): switch to `Ignore`

--- runtime/kusama/src/governance/fellowship.rs
+++ runtime/kusama/src/governance/fellowship.rs
@@ -23,7 +23,7 @@ use frame_support::traits::{MapSuccess, TryMapSuccess};
 use sp_arithmetic::traits::CheckedSub;
 use sp_runtime::{
 	morph_types,
-	traits::{ConstU16, Replace, ReplaceWithDefaultFor, TypedGet},
+	traits::{ConstU16, Ignore, Replace, TypedGet},
 };
 
 use super::*;
@@ -334,7 +334,7 @@ impl pallet_ranked_collective::Config<FellowshipCollectiveInstance> for Runtime 
 	// - Root.
 	// - the FellowshipAdmin origin.
 	// - a Fellowship origin.
-	type AddOrigin = MapSuccess<Self::PromoteOrigin, ReplaceWithDefaultFor<()>>;
+	type AddOrigin = MapSuccess<Self::PromoteOrigin, Ignore>;
 	// Removing is by any of:
 	// - Root can remove arbitrarily.
 	// - the FellowshipAdmin origin (i.e. token holder referendum);
