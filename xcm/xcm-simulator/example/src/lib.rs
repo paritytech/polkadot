1c980dceea527a19274194b645cdc183127bc131
Just van Stam <just.van.stam@gmail.com>
2023-07-27 16:27:03 +0200

    fixes

--- xcm/xcm-simulator/example/src/lib.rs
+++ xcm/xcm-simulator/example/src/lib.rs
@@ -24,7 +24,7 @@ use xcm_executor::traits::ConvertLocation;
 use xcm_simulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain, TestExt};
 
 pub const ALICE: sp_runtime::AccountId32 = sp_runtime::AccountId32::new([0u8; 32]);
-pub const BOB: sp_runtime::AccountId32 = sp_runtime::AccountId32::new([0u8; 32]);
+pub const BOB: sp_runtime::AccountId32 = sp_runtime::AccountId32::new([1u8; 32]);
 pub const INITIAL_BALANCE: u128 = 1_000_000_000;
 
 decl_test_parachain! {
