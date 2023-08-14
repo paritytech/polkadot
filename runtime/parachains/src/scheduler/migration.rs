2468d0cfcccefa49b903aafcb2cb27559ead8be7
Chris Sosnin <chris125_@live.com>
2023-08-14 23:52:24 +0400

    scheduler migration tests

--- runtime/parachains/src/scheduler/migration.rs
+++ runtime/parachains/src/scheduler/migration.rs
@@ -16,17 +16,21 @@
 
 //! A module that is responsible for migration of storage.
 
+use crate::scheduler::{self, Config, Pallet, Scheduled};
 use frame_support::traits::StorageVersion;
 
 /// The current storage version.
 pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
 mod v0 {
-	use crate::scheduler::{self, AssignmentKind};
+	use super::*;
+	use crate::scheduler::AssignmentKind;
+	use frame_support::pallet_prelude::*;
 	use parity_scale_codec::{Decode, Encode};
 	use primitives::{CoreIndex, GroupIndex, Id as ParaId};
 
-	#[derive(Encode, Decode)]
+	#[derive(Clone, Encode, Decode, TypeInfo)]
+	#[cfg_attr(feature = "std", derive(PartialEq, Debug))]
 	pub struct CoreAssignment {
 		pub core: CoreIndex,
 		pub para_id: ParaId,
@@ -39,13 +43,15 @@ mod v0 {
 			Self { core: old.core, para_id: old.para_id, kind: old.kind }
 		}
 	}
+
+	#[frame_support::storage_alias]
+	pub(super) type Scheduled<T: Config> = StorageValue<Pallet<T>, Vec<CoreAssignment>, ValueQuery>;
 }
 
 /// V1: Group index is dropped from the core assignment, it's explicitly computed during
 /// candidates processing.
 pub mod v1 {
 	use super::*;
-	use crate::scheduler::{self, Config, Pallet, Scheduled};
 	use frame_support::{pallet_prelude::*, traits::OnRuntimeUpgrade, weights::Weight};
 	use sp_std::vec::Vec;
 
@@ -71,7 +77,7 @@ pub mod v1 {
 		}
 	}
 
-	fn migrate<T: Config>() -> Weight {
+	pub(super) fn migrate<T: Config>() -> Weight {
 		let _ = Scheduled::<T>::translate(|scheduled: Option<Vec<v0::CoreAssignment>>| {
 			scheduled.map(|scheduled| {
 				scheduled.into_iter().map(|old| scheduler::CoreAssignment::from(old)).collect()
@@ -79,5 +85,62 @@ pub mod v1 {
 		});
 
 		T::DbWeight::get().reads_writes(1, 1)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::{
+		mock::{new_test_ext, Test},
+		scheduler::{self, AssignmentKind},
+	};
+	use primitives::{CoreIndex, GroupIndex, Id as ParaId};
+
+	#[test]
+	fn test_migrate_to_v7() {
+		let v0 = vec![
+			v0::CoreAssignment {
+				core: CoreIndex(0),
+				para_id: ParaId::from(0),
+				kind: AssignmentKind::Parachain,
+				group_idx: GroupIndex(0),
+			},
+			v0::CoreAssignment {
+				core: CoreIndex(1),
+				para_id: ParaId::from(1),
+				kind: AssignmentKind::Parachain,
+				group_idx: GroupIndex(1),
+			},
+			v0::CoreAssignment {
+				core: CoreIndex(2),
+				para_id: ParaId::from(2),
+				kind: AssignmentKind::Parachain,
+				group_idx: GroupIndex(2),
+			},
+		];
+		let expected_v1: Vec<_> =
+			v0.clone().into_iter().map(scheduler::CoreAssignment::from).collect();
+
+		new_test_ext(Default::default()).execute_with(|| {
+			v0::Scheduled::<Test>::set(v0.clone());
+
+			v1::migrate::<Test>();
+			let v1 = Scheduled::<Test>::get();
+
+			assert_eq!(v1, expected_v1);
+		});
+	}
+
+	#[test]
+	fn test_migrate_to_v7_no_entry() {
+		new_test_ext(Default::default()).execute_with(|| {
+			v0::Scheduled::<Test>::kill();
+
+			v1::migrate::<Test>();
+			let v1 = Scheduled::<Test>::get();
+
+			assert!(v1.is_empty());
+		});
 	}
 }
