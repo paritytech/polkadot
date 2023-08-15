b481b12244cef79f7e4f0ebc5fc3a4e42ca3ff62
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2023-08-15 04:30:12 +0200

    Update xcm/xcm-executor/src/lib.rs
    Co-authored-by: Keith Yeung <kungfukeith11@gmail.com>

--- xcm/xcm-executor/src/lib.rs
+++ xcm/xcm-executor/src/lib.rs
@@ -570,7 +570,7 @@ impl<Config: config::Config> XcmExecutor<Config> {
 					};
 				let actual_weight = maybe_actual_weight.unwrap_or(weight);
 				let surplus = require_weight_at_most.saturating_sub(actual_weight);
-				// We assume that the `Config::Weigher` will counts the `require_weight_at_most`
+				// We assume that the `Config::Weigher` will count the `require_weight_at_most`
 				// for the estimate of how much weight this instruction will take. Now that we know
 				// that it's less, we credit it.
 				//
