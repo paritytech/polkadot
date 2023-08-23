c658bc450cb9c8ee650b6706a9bf5973966a5189
Sophia Gold <sophiagoldnyc@gmail.com>
2023-08-23 13:36:12 -0400

    enable async backing in rococo runtime

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -1715,6 +1715,7 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 
+        #[api_version(99)]
 	impl primitives::runtime_api::ParachainHost<Block, Hash, BlockNumber> for Runtime {
 		fn validators() -> Vec<ValidatorId> {
 			parachains_runtime_api_impl::validators::<Runtime>()
@@ -1844,6 +1845,14 @@ sp_api::impl_runtime_apis! {
 				dispute_proof,
 				key_ownership_proof,
 			)
+		}
+
+	        fn staging_para_backing_state(para_id: ParaId) -> Option<primitives::vstaging::BackingState> {
+			runtime_parachains::runtime_api_impl::vstaging::backing_state::<Runtime>(para_id)
+		}
+
+		fn staging_async_backing_params() -> primitives::vstaging::AsyncBackingParams {
+			runtime_parachains::runtime_api_impl::vstaging::async_backing_params::<Runtime>()
 		}
 	}
 
